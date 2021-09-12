11aef64cb22fe989da87a9c1f0ff0da3b57ef92d
Georgios Konstantopoulos <me@gakonst.com>
2021-09-12 21:28:07 +0300

    feat: allow printing test results in json & refactor blockchain context

--- src/bin/dapp.rs
+++ src/bin/dapp.rs
@@ -1,20 +1,20 @@
 use structopt::StructOpt;
 
 use dapptools::dapp::{Executor, MultiContractRunner};
-use evm::Config;
+use evm::{backend::MemoryVicinity, Config};
 
 use ansi_term::Colour;
 use std::path::PathBuf;
 
 #[derive(Debug, StructOpt)]
-pub struct Opts {
+struct Opts {
     #[structopt(subcommand)]
-    pub sub: Subcommands,
+    sub: Subcommands,
 }
 
 #[derive(Debug, StructOpt)]
 #[structopt(about = "Perform Ethereum RPC calls from the comfort of your command line.")]
-pub enum Subcommands {
+enum Subcommands {
     Test {
         #[structopt(
             help = "glob path to your smart contracts",
@@ -31,8 +31,26 @@ pub enum Subcommands {
             default_value = "./out/dapp.sol.json"
         )]
         out_path: PathBuf,
-        // TODO: Add extra configuration options around blockchain context
+
+        #[structopt(help = "print the test results in json format", long, short)]
+        json: bool,
+
+        #[structopt(flatten)]
+        env: Env,
     },
+}
+
+#[derive(Debug, StructOpt)]
+struct Env {
+    #[structopt(help = "the block gas limit", long, short, default_value = "25000000")]
+    pub gas_limit: u64,
+    // TODO: Add extra configuration options around blockchain context
+}
+
+impl Env {
+    fn vicinity(&self) -> MemoryVicinity {
+        Executor::new_vicinity()
+    }
 }
 
 fn main() -> eyre::Result<()> {
@@ -41,31 +59,41 @@ fn main() -> eyre::Result<()> {
         Subcommands::Test {
             contracts,
             out_path,
+            env,
+            json,
         } => {
             let cfg = Config::istanbul();
-            let gas_limit = 12_500_000;
-            let env = Executor::new_vicinity();
 
-            let runner = MultiContractRunner::new(&contracts, out_path, &cfg, gas_limit, env)?;
+            let runner = MultiContractRunner::new(
+                &contracts,
+                out_path,
+                &cfg,
+                env.gas_limit,
+                env.vicinity(),
+            )?;
             let results = runner.test()?;
 
-            // TODO: Once we add traces in the VM, proceed to print them in a nice and structured
-            // way
-            for (i, (contract_name, tests)) in results.iter().enumerate() {
-                if !tests.is_empty() {
+            if json {
+                let res = serde_json::to_string(&results)?;
+                println!("{}", res);
+            } else {
+                // Dapptools-style printing
+                for (i, (contract_name, tests)) in results.iter().enumerate() {
                     if i > 0 {
                         println!()
                     }
-                    println!("Running {} tests for {}", tests.len(), contract_name);
-                }
+                    if !tests.is_empty() {
+                        println!("Running {} tests for {}", tests.len(), contract_name);
+                    }
 
-                for (name, result) in tests {
-                    let status = if result.success {
-                        Colour::Green.paint("[PASS]")
-                    } else {
-                        Colour::Red.paint("[FAIL]")
-                    };
-                    println!("{} {} (gas: {})", status, name, result.gas_used);
+                    for (name, result) in tests {
+                        let status = if result.success {
+                            Colour::Green.paint("[PASS]")
+                        } else {
+                            Colour::Red.paint("[FAIL]")
+                        };
+                        println!("{} {} (gas: {})", status, name, result.gas_used);
+                    }
                 }
             }
         }
