e3b9e764daac0a5256354ec61e2e94060d4d85c2
Georgios Konstantopoulos <me@gakonst.com>
2021-09-28 12:17:02 +0300

    feat: forward all calls to the internal executor

--- evm-adapters/src/sputnik/cheatcode_stack_executor.rs
+++ evm-adapters/src/sputnik/cheatcode_stack_executor.rs
@@ -1,12 +1,14 @@
 use std::ops::Deref;
 
-use super::Executor;
+use super::{Executor, SputnikExecutor};
 use sputnik::{
     backend::Backend,
     executor::{MemoryStackState, Precompile, StackExecutor, StackState, StackSubstateMetadata},
-    Config,
+    Config, ExitReason,
 };
 use std::marker::PhantomData;
+
+use ethers::types::{H160, H256, U256};
 
 struct CheatcodeStackExecutor<'backend, 'config, S, B> {
     executor: StackExecutor<'config, S>,
@@ -24,6 +26,41 @@ where
         precompile: Precompile,
     ) -> Self {
         Self { executor: StackExecutor::new_with_precompile(state, config, precompile), backend }
+    }
+}
+
+// The implementation for the base Stack Executor just forwards to the internal methods.
+impl<'a, S: StackState<'a>, B: Backend> SputnikExecutor<S, Config>
+    for CheatcodeStackExecutor<'a, 'a, S, B>
+{
+    fn config(&self) -> &Config {
+        self.executor.config()
+    }
+
+    fn state(&self) -> &S {
+        self.executor.state()
+    }
+
+    fn state_mut(&mut self) -> &mut S {
+        self.executor.state_mut()
+    }
+
+    fn gas_left(&self) -> U256 {
+        // NB: We do this to avoid `function cannot return without recursing`
+        U256::from(self.state().metadata().gasometer().gas())
+    }
+
+    fn transact_call(
+        &mut self,
+        caller: H160,
+        address: H160,
+        value: U256,
+        data: Vec<u8>,
+        gas_limit: u64,
+        access_list: Vec<(H160, Vec<H256>)>,
+    ) -> (ExitReason, Vec<u8>) {
+        // TODO: Implement cheat code interception logic.
+        self.executor.transact_call(caller, address, value, data, gas_limit, access_list)
     }
 }
 
