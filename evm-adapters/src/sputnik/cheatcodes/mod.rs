62bd092ac6c13be79e315224c59e31786adb6ef9
Georgios Konstantopoulos <me@gakonst.com>
2021-09-30 12:25:11 +0300

    feat(backend): add cheatcode backend (wrapper around backends)

--- evm-adapters/src/sputnik/cheatcodes/mod.rs
+++ evm-adapters/src/sputnik/cheatcodes/mod.rs
@@ -1,1 +1,15 @@
-mod memory_stackstate_owned;
+pub mod memory_stackstate_owned;
+
+mod backend;
+
+use ethers::types::U256;
+
+#[derive(Clone, Debug, Default)]
+/// Cheatcodes can be used to control the EVM context during setup or runtime,
+/// which can be useful for simulations or specialized unti tests
+pub struct Cheatcodes {
+    pub block_number: Option<U256>,
+    pub block_timestamp: Option<U256>,
+}
+
+// TODO: Add Lazy ethabi instance of the cheatcode function signatures
