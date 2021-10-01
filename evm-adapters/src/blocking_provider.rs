1a8ac3247175fe914dc7c4a709b599f99cf052e7
Georgios Konstantopoulos <me@gakonst.com>
2021-10-01 13:15:59 +0300

    fix(blocking-provider): do not tokio::spawn
    From @mattsse:
    > Executing multiple requests on a single core is pretty efficient,
    > you can run quite a lot of them concurrently without getting a bottleneck.
    > I wrote a scraper crate and misconfigured the max concurrent threshold to something
    > in the 5 figure range during testing of a scraper that follows every link it finds
    > on Wikipedia, which I also ran multiple instances in parallel of, crashed my homenetwork
    > and my Ip got banned on Wikipedia -.-

--- evm-adapters/src/blocking_provider.rs
+++ evm-adapters/src/blocking_provider.rs
@@ -1,15 +1,14 @@
 use ethers::{
     providers::Middleware,
-    types::{Address, BlockId, Bytes, H256, U256, U64},
+    types::{Address, Block, BlockId, Bytes, TxHash, H256, U256, U64},
 };
-use std::sync::Arc;
 use tokio::runtime::Runtime;
 
 #[derive(Debug)]
 /// Blocking wrapper around an Ethers middleware, for use in synchronous contexts
 /// (powered by a tokio runtime)
 pub struct BlockingProvider<M> {
-    provider: Arc<M>,
+    provider: M,
     runtime: Runtime,
 }
 
@@ -19,43 +18,42 @@ impl<M: Clone> Clone for BlockingProvider<M> {
     }
 }
 
-impl<M: Middleware> BlockingProvider<M> {
+impl<M: Middleware> BlockingProvider<M>
+where
+    M::Error: 'static,
+{
     pub fn new(provider: M) -> Self {
-        Self { provider: Arc::new(provider), runtime: Runtime::new().unwrap() }
+        Self { provider, runtime: Runtime::new().unwrap() }
     }
 
     fn block_on<F: std::future::Future>(&self, f: F) -> F::Output {
         self.runtime.block_on(f)
     }
 
+    pub fn block_and_chainid(&self, block_id: BlockId) -> eyre::Result<(Block<TxHash>, U256)> {
+        let f = async {
+            let block = self.provider.get_block(block_id);
+            let chain_id = self.provider.get_chainid();
+            tokio::try_join!(block, chain_id)
+        };
+        let (block, chain_id) = self.block_on(f)?;
+        Ok((block.ok_or_else(|| eyre::eyre!("block {:?} not found", block_id))?, chain_id))
+    }
+
     pub fn get_account(
         &self,
         address: Address,
-        block: Option<BlockId>,
-    ) -> eyre::Result<(U256, U256, Bytes)>
-    where
-        M: 'static,
-    {
-        // We clone the provider each time so that we can tokio::spawn it. Without
-        // tokio::spawning, we'd get concurrent polling of the combined future, but the
-        // actual underlying requests would not be done in parallel, c.f. try_join's
-        // docs.
+        block_id: Option<BlockId>,
+    ) -> eyre::Result<(U256, U256, Bytes)> {
         let f = async {
-            let provider = self.provider.clone();
-            let balance = tokio::spawn(async move { provider.get_balance(address, block).await });
-
-            let provider = self.provider.clone();
-            let nonce =
-                tokio::spawn(async move { provider.get_transaction_count(address, block).await });
-
-            let provider = self.provider.clone();
-            let code = tokio::spawn(async move { provider.get_code(address, block).await });
-
+            let balance = self.provider.get_balance(address, block_id);
+            let nonce = self.provider.get_transaction_count(address, block_id);
+            let code = self.provider.get_code(address, block_id);
             tokio::try_join!(balance, nonce, code)
         };
         let (balance, nonce, code) = self.block_on(f)?;
 
-        Ok((nonce?, balance?, code?))
+        Ok((nonce, balance, code))
     }
 
     pub fn get_block_number(&self) -> Result<U64, M::Error> {
