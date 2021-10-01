a84a36c7bb0af734450fa305e197c9211a4784ba
Georgios Konstantopoulos <me@gakonst.com>
2021-10-01 11:52:19 +0300

    feat(blocking-provider): parallelize rpc requests

--- evm-adapters/src/blocking_provider.rs
+++ evm-adapters/src/blocking_provider.rs
@@ -2,13 +2,14 @@ use ethers::{
     providers::Middleware,
     types::{Address, BlockId, Bytes, H256, U256, U64},
 };
+use std::sync::Arc;
 use tokio::runtime::Runtime;
 
 #[derive(Debug)]
 /// Blocking wrapper around an Ethers middleware, for use in synchronous contexts
 /// (powered by a tokio runtime)
 pub struct BlockingProvider<M> {
-    provider: M,
+    provider: Arc<M>,
     runtime: Runtime,
 }
 
@@ -20,11 +21,41 @@ impl<M: Clone> Clone for BlockingProvider<M> {
 
 impl<M: Middleware> BlockingProvider<M> {
     pub fn new(provider: M) -> Self {
-        Self { provider, runtime: Runtime::new().unwrap() }
+        Self { provider: Arc::new(provider), runtime: Runtime::new().unwrap() }
     }
 
     fn block_on<F: std::future::Future>(&self, f: F) -> F::Output {
         self.runtime.block_on(f)
+    }
+
+    pub fn get_account(
+        &self,
+        address: Address,
+        block: Option<BlockId>,
+    ) -> eyre::Result<(U256, U256, Bytes)>
+    where
+        M: 'static,
+    {
+        // We clone the provider each time so that we can tokio::spawn it. Without
+        // tokio::spawning, we'd get concurrent polling of the combined future, but the
+        // actual underlying requests would not be done in parallel, c.f. try_join's
+        // docs.
+        let f = async {
+            let provider = self.provider.clone();
+            let balance = tokio::spawn(async move { provider.get_balance(address, block).await });
+
+            let provider = self.provider.clone();
+            let nonce =
+                tokio::spawn(async move { provider.get_transaction_count(address, block).await });
+
+            let provider = self.provider.clone();
+            let code = tokio::spawn(async move { provider.get_code(address, block).await });
+
+            tokio::try_join!(balance, nonce, code)
+        };
+        let (balance, nonce, code) = self.block_on(f)?;
+
+        Ok((nonce?, balance?, code?))
     }
 
     pub fn get_block_number(&self) -> Result<U64, M::Error> {
