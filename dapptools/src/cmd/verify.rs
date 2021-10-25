6dc161b3a6efc0b93ea4095532869ff2e5119633
Georgios Konstantopoulos <me@gakonst.com>
2021-10-25 16:00:56 +0300

    feat: bump ethers (#119)
    * feat: bump ethers
    
    * ci: remove msrv workflow

--- dapptools/src/cmd/verify.rs
+++ dapptools/src/cmd/verify.rs
@@ -4,11 +4,13 @@ use crate::utils;
 
 use ethers::{
     abi::{Address, Function, FunctionExt},
+    core::types::Chain,
     prelude::Provider,
+    providers::Middleware,
 };
-use ethers_etherscan::{Client, VerifyContract};
+use ethers_etherscan::{contract::VerifyContract, Client};
 use eyre::ContextCompat;
-use seth::{Seth, SimpleSeth};
+use seth::SimpleSeth;
 use std::convert::TryFrom;
 
 /// Run the verify command to submit the contract's source code for verification on etherscan
@@ -20,17 +22,23 @@ pub async fn run(
 ) -> eyre::Result<()> {
     let etherscan_api_key = utils::etherscan_api_key()?;
     let rpc_url = utils::rpc_url();
-    let provider = Seth::new(Provider::try_from(rpc_url)?);
-
-    let chain = provider.chain().await.map_err(|err| {
-        err.wrap_err(
-            r#"Please make sure that you are running a local Ethereum node:
+    let provider = Provider::try_from(rpc_url)?;
+    let chain = provider
+        .get_chainid()
+        .await
+        .map_err(|err| {
+            eyre::eyre!(
+                r#"Please make sure that you are running a local Ethereum node:
         For example, try running either `parity' or `geth --rpc'.
         You could also try connecting to an external Ethereum node:
         For example, try `export ETH_RPC_URL=https://mainnet.infura.io'.
-        If you have an Infura API key, add it to the end of the URL."#,
-        )
-    })?;
+        If you have an Infura API key, add it to the end of the URL.
+
+        Error: {}"#,
+                err
+            )
+        })?
+        .as_u64();
 
     let contract = utils::find_dapp_json_contract(&path, &name)?;
     std::fs::write("meta.json", serde_json::to_string_pretty(&contract).unwrap()).unwrap();
@@ -53,6 +61,15 @@ pub async fn run(
         eyre::bail!("No constructor found but contract arguments provided")
     }
 
+    let chain = match chain {
+        1 => Chain::Mainnet,
+        3 => Chain::Ropsten,
+        4 => Chain::Rinkeby,
+        5 => Chain::Goerli,
+        42 => Chain::Kovan,
+        100 => Chain::XDai,
+        _ => eyre::bail!("unexpected chain {}", chain),
+    };
     let etherscan = Client::new(chain, etherscan_api_key)
         .map_err(|err| eyre::eyre!("Failed to create etherscan client: {}", err))?;
 
