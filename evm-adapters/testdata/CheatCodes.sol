f1050a15ca91750905c9bdf8e54eeffc9d3d9cb0
Brock <brock.elmore@gmail.com>
2021-11-18 13:19:21 -0700

    Adds etch (setCode) cheatcode

--- evm-adapters/testdata/CheatCodes.sol
+++ evm-adapters/testdata/CheatCodes.sol
@@ -6,15 +6,26 @@ pragma experimental ABIEncoderV2;
 import "./DsTest.sol";
 
 interface Hevm {
+    // Set block.timestamp (newTimestamp)
     function warp(uint256) external;
+    // Set block.height (newHeight)
     function roll(uint256) external;
+    // Loads a storage slot from an address (who, slot)
     function load(address,bytes32) external returns (bytes32);
+    // Stores a value to an address' storage slot, (who, slot, value)
     function store(address,bytes32,bytes32) external;
+    // Signs data, (privateKey, digest) => (r, v, s)
     function sign(uint256,bytes32) external returns (uint8,bytes32,bytes32);
+    // Gets address for a given private key, (privateKey) => (address)
     function addr(uint256) external returns (address);
+    // Performs a foreign function call via terminal, (stringInputs) => (result)
     function ffi(string[] calldata) external returns (bytes memory);
-    function prank(address,address,bytes calldata) external payable returns (bool, bytes memory);
+    // Calls another contract with a specified `msg.sender`, (newSender, contract, input) => (success, returnData)
+    function prank(address, address, bytes calldata) external payable returns (bool, bytes memory);
+    // Sets an address' balance, (who, newBalance)
     function deal(address, uint256) external;
+    // Sets an address' code, (who, newCode)
+    function etch(address, bytes calldata) external;
 }
 
 contract HasStorage {
@@ -187,6 +198,31 @@ contract CheatCodes is DSTest {
         // make sure we returned back to normal
         assertEq(origin, tx.origin);
         assertEq(sender, msg.sender);
+    }
+
+    function testEtch() public {
+        address rewriteCode = address(1337);
+        
+        bytes memory newCode = hex"1337";
+        hevm.etch(rewriteCode, newCode);
+        bytes memory n_code = getCode(rewriteCode);
+        assertEq(string(newCode), string(n_code));
+    }
+
+    function getCode(address who) internal returns (bytes memory o_code) {
+        assembly {
+            // retrieve the size of the code, this needs assembly
+            let size := extcodesize(who)
+            // allocate output byte array - this could also be done without assembly
+            // by using o_code = new bytes(size)
+            o_code := mload(0x40)
+            // new "memory end" including padding
+            mstore(0x40, add(o_code, and(add(add(size, 0x20), 0x1f), not(0x1f))))
+            // store length in memory
+            mstore(o_code, size)
+            // actually retrieve the code, this needs assembly
+            extcodecopy(who, add(o_code, 0x20), 0, size)
+        }
     }
 }
 
