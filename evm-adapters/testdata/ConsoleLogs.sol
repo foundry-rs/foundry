a5d1be1a6af1befc21514c70fcef3a1c12dfc5e0
Josh Levine <joshuapark95@gmail.com>
2021-11-28 01:42:41 -0600

    test(evm-adapters): ensure logs are emitted from external contracts

--- evm-adapters/testdata/ConsoleLogs.sol
+++ evm-adapters/testdata/ConsoleLogs.sol
@@ -8,4 +8,16 @@ contract ConsoleLogs {
 		console.log("Hi");
 		console.log("Hi", "Hi");
     }
+	function test_log_elsewhere() public {
+		_OtherContract _otherContract = new _OtherContract();
+		_otherContract.test_log();
+    }
+}
+
+contract _OtherContract {
+    function test_log() public {
+		console.log(0x1111111111111111111111111111111111111111);
+		console.log("Hi");
+		console.log("Hi", "Hi");
+    }
 }
