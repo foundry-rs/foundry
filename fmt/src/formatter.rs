be26ca2ce69a860a22cb4264d93bc3352e3f7a4a
Alexey Shekhirin <a.shekhirin@gmail.com>
2021-12-09 19:52:33 +0300

    use indent_write, implement more visitors

--- fmt/src/formatter.rs
+++ fmt/src/formatter.rs
@@ -2,26 +2,67 @@
 
 use std::fmt::Write;
 
-use indenter::CodeFormatter;
-use solang::parser::pt::{ContractDefinition, EnumDefinition, SourceUnit};
+use indent_write::fmt::IndentWriter;
+use itertools::Itertools;
+use solang::parser::pt::{
+    ContractDefinition, EnumDefinition, Identifier, SourceUnit, StringLiteral,
+};
 
 use crate::visit::{VResult, Visitable, Visitor};
 
 /// A solidity formatter
 pub struct Formatter<'a, W> {
     config: FormatterConfig,
-    /// indent aware code formatter that drives the output
-    f: CodeFormatter<'a, W>,
+    level: usize,
+    pending_indent: bool,
+    w: &'a mut W,
 }
 
 impl<'a, W: Write> Formatter<'a, W> {
     pub fn new(w: &'a mut W, config: FormatterConfig) -> Self {
-        let f = CodeFormatter::new(w, " ".repeat(config.tab_width));
-        Self { config, f }
+        Self { config, level: 0, pending_indent: true, w }
     }
 
+    fn indent(&mut self, level: usize) {
+        self.level = self.level.saturating_add(level)
+    }
+
+    fn dedent(&mut self, level: usize) {
+        self.level = self.level.saturating_sub(level)
+    }
+
+    /// Respects the `config.bracket_spacing` setting:
+    /// `"{ "` if `true`, `"{"` if `false`
+    fn opening_bracket(&self) -> String {
+        if self.config.bracket_spacing { "{ " } else { "{" }.to_string()
+    }
+
+    /// Respects the `config.bracket_spacing` setting:
+    /// `" }"` if `true`, `"}"` if `false`
+    fn closing_bracket(&self) -> String {
+        if self.config.bracket_spacing { " }" } else { "}" }.to_string()
+    }
+
+    /// Respects the `config.bracket_spacing` setting:
+    /// `"{ }"` if `true`, `"{}"` if `false`
     fn empty_brackets(&self) -> String {
         if self.config.bracket_spacing { "{ }" } else { "{}" }.to_string()
+    }
+}
+
+impl<'a, W: Write> Write for Formatter<'a, W> {
+    fn write_str(&mut self, s: &str) -> std::fmt::Result {
+        let indent = " ".repeat(self.config.tab_width * self.level);
+
+        if self.pending_indent {
+            IndentWriter::new(&indent, &mut self.w).write_str(s)?;
+        } else {
+            self.w.write_str(s)?;
+        }
+
+        self.pending_indent = s.ends_with('\n');
+
+        Ok(())
     }
 }
 
@@ -29,58 +70,109 @@ impl<'a, W: Write> Formatter<'a, W> {
 impl<'a, W: Write> Visitor for Formatter<'a, W> {
     // TODO implement all visit callback and write the formatted output to the underlying
 
-    fn visit_source_unit(&mut self, source_unit: &SourceUnit) -> VResult {
-        for (i, unit) in source_unit.0.iter().enumerate() {
+    fn visit_source_unit(&mut self, source_unit: &mut SourceUnit) -> VResult {
+        let source_unit_parts = source_unit.0.len();
+        for (i, unit) in source_unit.0.iter_mut().enumerate() {
             unit.visit(self)?;
 
-            if i != source_unit.0.len() - 1 {
-                writeln!(self.f)?;
+            if i != source_unit_parts - 1 {
+                writeln!(self)?;
             }
         }
 
         Ok(())
     }
 
-    fn visit_contract(&mut self, contract: &ContractDefinition) -> VResult {
+    fn visit_contract(&mut self, contract: &mut ContractDefinition) -> VResult {
+        write!(self, "contract {} ", &contract.name.name)?;
         if contract.parts.is_empty() {
-            writeln!(self.f, "contract {} {}", &contract.name.name, self.empty_brackets())?;
+            writeln!(self, "{}", self.empty_brackets())?;
         } else {
-            writeln!(self.f, "contract {} {{", &contract.name.name)?;
+            writeln!(self, "{{")?;
 
-            self.f.indent(1);
-            for (i, part) in contract.parts.iter().enumerate() {
+            self.indent(1);
+
+            let contract_parts = contract.parts.len();
+            for (i, part) in contract.parts.iter_mut().enumerate() {
                 part.visit(self)?;
 
-                if i != contract.parts.len() - 1 {
-                    writeln!(self.f)?;
+                if i != contract_parts - 1 {
+                    writeln!(self)?;
                 }
             }
-            self.f.dedent(1);
+            self.dedent(1);
 
-            writeln!(self.f, "}}")?;
+            writeln!(self, "}}")?;
         }
 
         Ok(())
     }
 
-    fn visit_enum(&mut self, enumeration: &EnumDefinition) -> VResult {
+    fn visit_pragma(&mut self, ident: &mut Identifier, str: &mut StringLiteral) -> VResult {
+        writeln!(self, "pragma {} {};", &ident.name, &str.string)?;
+
+        Ok(())
+    }
+
+    fn visit_import_plain(&mut self, import: &mut StringLiteral) -> VResult {
+        writeln!(self, "import \"{}\";", &import.string)?;
+
+        Ok(())
+    }
+
+    fn visit_import_global(
+        &mut self,
+        global: &mut StringLiteral,
+        alias: &mut Identifier,
+    ) -> VResult {
+        writeln!(self, "import \"{}\" as {};", global.string, alias.name)?;
+
+        Ok(())
+    }
+
+    fn visit_import_renames(
+        &mut self,
+        imports: &mut Vec<(Identifier, Option<Identifier>)>,
+        from: &mut StringLiteral,
+    ) -> VResult {
+        write!(self, "import {}", self.opening_bracket())?;
+        write!(
+            self,
+            "{}",
+            imports
+                .iter()
+                .map(|(ident, alias)| format!(
+                    "{}{}",
+                    ident.name,
+                    alias.as_ref().map_or("".to_string(), |alias| format!(" as {}", alias.name))
+                ))
+                .join(", ")
+        )?;
+        writeln!(self, "{} from \"{}\";", self.closing_bracket(), from.string)?;
+
+        Ok(())
+    }
+
+    fn visit_enum(&mut self, enumeration: &mut EnumDefinition) -> VResult {
+        write!(self, "enum {} ", &enumeration.name.name)?;
         if enumeration.values.is_empty() {
-            writeln!(self.f, "\nenum {} {}", &enumeration.name.name, self.empty_brackets())?;
+            writeln!(self, "{}", self.empty_brackets())?;
         } else {
-            write!(self.f, "\nenum {} {{", &enumeration.name.name)?;
+            writeln!(self, "{{")?;
 
-            self.f.indent(1);
+            self.indent(1);
             for (i, value) in enumeration.values.iter().enumerate() {
-                writeln!(
-                    self.f,
-                    "\n{}{}",
-                    &value.name,
-                    if i != enumeration.values.len() - 1 { "," } else { "" }
-                )?;
-            }
-            self.f.dedent(1);
+                write!(self, "{}", &value.name)?;
 
-            writeln!(self.f, "\n}}")?;
+                if i != enumeration.values.len() - 1 {
+                    write!(self, ",")?;
+                }
+
+                writeln!(self)?;
+            }
+            self.dedent(1);
+
+            writeln!(self, "}}")?;
         }
 
         Ok(())
@@ -108,7 +200,7 @@ mod tests {
 
     use super::*;
 
-    fn test_formatter(source: &str, expected: &str) {
+    fn test_formatter(config: FormatterConfig, source: &str, expected: &str) {
         use pretty_assertions::assert_eq;
         use std::fmt;
 
@@ -123,9 +215,9 @@ mod tests {
             }
         }
 
-        let s = solang::parser::parse(source, 1).unwrap();
+        let mut s = solang::parser::parse(source, 1).unwrap();
         let mut out = String::new();
-        let mut f = Formatter::new(&mut out, Default::default());
+        let mut f = Formatter::new(&mut out, config);
 
         s.visit(&mut f).unwrap();
 
@@ -136,53 +228,102 @@ mod tests {
 
     #[test]
     fn contract() {
-        let source = r#"
+        test_formatter(
+            FormatterConfig { bracket_spacing: false, ..Default::default() },
+            r#"
 contract Empty {
     
 }
 contract Enums {enum Empty {}}
-"#;
-
-        let expected = r#"
+"#,
+            r#"
 contract Empty {}
 
 contract Enums {
     enum Empty {}
 }
-"#;
+"#,
+        );
+        test_formatter(
+            FormatterConfig { bracket_spacing: true, ..Default::default() },
+            r#"
+contract Empty {
+}
+"#,
+            r#"
+contract Empty { }
+"#,
+        );
+    }
 
-        test_formatter(source, expected);
+    #[test]
+    fn pragma() {
+        test_formatter(
+            Default::default(),
+            "pragma    solidity  ^0.5.0;",
+            "pragma solidity ^0.5.0;\n",
+        );
+    }
+
+    #[test]
+    fn import() {
+        test_formatter(
+            Default::default(),
+            "import    \"library.sol\"  ;",
+            "import \"library.sol\";\n",
+        );
+        test_formatter(
+            Default::default(),
+            "import    \"library.sol\"   as   file  ;",
+            "import \"library.sol\" as file;\n",
+        );
+        test_formatter(
+            FormatterConfig { bracket_spacing: false, ..Default::default() },
+            "import{  symbol1  as   alias,   symbol2  } from   \"filename\"   ;",
+            "import {symbol1 as alias, symbol2} from \"filename\";\n",
+        );
+        test_formatter(
+            FormatterConfig { bracket_spacing: true, ..Default::default() },
+            "import{  symbol1  as   alias,   symbol2  } from   \"filename\"   ;",
+            "import { symbol1 as alias, symbol2 } from \"filename\";\n",
+        );
     }
 
     #[test]
     fn enumeration() {
-        let source = r#"
-contract EnumDefinitions {
-    enum Empty {
-        
-    }
-    enum States { State1, State2, State3, State4, State5, State6, State7, State8, State9 }
+        test_formatter(
+            FormatterConfig { bracket_spacing: false, ..Default::default() },
+            r#"
+enum Empty {
+    
 }
-"#;
+enum States { State1, State2, State3, State4, State5, State6, State7, State8, State9 }
+"#,
+            r#"
+enum Empty {}
 
-        let expected = r#"
-contract EnumDefinitions {
-    enum Empty {}
-
-    enum States {
-        State1,
-        State2,
-        State3,
-        State4,
-        State5,
-        State6,
-        State7,
-        State8,
-        State9
-    }
+enum States {
+    State1,
+    State2,
+    State3,
+    State4,
+    State5,
+    State6,
+    State7,
+    State8,
+    State9
 }
-"#;
-
-        test_formatter(source, expected);
+"#,
+        );
+        test_formatter(
+            FormatterConfig { bracket_spacing: true, ..Default::default() },
+            r#"
+enum Empty {
+}
+"#,
+            r#"
+enum Empty { }
+"#,
+        );
     }
 }
--- fmt/src/visit.rs
+++ fmt/src/visit.rs
@@ -9,35 +9,35 @@ pub type VResult = Result<(), Box<dyn std::error::Error>>;
 ///
 /// Each method of the `Visitor` trait is a hook that can be potentially overridden.
 pub trait Visitor {
-    fn visit_source_unit(&mut self, _source_unit: &SourceUnit) -> VResult {
+    fn visit_source_unit(&mut self, _source_unit: &mut SourceUnit) -> VResult {
         Ok(())
     }
 
-    fn visit_stmt(&mut self, _stmt: &Statement) -> VResult {
+    fn visit_stmt(&mut self, _stmt: &mut Statement) -> VResult {
         Ok(())
     }
 
-    fn visit_assembly(&mut self, _stmt: &AssemblyStatement) -> VResult {
+    fn visit_assembly(&mut self, _stmt: &mut AssemblyStatement) -> VResult {
         Ok(())
     }
 
-    fn visit_arg(&mut self, _stmt: &NamedArgument) -> VResult {
+    fn visit_arg(&mut self, _stmt: &mut NamedArgument) -> VResult {
         Ok(())
     }
 
-    fn visit_expr(&mut self, _expr: &Expression) -> VResult {
+    fn visit_expr(&mut self, _expr: &mut Expression) -> VResult {
         Ok(())
     }
 
-    fn visit_emit(&mut self, _stmt: &Expression) -> VResult {
+    fn visit_emit(&mut self, _stmt: &mut Expression) -> VResult {
         Ok(())
     }
 
-    fn visit_var_def(&mut self, _var: &VariableDefinition) -> VResult {
+    fn visit_var_def(&mut self, _var: &mut VariableDefinition) -> VResult {
         Ok(())
     }
 
-    fn visit_return(&mut self, _expr: &Option<Expression>) -> VResult {
+    fn visit_return(&mut self, _expr: &mut Option<Expression>) -> VResult {
         Ok(())
     }
 
@@ -49,35 +49,35 @@ pub trait Visitor {
         Ok(())
     }
 
-    fn visit_do_while(&mut self, _stmt: &Statement, _expr: &Expression) -> VResult {
+    fn visit_do_while(&mut self, _stmt: &mut Statement, _expr: &mut Expression) -> VResult {
         Ok(())
     }
 
-    fn visit_while(&mut self, _expr: &Expression, _stmt: &Statement) -> VResult {
+    fn visit_while(&mut self, _expr: &mut Expression, _stmt: &mut Statement) -> VResult {
         Ok(())
     }
 
-    fn visit_function(&mut self, _func: &FunctionDefinition) -> VResult {
+    fn visit_function(&mut self, _func: &mut FunctionDefinition) -> VResult {
         Ok(())
     }
 
-    fn visit_contract(&mut self, _contract: &ContractDefinition) -> VResult {
+    fn visit_contract(&mut self, _contract: &mut ContractDefinition) -> VResult {
         Ok(())
     }
 
-    fn visit_struct(&mut self, _struct: &StructDefinition) -> VResult {
+    fn visit_struct(&mut self, _struct: &mut StructDefinition) -> VResult {
         Ok(())
     }
 
-    fn visit_event(&mut self, _event: &EventDefinition) -> VResult {
+    fn visit_event(&mut self, _event: &mut EventDefinition) -> VResult {
         Ok(())
     }
 
-    fn visit_event_parameter(&mut self, _param: &EventParameter) -> VResult {
+    fn visit_event_parameter(&mut self, _param: &mut EventParameter) -> VResult {
         Ok(())
     }
 
-    fn visit_enum(&mut self, _enum: &EnumDefinition) -> VResult {
+    fn visit_enum(&mut self, _enum: &mut EnumDefinition) -> VResult {
         Ok(())
     }
 
@@ -85,15 +85,31 @@ pub trait Visitor {
         Ok(())
     }
 
-    fn visit_using(&mut self, _using: &Using) -> VResult {
+    fn visit_using(&mut self, _using: &mut Using) -> VResult {
         Ok(())
     }
 
-    fn visit_import(&mut self, _import: &Import) -> VResult {
+    fn visit_import_plain(&mut self, _import: &mut StringLiteral) -> VResult {
         Ok(())
     }
 
-    fn visit_pragma(&mut self, _ident: &Identifier, _str: &StringLiteral) -> VResult {
+    fn visit_import_global(
+        &mut self,
+        _global: &mut StringLiteral,
+        _alias: &mut Identifier,
+    ) -> VResult {
+        Ok(())
+    }
+
+    fn visit_import_renames(
+        &mut self,
+        _imports: &mut Vec<(Identifier, Option<Identifier>)>,
+        _from: &mut StringLiteral,
+    ) -> VResult {
+        Ok(())
+    }
+
+    fn visit_pragma(&mut self, _ident: &mut Identifier, _str: &mut StringLiteral) -> VResult {
         Ok(())
     }
 
@@ -102,11 +118,11 @@ pub trait Visitor {
 
 /// The trait for solidity AST nodes
 pub trait Visitable {
-    fn visit(&self, v: &mut impl Visitor) -> VResult;
+    fn visit(&mut self, v: &mut impl Visitor) -> VResult;
 }
 
 impl<T: Visitable> Visitable for Vec<T> {
-    fn visit(&self, v: &mut impl Visitor) -> VResult {
+    fn visit(&mut self, v: &mut impl Visitor) -> VResult {
         for t in self {
             t.visit(v)?;
         }
@@ -115,7 +131,7 @@ impl<T: Visitable> Visitable for Vec<T> {
 }
 
 impl Visitable for SourceUnit {
-    fn visit(&self, v: &mut impl Visitor) -> VResult {
+    fn visit(&mut self, v: &mut impl Visitor) -> VResult {
         v.visit_source_unit(self)
     }
 }
@@ -123,11 +139,11 @@ impl Visitable for SourceUnit {
 // TODO implement Visitable for all AST nodes
 
 impl Visitable for SourceUnitPart {
-    fn visit(&self, v: &mut impl Visitor) -> VResult {
+    fn visit(&mut self, v: &mut impl Visitor) -> VResult {
         match self {
             SourceUnitPart::ContractDefinition(contract) => v.visit_contract(contract),
             SourceUnitPart::PragmaDirective(_, ident, str) => v.visit_pragma(ident, str),
-            SourceUnitPart::ImportDirective(_, import) => v.visit_import(import),
+            SourceUnitPart::ImportDirective(_, import) => import.visit(v),
             SourceUnitPart::EnumDefinition(enumeration) => v.visit_enum(enumeration),
             SourceUnitPart::StructDefinition(structure) => v.visit_struct(structure),
             SourceUnitPart::EventDefinition(event) => v.visit_event(event),
@@ -138,8 +154,18 @@ impl Visitable for SourceUnitPart {
     }
 }
 
+impl Visitable for Import {
+    fn visit(&mut self, v: &mut impl Visitor) -> VResult {
+        match self {
+            Import::Plain(import) => v.visit_import_plain(import),
+            Import::GlobalSymbol(global, import_as) => v.visit_import_global(global, import_as),
+            Import::Rename(from, imports) => v.visit_import_renames(imports, from),
+        }
+    }
+}
+
 impl Visitable for ContractPart {
-    fn visit(&self, v: &mut impl Visitor) -> VResult {
+    fn visit(&mut self, v: &mut impl Visitor) -> VResult {
         match self {
             ContractPart::StructDefinition(structure) => v.visit_struct(structure),
             ContractPart::EventDefinition(event) => v.visit_event(event),
