d52b4cb0a03e185801fb04a603ff79432fb31438
Georgios Konstantopoulos <me@gakonst.com>
2021-12-10 14:09:50 -0700

    chore: fix clippy error

--- evm-adapters/src/sputnik/forked_backend/rpc.rs
+++ evm-adapters/src/sputnik/forked_backend/rpc.rs
@@ -82,14 +82,13 @@ where
 
     fn block_number(&self) -> U256 {
         self.pin_block
-            .map(|block| match block {
+            .and_then(|block| match block {
                 BlockId::Number(num) => match num {
                     BlockNumber::Number(num) => Some(num.as_u64().into()),
                     _ => None,
                 },
                 BlockId::Hash(_) => None,
             })
-            .flatten()
             .unwrap_or_else(|| self.backend.block_number())
     }
 
