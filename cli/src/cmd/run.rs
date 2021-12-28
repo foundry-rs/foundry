e763aacea56afd675d8fb809c488564851df35e7
Georgios Konstantopoulos <me@gakonst.com>
2021-12-28 17:58:09 +0200

    feat: add command for building the contract

--- cli/src/cmd/run.rs
+++ cli/src/cmd/run.rs
@@ -55,3 +55,71 @@ impl Cmd for RunArgs {
         Ok(())
     }
 }
+
+impl RunArgs {
+    /// Compiles the file with auto-detection and compiler params.
+    // TODO: This is too verbose. We definitely want an easier way to do "take this file, detect
+    // its solc version and give me all its ABIs & Bytecodes in memory w/o touching disk".
+    pub fn build(&self) -> eyre::Result<CompactContract> {
+        let paths = ProjectPathsConfig::builder().root(&self.path).sources(&self.path).build()?;
+
+        let optimizer = Optimizer {
+            enabled: Some(self.compiler.optimize),
+            runs: Some(self.compiler.optimize_runs as usize),
+        };
+
+        let solc_settings = Settings {
+            optimizer,
+            evm_version: Some(self.compiler.evm_version),
+            ..Default::default()
+        };
+        let solc_cfg = SolcConfig::builder().settings(solc_settings).build()?;
+
+        // setup the compiler
+        let mut builder = Project::builder()
+            .paths(paths)
+            .allowed_path(&self.path)
+            .solc_config(solc_cfg)
+            // we do not want to generate any compilation artifacts in the script run mode
+            .no_artifacts()
+            // no cache
+            .ephemeral();
+        if self.no_auto_detect {
+            builder = builder.no_auto_detect();
+        }
+        let project = builder.build()?;
+
+        println!("compiling...");
+        let output = project.compile()?;
+        if output.has_compiler_errors() {
+            // return the diagnostics error back to the user.
+            eyre::bail!(output.to_string())
+        } else if output.is_unchanged() {
+            println!("no files changed, compilation skippped.");
+        } else {
+            println!("success.");
+        };
+
+        // get the contracts
+        let contracts = output.output();
+
+        // get the specific contract
+        let contract = if let Some(ref contract) = self.contract {
+            let contract = contracts.find(contract).ok_or_else(|| {
+                eyre::Error::msg("contract not found, did you type the name wrong?")
+            })?;
+            CompactContract::from(contract)
+        } else {
+            let mut contracts =
+                contracts.contracts_into_iter().filter(|(_, contract)| contract.evm.is_some());
+            let contract = contracts.next().ok_or_else(|| eyre::Error::msg("no contract found"))?.1;
+            if contracts.peekable().peek().is_some() {
+                eyre::bail!(
+                    ">1 contracts found, please provide a contract name to choose one of them"
+                )
+            }
+            CompactContract::from(contract)
+        };
+        Ok(contract)
+    }
+}
