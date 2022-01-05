e8b85b0ad530ccda4b571b9bca53b81d760fda4c
Frankie <frankieislost@protonmail.com>
2022-01-04 17:37:14 -0800

    failing test case

--- evm-adapters/testdata/ConsoleLogs.sol
+++ evm-adapters/testdata/ConsoleLogs.sol
@@ -11,4 +11,11 @@ contract ConsoleLogs {
 		console.log(1337, 1245);
 		console.log("Hi", 1337);
     }
+
+	function test_log_types() public {
+		console.logString("String");
+		console.logInt(1337);
+		console.logUint(1245);
+		console.logBool(true);
+	}
 }
