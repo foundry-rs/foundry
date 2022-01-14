e1c09373651037c82ff671b92b7eda8b219fb3da
Georgios Konstantopoulos <me@gakonst.com>
2022-01-14 14:13:57 +0200

    test(utils): fix sol test vectors

--- utils/testdata/interfaceTest.sol
+++ utils/testdata/interfaceTest.sol
@@ -1,4 +1,4 @@
 interface test {
-     function guess(uint8 n, address x) payable external;
-     function isComplete() view external returns (bool example, string);
+    function guess(uint8 n, address x) payable external;
+    function isComplete() view external returns (bool example, string memory);
 }
