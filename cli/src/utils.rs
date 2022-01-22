54645580a208f2ff2017a1bb63da7a7a14053570
Matthias Seitz <matthias.seitz@outlook.de>
2022-01-22 11:13:32 +0100

    feat: verbose version message

--- cli/src/utils.rs
+++ cli/src/utils.rs
@@ -18,6 +18,17 @@ const LOCAL_RPC_URL: &str = "http://127.0.0.1:8545";
 /// Default Path to where the contract artifacts are stored
 pub const DAPP_JSON: &str = "./out/dapp.sol.json";
 
+/// The version message for the current program, like
+/// `forge 0.1.0 (f01b232bc 2022-01-22T23:28:39.493201+00:00)`
+pub(crate) const VERSION_MESSAGE: &str = concat!(
+    env!("CARGO_PKG_VERSION"),
+    " (",
+    env!("VERGEN_GIT_SHA_SHORT"),
+    " ",
+    env!("VERGEN_BUILD_TIMESTAMP"),
+    ")"
+);
+
 /// Initializes a tracing Subscriber for logging
 #[allow(dead_code)]
 pub fn subscriber() {
