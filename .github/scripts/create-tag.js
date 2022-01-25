8ccfe493a6a03f0decf26b5643ffeece7569816b
Bjerg <onbjerg@users.noreply.github.com>
2022-01-25 12:40:59 +0100

    Nightly backwards compatability (#574)
    * Always maintain a nightly release tagged `nightly`
    
    This works as a release channel: it is always possible to
    download the latest nightly from the release tagged `nightly`.
    
    Additionally, 3 nightlies tagged with `nightly-${SHA}` are kept,
    allowing users to rollback to a specific nightly.
    
    * Move `nightly` tag last
    
    Ensures that newer `foundryup` installations don't fail
    to update while the new `nightly-${SHA}` release is still missing
    some release assets.

---
+++ .github/scripts/create-tag.js
@@ -0,0 +1,14 @@
+module.exports = async ({github, context}, tagName) => {
+  try {
+    await github.rest.git.createRef({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      ref: `refs/tags/${tagName}`,
+      sha: context.sha,
+      force: true
+    })
+  } catch (err) {
+    console.error(`Failed to create tag: ${tagName}`)
+    console.error(err)
+  }
+}
