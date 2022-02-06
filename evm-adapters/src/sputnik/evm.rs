c50ba3045ad4cd6a28b0833a9b5ba0ed1a4d99e8
Matthias Seitz <matthias.seitz@outlook.de>
2022-02-06 22:24:40 +0100

    chore: reenable all impls

--- evm-adapters/src/sputnik/evm.rs
+++ evm-adapters/src/sputnik/evm.rs
@@ -336,8 +336,7 @@ pub mod helpers {
         let cfg = proptest::test_runner::Config { failure_persistence: None, ..Default::default() };
 
         let runner = proptest::test_runner::TestRunner::new(cfg);
-        // FuzzedExecutor::new(evm, runner, Address::zero())
-        todo!("reimplement again")
+        FuzzedExecutor::new(evm, runner, Address::zero())
     }
 
     pub fn new_backend(vicinity: &MemoryVicinity, state: MemoryState) -> MemoryBackend<'_> {
