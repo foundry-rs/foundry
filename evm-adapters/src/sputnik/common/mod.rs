9e7b257588861d345cfb34620478147ed7917937
Matthias Seitz <matthias.seitz@outlook.de>
2022-02-06 16:03:16 +0100

    feat: impl handler

--- evm-adapters/src/sputnik/common/mod.rs
+++ evm-adapters/src/sputnik/common/mod.rs
@@ -337,6 +337,14 @@ where
     fn stack_executor_mut(&mut self) -> &mut StackExecutor<'a, 'b, State, Precom> {
         self.handler_mut().stack_executor_mut()
     }
+
+    fn state(&self) -> &State {
+        self.handler().stack_executor().state()
+    }
+
+    fn state_mut(&mut self) -> &mut State {
+        self.handler_mut().stack_executor_mut().state_mut()
+    }
 }
 
 impl<'handler, 'a, 'b, Back, Precom: 'b, ExecHandler>
@@ -354,6 +362,64 @@ where
     Back: Backend,
     Precom: PrecompileSet,
 {
+    fn config(&self) -> &Config {
+        self.handler().stack_executor().config()
+    }
+
+    // NB: This function is copy-pasted from upstream's `execute`, adjusted so that we call the
+    // Runtime with our own handler
+    pub fn execute(&mut self, runtime: &mut Runtime) -> ExitReason {
+        match runtime.run(self) {
+            Capture::Exit(s) => s,
+            Capture::Trap(_) => unreachable!("Trap is Infallible"),
+        }
+    }
+
+    pub fn start_trace(
+        &mut self,
+        address: H160,
+        input: Vec<u8>,
+        transfer: U256,
+        creation: bool,
+    ) -> Option<CallTrace> {
+        start_trace!(self, address, input, transfer, creation)
+    }
+
+    pub fn fill_trace(
+        &mut self,
+        new_trace: &Option<CallTrace>,
+        success: bool,
+        output: Option<Vec<u8>>,
+        pre_trace_index: usize,
+    ) {
+        self.handler.fill_trace(new_trace, success, output, pre_trace_index)
+    }
+
+    // NB: This function is copy-pasted from upstream's call_inner
+    #[allow(clippy::too_many_arguments)]
+    pub fn call_inner(
+        &mut self,
+        code_address: H160,
+        transfer: Option<Transfer>,
+        input: Vec<u8>,
+        target_gas: Option<u64>,
+        is_static: bool,
+        take_l64: bool,
+        take_stipend: bool,
+        context: Context,
+    ) -> Capture<(ExitReason, Vec<u8>), Infallible> {
+        call_inner!(
+            self,
+            code_address,
+            transfer,
+            input,
+            target_gas,
+            is_static,
+            take_l64,
+            take_stipend,
+            context
+        )
+    }
 }
 
 impl<'handler, 'a, 'b, Back, Precom: 'b, ExecHandler> Handler
@@ -377,95 +443,95 @@ where
     type CallFeedback = Infallible;
 
     fn balance(&self, address: H160) -> U256 {
-        todo!()
+        self.handler().balance(address)
     }
 
     fn code_size(&self, address: H160) -> U256 {
-        todo!()
+        self.handler().code_size(address)
     }
 
     fn code_hash(&self, address: H160) -> H256 {
-        todo!()
+        self.handler().code_hash(address)
     }
 
     fn code(&self, address: H160) -> Vec<u8> {
-        todo!()
+        self.handler().code(address)
     }
 
     fn storage(&self, address: H160, index: H256) -> H256 {
-        todo!()
+        self.handler().storage(address, index)
     }
 
     fn original_storage(&self, address: H160, index: H256) -> H256 {
-        todo!()
+        self.handler().original_storage(address, index)
     }
 
     fn gas_left(&self) -> U256 {
-        todo!()
+        self.handler().gas_left()
     }
 
     fn gas_price(&self) -> U256 {
-        todo!()
+        self.handler().gas_price()
     }
 
     fn origin(&self) -> H160 {
-        todo!()
+        self.handler().origin()
     }
 
     fn block_hash(&self, number: U256) -> H256 {
-        todo!()
+        self.handler().block_hash(number)
     }
 
     fn block_number(&self) -> U256 {
-        todo!()
+        self.handler().block_number()
     }
 
     fn block_coinbase(&self) -> H160 {
-        todo!()
+        self.handler().block_coinbase()
     }
 
     fn block_timestamp(&self) -> U256 {
-        todo!()
+        self.handler().block_timestamp()
     }
 
     fn block_difficulty(&self) -> U256 {
-        todo!()
+        self.handler().block_difficulty()
     }
 
     fn block_gas_limit(&self) -> U256 {
-        todo!()
+        self.handler().block_gas_limit()
     }
 
     fn block_base_fee_per_gas(&self) -> U256 {
-        todo!()
+        self.handler().block_base_fee_per_gas()
     }
 
     fn chain_id(&self) -> U256 {
-        todo!()
+        self.handler().chain_id()
     }
 
     fn exists(&self, address: H160) -> bool {
-        todo!()
+        self.handler().exists(address)
     }
 
     fn deleted(&self, address: H160) -> bool {
-        todo!()
+        self.handler().deleted(address)
     }
 
     fn is_cold(&self, address: H160, index: Option<H256>) -> bool {
-        todo!()
+        self.handler().is_cold(address, index)
     }
 
     fn set_storage(&mut self, address: H160, index: H256, value: H256) -> Result<(), ExitError> {
-        todo!()
+        self.handler_mut().set_storage(address, index, value)
     }
 
     fn log(&mut self, address: H160, topics: Vec<H256>, data: Vec<u8>) -> Result<(), ExitError> {
-        todo!()
+        macros::log!(self, address, topics, data)
     }
 
     fn mark_delete(&mut self, address: H160, target: H160) -> Result<(), ExitError> {
-        todo!()
+        self.handler_mut().mark_delete(address, target)
     }
 
     fn create(
@@ -476,7 +542,7 @@ where
         init_code: Vec<u8>,
         target_gas: Option<u64>,
     ) -> Capture<(ExitReason, Option<H160>, Vec<u8>), Self::CreateInterrupt> {
-        todo!()
+        self.handler_mut().do_create(caller, scheme, value, init_code, target_gas)
     }
 
     fn call(
@@ -488,7 +554,7 @@ where
         is_static: bool,
         context: Context,
     ) -> Capture<(ExitReason, Vec<u8>), Self::CallInterrupt> {
-        todo!()
+        self.handler_mut().do_call(code_address, transfer, input, target_gas, is_static, context)
     }
 
     fn pre_validate(
@@ -497,7 +563,7 @@ where
         opcode: Opcode,
         stack: &Stack,
     ) -> Result<(), ExitError> {
-        todo!()
+        self.handler_mut().pre_validate(context, opcode, stack)
     }
 }
 
