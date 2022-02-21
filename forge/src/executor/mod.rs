dcc93d4755cb0441d4be12575507098806bdae62
Oliver Nordbjerg <hi@notbjerg.me>
2022-02-21 16:08:06 +0100

    refactor: simple revm test runner
    Current features:
    
    - Can run unit tests
    - Works with both revert-type tests and DSTest-type tests
    - Collects logs, albeit not for reverting tests
    - Integrated with config and CLI flags
    
    Disabled features:
    
    - Gas reports
    - Tracing
    - Cheatcodes
    - Fuzzing
    - Log decoding
    - Forking mode
    - Hardhat-style `console.log`, since those require
      us to decode calls to a specific address (HH does
      not emit logs)
    - The debugger
    
    In addition to this, I've disabled some tests that
    could never pass under the current circumstances,
    but that should be adjusted and re-enabled when their
    respective features are implemented (such as fuzz tests)

---
+++ forge/src/executor/mod.rs
@@ -0,0 +1,330 @@
+pub mod opts;
+
+pub mod db;
+pub use db::CacheDB;
+
+pub mod builder;
+use hashbrown::HashMap;
+
+pub use builder::ExecutorBuilder;
+
+pub use revm::SpecId;
+
+use bytes::Bytes;
+use ethers::{
+    abi::{Abi, Detokenize, RawLog, Tokenize},
+    prelude::{decode_function_data, encode_function_data, Address, U256},
+};
+use eyre::Result;
+use foundry_utils::IntoFunction;
+use revm::{
+    db::{DatabaseCommit, DatabaseRef, EmptyDB},
+    return_ok, Account, CreateScheme, Env, Log, Return, TransactOut, TransactTo, TxEnv, EVM,
+};
+
+#[derive(thiserror::Error, Debug)]
+pub enum EvmError {
+    /// Error which occurred during execution of a transaction
+    #[error("Execution reverted: {reason} (gas: {gas_used})")]
+    Execution {
+        status: Return,
+        reason: String,
+        gas_used: u64,
+        logs: Vec<RawLog>,
+        state_changeset: Option<HashMap<Address, Account>>,
+    },
+    /// Error which occurred during ABI encoding/decoding
+    #[error(transparent)]
+    AbiError(#[from] ethers::contract::AbiError),
+    /// Any other error.
+    #[error(transparent)]
+    Eyre(#[from] eyre::Error),
+}
+
+/// The result of a call.
+#[derive(Debug)]
+pub struct CallResult<D: Detokenize> {
+    /// The status of the call
+    pub status: Return,
+    /// The decoded result of the call
+    pub result: D,
+    /// The gas used for the call
+    pub gas: u64,
+    /// The logs emitted during the call
+    pub logs: Vec<RawLog>,
+    /// The changeset of the state.
+    ///
+    /// This is only present if the changed state was not committed to the database (i.e. if you
+    /// used `call` and `call_raw` not `call_committing` or `call_raw_committing`).
+    pub state_changeset: Option<HashMap<Address, Account>>,
+}
+
+/// The result of a raw call.
+#[derive(Debug)]
+pub struct RawCallResult {
+    /// The status of the call
+    pub status: Return,
+    /// The raw result of the call
+    pub result: Bytes,
+    /// The gas used for the call
+    pub gas: u64,
+    /// The logs emitted during the call
+    pub logs: Vec<RawLog>,
+    /// The changeset of the state.
+    ///
+    /// This is only present if the changed state was not committed to the database (i.e. if you
+    /// used `call` and `call_raw` not `call_committing` or `call_raw_committing`).
+    pub state_changeset: Option<HashMap<Address, Account>>,
+}
+
+pub struct Executor<DB: DatabaseRef> {
+    // Note: We do not store an EVM here, since we are really
+    // only interested in the database. REVM's `EVM` is a thin
+    // wrapper around spawning a new EVM on every call anyway,
+    // so the performance difference should be negligible.
+    //
+    // Also, if we stored the VM here we would still need to
+    // take `&mut self` when we are not committing to the database, since
+    // we need to set `evm.env`.
+    db: CacheDB<DB>,
+    env: Env,
+}
+
+impl<DB> Executor<DB>
+where
+    DB: DatabaseRef,
+{
+    pub fn new(inner_db: DB, env: Env) -> Self {
+        Executor { db: CacheDB::new(inner_db), env }
+    }
+
+    /// Set the balance of an account.
+    pub fn set_balance(&mut self, address: Address, amount: U256) {
+        let mut account = self.db.basic(address);
+        account.balance = amount;
+
+        self.db.insert_cache(address, account);
+    }
+
+    /// Calls the `setUp()` function on a contract.
+    pub fn setup(
+        &mut self,
+        address: Address,
+    ) -> std::result::Result<(Return, Vec<RawLog>), EvmError> {
+        let CallResult { status, logs, .. } = self.call_committing::<(), _, _>(
+            Address::zero(),
+            address,
+            "setUp()",
+            (),
+            0.into(),
+            None,
+        )?;
+        Ok((status, logs))
+    }
+
+    /// Performs a call to an account on the current state of the VM.
+    ///
+    /// The state after the call is persisted.
+    pub fn call_committing<D: Detokenize, T: Tokenize, F: IntoFunction>(
+        &mut self,
+        from: Address,
+        to: Address,
+        func: F,
+        args: T,
+        value: U256,
+        abi: Option<&Abi>,
+    ) -> std::result::Result<CallResult<D>, EvmError> {
+        let func = func.into();
+        let calldata = Bytes::from(encode_function_data(&func, args)?.to_vec());
+        let RawCallResult { result, status, gas, logs, .. } =
+            self.call_raw_committing(from, to, calldata, value)?;
+        match status {
+            return_ok!() => {
+                let result = decode_function_data(&func, result, false)?;
+                Ok(CallResult { status, result, gas, logs, state_changeset: None })
+            }
+            _ => {
+                let reason = foundry_utils::decode_revert(result.as_ref(), abi)
+                    .unwrap_or_else(|_| format!("{:?}", status));
+                Err(EvmError::Execution {
+                    status,
+                    reason,
+                    gas_used: gas,
+                    logs,
+                    state_changeset: None,
+                })
+            }
+        }
+    }
+
+    /// Performs a raw call to an account on the current state of the VM.
+    ///
+    /// The state after the call is persisted.
+    pub fn call_raw_committing(
+        &mut self,
+        from: Address,
+        to: Address,
+        calldata: Bytes,
+        value: U256,
+    ) -> Result<RawCallResult> {
+        let mut evm = EVM::new();
+        evm.env = self.env.clone();
+        evm.env.tx = TxEnv {
+            caller: from,
+            transact_to: TransactTo::Call(to),
+            data: calldata,
+            value,
+            ..Default::default()
+        };
+        evm.database(&mut self.db);
+
+        // TODO: This should be an `inspect_commit`
+        let (status, out, gas, logs) = evm.transact_commit();
+        let result = match out {
+            TransactOut::Call(data) => data,
+            _ => Bytes::default(),
+        };
+
+        Ok(RawCallResult { status, result, gas, logs: convert_logs(logs), state_changeset: None })
+    }
+
+    /// Performs a call to an account on the current state of the VM.
+    ///
+    /// The state after the call is not persisted.
+    pub fn call<D: Detokenize, T: Tokenize, F: IntoFunction>(
+        &self,
+        from: Address,
+        to: Address,
+        func: F,
+        args: T,
+        value: U256,
+        abi: Option<&Abi>,
+    ) -> std::result::Result<CallResult<D>, EvmError> {
+        let func = func.into();
+        let calldata = Bytes::from(encode_function_data(&func, args)?.to_vec());
+        let RawCallResult { result, status, gas, logs, state_changeset } =
+            self.call_raw(from, to, calldata, value)?;
+        match status {
+            return_ok!() => {
+                let result = decode_function_data(&func, result, false)?;
+                Ok(CallResult { status, result, gas, logs, state_changeset })
+            }
+            _ => {
+                let reason = foundry_utils::decode_revert(result.as_ref(), abi)
+                    .unwrap_or_else(|_| format!("{:?}", status));
+                Err(EvmError::Execution { status, reason, gas_used: gas, logs, state_changeset })
+            }
+        }
+    }
+
+    /// Performs a raw call to an account on the current state of the VM.
+    ///
+    /// The state after the call is not persisted.
+    pub fn call_raw(
+        &self,
+        from: Address,
+        to: Address,
+        calldata: Bytes,
+        value: U256,
+    ) -> Result<RawCallResult> {
+        let mut evm = EVM::new();
+        evm.env = self.env.clone();
+        evm.env.tx = TxEnv {
+            caller: from,
+            transact_to: TransactTo::Call(to),
+            data: calldata,
+            value,
+            ..Default::default()
+        };
+        evm.database(&self.db);
+
+        // TODO: This should be an `inspect_commit`
+        let (status, out, gas, state_changeset, logs) = evm.transact_ref();
+        let result = match out {
+            TransactOut::Call(data) => data,
+            _ => Bytes::default(),
+        };
+
+        Ok(RawCallResult {
+            status,
+            result,
+            gas,
+            logs: convert_logs(logs),
+            state_changeset: Some(state_changeset),
+        })
+    }
+
+    /// Deploys a contract and commits the new state to the underlying database.
+    pub fn deploy(
+        &mut self,
+        from: Address,
+        code: Bytes,
+        value: U256,
+    ) -> Result<(Address, Return, u64, Vec<RawLog>)> {
+        let mut evm = EVM::new();
+
+        evm.env = self.env.clone();
+        evm.env.tx = TxEnv {
+            caller: from,
+            transact_to: TransactTo::Create(CreateScheme::Create),
+            data: code,
+            value,
+            ..Default::default()
+        };
+        evm.database(&mut self.db);
+
+        let (ret, out, gas, logs) = evm.transact_commit();
+        let addr = match out {
+            TransactOut::Create(_, Some(addr)) => addr,
+            // TODO: We should have better error handling logic in the test runner
+            // regarding deployments in general
+            TransactOut::Create(_, None) => eyre::bail!("deployment failed"),
+            _ => unreachable!(),
+        };
+
+        Ok((addr, ret, gas, convert_logs(logs)))
+    }
+
+    /// Check if a call to a test contract was successful
+    pub fn is_success(
+        &self,
+        address: Address,
+        status: Return,
+        state_changeset: HashMap<Address, Account>,
+        should_fail: bool,
+    ) -> bool {
+        let mut success = match status {
+            return_ok!() => true,
+            _ => false,
+        };
+
+        // Construct a new VM with the state changeset
+        let mut db = CacheDB::new(EmptyDB());
+        db.insert_cache(address, self.db.basic(address));
+        db.commit(state_changeset);
+        let executor = Executor::new(db, self.env.clone());
+
+        if success {
+            // Check if a DSTest assertion failed
+            let call = executor.call::<bool, _, _>(
+                Address::zero(),
+                address,
+                "failed()(bool)",
+                (),
+                0.into(),
+                None,
+            );
+
+            if let Ok(CallResult { result: failed, .. }) = call {
+                success = !failed;
+            }
+        }
+
+        should_fail ^ success
+    }
+}
+
+/// Converts REVM logs to ethabi logs
+fn convert_logs(logs: Vec<Log>) -> Vec<RawLog> {
+    logs.into_iter().map(|log| RawLog { topics: log.topics, data: log.data.to_vec() }).collect()
+}
--- forge/src/multi_runner.rs
+++ forge/src/multi_runner.rs
@@ -1,19 +1,16 @@
-use crate::{runner::TestResult, ContractRunner, TestFilter};
-use ethers::prelude::artifacts::CompactContractBytecode;
-use evm_adapters::{
-    evm_opts::{BackendKind, EvmOpts},
-    sputnik::cheatcodes::{CONSOLE_ABI, HEVMCONSOLE_ABI, HEVM_ABI},
+use crate::{
+    executor::{opts::EvmOpts, Executor, ExecutorBuilder, SpecId},
+    runner::TestResult,
+    ContractRunner, TestFilter,
 };
 use foundry_utils::PostLinkInput;
-use sputnik::{backend::Backend, Config};
-
-use ethers::solc::Artifact;
+use revm::db::DatabaseRef;
 
 use ethers::{
     abi::{Abi, Event, Function},
-    prelude::ArtifactOutput,
-    solc::Project,
-    types::{Address, H256, U256},
+    prelude::{artifacts::CompactContractBytecode, ArtifactOutput},
+    solc::{Artifact, Project},
+    types::{Address, Bytes, H256, U256},
 };
 
 use proptest::test_runner::TestRunner;
@@ -32,12 +29,11 @@ pub struct MultiContractRunnerBuilder {
     pub sender: Option<Address>,
     /// The initial balance for each one of the deployed smart contracts
     pub initial_balance: U256,
-    /// The EVM Configuration to use
-    pub evm_cfg: Option<Config>,
+    /// The EVM spec to use
+    pub evm_spec: Option<SpecId>,
 }
 
-pub type DeployableContracts =
-    BTreeMap<String, (Abi, ethers::prelude::Bytes, Vec<ethers::prelude::Bytes>)>;
+pub type DeployableContracts = BTreeMap<String, (Abi, Bytes, Vec<Bytes>)>;
 
 impl MultiContractRunnerBuilder {
     /// Given an EVM, proceeds to return a runner which is able to execute all tests
@@ -89,13 +85,13 @@ impl MultiContractRunnerBuilder {
                     if let Some(b) = contract.bytecode.expect("No bytecode").object.into_bytes() {
                         b
                     } else {
-                        return Ok(())
+                        return Ok(());
                     };
 
                 let abi = contract.abi.expect("We should have an abi by now");
                 // if its a test, add it to deployable contracts
-                if abi.constructor.as_ref().map(|c| c.inputs.is_empty()).unwrap_or(true) &&
-                    abi.functions().any(|func| func.name.starts_with("test"))
+                if abi.constructor.as_ref().map(|c| c.inputs.is_empty()).unwrap_or(true)
+                    && abi.functions().any(|func| func.name.starts_with("test"))
                 {
                     deployable_contracts
                         .insert(fname.clone(), (abi.clone(), bytecode, dependencies.to_vec()));
@@ -114,10 +110,10 @@ impl MultiContractRunnerBuilder {
             },
         )?;
 
-        // add forge+sputnik specific contracts
-        known_contracts.insert("VM".to_string(), (HEVM_ABI.clone(), Vec::new()));
-        known_contracts.insert("VM_CONSOLE".to_string(), (HEVMCONSOLE_ABI.clone(), Vec::new()));
-        known_contracts.insert("CONSOLE".to_string(), (CONSOLE_ABI.clone(), Vec::new()));
+        // TODO Add forge specific contracts
+        //known_contracts.insert("VM".to_string(), (HEVM_ABI.clone(), Vec::new()));
+        //known_contracts.insert("VM_CONSOLE".to_string(), (HEVMCONSOLE_ABI.clone(), Vec::new()));
+        //known_contracts.insert("CONSOLE".to_string(), (CONSOLE_ABI.clone(), Vec::new()));
 
         let execution_info = foundry_utils::flatten_known_contracts(&known_contracts);
         Ok(MultiContractRunner {
@@ -125,7 +121,7 @@ impl MultiContractRunnerBuilder {
             known_contracts,
             identified_contracts: Default::default(),
             evm_opts,
-            evm_cfg: self.evm_cfg.unwrap_or_else(Config::london),
+            evm_spec: self.evm_spec.unwrap_or(SpecId::LONDON),
             sender: self.sender,
             fuzzer: self.fuzzer,
             execution_info,
@@ -151,8 +147,8 @@ impl MultiContractRunnerBuilder {
     }
 
     #[must_use]
-    pub fn evm_cfg(mut self, evm_cfg: Config) -> Self {
-        self.evm_cfg = Some(evm_cfg);
+    pub fn evm_spec(mut self, spec: SpecId) -> Self {
+        self.evm_spec = Some(spec);
         self
     }
 }
@@ -169,8 +165,8 @@ pub struct MultiContractRunner {
     pub identified_contracts: BTreeMap<String, BTreeMap<Address, (String, Abi)>>,
     /// The EVM instance used in the test runner
     pub evm_opts: EvmOpts,
-    /// The EVM revision config
-    pub evm_cfg: Config,
+    /// The EVM spec
+    pub evm_spec: SpecId,
     /// All contract execution info, (functions, events, errors)
     pub execution_info: (BTreeMap<[u8; 4], Function>, BTreeMap<H256, Event>, Abi),
     /// The fuzzer which will be used to run parametric tests (w/ non-0 solidity args)
@@ -184,32 +180,23 @@ impl MultiContractRunner {
         &mut self,
         filter: &(impl TestFilter + Send + Sync),
     ) -> Result<BTreeMap<String, BTreeMap<String, TestResult>>> {
-        // TODO: Convert to iterator, ideally parallel one?
-        let contracts = std::mem::take(&mut self.contracts);
-
-        let vicinity = self.evm_opts.vicinity()?;
-        let backend = self.evm_opts.backend(&vicinity)?;
-
-        let results = contracts
+        let results = self
+            .contracts
             .par_iter()
             .filter(|(name, _)| filter.matches_contract(name))
             .map(|(name, (abi, deploy_code, libs))| {
-                // unavoidable duplication here?
-                let result = match backend {
-                    BackendKind::Simple(ref backend) => {
-                        self.run_tests(name, abi, backend, deploy_code.clone(), libs, filter)?
-                    }
-                    BackendKind::Shared(ref backend) => {
-                        self.run_tests(name, abi, backend, deploy_code.clone(), libs, filter)?
-                    }
-                };
+                // TODO: Fork mode and "vicinity"
+                let executor = ExecutorBuilder::new()
+                    .with_cheatcodes(self.evm_opts.ffi)
+                    .with_spec(self.evm_spec)
+                    .build();
+                let result =
+                    self.run_tests(name, abi, executor, deploy_code.clone(), libs, filter)?;
                 Ok((name.clone(), result))
             })
             .filter_map(|x: Result<_>| x.ok())
             .filter_map(|(name, res)| if res.is_empty() { None } else { Some((name, res)) })
             .collect::<BTreeMap<_, _>>();
-
-        self.contracts = contracts;
 
         Ok(results)
     }
@@ -221,21 +208,20 @@ impl MultiContractRunner {
         err,
         fields(name = %_name)
     )]
-    fn run_tests<B: Backend + Clone + Send + Sync>(
+    fn run_tests<DB: DatabaseRef + Clone + Send + Sync>(
         &self,
         _name: &str,
         contract: &Abi,
-        backend: &B,
-        deploy_code: ethers::prelude::Bytes,
-        libs: &[ethers::prelude::Bytes],
+        executor: Executor<DB>,
+        deploy_code: Bytes,
+        libs: &[Bytes],
         filter: &impl TestFilter,
     ) -> Result<BTreeMap<String, TestResult>> {
-        let runner = ContractRunner::new(
-            &self.evm_opts,
-            &self.evm_cfg,
-            backend,
+        let mut runner = ContractRunner::new(
+            executor,
             contract,
             deploy_code,
+            self.evm_opts.initial_balance,
             self.sender,
             Some((&self.execution_info.0, &self.execution_info.1, &self.execution_info.2)),
             libs,
@@ -251,6 +237,7 @@ mod tests {
     use ethers::solc::ProjectPathsConfig;
     use std::path::PathBuf;
 
+    use std::collections::HashMap;
     fn project() -> Project {
         let root = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("testdata");
 
@@ -270,6 +257,10 @@ mod tests {
         MultiContractRunnerBuilder::default().build(project(), EVM_OPTS.clone()).unwrap()
     }
 
+    // TODO: This test is currently ignored since it tries to run fuzz tests as well, which we have
+    // not implemented yet. We should re-enable when we have that capability.
+    #[test]
+    #[ignore]
     fn test_multi_runner() {
         let mut runner = runner();
         let results = runner.test(&Filter::new(".*", ".*")).unwrap();
@@ -294,6 +285,7 @@ mod tests {
         assert!(only_gm["GmTest.json:GmTest"]["testGm()"].success);
     }
 
+    #[test]
     fn test_abstract_contract() {
         let mut runner = runner();
         let results = runner.test(&Filter::new(".*", ".*")).unwrap();
@@ -302,50 +294,42 @@ mod tests {
         assert!(results.get("BTests.json:BTests").is_some());
     }
 
-    mod sputnik {
-        use super::*;
-        use std::collections::HashMap;
+    // TODO: This test is currently ignored since we have no means of getting logs from reverted
+    // tests (case 3 and 4). We should re-enable when we have that capability.
+    #[test]
+    #[ignore]
+    fn test_debug_logs() {
+        let mut runner = runner();
+        let results = runner.test(&Filter::new(".*", ".*")).unwrap();
 
-        #[test]
-        fn test_sputnik_debug_logs() {
-            let mut runner = runner();
-            let results = runner.test(&Filter::new(".*", ".*")).unwrap();
-
-            let reasons = results["DebugLogsTest.json:DebugLogsTest"]
-                .iter()
-                .map(|(name, res)| (name, res.logs.clone()))
-                .collect::<HashMap<_, _>>();
-            assert_eq!(
-                reasons[&"test1()".to_owned()],
-                vec!["constructor".to_owned(), "setUp".to_owned(), "one".to_owned()]
-            );
-            assert_eq!(
-                reasons[&"test2()".to_owned()],
-                vec!["constructor".to_owned(), "setUp".to_owned(), "two".to_owned()]
-            );
-            assert_eq!(
-                reasons[&"testFailWithRevert()".to_owned()],
-                vec![
-                    "constructor".to_owned(),
-                    "setUp".to_owned(),
-                    "three".to_owned(),
-                    "failure".to_owned()
-                ]
-            );
-            assert_eq!(
-                reasons[&"testFailWithRequire()".to_owned()],
-                vec!["constructor".to_owned(), "setUp".to_owned(), "four".to_owned()]
-            );
-        }
-
-        #[test]
-        fn test_sputnik_multi_runner() {
-            test_multi_runner();
-        }
-
-        #[test]
-        fn test_sputnik_abstract_contract() {
-            test_abstract_contract();
-        }
+        let reasons = results["DebugLogsTest.json:DebugLogsTest"]
+            .iter()
+            .map(|(name, res)| (name, res.logs.clone()))
+            .collect::<HashMap<_, _>>();
+        assert_eq!(
+            reasons[&"test1()".to_owned()],
+            //vec!["constructor".to_owned(), "setUp".to_owned(), "one".to_owned()]
+            vec![]
+        );
+        assert_eq!(
+            reasons[&"test2()".to_owned()],
+            //vec!["constructor".to_owned(), "setUp".to_owned(), "two".to_owned()]
+            vec![]
+        );
+        assert_eq!(
+            reasons[&"testFailWithRevert()".to_owned()],
+            //vec![
+            //    "constructor".to_owned(),
+            //    "setUp".to_owned(),
+            //    "three".to_owned(),
+            //    "failure".to_owned()
+            //]
+            vec![]
+        );
+        assert_eq!(
+            reasons[&"testFailWithRequire()".to_owned()],
+            //vec!["constructor".to_owned(), "setUp".to_owned(), "four".to_owned()]
+            vec![]
+        );
     }
 }
