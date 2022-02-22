abf8d59d4b9487966fbc1b36e4d0ec62aa75e35b
Bjerg <onbjerg@users.noreply.github.com>
2022-02-22 18:06:08 +0100

    Simple REVM test runner (#788)
    * refactor: nuke `evm-adapters`
    
    * refactor: simple revm test runner
    
    Current features:
    
    - Can run unit tests
    - Works with both revert-type tests and DSTest-type tests
    - Collects logs, albeit not for reverting tests
    - Integrated with config and CLI flags
    
    Disabled features:
    
    - Gas reports
    - Tracing
    - Cheatcodes
    - Fuzzing
    - Log decoding
    - Forking mode
    - Hardhat-style `console.log`, since those require
      us to decode calls to a specific address (HH does
      not emit logs)
    - The debugger
    
    In addition to this, I've disabled some tests that
    could never pass under the current circumstances,
    but that should be adjusted and re-enabled when their
    respective features are implemented (such as fuzz tests)
    
    * refactor: adjust CLI to new runner API
    
    * feat: log collector inspector
    
    * feat: hardhat logs
    
    * chore: lint
    
    * refactor: extract hh log converter to helper fn
    
    * refactor: return single test result if setup fails
    
    * build: use upstream revm
    
    chore: renuke `evm-adapters`

---
+++ forge/src/executor/opts.rs
@@ -0,0 +1,85 @@
+use ethers::types::{Address, U256};
+use revm::{BlockEnv, CfgEnv, SpecId, TxEnv};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize, Default)]
+pub struct EvmOpts {
+    #[serde(flatten)]
+    pub env: Env,
+
+    /// fetch state over a remote instead of starting from empty state
+    #[serde(rename = "eth_rpc_url")]
+    pub fork_url: Option<String>,
+
+    /// pins the block number for the state fork
+    pub fork_block_number: Option<u64>,
+
+    /// the initial balance of each deployed test contract
+    pub initial_balance: U256,
+
+    /// the address which will be executing all tests
+    pub sender: Address,
+
+    /// enables the FFI cheatcode
+    pub ffi: bool,
+
+    /// Verbosity mode of EVM output as number of occurences
+    pub verbosity: u8,
+
+    /// enable debugger
+    pub debug: bool,
+}
+
+#[derive(Debug, Clone, Default, Serialize, Deserialize)]
+pub struct Env {
+    /// the block gas limit
+    pub gas_limit: u64,
+
+    /// the chainid opcode value
+    pub chain_id: Option<u64>,
+
+    /// the tx.gasprice value during EVM execution
+    pub gas_price: u64,
+
+    /// the base fee in a block
+    pub block_base_fee_per_gas: u64,
+
+    /// the tx.origin value during EVM execution
+    pub tx_origin: Address,
+
+    /// the block.coinbase value during EVM execution
+    pub block_coinbase: Address,
+
+    /// the block.timestamp value during EVM execution
+    pub block_timestamp: u64,
+
+    /// the block.number value during EVM execution"
+    pub block_number: u64,
+
+    /// the block.difficulty value during EVM execution
+    pub block_difficulty: u64,
+
+    /// the block.gaslimit value during EVM execution
+    pub block_gas_limit: Option<u64>,
+}
+
+impl Env {
+    pub fn evm_env(&self) -> revm::Env {
+        revm::Env {
+            block: BlockEnv {
+                number: self.block_number.into(),
+                coinbase: self.block_coinbase,
+                timestamp: self.block_timestamp.into(),
+                difficulty: self.block_difficulty.into(),
+                basefee: self.block_base_fee_per_gas.into(),
+                gas_limit: self.block_gas_limit.unwrap_or(self.gas_limit).into(),
+            },
+            cfg: CfgEnv {
+                chain_id: self.chain_id.unwrap_or(99).into(),
+                spec_id: SpecId::LONDON,
+                perf_all_precompiles_have_balance: false,
+            },
+            tx: TxEnv { gas_price: self.gas_price.into(), ..Default::default() },
+        }
+    }
+}
