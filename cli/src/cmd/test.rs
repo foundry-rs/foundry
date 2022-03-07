ee5eb4d47b8455b6784502f3f504333fd13aa4ec
Oliver Nordbjerg <hi@notbjerg.me>
2022-03-07 18:29:02 +0100

    fix: add missing stuff to test command
    Some stuff was missing from the rebase

--- cli/src/cmd/test.rs
+++ cli/src/cmd/test.rs
@@ -1,13 +1,16 @@
 //! Test command
-
 use crate::{
     cmd::{build::BuildArgs, Cmd},
     opts::evm::EvmArgs,
 };
 use ansi_term::Colour;
 use clap::{AppSettings, Parser};
-use ethers::solc::{ArtifactOutput, Project};
-use forge::{MultiContractRunnerBuilder, TestFilter, TestResult};
+use ethers::{
+    abi::RawLog,
+    contract::EthLogDecode,
+    solc::{ArtifactOutput, Project},
+};
+use forge::{executor::opts::EvmOpts, MultiContractRunnerBuilder, TestFilter, TestResult};
 use foundry_config::{figment::Figment, Config};
 use regex::Regex;
 use std::{collections::BTreeMap, str::FromStr, sync::mpsc::channel, thread};
@@ -182,13 +185,11 @@ impl Cmd for TestArgs {
         let project = config.project()?;
 
         // prepare the test builder
-        let mut evm_cfg = crate::utils::sputnik_cfg(&config.evm_version);
-        evm_cfg.create_contract_limit = None;
-
+        let evm_spec = crate::utils::evm_spec(&config.evm_version);
         let builder = MultiContractRunnerBuilder::default()
             .fuzzer(fuzzer)
             .initial_balance(evm_opts.initial_balance)
-            .evm_cfg(evm_cfg)
+            .evm_spec(evm_spec)
             .sender(evm_opts.sender);
 
         test(
@@ -335,7 +336,7 @@ fn test<A: ArtifactOutput + 'static>(
         // TODO: Re-enable when ported
         //let mut gas_report = GasReport::new(gas_reports.1);
         let (tx, rx) = channel::<(String, BTreeMap<String, TestResult>)>();
-        let known_contracts = runner.known_contracts.clone();
+        //let known_contracts = runner.known_contracts.clone();
         // TODO: Re-enable when ported
         //let execution_info = runner.execution_info.clone();
 
@@ -351,11 +352,17 @@ fn test<A: ArtifactOutput + 'static>(
                     // adds a linebreak only if there were any traces or logs, so that the
                     // output does not look like 1 big block.
                     let mut add_newline = false;
-                    if verbosity > 1 && !result.logs.is_empty() {
-                        add_newline = true;
-                        println!("Logs:");
-                        for log in &result.logs {
-                            println!("  {}", log);
+                    if verbosity > 1 {
+                        // We only decode logs from Hardhat and DS-style console events
+                        let console_logs: Vec<String> =
+                            result.logs.iter().filter_map(decode_console_log).collect();
+
+                        if !console_logs.is_empty() {
+                            add_newline = true;
+                            println!("Logs:");
+                            for log in console_logs {
+                                println!("  {}", log);
+                            }
                         }
                     }
                     // TODO: Re-enable this when traces are ported
@@ -442,4 +449,42 @@ fn test<A: ArtifactOutput + 'static>(
         }*/
         Ok(TestOutcome::new(results, allow_failure))
     }
+}
+
+fn decode_console_log(log: &RawLog) -> Option<String> {
+    use forge::abi::ConsoleEvents::{self, *};
+
+    let decoded = match ConsoleEvents::decode_log(log).ok()? {
+        LogsFilter(inner) => format!("{}", inner.0),
+        LogBytesFilter(inner) => format!("{}", inner.0),
+        LogNamedAddressFilter(inner) => format!("{}: {:?}", inner.key, inner.val),
+        LogNamedBytes32Filter(inner) => {
+            format!("{}: 0x{}", inner.key, hex::encode(inner.val))
+        }
+        LogNamedDecimalIntFilter(inner) => {
+            let (sign, val) = inner.val.into_sign_and_abs();
+            format!(
+                "{}: {}{}",
+                inner.key,
+                sign,
+                ethers::utils::format_units(val, inner.decimals.as_u32()).unwrap()
+            )
+        }
+        LogNamedDecimalUintFilter(inner) => {
+            format!(
+                "{}: {}",
+                inner.key,
+                ethers::utils::format_units(inner.val, inner.decimals.as_u32()).unwrap()
+            )
+        }
+        LogNamedIntFilter(inner) => format!("{}: {:?}", inner.key, inner.val),
+        LogNamedUintFilter(inner) => format!("{}: {:?}", inner.key, inner.val),
+        LogNamedBytesFilter(inner) => {
+            format!("{}: 0x{}", inner.key, hex::encode(inner.val))
+        }
+        LogNamedStringFilter(inner) => format!("{}: {}", inner.key, inner.val),
+
+        e => e.to_string(),
+    };
+    Some(decoded)
 }
