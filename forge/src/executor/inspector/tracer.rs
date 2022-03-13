2ab9081825d81e0dc72fbc6dee4f2415b0c9663c
Bjerg <onbjerg@users.noreply.github.com>
2022-03-13 20:55:10 +0100

    REVM tracing and gas reports (#867)
    * feat: very simple traces
    
    * feat: creation traces
    
    * feat: setup and revert traces
    
    * fix: fix lib addresses
    
    * refactor: simplify tracer inspector
    
    * fix: fill traces in correct order
    
    * build: bump revm
    
    * fix: get code for newly created contracts
    
    * refactor: unify log extraction logic
    
    * feat: trace logs
    
    * refactor: unify labels and names
    
    * refactor: return string from trace
    
    Instead of passing in an empty string we then pass
    around inside the trace display logic, we just return
    strings where appropriate.
    
    * refactor: remove identified contracts
    
    * refactor: remove unused vars
    
    * refactor: simplify `construct_func_call`
    
    * refactor: name special characters in traces
    
    * refactor: rework all display logic
    
    * feat: first pass identify/decode for traces
    
    * refactor: move tracing to own module
    
    * refactor: simplify `test`
    
    * feat: traces for fuzz tests
    
    * fix: make fuzz revert reasons less verbose
    
    * feat: port gas reports
    
    * refactor: small readability nits
    
    * feat: run fuzz *and* unit tests in parallel
    
    Previously we would run each test contract in parallel,
    but within each `ContractRunner` we would run unit tests
    first (in parallel) and then fuzz tests (in parallel).
    
    * refactor: move colouring logic to its own function
    
    * fix: test contract identification
    
    We now include three kinds of traces that are used for
    identification of contracts:
    
    - Deployment traces: these are the initial deployments
      of the test contract and libraries
    - Setup traces: these are traces of calls to the `setUp`
      function
    - Execution traces: these are the traces of calls to
      the test contract itself
    
    * fix: mark setup trace as a setup trace
    
    * fix: get correct nonce in tracer
    
    * fix: log extraction outside of current memory
    
    * chore: clean up complex types
    
    * chore: remove outdated comment
    
    * fix: make tests compile
    
    * fix: add missing test filter function
    
    * feat: display full address in traces
    
    * fix: color "new" keyword in traces
    
    * fix: filter out `console.log` calls from traces
    
    * chore: remove unnecessary comment
    
    * feat: add gas cost to creation traces
    
    * fix: properly decode outputs
    
    * refactor: destructure `TestSetup` in test funcs
    
    * fix: ignore address for func output decoding
    
    * fix: fix expect emit
    
    Co-authored-by: Georgios Konstantopoulos <me@gakonst.com>
    Co-authored-by: brockelmore <brockelmore@users.noreply.github.com>

---
+++ forge/src/executor/inspector/tracer.rs
@@ -0,0 +1,166 @@
+use super::logs::extract_log;
+use crate::{
+    executor::HARDHAT_CONSOLE_ADDRESS,
+    trace::{
+        CallTrace, CallTraceArena, LogCallOrder, RawOrDecodedCall, RawOrDecodedLog,
+        RawOrDecodedReturnData,
+    },
+};
+use bytes::Bytes;
+use ethers::{
+    types::{Address, U256},
+    utils::{get_contract_address, get_create2_address},
+};
+use revm::{
+    return_ok, CallInputs, CreateInputs, CreateScheme, Database, EVMData, Gas, Inspector,
+    Interpreter, Return,
+};
+
+/// An inspector that collects call traces.
+#[derive(Default, Debug)]
+pub struct Tracer {
+    pub trace_stack: Vec<usize>,
+    pub traces: CallTraceArena,
+}
+
+impl Tracer {
+    pub fn new() -> Self {
+        Default::default()
+    }
+
+    pub fn start_trace(
+        &mut self,
+        depth: usize,
+        address: Address,
+        data: Vec<u8>,
+        value: U256,
+        created: bool,
+    ) {
+        self.trace_stack.push(self.traces.push_trace(
+            0,
+            CallTrace {
+                depth,
+                address,
+                created,
+                data: RawOrDecodedCall::Raw(data),
+                value,
+                ..Default::default()
+            },
+        ));
+    }
+
+    pub fn fill_trace(&mut self, success: bool, cost: u64, output: Vec<u8>) {
+        let trace = &mut self.traces.arena
+            [self.trace_stack.pop().expect("more traces were filled than started")]
+        .trace;
+        trace.success = success;
+        trace.gas_cost = cost;
+        trace.output = RawOrDecodedReturnData::Raw(output);
+    }
+}
+
+impl<DB> Inspector<DB> for Tracer
+where
+    DB: Database,
+{
+    fn call(
+        &mut self,
+        data: &mut EVMData<'_, DB>,
+        call: &CallInputs,
+        _: bool,
+    ) -> (Return, Gas, Bytes) {
+        if call.contract != *HARDHAT_CONSOLE_ADDRESS {
+            self.start_trace(
+                data.subroutine.depth() as usize,
+                call.contract,
+                call.input.to_vec(),
+                call.transfer.value,
+                false,
+            );
+        }
+
+        (Return::Continue, Gas::new(call.gas_limit), Bytes::new())
+    }
+
+    fn step(
+        &mut self,
+        interpreter: &mut Interpreter,
+        _: &mut EVMData<'_, DB>,
+        _is_static: bool,
+    ) -> Return {
+        if let Some(log) = extract_log(interpreter) {
+            let node = &mut self.traces.arena[*self.trace_stack.last().expect("no ongoing trace")];
+            node.ordering.push(LogCallOrder::Log(node.logs.len()));
+            node.logs.push(RawOrDecodedLog::Raw(log));
+        }
+
+        Return::Continue
+    }
+
+    fn call_end(
+        &mut self,
+        _: &mut EVMData<'_, DB>,
+        call: &CallInputs,
+        gas: Gas,
+        status: Return,
+        retdata: Bytes,
+        _: bool,
+    ) -> (Return, Gas, Bytes) {
+        if call.contract != *HARDHAT_CONSOLE_ADDRESS {
+            self.fill_trace(matches!(status, return_ok!()), gas.spend(), retdata.to_vec());
+        }
+
+        (status, gas, retdata)
+    }
+
+    fn create(
+        &mut self,
+        data: &mut EVMData<'_, DB>,
+        call: &CreateInputs,
+    ) -> (Return, Option<Address>, Gas, Bytes) {
+        // TODO: Does this increase gas cost?
+        data.subroutine.load_account(call.caller, data.db);
+        let nonce = data.subroutine.account(call.caller).info.nonce;
+
+        self.start_trace(
+            data.subroutine.depth() as usize,
+            match call.scheme {
+                CreateScheme::Create => get_contract_address(call.caller, nonce),
+                CreateScheme::Create2 { salt } => {
+                    let mut buffer: [u8; 4 * 8] = [0; 4 * 8];
+                    salt.to_big_endian(&mut buffer);
+                    get_create2_address(call.caller, buffer, call.init_code.clone())
+                }
+            },
+            call.init_code.to_vec(),
+            call.value,
+            true,
+        );
+
+        (Return::Continue, None, Gas::new(call.gas_limit), Bytes::new())
+    }
+
+    fn create_end(
+        &mut self,
+        data: &mut EVMData<'_, DB>,
+        _: &CreateInputs,
+        status: Return,
+        address: Option<Address>,
+        gas: Gas,
+        retdata: Bytes,
+    ) -> (Return, Option<Address>, Gas, Bytes) {
+        let code = match address {
+            Some(address) => data
+                .subroutine
+                .account(address)
+                .info
+                .code
+                .as_ref()
+                .map_or(vec![], |code| code.to_vec()),
+            None => vec![],
+        };
+        self.fill_trace(matches!(status, return_ok!()), gas.spend(), code);
+
+        (status, address, gas, retdata)
+    }
+}
