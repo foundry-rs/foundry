ae46da959d38fdca2bc383b1d8be7504fe44981b
Matthias Seitz <matthias.seitz@outlook.de>
2022-03-30 14:14:23 +0200

    debug platform

--- cli/build.rs
+++ cli/build.rs
@@ -1,9 +1,3 @@
-use vergen::{Config, ShaKind};
-
 fn main() {
-    let mut config = Config::default();
-    // Change the SHA output to the short variant
-    *config.git_mut().sha_kind_mut() = ShaKind::Short;
-    vergen::vergen(config)
-        .unwrap_or_else(|e| panic!("vergen crate failed to generate version information! {}", e));
+    panic!("OS: {} ARCH: {}", std::env::consts::OS, std::env::consts::ARCH);
 }
