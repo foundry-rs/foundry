2ee061717b1cf642c15ddffb59095296a963890f
Matthias Seitz <matthias.seitz@outlook.de>
2022-04-05 11:03:46 +0200

    chore: simplify Db trait

--- node/src/eth/backend/db.rs
+++ node/src/eth/backend/db.rs
@@ -10,27 +10,5 @@ use foundry_evm::{
 /// This bundles all required revm traits
 pub trait Db: DatabaseRef + Database + DatabaseCommit + Send + Sync + 'static {}
 
-// no auto_impl for &mut DatabaseRef but need to implement to satisfy `Db` trait
-impl<'a> DatabaseRef for &'a mut (dyn Db + 'a) {
-    fn basic(&self, address: H160) -> AccountInfo {
-        <dyn Db as DatabaseRef>::basic(self, address)
-    }
-
-    fn code_by_hash(&self, code_hash: H256) -> Bytes {
-        <dyn Db as DatabaseRef>::code_by_hash(self, code_hash)
-    }
-
-    fn storage(&self, address: H160, index: U256) -> U256 {
-        <dyn Db as DatabaseRef>::storage(self, address, index)
-    }
-
-    fn block_hash(&self, number: U256) -> H256 {
-        <dyn Db as DatabaseRef>::block_hash(self, number)
-    }
-}
-
-// Blanket impl for mutable dyn references
-impl<'a> Db for &'a mut (dyn Db + 'a) {}
-
 /// Implement the helper
 impl<ExtDB: DatabaseRef + Send + Sync + 'static> Db for CacheDB<ExtDB> {}
