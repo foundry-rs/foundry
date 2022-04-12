171cc3240d9009862d73a9c17dd399f6d9a4c6b3
Matthias Seitz <matthias.seitz@outlook.de>
2022-04-12 11:16:48 +0200

    misc: cleanup

--- anvil/anvil-core/src/eth/block.rs
+++ anvil/anvil-core/src/eth/block.rs
@@ -10,11 +10,10 @@ use ethers_core::{
         rlp::{Decodable, DecoderError, Encodable, Rlp, RlpStream},
     },
 };
-use rlp_derive::{RlpDecodable, RlpEncodable};
 use serde::{Deserialize, Serialize};
 
 /// Container type that gathers all block data
-#[derive(Debug, Clone, RlpEncodable, RlpDecodable)]
+#[derive(Debug, Clone)]
 pub struct BlockInfo {
     pub block: Block,
     pub transactions: Vec<TransactionInfo>,
@@ -49,8 +48,23 @@ impl Block {
     }
 }
 
+impl Encodable for Block {
+    fn rlp_append(&self, s: &mut RlpStream) {
+        s.begin_list(3);
+        s.append(&self.header);
+        s.append_list(&self.transactions);
+        s.append_list(&self.ommers);
+    }
+}
+
+impl Decodable for Block {
+    fn decode(rlp: &Rlp) -> Result<Self, DecoderError> {
+        Ok(Self { header: rlp.val_at(0)?, transactions: rlp.list_at(1)?, ommers: rlp.list_at(2)? })
+    }
+}
+
 /// ethereum block header
-#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize, RlpEncodable, RlpDecodable)]
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Header {
     pub parent_hash: H256,
     pub ommers_hash: H256,
@@ -94,6 +108,50 @@ impl Header {
 
     pub fn hash(&self) -> H256 {
         H256::from_slice(keccak256(&rlp::encode(self)).as_slice())
+    }
+}
+
+impl rlp::Encodable for Header {
+    fn rlp_append(&self, s: &mut rlp::RlpStream) {
+        s.begin_list(15);
+        s.append(&self.parent_hash);
+        s.append(&self.ommers_hash);
+        s.append(&self.beneficiary);
+        s.append(&self.state_root);
+        s.append(&self.transactions_root);
+        s.append(&self.receipts_root);
+        s.append(&self.logs_bloom);
+        s.append(&self.difficulty);
+        s.append(&self.number);
+        s.append(&self.gas_limit);
+        s.append(&self.gas_used);
+        s.append(&self.timestamp);
+        s.append(&self.extra_data.as_ref());
+        s.append(&self.mix_hash);
+        s.append(&self.nonce);
+    }
+}
+
+impl rlp::Decodable for Header {
+    fn decode(rlp: &rlp::Rlp) -> Result<Self, rlp::DecoderError> {
+        let result = Header {
+            parent_hash: rlp.val_at(0)?,
+            ommers_hash: rlp.val_at(1)?,
+            beneficiary: rlp.val_at(2)?,
+            state_root: rlp.val_at(3)?,
+            transactions_root: rlp.val_at(4)?,
+            receipts_root: rlp.val_at(5)?,
+            logs_bloom: rlp.val_at(6)?,
+            difficulty: rlp.val_at(7)?,
+            number: rlp.val_at(8)?,
+            gas_limit: rlp.val_at(9)?,
+            gas_used: rlp.val_at(10)?,
+            timestamp: rlp.val_at(11)?,
+            extra_data: rlp.val_at::<Vec<u8>>(12)?.into(),
+            mix_hash: rlp.val_at(13)?,
+            nonce: rlp.val_at(14)?,
+        };
+        Ok(result)
     }
 }
 
