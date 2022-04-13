3ce106f19d092f81959491c1264ffd0295ee786f
DC <dcposch@dcpos.ch>
2022-04-12 23:14:44 -0700

    test: correct finds_block output

--- cli/tests/it/cast.rs
+++ cli/tests/it/cast.rs
@@ -11,7 +11,7 @@ casttest!(finds_block, |_: TestProject, mut cmd: TestCommand| {
     // Skip fork tests if the RPC url is not set.
     if std::env::var("ETH_RPC_URL").is_err() {
         eprintln!("Skipping test finds_block. ETH_RPC_URL is not set.");
-        return
+        return;
     };
 
     // Construct args
@@ -24,5 +24,8 @@ casttest!(finds_block, |_: TestProject, mut cmd: TestCommand| {
     println!("{}", output);
 
     // Expect successful block query
-    assert!(output.contains("6574364"), "{}", output);
+    // Query: 1647843609, Mar 21 2022 06:20:09 UTC
+    // Output block: https://etherscan.io/block/14428082
+    // Output block time: Mar 21 2022 06:20:09 UTC
+    assert!(output.contains("14428082"), "{}", output);
 });
