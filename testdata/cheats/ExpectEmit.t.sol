eaa5544c60ba3dedfb183a965a555550b92c3841
Matthew Smith <m@lattejed.com>
2022-04-22 20:18:22 +0700

    Add test for second form of expectEmit(...)

--- testdata/cheats/ExpectEmit.t.sol
+++ testdata/cheats/ExpectEmit.t.sol
@@ -210,6 +210,13 @@ contract ExpectEmitTest is DSTest {
         );
     }
 
+    function testExpectEmitAddress() public {
+        cheats.expectEmit(true, true, true, true, address(emitter));
+        emit Something(1, 2, 3, 4);
+
+        emitter.emitEvent(1, 2, 3, 4);
+    }
+
     /// Ref: issue #760
     function testFailLowLevelWithoutEmit() public {
         LowLevelCaller caller = new LowLevelCaller();
