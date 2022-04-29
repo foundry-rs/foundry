ceeff188bcb62fc68e71b54a0f84bf60e8964eec
Noah Citron <noah@jeff.org>
2022-04-28 23:51:19 -0400

    add artifact support to cast inspect

--- cast/src/lib.rs
+++ cast/src/lib.rs
@@ -667,9 +667,18 @@ impl SimpleCast {
         let (contract_abis, contract_names): (Vec<Abi>, Vec<String>) = match address_or_path {
             InterfacePath::Local(path) => {
                 let file = std::fs::read_to_string(&path).wrap_err("unable to read abi file")?;
+                
+                let json: serde_json::Value = serde_json::from_str(&file)?;
+                let json = if !json["abi"].is_null() {
+                    json["abi"].clone()
+                } else {
+                    json
+                };
+
+                let abi: Abi = serde_json::from_value(json).wrap_err("unable to parse json ABI from file")?;
+
                 (
-                    vec![serde_json::from_str(&file)
-                        .wrap_err("unable to parse json ABI from file")?],
+                    vec![abi],
                     vec!["Interface".to_owned()],
                 )
             }
