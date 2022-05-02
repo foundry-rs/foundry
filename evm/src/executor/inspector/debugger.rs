3f77fa74d82ec14336ace6673691067dee8b6f14
Oliver Nordbjerg <hi@notbjerg.me>
2022-05-02 15:37:26 +0200

    fix: debugger under/overflow in gas calc

--- evm/src/executor/inspector/debugger.rs
+++ evm/src/executor/inspector/debugger.rs
@@ -158,7 +158,10 @@ where
 
         // Calculate the current amount of gas used
         let gas = interpreter.gas();
-        let total_gas_spent = gas.spend() - self.previous_gas_block + self.current_gas_block;
+        let total_gas_spent = gas
+            .spend()
+            .saturating_sub(self.previous_gas_block)
+            .saturating_add(self.current_gas_block);
         if opcode_info.is_gas_block_end {
             self.previous_gas_block = interpreter.contract.gas_block(pc);
             self.current_gas_block = 0;
