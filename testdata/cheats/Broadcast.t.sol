48312f11de6b5b3ef3c3c8e74f54ecdae8aef5a4
joshieDo <ranriver@protonmail.com>
2022-05-05 16:37:07 +0200

    remove ganache references

--- testdata/cheats/Broadcast.t.sol
+++ testdata/cheats/Broadcast.t.sol
@@ -33,9 +33,9 @@ library F {
 contract BroadcastTest is DSTest {
     Cheats constant cheats = Cheats(HEVM_ADDRESS);
 
-    // ganache-cli -d 1st
+    // 1st anvil account
     address public ACCOUNT_A = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
-    // ganache-cli -d 2nd
+    // 2nd anvil account
     address public ACCOUNT_B = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8;
 
     function deploy() public {
