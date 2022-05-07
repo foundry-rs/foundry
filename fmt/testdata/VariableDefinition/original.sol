8e52e97fcaaaf13ed6e56e32fa350842e3c7c098
Alexey Shekhirin <a.shekhirin@gmail.com>
2022-05-07 11:44:53 +0400

    improve tests

--- fmt/testdata/VariableDefinition/original.sol
+++ fmt/testdata/VariableDefinition/original.sol
@@ -1,5 +1,9 @@
 contract Contract {
-    bytes32 private constant BYTES =
+    bytes32 constant private BYTES =
+        0x035aff83d86937d35b32e04f0ddc6ff469290eef2f1b692d8a815c89404d4749;
+    bytes32 private constant override immutable BYTES =
+        0x035aff83d86937d35b32e04f0ddc6ff469290eef2f1b692d8a815c89404d4749;
+    bytes32 private constant override immutable BYTES_VERY_VERY_VERY_LONG =
         0x035aff83d86937d35b32e04f0ddc6ff469290eef2f1b692d8a815c89404d4749;
     bytes32 private constant BYTES_VERY_VERY_LONG =
         0x035aff83d86937d35b32e04f0ddc6ff469290eef2f1b692d8a815c89404d4749;
