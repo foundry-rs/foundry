752dd63b80b4e78f38c851a233f6073a9d8d4819
Alexey Shekhirin <a.shekhirin@gmail.com>
2022-05-10 00:36:25 +0300

    feat(fmt): variable definition (#1545)
    * feat(fmt): variable definition
    
    * add testdata
    
    * improve fit check
    
    * fix len_indented_with_current
    
    * improve tests
    
    * more tests
    
    * update README.md
    
    * improve multiline check in event

---
+++ fmt/testdata/VariableDefinition/fmt.sol
@@ -0,0 +1,34 @@
+// config: line-length=40
+contract Contract {
+    bytes32 private constant BYTES;
+    bytes32
+        private
+        constant
+        immutable
+        override BYTES;
+    bytes32
+        private
+        constant
+        immutable
+        override
+        BYTES_VERY_VERY_VERY_LONG;
+
+    bytes32 private constant BYTES =
+        0x035aff83d86937d35b32e04f0ddc6ff469290eef2f1b692d8a815c89404d4749;
+    bytes32
+        private
+        constant
+        immutable
+        override BYTES =
+        0x035aff83d86937d35b32e04f0ddc6ff469290eef2f1b692d8a815c89404d4749;
+    bytes32
+        private
+        constant
+        immutable
+        override
+        BYTES_VERY_VERY_VERY_LONG =
+        0x035aff83d86937d35b32e04f0ddc6ff469290eef2f1b692d8a815c89404d4749;
+    bytes32 private constant
+        BYTES_VERY_VERY_LONG =
+        0x035aff83d86937d35b32e04f0ddc6ff469290eef2f1b692d8a815c89404d4749;
+}
