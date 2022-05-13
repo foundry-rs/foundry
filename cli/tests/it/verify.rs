99e8647220c4907edaf49916da4c148a71e7c8a2
Roman Krasiuk <rokrassyuk@gmail.com>
2022-05-13 10:39:04 +0300

    it tests

--- cli/tests/it/verify.rs
+++ cli/tests/it/verify.rs
@@ -6,11 +6,10 @@ use foundry_cli_test_utils::{
     forgetest,
     util::{TestCommand, TestProject},
 };
-use foundry_utils::Retry;
 
 /// Adds a `Unique` contract to the source directory of the project that can be imported as
 /// `import {Unique} from "./unique.sol";`
-fn add_unique(prj: &TestProject) {
+pub fn add_unique(prj: &TestProject) {
     let timestamp = utils::millis_since_epoch();
     prj.inner()
         .add_source(
@@ -52,69 +51,21 @@ fn verify_on_chain(info: Option<EnvExternalities>, prj: TestProject, mut cmd: Te
         let contract_path = "src/Verify.sol:Verify";
 
         cmd.arg("create");
-        cmd.args(info.create_args()).arg(contract_path);
+        cmd.args(info.create_args()).arg("--verify").arg(contract_path);
 
-        let out = cmd.stdout_lossy();
-        let address = utils::parse_deployed_address(out.as_str())
-            .unwrap_or_else(|| panic!("Failed to parse deployer {out}"));
-
-        cmd.forge_fuse().arg("verify-contract").root_arg().args([
-            "--chain-id".to_string(),
-            info.chain.to_string(),
-            "--compiler-version".to_string(),
-            "0.8.10".to_string(),
-            "--optimizer-runs".to_string(),
-            "200".to_string(),
-            "--watch".to_string(),
-            address,
-            contract_path.to_string(),
-            info.etherscan.to_string(),
-        ]);
-
-        // `verify-contract`
-        let guid = {
-            // give etherscan some time to detect the transaction
-            let retry = Retry::new(5, Some(60));
-            retry
-                .run(|| -> eyre::Result<String> {
-                    let output = cmd.unchecked_output();
-                    let out = String::from_utf8_lossy(&output.stdout);
-                    utils::parse_verification_guid(&out).ok_or_else(|| {
-                        eyre::eyre!(
-                            "Failed to get guid, stdout: {}, stderr: {}",
-                            out,
-                            String::from_utf8_lossy(&output.stderr)
-                        )
-                    })
-                })
-                .expect("Failed to get verify guid")
-        };
-
-        // `verify-check`
-        {
-            cmd.forge_fuse()
-                .arg("verify-check")
-                .arg("--chain-id")
-                .arg(info.chain.to_string())
-                .arg(guid)
-                .arg(info.etherscan);
-
-            // give etherscan some time to verify the contract
-            let retry = Retry::new(6, Some(30));
-            retry
-                .run(|| -> eyre::Result<()> {
-                    let output = cmd.unchecked_output();
-                    let out = String::from_utf8_lossy(&output.stdout);
-                    if out.contains("Contract successfully verified") {
-                        return Ok(())
-                    }
-                    eyre::bail!(
-                        "Failed to get verification, stdout: {}, stderr: {}",
-                        out,
-                        String::from_utf8_lossy(&output.stderr)
-                    )
-                })
-                .expect("Failed to verify check")
+        let output = cmd.unchecked_output();
+        let out = String::from_utf8_lossy(&output.stdout);
+        utils::parse_verification_guid(&out).expect(&format!(
+            "Failed to get guid, stdout: {}, stderr: {}",
+            out,
+            String::from_utf8_lossy(&output.stderr)
+        ));
+        if !out.contains("Contract successfully verified") {
+            panic!(
+                "Failed to get verification, stdout: {}, stderr: {}",
+                out,
+                String::from_utf8_lossy(&output.stderr)
+            );
         }
     }
 }
