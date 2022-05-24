243df202d31e4714c0bb16be457f7d5d871c9178
Shawn Harmsen <shawn@zksnarks.xyz>
2022-05-24 18:31:33 +0900

    remove help and revert debug value_name to TEST FUNCTION

--- cli/src/cmd/forge/test.rs
+++ cli/src/cmd/forge/test.rs
@@ -43,13 +43,7 @@ pub struct Filter {
     pub pattern: Option<regex::Regex>,
 
     /// Only run test functions matching the specified regex pattern.
-    #[clap(
-        long = "match-test",
-        alias = "mt",
-        conflicts_with = "pattern",
-        value_name = "REGEX",
-        help = "Only run test functions matching the specified regex pattern"
-    )]
+    #[clap(long = "match-test", alias = "mt", conflicts_with = "pattern", value_name = "REGEX")]
     pub test_pattern: Option<regex::Regex>,
 
     /// Only run test functions that do not match the specified regex pattern.
@@ -57,8 +51,7 @@ pub struct Filter {
         long = "no-match-test",
         alias = "nmt",
         conflicts_with = "pattern",
-        value_name = "REGEX",
-        help = "Only run test functions that do not match the specified regex pattern"
+        value_name = "REGEX"
     )]
     pub test_pattern_inverse: Option<regex::Regex>,
 
@@ -67,8 +60,7 @@ pub struct Filter {
         long = "match-contract",
         alias = "mc",
         conflicts_with = "pattern",
-        value_name = "REGEX",
-        help = "Only run tests in contracts matching the specified regex pattern"
+        value_name = "REGEX"
     )]
     pub contract_pattern: Option<regex::Regex>,
 
@@ -77,19 +69,12 @@ pub struct Filter {
         long = "no-match-contract",
         alias = "nmc",
         conflicts_with = "pattern",
-        value_name = "REGEX",
-        help = "Only run tests in contracts that do not match the specified regex pattern"
+        value_name = "REGEX"
     )]
     pub contract_pattern_inverse: Option<regex::Regex>,
 
     /// Only run tests in source files matching the specified glob pattern.
-    #[clap(
-        long = "match-path",
-        alias = "mp",
-        conflicts_with = "pattern",
-        value_name = "GLOB",
-        help = "Only run tests in source files matching the specified glob pattern"
-    )]
+    #[clap(long = "match-path", alias = "mp", conflicts_with = "pattern", value_name = "GLOB")]
     pub path_pattern: Option<globset::Glob>,
 
     /// Only run tests in source files that do not match the specified glob pattern.
@@ -98,8 +83,7 @@ pub struct Filter {
         long = "no-match-path",
         alias = "nmp",
         conflicts_with = "pattern",
-        value_name = "GLOB",
-        help = "Only run tests in source files that do not match the specified glob pattern"
+        value_name = "GLOB"
     )]
     pub path_pattern_inverse: Option<globset::Glob>,
 }
@@ -242,7 +226,7 @@ pub struct TestArgs {
     /// If the fuzz test does not fail, it will open the debugger on the last fuzz case.
     ///
     /// For more fine-grained control of which fuzz case is run, see forge run.
-    #[clap(long, value_name = "REGEX", help = "Run a test in the debugger")]
+    #[clap(long, value_name = "TEST FUNCTION")]
     debug: Option<Regex>,
 
     /// Print a gas report.
