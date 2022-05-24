a2bd10c13227a1f021ea0274098df7dc76004705
Mark Toda <toda.mark@gmail.com>
2022-05-24 13:21:46 -0700

    test(cast): add integration test for `cast upload-signature`

--- cli/tests/it/cast.rs
+++ cli/tests/it/cast.rs
@@ -23,3 +23,29 @@ casttest!(finds_block, |_: TestProject, mut cmd: TestCommand| {
     // Output block time: Mar 21 2022 06:20:09 UTC
     assert!(output.contains("14428082"), "{}", output);
 });
+
+// tests that the `cast upload-signatures` command works correctly
+casttest!(upload_signatures, |_: TestProject, mut cmd: TestCommand| {
+    // test no prefix is accepted as function
+    cmd.args(["upload-signature", "transfer(address,uint256)"]);
+    let output = cmd.stdout_lossy();
+    println!("{output}");
+
+    assert!(output.contains("Function transfer(address,uint256): 0xa9059cbb"), "{}", output);
+
+    // test event prefix
+    cmd.args(["upload-signature", "event Transfer(address,uint256)"]);
+    let output = cmd.stdout_lossy();
+    println!("{output}");
+
+    assert!(output.contains("Event Transfer(address,uint256): 0x69ca02dd4edd7bf0a4abb9ed3b7af3f14778db5d61921c7dc7cd545266326de2"), "{}", output);
+
+    // test multiple sigs
+    cmd.args(["upload-signature", "event Transfer(address,uint256)", "transfer(address,uint256)", "approve(address,uint256)"]);
+    let output = cmd.stdout_lossy();
+    println!("{output}");
+
+    assert!(output.contains("Event Transfer(address,uint256): 0x69ca02dd4edd7bf0a4abb9ed3b7af3f14778db5d61921c7dc7cd545266326de2"), "{}", output);
+    assert!(output.contains("Function transfer(address,uint256): 0xa9059cbb"), "{}", output);
+    assert!(output.contains("Function approve(address,uint256): 0x095ea7b3"), "{}", output);
+});
