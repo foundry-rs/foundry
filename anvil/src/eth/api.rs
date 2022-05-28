b1d230570453d3a311c9ee7324c8daf0da384b57
Matthias Seitz <matthias.seitz@outlook.de>
2022-05-28 18:02:45 +0200

    fix(anvil): use tx gas price for geth txpool rpc calls (#1756)

--- anvil/src/eth/api.rs
+++ anvil/src/eth/api.rs
@@ -1500,11 +1500,11 @@ impl EthApi {
     pub async fn txpool_inspect(&self) -> Result<TxpoolInspect> {
         node_info!("txpool_inspect");
         let mut inspect = TxpoolInspect::default();
-        let gas_price = self.backend.gas_price();
 
-        fn convert(tx: Arc<PoolTransaction>, gas_price: U256) -> TxpoolInspectSummary {
+        fn convert(tx: Arc<PoolTransaction>) -> TxpoolInspectSummary {
             let tx = &tx.pending_transaction.transaction;
             let to = tx.to().copied();
+            let gas_price = tx.gas_price();
             let value = tx.value();
             let gas = tx.gas_limit();
             TxpoolInspectSummary { to, value, gas, gas_price }
@@ -1519,12 +1519,12 @@ impl EthApi {
         for pending in self.pool.ready_transactions() {
             let entry = inspect.pending.entry(*pending.pending_transaction.sender()).or_default();
             let key = pending.pending_transaction.nonce().to_string();
-            entry.insert(key, convert(pending, gas_price));
+            entry.insert(key, convert(pending));
         }
         for queued in self.pool.pending_transactions() {
             let entry = inspect.pending.entry(*queued.pending_transaction.sender()).or_default();
             let key = queued.pending_transaction.nonce().to_string();
-            entry.insert(key, convert(queued, gas_price));
+            entry.insert(key, convert(queued));
         }
         Ok(inspect)
     }
@@ -1538,16 +1538,17 @@ impl EthApi {
     pub async fn txpool_content(&self) -> Result<TxpoolContent> {
         node_info!("txpool_content");
         let mut content = TxpoolContent::default();
-        let gas_price = self.backend.gas_price();
-        fn convert(tx: Arc<PoolTransaction>, gas_price: U256) -> EthersTransactionRequest {
+        fn convert(tx: Arc<PoolTransaction>) -> EthersTransactionRequest {
             let from = *tx.pending_transaction.sender();
             let tx = &tx.pending_transaction.transaction;
             let to = tx.to().copied();
             let gas = tx.gas_limit();
             let value = tx.value();
-            let data = tx.data().clone();
+            let data = tx.data();
+            let data = if data.as_ref().is_empty() { None } else { Some(data.clone()) };
             let nonce = *tx.nonce();
             let chain_id = tx.chain_id();
+            let gas_price = tx.gas_price();
 
             TransactionRequest {
                 from: Some(from),
@@ -1555,7 +1556,7 @@ impl EthApi {
                 gas: Some(gas),
                 gas_price: Some(gas_price),
                 value: Some(value),
-                data: Some(data),
+                data,
                 nonce: Some(nonce),
                 chain_id: chain_id.map(Into::into),
             }
@@ -1564,12 +1565,12 @@ impl EthApi {
         for pending in self.pool.ready_transactions() {
             let entry = content.pending.entry(*pending.pending_transaction.sender()).or_default();
             let key = pending.pending_transaction.nonce().to_string();
-            entry.insert(key, convert(pending, gas_price));
+            entry.insert(key, convert(pending));
         }
         for queued in self.pool.pending_transactions() {
             let entry = content.pending.entry(*queued.pending_transaction.sender()).or_default();
             let key = queued.pending_transaction.nonce().to_string();
-            entry.insert(key, convert(queued, gas_price));
+            entry.insert(key, convert(queued));
         }
 
         Ok(content)
