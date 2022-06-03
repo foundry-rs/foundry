a39fca7c9b9f829e54f2f8c574ce93e5e119619b
Julian Popescu <hi@julian.dev>
2022-06-04 00:27:37 +0200

    [wip] feat(fmt): expressions (#1809)
    * add an Operator trait for implementing Expression formatting
    
    * basic implementation of write operator expression
    
    * add grouping of expression parts by precedence

--- fmt/testdata/VariableDefinition/original.sol
+++ fmt/testdata/VariableDefinition/original.sol
@@ -13,5 +13,5 @@ contract Contract {
         0x035aff83d86937d35b32e04f0ddc6ff469290eef2f1b692d8a815c89404d4749;
 
     uint constant POWER_EXPRESSION = 10 ** 27;
-    uint constant ADD_EXPRESSION = 1 + 2;
+    uint constant ADDED_EXPRESSION = 1 + 2;
 }
