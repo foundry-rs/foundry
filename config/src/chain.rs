9db03d6dd659c3fcb2f5eb67f21f1a8900288b0b
Matthias Seitz <matthias.seitz@outlook.de>
2022-06-08 22:09:03 +0200

    chore(config): try converting to named chain first (#1887)

--- config/src/chain.rs
+++ config/src/chain.rs
@@ -46,7 +46,7 @@ impl From<ethers_core::types::Chain> for Chain {
 
 impl From<u64> for Chain {
     fn from(id: u64) -> Self {
-        Chain::Id(id)
+        ethers_core::types::Chain::try_from(id).map(Chain::Named).unwrap_or_else(|_| Chain::Id(id))
     }
 }
 
