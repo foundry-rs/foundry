c81484cb06aa369c1b7491cb11563666a67c975f
Matthias Seitz <matthias.seitz@outlook.de>
2022-06-13 12:31:45 +0200

    Update anvil/src/eth/backend/time.rs
    Co-authored-by: Bjerg <onbjerg@users.noreply.github.com>

--- anvil/src/eth/backend/time.rs
+++ anvil/src/eth/backend/time.rs
@@ -64,7 +64,7 @@ impl TimeManager {
             // the offset will be negative if the `next` timestamp is in the past
             let offset = (next as i128) - current;
             let mut current_offset = self.offset.write();
-            // increase the by one second, so that we don't yield the same timestamp twice if it's
+            // increase the offset by one second, so that we don't yield the same timestamp twice if it's
             // set manually
             *current_offset = offset.saturating_add(1);
             return next
