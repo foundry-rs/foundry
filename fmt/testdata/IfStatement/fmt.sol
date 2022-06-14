275e2b55008b1e2e2c4cbf6bcfe6c3f4ec420f3c
Roman Krasiuk <rokrassyuk@gmail.com>
2022-06-14 21:31:43 +0300

    fmt if statement

--- fmt/testdata/IfStatement/fmt.sol
+++ fmt/testdata/IfStatement/fmt.sol
@@ -1,7 +1,49 @@
-function test() returns (bool) {
+function execute() returns (bool) {
     if (true) {
         // always returns true
         return true;
     }
     return false;
+}
+
+contract IfStatement {
+    function test() external {
+        if (true) {
+            execute();
+        }
+
+        bool condition;
+        bool anotherLongCondition;
+        bool andAnotherVeryVeryLongCondition;
+        if (
+            condition && anotherLongCondition || andAnotherVeryVeryLongCondition
+        ) {
+            execute();
+        }
+
+        // comment
+        if (condition) {
+            execute();
+        } else if (anotherLongCondition) {
+            execute(); // differently
+        }
+
+        /* comment1 */
+        if ( /* comment2 */ /* comment3 */ condition) { // comment4
+            // comment5
+            execute();
+        } // comment6
+
+        if (condition) {
+            execute();
+        } // comment7
+        /* comment8 */
+        /* comment9 */
+        else if ( /* comment10 */ anotherLongCondition) { // comment11
+            /* comment12 */
+            execute();
+        } // comment13
+        /* comment14 */
+        else {} // comment15
+    }
 }
