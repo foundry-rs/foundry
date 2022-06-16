cf2354e936c1a09ea45ca4d6b029ecae0e109c38
Matthias Seitz <matthias.seitz@outlook.de>
2022-06-16 05:47:16 +0200

    test: check if next port is free (#1986)

--- cli/tests/it/main.rs
+++ cli/tests/it/main.rs
@@ -1,4 +1,7 @@
-use std::sync::atomic::{AtomicU16, Ordering};
+use std::{
+    net::TcpStream,
+    sync::atomic::{AtomicU16, Ordering},
+};
 #[cfg(not(feature = "external-integration-tests"))]
 mod cast;
 #[cfg(not(feature = "external-integration-tests"))]
@@ -27,8 +30,16 @@ mod integration;
 // keeps track of ports that can be used
 pub static NEXT_PORT: AtomicU16 = AtomicU16::new(8546);
 
+/// Returns the next free port to use
 pub fn next_port() -> u16 {
-    NEXT_PORT.fetch_add(1, Ordering::SeqCst)
+    loop {
+        let port = NEXT_PORT.fetch_add(1, Ordering::SeqCst);
+        // while simply incrementing ports is fine for a single test process, there might be
+        // multiple concurrent anvil related test process, so we check if the port is already in use
+        if TcpStream::connect(("0.0.0.0", port)).is_err() {
+            return port
+        }
+    }
 }
 
 fn main() {}
