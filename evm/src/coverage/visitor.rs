20bc69033f9b14746327dc17a105fe0418c9a3d3
Bjerg <onbjerg@users.noreply.github.com>
2022-06-23 21:30:12 +0200

    fix: start line number at 0 (#2101)

--- evm/src/coverage/visitor.rs
+++ evm/src/coverage/visitor.rs
@@ -366,7 +366,7 @@ impl Visitor {
         SourceLocation {
             start: loc.start,
             length: loc.length,
-            line: self.source[..loc.start].lines().count() + 1,
+            line: self.source[..loc.start].lines().count(),
         }
     }
 
