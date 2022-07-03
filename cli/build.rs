7fc57f7b27a12fdf25ba44223ea199fd4df4dffb
Kristian Gaylord <krlgaylord@gmail.com>
2022-07-03 01:52:42 -0400

    peg revm version (latest 1.6.0 broke build) and support apple silicon with linker flag

--- cli/build.rs
+++ cli/build.rs
@@ -6,4 +6,7 @@ fn main() {
     *config.git_mut().sha_kind_mut() = ShaKind::Short;
     vergen::vergen(config)
         .unwrap_or_else(|e| panic!("vergen crate failed to generate version information! {e}"));
+
+    // Supports new Apple Silicon packages installed by Homebrew
+    println!("cargo:rustc-link-search=/opt/homebrew/Cellar/libusb/1.0.26/lib");
 }
