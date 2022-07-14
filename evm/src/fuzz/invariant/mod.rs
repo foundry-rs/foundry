b79da1c64d599d43bcf679afd6c117369cb7b435
joshieDo <ranriver@protonmail.com>
2022-07-14 17:49:31 +0100

    docs

--- evm/src/fuzz/invariant/mod.rs
+++ evm/src/fuzz/invariant/mod.rs
@@ -243,22 +243,21 @@ impl RandomCallGenerator {
         if self.replay {
             self.last_sequence.write().pop().unwrap()
         } else {
-            let mut testrunner = self.runner.write();
-            let mut last_sequence = self.last_sequence.write();
+            // TODO: Do we want it to be 80% chance only too ?
             let new_caller = original_target;
 
-            // Set which contract we mostly want to generate calldata from.
+            // Set which contract we mostly (80% chance) want to generate calldata from.
             *self.target_reference.write() = original_caller;
 
             // `original_caller` has a 80% chance of being the `new_target`.
             let choice = self
                 .strategy
-                .new_tree(&mut testrunner)
+                .new_tree(&mut self.runner.write())
                 .unwrap()
                 .current()
                 .map(|(new_target, calldata)| (new_caller, (new_target, calldata)));
 
-            last_sequence.push(choice.clone());
+            self.last_sequence.write().push(choice.clone());
             choice
         }
     }
