ab8611929a405782e2db1e12872c17647baff8c9
Matthias Seitz <matthias.seitz@outlook.de>
2022-07-15 23:27:17 +0200

    misc: some cast simplifications (#2335)
    * misc: some cast simplifications
    
    * chore: rustfmt

--- cast/src/lib.rs
+++ cast/src/lib.rs
@@ -1,6 +1,6 @@
 //! Cast
 //!
-//! TODO
+//! Contains core function implementation for `cast`
 use crate::rlp_converter::Item;
 use chrono::NaiveDateTime;
 use ethers_core::{
@@ -19,7 +19,6 @@ pub use foundry_evm::*;
 use foundry_utils::encode_args;
 use print_utils::{get_pretty_block_attr, get_pretty_tx_attr, get_pretty_tx_receipt_attr};
 use rustc_hex::{FromHexIter, ToHex};
-use serde_json::Value;
 use std::{path::PathBuf, str::FromStr};
 pub use tx::TxBuilder;
 use tx::{TxBuilderOutput, TxBuilderPeekOutput};
@@ -141,9 +140,9 @@ where
     /// # Ok(())
     /// # }
     /// ```
-    pub async fn access_list<'a>(
+    pub async fn access_list(
         &self,
-        builder_output: TxBuilderPeekOutput<'a>,
+        builder_output: TxBuilderPeekOutput<'_>,
         block: Option<BlockId>,
         to_json: bool,
     ) -> Result<String> {
@@ -268,7 +267,7 @@ where
     /// # Ok(())
     /// # }
     /// ```
-    pub async fn estimate<'a>(&self, builder_output: TxBuilderPeekOutput<'a>) -> Result<U256> {
+    pub async fn estimate(&self, builder_output: TxBuilderPeekOutput<'_>) -> Result<U256> {
         let (tx, _) = builder_output;
 
         let res = self.provider.estimate_gas(tx).await?;
@@ -1143,7 +1142,7 @@ impl SimpleCast {
     /// }
     /// ```
     pub fn to_rlp(value: &str) -> Result<String> {
-        let val = serde_json::from_str(value).unwrap_or(Value::String(value.parse()?));
+        let val = serde_json::from_str(value).unwrap_or(serde_json::Value::String(value.parse()?));
         let item = Item::value_to_item(&val)?;
         Ok(format!("0x{}", hex::encode(rlp::encode(&item))))
     }
