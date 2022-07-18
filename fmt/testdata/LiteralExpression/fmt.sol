8cc1ae4cb53a8064e9ae2317fa23f4a691364fe2
Roman Krasiuk <rokrassyuk@gmail.com>
2022-07-18 23:10:19 +0300

    feat(fmt): checksummed addresses (#2371)
    * checksummed addresses
    
    * proper string address validation

--- fmt/testdata/LiteralExpression/fmt.sol
+++ fmt/testdata/LiteralExpression/fmt.sol
@@ -45,5 +45,7 @@ some words"; /* comment10 */
 
         // address literals
         0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+        // non checksummed address
+        0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     }
 }
