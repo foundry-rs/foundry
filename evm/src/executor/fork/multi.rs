5f94d1834cf3873ee45a36ef1c0c3522010896b6
Meet Mangukiya <meet@flamy.dev>
2022-07-19 12:28:06 +0000

    fix(evm): use consistent cache file path

--- evm/src/executor/fork/multi.rs
+++ evm/src/executor/fork/multi.rs
@@ -426,7 +426,7 @@ async fn create_fork(
 
     // determine the cache path if caching is enabled
     let cache_path = if fork.enable_caching {
-        Config::foundry_block_cache_dir(meta.cfg_env.chain_id.as_u64(), number)
+        Config::foundry_block_cache_file(meta.cfg_env.chain_id.as_u64(), number)
     } else {
         None
     };
