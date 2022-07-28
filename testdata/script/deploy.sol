2f4dc6cc514ff816589144622f1d6e183dddb13b
odysseas.eth <odyslam@gmail.com>
2022-07-28 20:12:57 +0200

    chore: Make consistent forge script artifacts (#2490)
    * chore: replace skip with Default
    
    * chore: replace tx with transaction
    
    * feat: add script and artifact to testdata
    
    * fix: rename 'type'; change defaults from null
    
    * fix: compiler warning
    
    * chore: forge fmt

---
+++ testdata/script/deploy.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity >=0.8.0;
+
+import {DSTest} from "../lib/ds-test/src/test.sol";
+import {Cheats} from "../cheats/Cheats.sol";
+
+contract Greeter {
+    string name;
+    uint256 age;
+
+    event Greet(string greet);
+
+    function greeting(string memory _name) public returns (string memory) {
+        name = _name;
+        string memory greet = string(abi.encodePacked("Hello ", _name));
+        emit Greet(greet);
+        return greet;
+    }
+
+    function setAge(uint256 _age) external {
+        age = _age;
+    }
+}
+
+contract Deploy is DSTest {
+    Cheats constant cheats = Cheats(HEVM_ADDRESS);
+
+    Greeter greeter;
+    string greeting;
+
+    function run() external {
+        cheats.startBroadcast();
+        greeter = new Greeter();
+        greeting = greeter.greeting("john");
+        greeter.setAge(123);
+        cheats.stopBroadcast();
+    }
+}
