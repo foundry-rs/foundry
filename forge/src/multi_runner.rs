faa10b8a993eae8fc14c60a7c84c874d694af409
Matthias Seitz <matthias.seitz@outlook.de>
2022-08-01 00:15:27 +0200

    set cases

--- forge/src/multi_runner.rs
+++ forge/src/multi_runner.rs
@@ -1254,7 +1254,11 @@ Reason: `setEnv` failed to set an environment variable `{}={}`",
     #[test]
     fn test_fuzz() {
         let mut runner = runner();
-        let cfg = proptest::test_runner::Config { failure_persistence: None, ..Default::default() };
+        let cfg = proptest::test_runner::Config {
+            failure_persistence: None,
+            cases: 256,
+            ..Default::default()
+        };
         runner.fuzzer = Some(proptest::test_runner::TestRunner::new(cfg));
 
         let suite_result = runner.test(&Filter::new(".*", ".*", ".*fuzz"), None, true).unwrap();
@@ -1265,7 +1269,7 @@ Reason: `setEnv` failed to set an environment variable `{}={}`",
             for (test_name, result) in test_results {
                 let logs = decode_console_logs(&result.logs);
 
-                match test_name.as_ref() {
+                match test_name.as_str() {
                     "testPositive(uint256)" |
                     "testSuccessfulFuzz(uint128,uint128)" |
                     "testToStringFuzz(bytes32)" => assert!(
