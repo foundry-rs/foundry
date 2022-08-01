d74b8d14d5b4ac58e58dc91521e0e2539e3f35fa
Roman Krasiuk <rokrassyuk@gmail.com>
2022-08-01 14:52:11 +0300

    ignore long string literals

--- fmt/testdata/VariableAssignment/fmt.sol
+++ fmt/testdata/VariableAssignment/fmt.sol
@@ -15,6 +15,21 @@ contract TestContract {
             uint256 listItem009,
             uint256 listItem010
         ) = (10, 20, 30, 40);
+        string memory source = "70708741044725766535585242414884609539555049888764130733849700923779599488691391677696419266840";
+        string memory search = "46095395550498887641307338497009";
+        string memory replacement = "320807383223517906783031356692334377159141";
+        string memory expectedResult = "707087410447257665355852424148832080738322351790678303135669233437715914123779599488691391677696419266840";
+
+        string memory source2 = "01234567890123456789012345678901_search_search_search_search_search_search_23456789012345678901234567890123456789_search_search_search_search_search_search";
+        string memory search2 = "search_search_search_search_search_search";
+        string memory replacement2 = "REPLACEMENT_REPLACEMENT_REPLACEMENT_REPLACEMENT_REPLACEMENT";
+        string memory expectedResult2 = "01234567890123456789012345678901_REPLACEMENT_REPLACEMENT_REPLACEMENT_REPLACEMENT_REPLACEMENT_23456789012345678901234567890123456789_REPLACEMENT_REPLACEMENT_REPLACEMENT_REPLACEMENT_REPLACEMENT";
+
+        (string memory result1, string memory result2) = (
+            keccak256(bytes(LibString.toHexString(type(uint256).max, 32))),
+            keccak256(bytes("0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"))
+        );
+
         return 1;
     }
 }
