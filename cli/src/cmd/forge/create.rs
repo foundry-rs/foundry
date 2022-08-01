d027e5f6c62429b1f3dc0ad584054bff19a881f7
Matthias Seitz <matthias.seitz@outlook.de>
2022-08-01 06:29:05 +0200

    slash artifact id

--- cli/src/cmd/forge/create.rs
+++ cli/src/cmd/forge/create.rs
@@ -99,6 +99,8 @@ impl CreateArgs {
         if let Some(ref mut path) = self.contract.path {
             // paths are absolute in the project's output
             *path = canonicalized(project.root().join(&path)).to_string_lossy().to_string();
+            dbg!(canonicalized(project.root().join(&path)));
+            dbg!(canonicalized(project.root().join(&path)).to_string_lossy().to_string());
         }
 
         let (abi, bin, _) = utils::remove_contract(&mut output, &self.contract)?;
