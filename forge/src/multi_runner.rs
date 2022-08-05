b3af602519d50dfbc112e4904569221dc393f9b2
Matthias Seitz <matthias.seitz@outlook.de>
2022-08-05 02:13:37 +0200

    fix(fork): update env correctly on roll fork (#2625)
    * fix(fork): update env correctly on roll fork
    
    * add docs
    
    * rm clone

--- forge/src/multi_runner.rs
+++ forge/src/multi_runner.rs
@@ -1206,6 +1206,28 @@ Reason: `setEnv` failed to set an environment variable `{}={}`",
         }
     }
 
+    // <https://github.com/foundry-rs/foundry/issues/2623>
+    #[test]
+    fn test_issue_2623() {
+        let mut runner = runner();
+        let suite_result =
+            runner.test(&Filter::new(".*", ".*", ".*repros/Issue2623"), None, TEST_OPTS).unwrap();
+        assert!(!suite_result.is_empty());
+
+        for (_, SuiteResult { test_results, .. }) in suite_result {
+            for (test_name, result) in test_results {
+                let logs = decode_console_logs(&result.logs);
+                assert!(
+                    result.success,
+                    "Test {} did not pass as expected.\nReason: {:?}\nLogs:\n{}",
+                    test_name,
+                    result.reason,
+                    logs.join("\n")
+                );
+            }
+        }
+    }
+
     /// Executes all non-reverting fork cheatcodes
     #[test]
     fn test_cheats_fork() {
