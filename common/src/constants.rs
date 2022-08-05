18919c3b4fef7457209f53fc9c91bf84324c066a
joshieDo <ranriver@protonmail.com>
2022-08-05 07:00:32 +0100

    change CONTRACT_MAX_SIZE to 24576

--- common/src/constants.rs
+++ common/src/constants.rs
@@ -6,5 +6,5 @@ pub const DEV_CHAIN_ID: u64 = 31337;
 /// The first four bytes of the call data for a function call specifies the function to be called.
 pub const SELECTOR_LEN: usize = 4;
 
-/// Maximum size in bytes that a contract can have.
-pub const CONTRACT_MAX_SIZE: usize = 0x6000;
+/// Maximum size in bytes (0x6000) that a contract can have.
+pub const CONTRACT_MAX_SIZE: usize = 24576;
