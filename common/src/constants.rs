504689b8c68842282d114be05599ae2f0a083db9
Matthias Seitz <matthias.seitz@outlook.de>
2022-08-08 19:21:59 +0200

    feat: add ProviderBuilder helper

--- common/src/constants.rs
+++ common/src/constants.rs
@@ -1,5 +1,7 @@
 //! Commonly used constants
 
+use std::time::Duration;
+
 /// The dev chain-id, inherited from hardhat
 pub const DEV_CHAIN_ID: u64 = 31337;
 
@@ -8,3 +10,6 @@ pub const SELECTOR_LEN: usize = 4;
 
 /// Maximum size in bytes (0x6000) that a contract can have.
 pub const CONTRACT_MAX_SIZE: usize = 24576;
+
+/// Default request timeout for http requests
+pub const REQUEST_TIMEOUT: Duration = Duration::from_millis(30_000);
