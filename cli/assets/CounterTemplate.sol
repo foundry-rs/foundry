0df0bc7182d772bdd7190f086634f94a87c2c97a
Tirth Patel <tirthnp@live.com>
2022-08-08 15:52:09 -0230

    Add to forge init Counter contract and add sample test

--- cli/assets/CounterTemplate.sol
+++ cli/assets/CounterTemplate.sol
@@ -1,4 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {}
+contract Counter {
+     uint256 public number;
+
+    function setNumber(uint256 newNumber) public {
+        number = newNumber;
+    }
+
+    function increment() public {
+        number++;
+    }
+}
--- cli/assets/CounterTemplate.t.sol
+++ cli/assets/CounterTemplate.t.sol
@@ -2,11 +2,22 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "../src/Counter.sol";
 
 contract CounterTest is Test {
-    function setUp() public {}
+    Counter public counter;
+    function setUp() public {
+       counter = new Counter();
+       counter.setNumber(0);
+    }
 
-    function testExample() public {
-        assertTrue(true);
+    function testIncrement() public {
+        counter.increment();
+        assert(counter.number() == 1);
+    }
+
+    function testSetNumber() public {
+        counter.setNumber(10);
+        assertTrue(counter.number() == 10);
     }
 }
