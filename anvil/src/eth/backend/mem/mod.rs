2ed98d5626c10a14407a4e2ca294bcbe45e6e236
Matthias Seitz <matthias.seitz@outlook.de>
2022-08-08 02:23:08 +0200

    docs(anvil): tokio lock rational

--- anvil/src/eth/backend/mem/mod.rs
+++ anvil/src/eth/backend/mem/mod.rs
@@ -73,9 +73,24 @@ pub type State = foundry_evm::HashMap<Address, Account>;
 /// Gives access to the [revm::Database]
 #[derive(Clone)]
 pub struct Backend {
-    /// access to revm's database related operations
-    /// This stores the actual state of the blockchain
-    /// Supports concurrent reads
+    /// Access to [`revm::Database`] abstraction.
+    ///
+    /// This will be used in combination with [`revm::Evm`] and is responsible for feeding data to
+    /// the evm during its execution.
+    ///
+    /// At time of writing, there are two different types of `Db`:
+    ///   - [`MemDb`](crate::mem::MemDb): everything is stored in memory
+    ///   - [`ForkDb`](crate::mem::fork_db::ForkedDatabase): forks off a remote client, missing
+    ///     data is retrieved via RPC-calls
+    ///
+    /// In order to commit changes to the [`revm::Database`], the [`revm::Evm`] requires mutable
+    /// access, which requires a write-lock from this `db`. In forking mode, the time during
+    /// which the write-lock is active depends on whether the `ForkDb` can provide all requested
+    /// data from memory or whether it has to retrieve it via RPC calls first. This means that it
+    /// potentially blocks for some time, even taking into account the rate limits of RPC
+    /// endpoints. Therefor the `Db` is guarded by a `tokio::sync::RwLock` here so calls that
+    /// need to read from it, while it's currently written to, don't block. E.g. a new block is
+    /// currently mined and a new [`Self::set_storage()`] request is being executed.
     db: Arc<AsyncRwLock<dyn Db>>,
     /// stores all block related data in memory
     blockchain: Blockchain,
