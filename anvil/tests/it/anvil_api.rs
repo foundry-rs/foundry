40e6032aaf46eca8909e4fb922ed32ed0306b99c
Matthias Seitz <matthias.seitz@outlook.de>
2022-08-10 17:40:33 +0200

    test: add bsc test

--- anvil/tests/it/anvil_api.rs
+++ anvil/tests/it/anvil_api.rs
@@ -5,7 +5,11 @@ use anvil_core::eth::EthRequest;
 use ethers::{
     abi::ethereum_types::BigEndianHash,
     prelude::{Middleware, SignerMiddleware},
-    types::{Address, BlockNumber, TransactionRequest, H256, U256},
+    types::{
+        transaction::eip2718::TypedTransaction, Address, BlockNumber, TransactionRequest, H256,
+        U256,
+    },
+    utils::hex,
 };
 use std::{
     sync::Arc,
@@ -205,4 +209,33 @@ async fn test_timestamp_interval() {
     let another_block = provider.get_block(BlockNumber::Latest).await.unwrap().unwrap();
     // check interval is disabled
     assert!(another_block.timestamp - new_block.timestamp < U256::from(interval));
+}
+
+// <https://github.com/foundry-rs/foundry/issues/2341>
+#[tokio::test(flavor = "multi_thread")]
+async fn test_can_set_storage_bsc_fork() {
+    let (api, handle) = spawn(NodeConfig::test().with_eth_rpc_url(Some(
+        "https://bsc.getblock.io/mainnet/?api_key=d5e88e54-8d12-4342-95ab-792ffdaac250",
+    )))
+    .await;
+    let provider = handle.http_provider();
+
+    let busd_addr: Address = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56".parse().unwrap();
+    let idx: U256 =
+        "0xa6eef7e35abe7026729641147f7915573c7e97b47efa546f5f6e3230263bcb49".parse().unwrap();
+    let value: H256 =
+        "0x0000000000000000000000000000000000000000000000000000000000003039".parse().unwrap();
+
+    api.anvil_set_storage_at(busd_addr, idx, value).await.unwrap();
+
+    let storage = api.storage_at(busd_addr, idx, None).await.unwrap();
+    assert_eq!(storage, value);
+
+    let input =
+        hex::decode("70a082310000000000000000000000000000000000000000000000000000000000000000")
+            .unwrap();
+
+    let tx: TypedTransaction = TransactionRequest::new().to(busd_addr).data(input).into();
+
+    let _resp = provider.call(&tx, None).await.unwrap();
 }
