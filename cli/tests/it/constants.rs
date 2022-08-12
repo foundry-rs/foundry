19f0a764312d4ce098fefb4f61c1297f6b32a0e1
Matthias Seitz <matthias.seitz@outlook.de>
2022-08-13 01:52:25 +0200

    use constants for contract names

---
+++ cli/tests/it/constants.rs
@@ -0,0 +1,9 @@
+//! various constants
+
+pub const TEMPLATE_CONTRACT: &str = "Counter";
+
+pub const TEMPLATE_TEST_CONTRACT: &str = "CounterTest";
+
+pub const TEMPLATE_CONTRACT_ARTIFACT_BASE: &str = "Counter.sol/Counter";
+
+pub const TEMPLATE_CONTRACT_ARTIFACT_JSON: &str = "Counter.sol/Counter.json";
