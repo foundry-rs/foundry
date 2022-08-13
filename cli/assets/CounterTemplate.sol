7bd6ef076652dcf19bd1b2ed5e52c117dc1d228a
Dongri Jin <dongrify@gmail.com>
2022-08-13 17:33:54 +0900

    Fix indent of template code

--- cli/assets/CounterTemplate.sol
+++ cli/assets/CounterTemplate.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 contract Counter {
-     uint256 public number;
+    uint256 public number;
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
