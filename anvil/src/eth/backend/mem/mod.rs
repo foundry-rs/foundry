4e778e859e4ee21a5b6926600c1ba0fdda6b4207
Matthias Seitz <matthias.seitz@outlook.de>
2022-08-16 19:15:52 +0200

    revert genesis perf

--- anvil/src/eth/backend/mem/mod.rs
+++ anvil/src/eth/backend/mem/mod.rs
@@ -275,17 +275,6 @@ impl Backend {
     /// Resets the fork to a fresh state
     pub async fn reset_fork(&self, forking: Forking) -> Result<(), BlockchainError> {
         if let Some(fork) = self.get_fork() {
-            // we can cache all dev accounts here, so we don't need to fetch
-            let genesis_accounts = {
-                let db = self.db.read().await;
-                self.genesis
-                    .accounts
-                    .iter()
-                    .copied()
-                    .map(|acc| (acc, db.basic(acc)))
-                    .collect::<Vec<_>>()
-            };
-
             // reset the fork entirely and reapply the genesis config
             fork.reset(forking.json_rpc_url.clone(), forking.block_number).await?;
             // update all settings related to the forked block
@@ -304,12 +293,7 @@ impl Backend {
                 BlockchainStorage::forked(fork.block_number(), fork.block_hash());
             self.states.write().clear();
 
-            let mut db = self.db.write().await;
-            for (acc, mut info) in genesis_accounts {
-                info.balance = self.genesis.balance;
-                db.insert_account(acc, info);
-            }
-
+            self.apply_genesis().await;
             Ok(())
         } else {
             Err(RpcError::invalid_params("Forking not enabled").into())
