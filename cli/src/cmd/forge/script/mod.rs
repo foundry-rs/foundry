684f1b03bd05317907b6d1147bb27e9f5eea8202
joshieDo <ranriver@protonmail.com>
2022-08-17 14:23:20 +0000

    always print all traces if script failed

--- cli/src/cmd/forge/script/mod.rs
+++ cli/src/cmd/forge/script/mod.rs
@@ -226,33 +226,29 @@ impl ScriptArgs {
         let verbosity = script_config.evm_opts.verbosity;
         let func = script_config.called_function.as_ref().expect("There should be a function.");
 
-        if verbosity >= 3 {
+        if !result.success || verbosity > 3 {
             if result.traces.is_empty() {
                 eyre::bail!("Unexpected error: No traces despite verbosity level. Please report this as a bug: https://github.com/foundry-rs/foundry/issues/new?assignees=&labels=T-bug&template=BUG-FORM.yml");
             }
 
-            if !result.success && verbosity == 3 || verbosity > 3 {
-                println!("Traces:");
-                for (kind, trace) in &mut result.traces {
-                    let should_include = match kind {
-                        TraceKind::Setup => (verbosity >= 5) || (verbosity == 4 && !result.success),
-                        TraceKind::Execution => verbosity > 3 || !result.success,
-                        _ => false,
-                    };
+            println!("Traces:");
+            for (kind, trace) in &mut result.traces {
+                let should_include = match kind {
+                    TraceKind::Setup => verbosity >= 5,
+                    TraceKind::Execution => verbosity > 3,
+                    _ => false,
+                } || !result.success;
 
-                    if should_include {
-                        decoder.decode(trace).await;
-                        println!("{trace}");
-                    }
+                if should_include {
+                    decoder.decode(trace).await;
+                    println!("{trace}");
                 }
-                println!();
             }
+            println!();
         }
 
         if result.success {
             println!("{}", Paint::green("Script ran successfully."));
-        } else {
-            println!("{}", Paint::red("Script failed."));
         }
 
         if script_config.evm_opts.fork_url.is_none() {
