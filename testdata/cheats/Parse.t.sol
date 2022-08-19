2566531bd15ef62286da947836c1e83201998be6
Alexey Shekhirin <a.shekhirin@gmail.com>
2022-08-19 21:50:13 +0100

    add more tests

--- testdata/cheats/Parse.t.sol
+++ testdata/cheats/Parse.t.sol
@@ -8,38 +8,53 @@ contract ParseTest is Test {
     Cheats constant cheats = Cheats(HEVM_ADDRESS);
 
     function testParseBytes() public {
-        string memory stringBytes = "7109709ECfa91a80626fF3989D68f67F5b1DD12D";
         bytes memory testBytes = hex"7109709ECfa91a80626fF3989D68f67F5b1DD12D";
+
+        string memory stringBytes = "0x7109709ECfa91a80626fF3989D68f67F5b1DD12D";
+        assertEq(testBytes, cheats.parseBytes(stringBytes));
+
+        stringBytes = "7109709ECfa91a80626fF3989D68f67F5b1DD12D";
         assertEq(testBytes, cheats.parseBytes(stringBytes));
     }
 
     function testParseAddress() public {
-        string memory stringAddress = "0x7109709ECfa91a80626fF3989D68f67F5b1DD12D";
         address testAddress = 0x7109709ECfa91a80626fF3989D68f67F5b1DD12D;
+
+        string memory stringAddress = "0x7109709ECfa91a80626fF3989D68f67F5b1DD12D";
+        assertEq(testAddress, cheats.parseAddress(stringAddress));
+
+        stringAddress = "7109709ECfa91a80626fF3989D68f67F5b1DD12D";
         assertEq(testAddress, cheats.parseAddress(stringAddress));
     }
 
     function testParseUint256() public {
-        string memory stringUint256 = "420";
         uint256 testUint256 = 420;
+
+        string memory stringUint256 = "420";
         assertEq(testUint256, cheats.parseUint256(stringUint256));
     }
 
     function testParseInt256() public {
-        string memory stringInt256 = "420";
         int256 testInt256 = 420;
+
+        string memory stringInt256 = "420";
         assertEq(testInt256, cheats.parseInt256(stringInt256));
     }
 
     function testParseBytes32() public {
-        string memory stringBytes = "7465737400000000000000000000000000000000000000000000000000000000";
         bytes32 testBytes = "test";
+
+        string memory stringBytes = "7465737400000000000000000000000000000000000000000000000000000000";
+        assertEq(testBytes, cheats.parseBytes32(stringBytes));
+
+        stringBytes = "0x7465737400000000000000000000000000000000000000000000000000000000";
         assertEq(testBytes, cheats.parseBytes32(stringBytes));
     }
 
     function testParseBool() public {
-        string memory stringBool = "true";
         bool testBool = true;
+
+        string memory stringBool = "true";
         assertEq(testBool, cheats.parseBool(stringBool));
     }
 }
