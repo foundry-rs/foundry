8252201567e885f4c47f73923798a25e0f026f10
Matthias Seitz <matthias.seitz@outlook.de>
2022-08-29 19:36:38 +0200

    feat: impl scanning and printing

--- cli/src/cmd/forge/geiger/find.rs
+++ cli/src/cmd/forge/geiger/find.rs
@@ -3,23 +3,27 @@
 use crate::cmd::forge::geiger::error::ScanFileError;
 use forge_fmt::{parse, Visitable, Visitor};
 use foundry_common::fs;
-use serde::{Deserialize, Serialize};
 use solang_parser::{
     diagnostics::Diagnostic,
-    pt::{Expression, Loc},
+    pt::{ContractDefinition, Expression, FunctionDefinition, Loc, SourceUnit, Statement},
 };
-use std::{convert::Infallible, ops::Add, path::Path};
+use std::{
+    convert::Infallible,
+    fmt,
+    path::{Path, PathBuf},
+};
+use yansi::Paint;
 
 /// Scan a single file for `unsafe` usage.
 pub fn find_cheatcodes_in_file(path: &Path) -> Result<SolFileMetrics, ScanFileError> {
     let content = fs::read_to_string(path)?;
     let cheatcodes = find_cheatcodes_in_string(&content)
         .map_err(|diagnostic| ScanFileError::ParseSol(diagnostic, path.to_path_buf()))?;
-    Ok(SolFileMetrics { cheatcodes })
+    Ok(SolFileMetrics { content, cheatcodes, file: path.to_path_buf() })
 }
 
 pub fn find_cheatcodes_in_string(src: &str) -> Result<CheatcodeCounter, Vec<Diagnostic>> {
-    let mut parsed = parse(&src)?;
+    let mut parsed = parse(src)?;
     let mut visitor = CheatcodeVisitor::default();
     let _ = parsed.pt.visit(&mut visitor);
     Ok(visitor.cheatcodes)
@@ -33,9 +37,85 @@ struct CheatcodeVisitor {
 impl Visitor for CheatcodeVisitor {
     type Error = Infallible;
 
-    fn visit_expr(&mut self, loc: Loc, expr: &mut Expression) -> Result<(), Self::Error> {
-        if let Expression::FunctionCall(loc, lhs, rhs) = expr {
-            dbg!(&lhs);
+    fn visit_source_unit(&mut self, source_unit: &mut SourceUnit) -> Result<(), Self::Error> {
+        source_unit.0.visit(self)
+    }
+
+    fn visit_contract(&mut self, contract: &mut ContractDefinition) -> Result<(), Self::Error> {
+        contract.parts.visit(self)
+    }
+
+    fn visit_block(
+        &mut self,
+        _loc: Loc,
+        _unchecked: bool,
+        statements: &mut Vec<Statement>,
+    ) -> Result<(), Self::Error> {
+        statements.visit(self)
+    }
+
+    fn visit_expr(&mut self, _loc: Loc, expr: &mut Expression) -> Result<(), Self::Error> {
+        if let Expression::FunctionCall(loc, lhs, _) = expr {
+            // all cheatcodes are accessd via <vm>.cheatcode
+            if let Expression::MemberAccess(_, expr, identifier) = &**lhs {
+                if let Expression::Variable(_) = &**expr {
+                    match identifier.name.as_str() {
+                        "ffi" => self.cheatcodes.ffi.push(*loc),
+                        "readFile" => self.cheatcodes.read_file.push(*loc),
+                        "writeFile" => self.cheatcodes.write_file.push(*loc),
+                        "readLine" => self.cheatcodes.read_line.push(*loc),
+                        "writeLine" => self.cheatcodes.write_line.push(*loc),
+                        "closeFile" => self.cheatcodes.close_file.push(*loc),
+                        "removeFile" => self.cheatcodes.remove_file.push(*loc),
+                        "setEnv" => self.cheatcodes.set_env.push(*loc),
+                        "deriveKey" => self.cheatcodes.derive_key.push(*loc),
+                        _ => {}
+                    }
+                }
+            }
+        }
+        Ok(())
+    }
+
+    fn visit_if(
+        &mut self,
+        _loc: Loc,
+        cond: &mut Expression,
+        if_branch: &mut Box<Statement>,
+        else_branch: &mut Option<Box<Statement>>,
+    ) -> Result<(), Self::Error> {
+        cond.visit(self)?;
+        if_branch.visit(self)?;
+        else_branch.visit(self)
+    }
+
+    fn visit_while(
+        &mut self,
+        _loc: Loc,
+        cond: &mut Expression,
+        body: &mut Statement,
+    ) -> Result<(), Self::Error> {
+        cond.visit(self)?;
+        body.visit(self)
+    }
+
+    fn visit_for(
+        &mut self,
+        _loc: Loc,
+        init: &mut Option<Box<Statement>>,
+        cond: &mut Option<Box<Expression>>,
+        update: &mut Option<Box<Statement>>,
+        body: &mut Option<Box<Statement>>,
+    ) -> Result<(), Self::Error> {
+        init.visit(self)?;
+        cond.visit(self)?;
+        update.visit(self)?;
+        body.visit(self)
+    }
+
+    fn visit_function(&mut self, func: &mut FunctionDefinition) -> Result<(), Self::Error> {
+        if let Some(ref mut body) = func.body {
+            body.visit(self)?;
         }
         Ok(())
     }
@@ -44,43 +124,131 @@ impl Visitor for CheatcodeVisitor {
 /// Scan result for a single `.sol` file.
 #[derive(Clone, Debug, Default, Eq, PartialEq)]
 pub struct SolFileMetrics {
+    /// The sol file
+    pub file: PathBuf,
+    /// The file's content
+    pub content: String,
     /// Cheatcode metrics.
     pub cheatcodes: CheatcodeCounter,
 }
 
+pub struct SolFileMetricsPrinter<'a, 'b> {
+    pub metrics: &'a SolFileMetrics,
+    pub root: &'b Path,
+}
+
+impl<'a, 'b> fmt::Display for SolFileMetricsPrinter<'a, 'b> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let SolFileMetricsPrinter { metrics, root } = self;
+
+        let file = metrics.file.strip_prefix(root).unwrap_or(&metrics.file);
+
+        macro_rules! print_unsafe_fn {
+            ($($name:literal => $field:ident),*) => {
+               $ (
+                    if !metrics.cheatcodes.$field.is_empty() {
+                        writeln!(f, "  {}  {}", Paint::red(metrics.cheatcodes.$field.len()), Paint::red($name))?;
+
+                        let mut iter = metrics.cheatcodes.$field.iter().peekable();
+                        while let Some(loc) = iter.next() {
+                            let function_call = &metrics.content.as_bytes()[loc.start().. loc.end()];
+                            let pos = format!("  --> {}:{}:{}", file.display(),  loc.start(), loc.end());
+                            writeln!(f,"{}", Paint::red(pos))?;
+                            let content = String::from_utf8_lossy(function_call);
+                            let mut lines = content.lines().peekable();
+                            while let Some(line) = lines.next() {
+                                writeln!(f, "      {}", Paint::red(line))?;
+                            }
+                        }
+                    }
+               )*
+
+            };
+        }
+
+        if metrics.cheatcodes.has_unsafe() {
+            writeln!(
+                f,
+                "{}    {}",
+                Paint::red(metrics.cheatcodes.count()),
+                Paint::red(file.display())
+            )?;
+            print_unsafe_fn!(
+                "ffi" => ffi,
+                "readFile" => read_file,
+                "readLine" => read_line,
+                "writeFile" => write_file,
+                "writeLine" => write_line,
+                "removeFile" => remove_file,
+                "closeFile" => close_file,
+                "setEnv" => set_env,
+                "deriveKey" => derive_key
+            );
+        } else {
+            writeln!(f, "0    {}", file.display())?
+        }
+
+        Ok(())
+    }
+}
+
 /// Unsafe usage metrics collection.
-#[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize)]
+#[derive(Clone, Debug, Default, Eq, PartialEq)]
 pub struct CheatcodeCounter {
-    pub ffi: usize,
-    pub read_file: usize,
-    pub read_line: usize,
-    pub write_file: usize,
-    pub write_line: usize,
-    pub remove_file: usize,
+    pub ffi: Vec<Loc>,
+    pub read_file: Vec<Loc>,
+    pub read_line: Vec<Loc>,
+    pub write_file: Vec<Loc>,
+    pub write_line: Vec<Loc>,
+    pub remove_file: Vec<Loc>,
+    pub close_file: Vec<Loc>,
+    pub set_env: Vec<Loc>,
+    pub derive_key: Vec<Loc>,
 }
 
 impl CheatcodeCounter {
     pub fn has_unsafe(&self) -> bool {
-        self.ffi > 0 ||
-            self.read_file > 0 ||
-            self.read_line > 0 ||
-            self.write_file > 0 ||
-            self.write_line > 0 ||
-            self.remove_file > 0
+        !self.ffi.is_empty() ||
+            !self.read_file.is_empty() ||
+            !self.read_line.is_empty() ||
+            !self.write_file.is_empty() ||
+            !self.write_line.is_empty() ||
+            !self.close_file.is_empty() ||
+            !self.set_env.is_empty() ||
+            !self.derive_key.is_empty() ||
+            !self.remove_file.is_empty()
+    }
+
+    pub fn count(&self) -> usize {
+        self.ffi.len() +
+            self.read_file.len() +
+            self.read_line.len() +
+            self.write_file.len() +
+            self.write_line.len() +
+            self.close_file.len() +
+            self.set_env.len() +
+            self.derive_key.len() +
+            self.remove_file.len()
     }
 }
 
-impl Add for CheatcodeCounter {
-    type Output = CheatcodeCounter;
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    fn add(self, other: CheatcodeCounter) -> CheatcodeCounter {
-        CheatcodeCounter {
-            ffi: self.ffi + other.ffi,
-            read_file: self.read_file + other.read_file,
-            read_line: self.read_line + other.read_line,
-            write_file: self.write_file + other.write_file,
-            write_line: self.write_line + other.write_line,
-            remove_file: self.remove_file + other.remove_file,
+    #[test]
+    fn can_find_calls() {
+        let s = r#"
+        contract A is Test {
+            function do_ffi() public {
+                string[] memory inputs = new string[](1);
+                vm.ffi(inputs);
+            }
         }
+        "#;
+
+        let count = find_cheatcodes_in_string(s).unwrap();
+        assert_eq!(count.ffi.len(), 1);
+        assert!(count.has_unsafe());
     }
 }
--- cli/src/cmd/forge/geiger/mod.rs
+++ cli/src/cmd/forge/geiger/mod.rs
@@ -1,29 +1,20 @@
-use crate::cmd::{Cmd, LoadConfig};
+use crate::cmd::{
+    forge::geiger::find::{find_cheatcodes_in_file, SolFileMetricsPrinter},
+    Cmd, LoadConfig,
+};
 use clap::{Parser, ValueHint};
-use foundry_config::impl_figment_convert_basic;
+use ethers::solc::Graph;
+use eyre::WrapErr;
+use foundry_config::{impl_figment_convert_basic, Config};
 use rayon::prelude::*;
-use std::path::PathBuf;
+use std::{collections::HashSet, path::PathBuf};
+use yansi::Paint;
 
 mod error;
 mod find;
 
 #[derive(Debug, Clone, Parser)]
 pub struct GeigerArgs {
-    #[clap(
-        help = "The project's root path.",
-        long_help = "The project's root path. By default, this is the root directory of the current Git repository, or the current working directory.",
-        long,
-        value_hint = ValueHint::DirPath,
-        value_name = "PATH"
-    )]
-    root: Option<PathBuf>,
-    #[clap(
-        help = "Path to the config file.",
-        long = "config-path",
-        value_hint = ValueHint::FilePath,
-        value_name = "FILE"
-    )]
-    config_path: Option<PathBuf>,
     #[clap(
         help = "path to a file or directory to detect",
         conflicts_with = "root",
@@ -33,30 +24,67 @@ pub struct GeigerArgs {
     )]
     paths: Vec<PathBuf>,
     #[clap(
-        help = "name(s) of the dependencies to scan",
-        conflicts_with = "paths",
-        value_name = "DIR_NAME",
-        multiple = true
+        help = "The project's root path.",
+        long_help = "The project's root path. By default, this is the root directory of the current Git repository, or the current working directory.",
+        long,
+        value_hint = ValueHint::DirPath,
+        value_name = "PATH"
     )]
-    libs: Vec<String>,
+    root: Option<PathBuf>,
     #[clap(
         help = "run in 'check' mode. Exits with 0 if no unsafe cheat codes were found. Exits with 1 if unsafe cheat codes are detected.",
         long
     )]
     check: bool,
+    #[clap(help = "print a full report of all files even if no unsafe functions are found.", long)]
+    full: bool,
 }
 
 impl_figment_convert_basic!(GeigerArgs);
 
 // === impl GeigerArgs ===
 
-impl GeigerArgs {}
+impl GeigerArgs {
+    pub fn sources(&self, config: &Config) -> eyre::Result<Vec<PathBuf>> {
+        if !self.paths.is_empty() {
+            let mut files = HashSet::new();
+            for path in &self.paths {
+                files.extend(foundry_common::fs::files_with_ext(path, "sol"));
+            }
+            return Ok(files.into_iter().collect())
+        }
+
+        let graph = Graph::resolve(&config.project_paths())?;
+        Ok(graph.files().keys().cloned().collect())
+    }
+}
 
 impl Cmd for GeigerArgs {
     type Output = ();
 
     fn run(self) -> eyre::Result<Self::Output> {
         let config = self.load_config_emit_warnings();
+        let sources = self.sources(&config).wrap_err("Failed to resolve files")?;
+
+        if config.ffi {
+            eprintln!("{}\n", Paint::red("ffi enabled"));
+        }
+
+        let root = config.__root.0;
+
+        sources.par_iter().map(|file| find_cheatcodes_in_file(file)).for_each(|res| {
+            match res {
+                Ok(metrics) => {
+                    let printer = SolFileMetricsPrinter { metrics: &metrics, root: &root };
+                    if self.full || printer.metrics.cheatcodes.has_unsafe() {
+                        eprint!("{}", printer);
+                    }
+                }
+                Err(err) => {
+                    eprintln!("{}", err);
+                }
+            };
+        });
 
         Ok(())
     }
