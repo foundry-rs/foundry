631007b8242fc03775397993e5d756f227ffa8c3
Matthias Seitz <matthias.seitz@outlook.de>
2022-09-10 20:12:13 +0200

    fix(anvil): include total difficulty (#3156)
    * fix(anvil): include total difficulty
    
    * refactor: set difficulty to 0 for anvil mined blocks

--- anvil/src/config.rs
+++ anvil/src/config.rs
@@ -791,6 +791,7 @@ impl NodeConfig {
                         retries: self.fork_request_retries,
                         backoff: self.fork_retry_backoff,
                         compute_units_per_second: self.compute_units_per_second,
+                        total_difficulty: block.total_difficulty.unwrap_or_default(),
                     },
                     Arc::clone(&db),
                 );
