3878939c17c1ba63e0c5baad350f4201b148eaa2
Matthias Seitz <matthias.seitz@outlook.de>
2022-09-24 20:10:21 +0200

    redact remaining cheatcodes

--- evm/src/trace/utils.rs
+++ evm/src/trace/utils.rs
@@ -5,6 +5,7 @@ use ethers::{
     abi::{Abi, Address, Function, ParamType, Token},
     core::utils::to_checksum,
 };
+use foundry_common::SELECTOR_LEN;
 use foundry_utils::format_token;
 use std::collections::HashMap;
 
@@ -35,8 +36,8 @@ pub(crate) fn decode_cheatcode_inputs(
         "expectRevert" => {
             decode::decode_revert(data, Some(errors), None).ok().map(|decoded| vec![decoded])
         }
-        "sign" | "startBroadcast" | "broadcast" => {
-            // sign and broadcast functions accept a private key as uint256, which should not be
+        "rememberKey" | "addr" | "startBroadcast" | "broadcast" => {
+            // these functions accept a private key as uint256, which should not be
             // converted to plain text
             if !func.inputs.is_empty() && matches!(&func.inputs[0].kind, ParamType::Uint(_)) {
                 // redact private key input
@@ -45,6 +46,15 @@ pub(crate) fn decode_cheatcode_inputs(
                 None
             }
         }
+        "sign" => {
+            // sign(uint256,bytes32)
+            let mut decoded = func.decode_input(&data[SELECTOR_LEN..]).ok()?;
+            if !decoded.is_empty() && matches!(&func.inputs[0].kind, ParamType::Uint(_)) {
+                decoded[0] = Token::String("<pk>".to_string());
+            }
+            Some(decoded.iter().map(format_token).collect())
+        }
+        "deriveKey" => Some(vec!["<pk>".to_string()]),
 
         _ => None,
     }
@@ -55,6 +65,10 @@ pub(crate) fn decode_cheatcode_outputs(func: &Function, _data: &[u8]) -> Option<
     if func.name.starts_with("env") {
         // redacts the value stored in the env var
         return Some("<env var value>".to_string())
+    }
+    if func.name == "deriveKey" {
+        // redacts derived private key
+        return Some("<pl>".to_string())
     }
     None
 }
