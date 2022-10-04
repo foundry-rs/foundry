69e87f86a3e7cb67268bbddbe6ff275914cb8c9a
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2022-10-04 12:23:39 +0200

    fix(cast): invalid decimal base formatting (#3449)
    * fix(cast): invalid decimal base formatting
    
    * Revert "fix(cast): invalid decimal base formatting"
    
    This reverts commit 67d2ff15aed8c0f9cd596663c76794b24f29a50a.
    
    * fix(cast): invalid decimal base formatting
    
    * Revert "fix(cast): invalid decimal base formatting"
    
    This reverts commit 43e2dfe9fec3b45187e4e052fec27fe5e001ce0d.
    
    * fix: crlf to lf eol
    
    * fix(cast): invalid decimal base formatting

--- cast/src/base.rs
+++ cast/src/base.rs
@@ -381,8 +381,12 @@ impl NumberWithBase {
             // a custom formatter
             Base::Binary | Base::Octal => self.format_radix(),
             Base::Decimal => {
-                let s = I256::from_raw(self.number).to_string();
-                s.strip_prefix('-').unwrap_or(&s).to_string()
+                if self.is_nonnegative {
+                    self.number.to_string()
+                } else {
+                    let s = I256::from_raw(self.number).to_string();
+                    s.strip_prefix('-').unwrap_or(&s).to_string()
+                }
             }
             Base::Hexadecimal => format!("{:x}", self.number),
         }
