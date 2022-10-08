fc557685b77188c866c755205c0033fedfa85ebb
clabby <ben@clab.by>
2022-10-08 12:17:24 -0400

    Store raw source alongside parsed source units

--- chisel/src/env.rs
+++ chisel/src/env.rs
@@ -1,17 +1,36 @@
+use core::fmt;
+use std::rc::Rc;
+
 use ethers_solc::project_util::TempProject;
 use rustyline::Editor;
 
+/// Represents a parsed snippet of Solidity code.
+#[derive(Debug)]
+pub struct SolSnippet {
+    pub source_unit: (solang_parser::pt::SourceUnit, Vec<solang_parser::pt::Comment>),
+    pub raw: Rc<String>,
+}
+
+/// Display impl for `SolToken`
+impl fmt::Display for SolSnippet {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.raw)
+    }
+}
+
+/// A Chisel REPL environment.
 pub struct ChiselEnv {
     /// The `TempProject` created for the REPL contract.
     pub project: TempProject,
     /// The `rustyline` Editor
     pub rl: Editor<()>,
     /// The current session
-    /// A session contains an ordered vector of source units, parsed by the solang-parser.
-    pub session: Vec<(solang_parser::pt::SourceUnit, Vec<solang_parser::pt::Comment>)>,
+    /// A session contains an ordered vector of source units, parsed by the solang-parser,
+    /// as well as the raw source.
+    pub session: Vec<SolSnippet>,
 }
 
-/// A Chisel REPL environment
+/// Chisel REPL environment impl
 impl ChiselEnv {
     /// Create a new `ChiselEnv` with a specified `solc` version.
     pub fn new(solc_version: &'static str) -> Self {
@@ -38,7 +57,8 @@ impl ChiselEnv {
     }
 
     /// Render the full source code for the current session.
-    /// TODO - Render source correctly, not `SourceUnit`s.
+    /// TODO - Render source correctly rather than throwing
+    /// everything into the fallback.
     pub fn contract_source(&self) -> String {
         format!(
             r#"
@@ -46,12 +66,12 @@ impl ChiselEnv {
 pragma solidity {};
 contract REPL {{
     fallback() {{
-        {:?}
+        {}
     }}
 }}
         "#,
             "^0.8.17", // TODO: Grab version from TempProject's solc instance.
-            self.session
+            self.session.iter().map(|t| t.to_string()).collect::<Vec<String>>().join("\n")
         )
     }
 
