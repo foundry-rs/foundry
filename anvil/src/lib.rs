5c30108fa07f08fca87ef8ea291680699982da99
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2022-10-22 15:05:22 +0200

    fix(anvil): unused import warning

--- anvil/src/lib.rs
+++ anvil/src/lib.rs
@@ -37,7 +37,6 @@ use tokio::{
     runtime::Handle,
     task::{JoinError, JoinHandle},
 };
-use tracing::trace;
 
 /// contains the background service that drives the node
 mod service;
@@ -274,7 +273,7 @@ impl NodeHandle {
     #[cfg(not(windows))]
     pub async fn ipc_provider(&self) -> Option<Provider<ethers::providers::Ipc>> {
         let ipc_path = self.config.get_ipc_path()?;
-        trace!(target = "ipc", ?ipc_path, "connecting ipc provider");
+        tracing::trace!(target = "ipc", ?ipc_path, "connecting ipc provider");
         let provider = Provider::connect_ipc(&ipc_path).await.unwrap_or_else(|err| {
             panic!("Failed to connect to node's ipc endpoint {}: {:?}", ipc_path, err)
         });
