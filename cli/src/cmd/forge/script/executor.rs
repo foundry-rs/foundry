48fc112a43402ed79baa70082bce5cc0bf119ae1
Matthias Seitz <matthias.seitz@outlook.de>
2022-11-16 08:52:46 +0100

    chore(clippy): make clippy happy

--- cli/src/cmd/forge/script/executor.rs
+++ cli/src/cmd/forge/script/executor.rs
@@ -219,7 +219,7 @@ impl ScriptArgs {
 
                 for (_kind, trace) in &mut traces {
                     decoder.decode(trace).await;
-                    println!("{}", trace);
+                    println!("{trace}");
                 }
             }
 
