2b908fa6c45f4c756117977015a30da80fbab4f5
joshieDo <ranriver@protonmail.com>
2022-11-19 07:08:22 +0000

    fix tests

--- forge/tests/it/fork.rs
+++ forge/tests/it/fork.rs
@@ -27,7 +27,7 @@ fn test_cheats_fork_revert() {
         for (_, result) in test_results {
             assert_eq!(
                 result.reason.unwrap(),
-                "Contract 0xCe71065D4017F316EC606Fe4422e11eB2c47c246 does not exist on active fork with id `1`\n        But exists on non active forks: `[0]`"
+                "Contract 0x5615dEB798BB3E4dFa0139dFa1b3D433Cc23b72f does not exist on active fork with id `1`\n        But exists on non active forks: `[0]`"
             );
         }
     }
