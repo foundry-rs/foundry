19f4c67bcb78c0c6d7e4c806a535a5a6608bc632
joshieDo <ranriver@protonmail.com>
2022-11-19 07:13:43 +0000

    remove cheats import

--- testdata/repros/Issue3661.sol
+++ testdata/repros/Issue3661.sol
@@ -2,11 +2,9 @@
 pragma solidity >=0.8.0;
 
 import "ds-test/test.sol";
-import "../cheats/Cheats.sol";
 
 // https://github.com/foundry-rs/foundry/issues/3661
 contract Issue3661Test is DSTest {
-    Cheats constant vm = Cheats(HEVM_ADDRESS);
     address sender;
 
     function setUp() public {
