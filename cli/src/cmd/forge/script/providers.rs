9779a7f8691ead09e8d0dd7843abba60fe03e5b9
Matthias Seitz <matthias.seitz@outlook.de>
2022-12-09 07:36:55 +0100

    chore: replace bad panic with error (#3856)

--- cli/src/cmd/forge/script/providers.rs
+++ cli/src/cmd/forge/script/providers.rs
@@ -1,13 +1,15 @@
 use ethers::prelude::{Http, Middleware, Provider, RetryClient, U256};
+use eyre::WrapErr;
 use foundry_common::{get_http_provider, RpcUrl};
 use foundry_config::Chain;
 use std::{
     collections::{hash_map::Entry, HashMap},
+    ops::Deref,
     sync::Arc,
 };
 
-#[derive(Default)]
 /// Contains a map of RPC urls to single instances of [`ProviderInfo`].
+#[derive(Default)]
 pub struct ProvidersManager {
     pub inner: HashMap<RpcUrl, ProviderInfo>,
 }
@@ -29,14 +31,28 @@ impl ProvidersManager {
     }
 }
 
+impl Deref for ProvidersManager {
+    type Target = HashMap<RpcUrl, ProviderInfo>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
 /// Holds related metadata to each provider RPC.
 #[derive(Debug)]
 pub struct ProviderInfo {
     pub provider: Arc<Provider<RetryClient<Http>>>,
     pub chain: u64,
-    pub gas_price: Option<U256>,
-    pub eip1559_fees: Option<(U256, U256)>,
+    pub gas_price: GasPrice,
     pub is_legacy: bool,
+}
+
+/// Represents the outcome of a gas price request
+#[derive(Debug)]
+pub enum GasPrice {
+    Legacy(eyre::Result<U256>),
+    EIP1559(eyre::Result<(U256, U256)>),
 }
 
 impl ProviderInfo {
@@ -48,12 +64,28 @@ impl ProviderInfo {
             is_legacy |= chain.is_legacy();
         };
 
-        let (gas_price, eip1559_fees) = if is_legacy {
-            (provider.get_gas_price().await.ok(), None)
+        let gas_price = if is_legacy {
+            GasPrice::Legacy(
+                provider.get_gas_price().await.wrap_err("Failed to get legacy gas price"),
+            )
         } else {
-            (None, provider.estimate_eip1559_fees(None).await.ok())
+            GasPrice::EIP1559(
+                provider.estimate_eip1559_fees(None).await.wrap_err("Failed to get EIP-1559 fees"),
+            )
         };
 
-        Ok(ProviderInfo { provider, chain, gas_price, eip1559_fees, is_legacy })
+        Ok(ProviderInfo { provider, chain, gas_price, is_legacy })
+    }
+
+    /// Returns the gas price to use
+    pub fn gas_price(&self) -> eyre::Result<U256> {
+        let res = match &self.gas_price {
+            GasPrice::Legacy(res) => res.as_ref(),
+            GasPrice::EIP1559(res) => res.as_ref().map(|res| &res.0),
+        };
+        match res {
+            Ok(val) => Ok(*val),
+            Err(err) => Err(eyre::eyre!("{}", err)),
+        }
     }
 }
