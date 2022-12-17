1b8453770042e4a892ec31e6b64b02b96d9b9c6e
brock elmore <brock.elmore@gmail.com>
2022-12-17 15:41:56 -0800

    fix external call gas metering

--- testdata/cheats/GasMetering.t.sol
+++ testdata/cheats/GasMetering.t.sol
@@ -4,6 +4,12 @@ pragma solidity >=0.8.0;
 import "ds-test/test.sol";
 import "./Cheats.sol";
 
+contract B {
+    function a() public returns (uint256) {
+        return 100;
+    }
+}
+
 contract GasMeteringTest is DSTest {
     Cheats constant cheats = Cheats(HEVM_ADDRESS);
 
@@ -32,6 +38,32 @@ contract GasMeteringTest is DSTest {
         assertEq(gas_end_not_metered, 0);
     }
 
+    function testGasMeteringExternal() public {
+        B b = new B();
+        uint256 gas_start = gasleft();
+
+        b.a();
+
+        uint256 gas_end_normal = gas_start - gasleft();
+
+        cheats.pauseGasMetering();
+        uint256 gas_start_not_metered = gasleft();
+
+        b.a();
+
+        uint256 gas_end_not_metered = gas_start_not_metered - gasleft();
+        cheats.resumeGasMetering();
+
+        uint256 gas_start_metered = gasleft();
+
+        b.a();
+
+        uint256 gas_end_resume_metered = gas_start_metered - gasleft();
+
+        assertEq(gas_end_normal, gas_end_resume_metered);
+        assertEq(gas_end_not_metered, 0);
+    }
+
     function addInLoop() internal returns (uint256) {
         uint256 b;
         for (uint256 i; i < 10000; i++) {
@@ -39,4 +71,5 @@ contract GasMeteringTest is DSTest {
         }
         return b;
     }
+
 }
