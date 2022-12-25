3628783db836fd9bf76b4f0a92a0d00606ba3171
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2022-12-26 00:40:07 +0100

    chore: rm debugging

--- macros/impl/src/lib.rs
+++ macros/impl/src/lib.rs
@@ -8,9 +8,6 @@ use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(ConsoleFmt)]
 pub fn console_fmt(input: TokenStream) -> TokenStream {
-    eprintln!("INPUT ={input}");
     let input = parse_macro_input!(input as DeriveInput);
-    let out = TokenStream::from(console_fmt::console_fmt(input));
-    eprintln!("OUTPUT={out}");
-    out
+    console_fmt::console_fmt(input).into()
 }
