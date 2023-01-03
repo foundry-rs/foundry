a139b029a53ef5f987755ef983e5cbbcc6a86fa3
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2023-01-03 18:38:38 +0100

    add comment

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,3 +1,7 @@
+//! Foundry's procedural macros.
+
+// TODO: Remove dependency on foundry-common (currently only used for the UIfmt trait).
+
 mod console_fmt;
 pub use console_fmt::{console_format, ConsoleFmt, FormatSpec};
 
