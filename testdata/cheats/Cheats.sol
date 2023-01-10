aec60d1bb1088c1c8e8ca12aee7ae3f1e797148d
Odysseas.eth <odyslam@gmail.com>
2023-01-10 10:40:28 -0300

    add all cheatcodes to testdata interface

--- testdata/cheats/Cheats.sol
+++ testdata/cheats/Cheats.sol
@@ -401,6 +401,22 @@ interface Cheats {
 
     function parseJsonBoolArray(string calldata, string calldata) external returns (bool[] memory);
 
+    function parseJsonAddress(string calldata, string calldata) external returns (address);
+
+    function parseJsonAddress(string calldata, string calldata) external returns (address[] memory);
+
+    function parseJsonString(string calldata, string calldata) external returns (string memory);
+
+    function parseJsonString(string calldata, string calldata) external returns (string[] memory);
+
+    function parseJsonBytes(string calldata, string calldata) external returns (bytes memory);
+
+    function parseJsonBytes(string calldata, string calldata) external returns (bytes[] memory);
+
+    function parseJsonBytes32(string calldata, string calldata) external returns (bytes32);
+
+    function parseJsonBytes32(string calldata, string calldata) external returns (bytes32[]);
+
     function serializeBool(string calldata, string calldata, bool) external returns (string memory);
 
     function serializeUint(string calldata, string calldata, uint256) external returns (string memory);
