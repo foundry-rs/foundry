527c9266680ce84304c21241c93bf158b25fb80b
Matthias Seitz <matthias.seitz@outlook.de>
2023-01-26 11:22:06 +0100

    feat: disable eip3607 by default in forge test

--- evm/src/executor/opts.rs
+++ evm/src/executor/opts.rs
@@ -113,9 +113,12 @@ impl EvmOpts {
             cfg: CfgEnv {
                 chain_id: self.env.chain_id.unwrap_or(foundry_common::DEV_CHAIN_ID).into(),
                 spec_id: SpecId::MERGE,
-                perf_all_precompiles_have_balance: false,
                 limit_contract_code_size: self.env.code_size_limit.or(Some(usize::MAX)),
                 memory_limit: self.memory_limit,
+                // EIP-3607 rejects transactions from senders with deployed code.
+                // If EIP-3607 is enabled it can cause issues during fuzz/invariant tests if the
+                // caller is a contract. So we disable the check by default.
+                disable_eip3607: true,
                 ..Default::default()
             },
             tx: TxEnv {
