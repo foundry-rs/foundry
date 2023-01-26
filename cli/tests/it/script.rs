a84a8063fa35e7164cf18fc10f9d3c0e39b27295
Christian Koopmann <c.k.e.koopmann@gmail.com>
2023-01-26 16:14:35 +0800

    Add test case for setting manual gas limit for individual call

--- cli/tests/it/script.rs
+++ cli/tests/it/script.rs
@@ -126,6 +126,61 @@ contract Demo {
 });
 
 // Tests that the run command can run functions with arguments
+forgetest_async!(can_execute_script_command_with_manual_gas_limit, |prj: TestProject, mut cmd: TestCommand| async move {
+    foundry_cli_test_utils::util::initialize(prj.root());
+    let deploy_script = prj
+        .inner()
+        .add_source(
+            "Foo",
+            r#"
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.10;
+import "forge-std/Script.sol";
+
+contract GasWaster {
+    function wasteGas(uint256 minGas) public {
+        require(gasleft() >= minGas,  "Gas left needs to be higher");
+    }
+}
+contract DeployScript is Script {
+    function run() external returns (uint256 result, uint8) {
+        vm.startBroadcast();
+        GasWaster gasWaster = new GasWaster();
+        gasWaster.wasteGas{gas: 500000}(200000);
+    }
+}
+   "#,
+        )
+        .unwrap();
+
+        let deploy_contract = deploy_script.display().to_string() + ":DeployScript";
+
+        let node_config = NodeConfig::test()
+            .with_eth_rpc_url(Some(rpc::next_http_archive_rpc_endpoint()))
+            .silent();
+        let (_api, handle) = spawn(node_config).await;
+        let private_key =
+            "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80".to_string();
+        cmd.set_current_dir(prj.root());
+
+        cmd.args([
+            "script",
+            &deploy_contract,
+            "--root",
+            prj.root().to_str().unwrap(),
+            "--fork-url",
+            &handle.http_endpoint(),
+            "-vvvvv",
+            "--slow",
+            "--private-key",
+            &private_key,
+        ]);
+
+        let output = cmd.stdout_lossy();
+        println!("{}", output);
+});
+
+// Tests that the run command can run functions with arguments
 forgetest!(can_execute_script_command_with_args, |prj: TestProject, mut cmd: TestCommand| {
     let script = prj
         .inner()
