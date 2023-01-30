7f73d69a049a09c79c9c7cbb5645b43e5da6c89e
Roman Krasiuk <rokrassyuk@gmail.com>
2023-01-30 12:24:35 +0200

    fix(cast): estimate and call sender options

--- cli/src/cmd/cast/call.rs
+++ cli/src/cmd/cast/call.rs
@@ -10,7 +10,7 @@ use cast::{Cast, TxBuilder};
 use clap::Parser;
 use ethers::{
     providers::Middleware,
-    types::{Address, BlockId, NameOrAddress, U256},
+    types::{BlockId, NameOrAddress, U256},
 };
 use eyre::WrapErr;
 use foundry_common::try_get_http_provider;
@@ -81,8 +81,8 @@ impl CallArgs {
         let chain: Chain =
             if let Some(chain) = eth.chain { chain } else { provider.get_chainid().await?.into() };
 
-        let from = eth.wallet.from.unwrap_or(Address::zero());
-        let mut builder = TxBuilder::new(&provider, from, to, chain, tx.legacy).await?;
+        let sender = eth.sender().await;
+        let mut builder = TxBuilder::new(&provider, sender, to, chain, tx.legacy).await?;
         builder
             .gas(tx.gas_limit)
             .etherscan_api_key(config.get_etherscan_api_key(Some(chain)))
