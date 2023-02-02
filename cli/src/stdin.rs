4ef84712d13e00f19db93190efd4417db9e215de
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2023-02-02 19:07:11 +0100

    fix: doctest

--- cli/src/stdin.rs
+++ cli/src/stdin.rs
@@ -14,11 +14,12 @@ use std::{
 /// # Examples
 ///
 /// ```no_run
+/// # use foundry_cli::prompt;
 /// let response: String = prompt!("Would you like to continue? [y/N] ")?;
 /// if !matches!(response.as_str(), "y" | "Y") {
 ///     return Ok(())
 /// }
-/// # Ok::<(), Box<dyn std::error::Error>>()
+/// # Ok::<(), Box<dyn std::error::Error>>(())
 /// ```
 #[macro_export]
 macro_rules! prompt {
