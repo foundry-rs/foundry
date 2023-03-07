261d951cc6c17bb7c0530865232e0b57ebf7c7a0
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2023-03-07 17:31:37 +0100

    fix: chain parsing

--- cli/src/opts/chain.rs
+++ cli/src/opts/chain.rs
@@ -27,12 +27,19 @@ impl TypedValueParser for ChainValueParser {
         arg: Option<&clap::Arg>,
         value: &OsStr,
     ) -> Result<Self::Value, clap::Error> {
-        if let Some(id) = value.to_str().and_then(|value| value.parse::<u64>().ok()) {
+        let s =
+            value.to_str().ok_or_else(|| clap::Error::new(clap::error::ErrorKind::InvalidUtf8))?;
+        if let Ok(id) = s.parse() {
             Ok(Chain::Id(id))
         } else {
-            let string = self.inner.parse_ref(cmd, arg, value)?;
-            let named = string.parse::<NamedChain>().expect("Already validated");
-            Ok(Chain::Named(named))
+            // NamedChain::VARIANTS is a subset of all possible variants, since there are aliases:
+            // mumbai instead of polygon-mumbai etc
+            //
+            // Parse first as NamedChain, if it fails parse with NamedChain::VARIANTS for displaying
+            // the error to the user
+            s.parse()
+                .map_err(|_| self.inner.parse_ref(cmd, arg, value).unwrap_err())
+                .map(Chain::Named)
         }
     }
 }
