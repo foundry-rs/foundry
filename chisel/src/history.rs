316b271a360a1d20a606fb324c6f846cae894965
Matthias Seitz <matthias.seitz@outlook.de>
2023-04-06 22:48:39 +0200

    clippy

--- chisel/src/history.rs
+++ chisel/src/history.rs
@@ -3,7 +3,7 @@
 use std::path::PathBuf;
 
 /// The name of the chisel history file
-pub const CHISEL_HISTORY_FILE_NAME: &'static str = ".chisel_history";
+pub const CHISEL_HISTORY_FILE_NAME: &str = ".chisel_history";
 
 /// Returns the path to foundry's global toml file that's stored at `~/.foundry/.chisel_history`
 pub fn chisel_history_file() -> Option<PathBuf> {
