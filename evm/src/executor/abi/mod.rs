dedc31eed0683764ae542b9b575c21f6bd2a5c60
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2023-04-22 11:45:21 +0200

    chore: small cheatcode improvements (#4799)
    * fmt: abigens
    
    * Pass by reference when patching Hardhat selector

--- evm/src/executor/abi/mod.rs
+++ evm/src/executor/abi/mod.rs
@@ -1,4 +1,7 @@
-use ethers::types::{Address, Selector, H160};
+use ethers::{
+    contract::abigen,
+    types::{Address, Selector, H160},
+};
 use once_cell::sync::Lazy;
 use std::collections::HashMap;
 
@@ -12,184 +15,203 @@ pub const CHEATCODE_ADDRESS: Address = H160([
 ]);
 
 // Bindings for cheatcodes
-ethers::contract::abigen!(
+abigen!(
     HEVM,
-    r#"[
-            struct Log {bytes32[] topics; bytes data;}
-            struct Rpc {string name; string url;}
-            struct FsMetadata {bool isDir; bool isSymlink; uint256 length; bool readOnly; uint256 modified; uint256 accessed; uint256 created;}
-            roll(uint256)
-            warp(uint256)
-            difficulty(uint256)
-            fee(uint256)
-            coinbase(address)
-            store(address,bytes32,bytes32)
-            load(address,bytes32)(bytes32)
-            ffi(string[])(bytes)
-            setEnv(string,string)
-            breakpoint(string)
-            envBool(string)(bool)
-            envUint(string)(uint256)
-            envInt(string)(int256)
-            envAddress(string)(address)
-            envBytes32(string)(bytes32)
-            envString(string)(string)
-            envBytes(string)(bytes)
-            envBool(string,string)(bool[])
-            envUint(string,string)(uint256[])
-            envInt(string,string)(int256[])
-            envAddress(string,string)(address[])
-            envBytes32(string,string)(bytes32[])
-            envString(string,string)(string[])
-            envBytes(string,string)(bytes[])
-            envOr(string,bool)(bool)
-            envOr(string,uint256)(uint256)
-            envOr(string,int256)(int256)
-            envOr(string,address)(address)
-            envOr(string,bytes32)(bytes32)
-            envOr(string,string)(string)
-            envOr(string,bytes)(bytes)
-            envOr(string,string,bool[])(bool[])
-            envOr(string,string,uint256[])(uint256[])
-            envOr(string,string,int256[])(int256[])
-            envOr(string,string,address[])(address[])
-            envOr(string,string,bytes32[])(bytes32[])
-            envOr(string,string,string[])(string[])
-            envOr(string,string,bytes[])(bytes[])
-            addr(uint256)(address)
-            sign(uint256,bytes32)(uint8,bytes32,bytes32)
-            deriveKey(string,uint32)(uint256)
-            deriveKey(string,string,uint32)(uint256)
-            rememberKey(uint256)(address)
-            prank(address)
-            startPrank(address)
-            prank(address,address)
-            startPrank(address,address)
-            stopPrank()
-            deal(address,uint256)
-            etch(address,bytes)
-            expectRevert()
-            expectRevert(bytes)
-            expectRevert(bytes4)
-            record()
-            accesses(address)(bytes32[],bytes32[])
-            recordLogs()
-            getRecordedLogs()(Log[])
-            expectEmit()
-            expectEmit(address)
-            expectEmit(bool,bool,bool,bool)
-            expectEmit(bool,bool,bool,bool,address)
-            mockCall(address,bytes,bytes)
-            mockCall(address,uint256,bytes,bytes)
-            mockCallRevert(address,bytes,bytes)
-            mockCallRevert(address,uint256,bytes,bytes)
-            clearMockedCalls()
-            expectCall(address,bytes)
-            expectCall(address,uint256,bytes)
-            expectCall(address,uint256,uint64,bytes)
-            expectCallMinGas(address,uint256,uint64,bytes)
-            expectSafeMemory(uint64,uint64)
-            expectSafeMemoryCall(uint64,uint64)
-            getCode(string)
-            getDeployedCode(string)
-            label(address,string)
-            assume(bool)
-            setNonce(address,uint64)
-            getNonce(address)
-            chainId(uint256)
-            txGasPrice(uint256)
-            broadcast()
-            broadcast(address)
-            broadcast(uint256)
-            startBroadcast()
-            startBroadcast(address)
-            startBroadcast(uint256)
-            stopBroadcast()
-            projectRoot()(string)
-            readFile(string)(string)
-            readFileBinary(string)(bytes)
-            writeFile(string,string)
-            writeFileBinary(string,bytes)
-            openFile(string)
-            readLine(string)(string)
-            writeLine(string,string)
-            closeFile(string)
-            removeFile(string)
-            fsMetadata(string)(FsMetadata)
-            toString(bytes)
-            toString(address)
-            toString(uint256)
-            toString(int256)
-            toString(bytes32)
-            toString(bool)
-            parseBytes(string)(bytes)
-            parseAddress(string)(address)
-            parseUint(string)(uint256)
-            parseInt(string)(int256)
-            parseBytes32(string)(bytes32)
-            parseBool(string)(bool)
-            snapshot()(uint256)
-            revertTo(uint256)(bool)
-            createFork(string,uint256)(uint256)
-            createFork(string,bytes32)(uint256)
-            createFork(string)(uint256)
-            createSelectFork(string,uint256)(uint256)
-            createSelectFork(string,bytes32)(uint256)
-            createSelectFork(string)(uint256)
-            selectFork(uint256)
-            activeFork()(uint256)
-            transact(bytes32)
-            transact(uint256,bytes32)
-            makePersistent(address)
-            makePersistent(address,address)
-            makePersistent(address,address,address)
-            makePersistent(address[])
-            revokePersistent(address)
-            revokePersistent(address[])
-            isPersistent(address)(bool)
-            rollFork(uint256)
-            rollFork(bytes32)
-            rollFork(uint256,uint256)
-            rollFork(uint256,bytes32)
-            rpcUrl(string)(string)
-            rpcUrls()(string[2][])
-            rpcUrlStructs()(Rpc[])
-            parseJson(string)(bytes)
-            parseJson(string, string)(bytes)
-            parseJsonUint(string, string)(uint256)
-            parseJsonUintArray(string, string)(uint256[])
-            parseJsonInt(string, string)(int256)
-            parseJsonIntArray(string, string)(int256[])
-            parseJsonString(string, string)(string)
-            parseJsonStringArray(string, string)(string[])
-            parseJsonAddress(string, string)(address)
-            parseJsonAddressArray(string, string)(address[])
-            parseJsonBool(string, string)(bool)
-            parseJsonBoolArray(string, string)(bool[])
-            parseJsonBytes(string, string)(bytes)
-            parseJsonBytesArray(string, string)(bytes[])
-            parseJsonBytes32(string, string)(bytes32)
-            parseJsonBytes32Array(string, string)(bytes32[])
-            allowCheatcodes(address)
-            serializeBool(string,string,bool)(string)
-            serializeBool(string,string,bool[])(string)
-            serializeUint(string,string,uint256)(string)
-            serializeUint(string,string,uint256[])(string)
-            serializeInt(string,string,int256)(string)
-            serializeInt(string,string,int256[])(string)
-            serializeAddress(string,string,address)(string)
-            serializeAddress(string,string,address[])(string)
-            serializeBytes32(string,string,bytes32)(string)
-            serializeBytes32(string,string,bytes32[])(string)
-            serializeString(string,string,string)(string)
-            serializeString(string,string,string[])(string)
-            serializeBytes(string,string,bytes)(string)
-            serializeBytes(string,string,bytes[])(string)
-            writeJson(string, string)
-            writeJson(string, string, string)
-            pauseGasMetering()
-            resumeGasMetering()
-    ]"#,
+    "[
+        struct Log { bytes32[] topics; bytes data; }
+        struct Rpc { string name; string url; }
+        struct FsMetadata { bool isDir; bool isSymlink; uint256 length; bool readOnly; uint256 modified; uint256 accessed; uint256 created; }
+
+        allowCheatcodes(address)
+
+        ffi(string[])(bytes)
+
+        breakpoint(string)
+
+        roll(uint256)
+        warp(uint256)
+        difficulty(uint256)
+        fee(uint256)
+        coinbase(address)
+        store(address,bytes32,bytes32)
+        load(address,bytes32)(bytes32)
+
+        setEnv(string,string)
+        envBool(string)(bool)
+        envUint(string)(uint256)
+        envInt(string)(int256)
+        envAddress(string)(address)
+        envBytes32(string)(bytes32)
+        envString(string)(string)
+        envBytes(string)(bytes)
+        envBool(string,string)(bool[])
+        envUint(string,string)(uint256[])
+        envInt(string,string)(int256[])
+        envAddress(string,string)(address[])
+        envBytes32(string,string)(bytes32[])
+        envString(string,string)(string[])
+        envBytes(string,string)(bytes[])
+        envOr(string,bool)(bool)
+        envOr(string,uint256)(uint256)
+        envOr(string,int256)(int256)
+        envOr(string,address)(address)
+        envOr(string,bytes32)(bytes32)
+        envOr(string,string)(string)
+        envOr(string,bytes)(bytes)
+        envOr(string,string,bool[])(bool[])
+        envOr(string,string,uint256[])(uint256[])
+        envOr(string,string,int256[])(int256[])
+        envOr(string,string,address[])(address[])
+        envOr(string,string,bytes32[])(bytes32[])
+        envOr(string,string,string[])(string[])
+        envOr(string,string,bytes[])(bytes[])
+
+        addr(uint256)(address)
+        sign(uint256,bytes32)(uint8,bytes32,bytes32)
+        deriveKey(string,uint32)(uint256)
+        deriveKey(string,string,uint32)(uint256)
+        rememberKey(uint256)(address)
+
+        prank(address)
+        prank(address,address)
+        startPrank(address)
+        startPrank(address,address)
+        stopPrank()
+
+        deal(address,uint256)
+        etch(address,bytes)
+        expectRevert()
+        expectRevert(bytes)
+        expectRevert(bytes4)
+        record()
+        accesses(address)(bytes32[],bytes32[])
+
+        recordLogs()
+        getRecordedLogs()(Log[])
+
+        expectEmit()
+        expectEmit(address)
+        expectEmit(bool,bool,bool,bool)
+        expectEmit(bool,bool,bool,bool,address)
+
+        mockCall(address,bytes,bytes)
+        mockCall(address,uint256,bytes,bytes)
+        mockCallRevert(address,bytes,bytes)
+        mockCallRevert(address,uint256,bytes,bytes)
+        clearMockedCalls()
+
+        expectCall(address,bytes)
+        expectCall(address,uint256,bytes)
+        expectCall(address,uint256,uint64,bytes)
+        expectCallMinGas(address,uint256,uint64,bytes)
+        expectSafeMemory(uint64,uint64)
+        expectSafeMemoryCall(uint64,uint64)
+
+        getCode(string)
+        getDeployedCode(string)
+        label(address,string)
+        assume(bool)
+        setNonce(address,uint64)
+        getNonce(address)
+        chainId(uint256)
+        txGasPrice(uint256)
+
+        broadcast()
+        broadcast(address)
+        broadcast(uint256)
+        startBroadcast()
+        startBroadcast(address)
+        startBroadcast(uint256)
+        stopBroadcast()
+
+        projectRoot()(string)
+        readFile(string)(string)
+        readFileBinary(string)(bytes)
+        writeFile(string,string)
+        writeFileBinary(string,bytes)
+        openFile(string)
+        readLine(string)(string)
+        writeLine(string,string)
+        closeFile(string)
+        removeFile(string)
+        fsMetadata(string)(FsMetadata)
+
+        toString(bytes)
+        toString(address)
+        toString(uint256)
+        toString(int256)
+        toString(bytes32)
+        toString(bool)
+        parseBytes(string)(bytes)
+        parseAddress(string)(address)
+        parseUint(string)(uint256)
+        parseInt(string)(int256)
+        parseBytes32(string)(bytes32)
+        parseBool(string)(bool)
+
+        snapshot()(uint256)
+        revertTo(uint256)(bool)
+        createFork(string,uint256)(uint256)
+        createFork(string,bytes32)(uint256)
+        createFork(string)(uint256)
+        createSelectFork(string,uint256)(uint256)
+        createSelectFork(string,bytes32)(uint256)
+        createSelectFork(string)(uint256)
+        selectFork(uint256)
+        activeFork()(uint256)
+        transact(bytes32)
+        transact(uint256,bytes32)
+        makePersistent(address)
+        makePersistent(address,address)
+        makePersistent(address,address,address)
+        makePersistent(address[])
+        revokePersistent(address)
+        revokePersistent(address[])
+        isPersistent(address)(bool)
+        rollFork(uint256)
+        rollFork(bytes32)
+        rollFork(uint256,uint256)
+        rollFork(uint256,bytes32)
+        rpcUrl(string)(string)
+        rpcUrls()(string[2][])
+        rpcUrlStructs()(Rpc[])
+
+        writeJson(string, string)
+        writeJson(string, string, string)
+        parseJson(string)(bytes)
+        parseJson(string, string)(bytes)
+        parseJsonUint(string, string)(uint256)
+        parseJsonUintArray(string, string)(uint256[])
+        parseJsonInt(string, string)(int256)
+        parseJsonIntArray(string, string)(int256[])
+        parseJsonString(string, string)(string)
+        parseJsonStringArray(string, string)(string[])
+        parseJsonAddress(string, string)(address)
+        parseJsonAddressArray(string, string)(address[])
+        parseJsonBool(string, string)(bool)
+        parseJsonBoolArray(string, string)(bool[])
+        parseJsonBytes(string, string)(bytes)
+        parseJsonBytesArray(string, string)(bytes[])
+        parseJsonBytes32(string, string)(bytes32)
+        parseJsonBytes32Array(string, string)(bytes32[])
+        serializeBool(string,string,bool)(string)
+        serializeBool(string,string,bool[])(string)
+        serializeUint(string,string,uint256)(string)
+        serializeUint(string,string,uint256[])(string)
+        serializeInt(string,string,int256)(string)
+        serializeInt(string,string,int256[])(string)
+        serializeAddress(string,string,address)(string)
+        serializeAddress(string,string,address[])(string)
+        serializeBytes32(string,string,bytes32)(string)
+        serializeBytes32(string,string,bytes32[])(string)
+        serializeString(string,string,string)(string)
+        serializeString(string,string,string[])(string)
+        serializeBytes(string,string,bytes)(string)
+        serializeBytes(string,string,bytes[])(string)
+
+        pauseGasMetering()
+        resumeGasMetering()
+    ]",
 );
 pub use hevm::{HEVMCalls, HEVM_ABI};
 
@@ -203,51 +225,51 @@ pub static HARDHAT_CONSOLE_ADDRESS: Address = H160([
 
 // Bindings for DS-style event logs. Note that the array logs below are not actually part of DSTest,
 // but are part of forge-std, so are included here to ensure they are decoded in output logs.
-ethers::contract::abigen!(
+abigen!(
     Console,
     r#"[
-            event log(string)
-            event logs                   (bytes)
-            event log_address            (address)
-            event log_bytes32            (bytes32)
-            event log_int                (int)
-            event log_uint               (uint)
-            event log_bytes              (bytes)
-            event log_string             (string)
-            event log_array              (uint256[] val)
-            event log_array              (int256[] val)
-            event log_array              (address[] val)
-            event log_named_address      (string key, address val)
-            event log_named_bytes32      (string key, bytes32 val)
-            event log_named_decimal_int  (string key, int val, uint decimals)
-            event log_named_decimal_uint (string key, uint val, uint decimals)
-            event log_named_int          (string key, int val)
-            event log_named_uint         (string key, uint val)
-            event log_named_bytes        (string key, bytes val)
-            event log_named_string       (string key, string val)
-            event log_named_array        (string key, uint256[] val)
-            event log_named_array        (string key, int256[] val)
-            event log_named_array        (string key, address[] val)
+        event log(string)
+        event logs                   (bytes)
+        event log_address            (address)
+        event log_bytes32            (bytes32)
+        event log_int                (int)
+        event log_uint               (uint)
+        event log_bytes              (bytes)
+        event log_string             (string)
+        event log_array              (uint256[] val)
+        event log_array              (int256[] val)
+        event log_array              (address[] val)
+        event log_named_address      (string key, address val)
+        event log_named_bytes32      (string key, bytes32 val)
+        event log_named_decimal_int  (string key, int val, uint decimals)
+        event log_named_decimal_uint (string key, uint val, uint decimals)
+        event log_named_int          (string key, int val)
+        event log_named_uint         (string key, uint val)
+        event log_named_bytes        (string key, bytes val)
+        event log_named_string       (string key, string val)
+        event log_named_array        (string key, uint256[] val)
+        event log_named_array        (string key, int256[] val)
+        event log_named_array        (string key, address[] val)
     ]"#,
 );
 pub use console::{ConsoleEvents, CONSOLE_ABI};
 
 // Bindings for Hardhat console
-ethers::contract::abigen!(HardhatConsole, "./abi/console.json", event_derives (foundry_macros::ConsoleFmt););
+abigen!(HardhatConsole, "./abi/console.json", event_derives(foundry_macros::ConsoleFmt));
 pub use hardhat_console::HARDHATCONSOLE_ABI as HARDHAT_CONSOLE_ABI;
 
 /// If the input starts with a known `hardhat/console.log` `uint` selector, then this will replace
 /// it with the selector `abigen!` bindings expect.
-pub fn patch_hardhat_console_selector(mut input: Vec<u8>) -> Vec<u8> {
+pub fn patch_hardhat_console_selector(input: &mut [u8]) {
     if input.len() < 4 {
-        return input
+        return
     }
 
-    let selector = Selector::try_from(&input[..4]).unwrap();
-    if let Some(abigen_selector) = HARDHAT_CONSOLE_SELECTOR_PATCHES.get(&selector) {
-        input.splice(..4, *abigen_selector);
+    // SAFETY: length checked above; see [<[T]>::split_array_mut].
+    let selector = unsafe { &mut *(input.get_unchecked_mut(..4) as *mut [u8] as *mut [u8; 4]) };
+    if let Some(abigen_selector) = HARDHAT_CONSOLE_SELECTOR_PATCHES.get(selector) {
+        *selector = *abigen_selector;
     }
-    input
 }
 
 /// This contains a map with all the  `hardhat/console.log` log selectors that use `uint` or `int`
@@ -777,8 +799,9 @@ mod tests {
     #[test]
     fn hardhat_console_path_works() {
         for (hh, abigen) in HARDHAT_CONSOLE_SELECTOR_PATCHES.iter() {
-            let patched = patch_hardhat_console_selector(hh.to_vec());
-            assert_eq!(abigen.to_vec(), patched);
+            let mut hh = *hh;
+            patch_hardhat_console_selector(&mut hh);
+            assert_eq!(*abigen, hh);
         }
     }
 }
