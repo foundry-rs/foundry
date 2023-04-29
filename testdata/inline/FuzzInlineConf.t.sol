2119106004ff09df080d9fc24ab6a6bfdaf7554d
Andrea Simeoni <andreasimeoni84@gmail.com>
2023-04-29 09:45:52 +0200

    Added test for block comments

--- testdata/inline/FuzzInlineConf.t.sol
+++ testdata/inline/FuzzInlineConf.t.sol
@@ -4,8 +4,10 @@ pragma solidity >=0.8.0;
 import "ds-test/test.sol";
 
 contract FuzzInlineConf is DSTest {
-    /// forge-config: default.fuzz.runs = 1024
-    /// forge-config: default.fuzz.max-test-rejects = 500
+    /**
+     * forge-config: default.fuzz.runs = 1024
+     * forge-config: default.fuzz.max-test-rejects = 500
+     */
     function testInlineConfFuzz(uint8 x) public {
         require(true, "this is not going to revert");
     }
