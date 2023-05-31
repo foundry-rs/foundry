33d90c95ecc7b818950ceda19964cb74a97b22ad
Enrique Ortiz <hi@enriqueortiz.dev>
2023-05-31 13:19:10 -0400

    chore: override evm version and set default to paris

--- config/src/lib.rs
+++ config/src/lib.rs
@@ -1723,7 +1723,7 @@ impl Default for Config {
             allow_paths: vec![],
             include_paths: vec![],
             force: false,
-            evm_version: Default::default(),
+            evm_version: EvmVersion::Paris,
             gas_reports: vec!["*".to_string()],
             gas_reports_ignore: vec![],
             solc: None,
