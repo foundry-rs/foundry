cea78d4f75374d732b65ff46759ebaf57d421c59
Matthias Seitz <matthias.seitz@outlook.de>
2023-06-09 16:40:37 +0200

    fix(anvil): disable basefee if manually set to 0

--- anvil/src/eth/backend/mem/mod.rs
+++ anvil/src/eth/backend/mem/mod.rs
@@ -781,7 +781,14 @@ impl Backend {
         let (outcome, header, block_hash) = {
             let current_base_fee = self.base_fee();
 
-            let mut env = self.env.read().clone();
+            let mut env = self.env().read().clone();
+
+            if env.block.basefee == revm::primitives::U256::ZERO {
+                // this is an edge case because the evm fails if `tx.effective_gas_price < base_fee`
+                // 0 is only possible if it's manually set
+                env.cfg.disable_base_fee = true;
+            }
+
             // increase block number for this block
             env.block.number = env.block.number.saturating_add(rU256::from(1));
             env.block.basefee = current_base_fee.into();
