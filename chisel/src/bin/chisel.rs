7f95ecffc83d0b472e41cfb243a6b1c399d86ebf
Arsenii Kulikov <klkvrr@gmail.com>
2023-06-29 17:26:59 +0400

    add dotenv reading to chisel

--- chisel/src/bin/chisel.rs
+++ chisel/src/bin/chisel.rs
@@ -9,6 +9,7 @@ use chisel::{
 };
 use clap::Parser;
 use foundry_cli::cmd::{forge::build::BuildArgs, LoadConfig};
+use foundry_cli::{utils};
 use foundry_common::evm::EvmArgs;
 use foundry_config::{
     figment::{
@@ -75,6 +76,8 @@ async fn main() -> eyre::Result<()> {
     if !Paint::enable_windows_ascii() {
         Paint::disable()
     }
+
+    utils::load_dotenv();
 
     // Parse command args
     let args = ChiselParser::parse();
