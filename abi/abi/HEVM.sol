7079022b21b1d942c0398795ce3557a8833ebe99
Trevor Johnson <27569194+trevorgjohnson@users.noreply.github.com>
2023-07-07 11:03:38 -0500

    chore: add Wallet struct and other relevant functions to 'HEVM.sol'

--- abi/abi/HEVM.sol
+++ abi/abi/HEVM.sol
@@ -2,6 +2,7 @@ struct Log { bytes32[] topics; bytes data; }
 struct Rpc { string name; string url; }
 struct DirEntry { string errorMessage; string path; uint64 depth; bool isDir; bool isSymlink; }
 struct FsMetadata { bool isDir; bool isSymlink; uint256 length; bool readOnly; uint256 modified; uint256 accessed; uint256 created; }
+struct Wallet { address addr; uint256 publicKeyX; uint256 publicKeyY; uint256 privateKey; }
 
 allowCheatcodes(address)
 
@@ -56,6 +57,12 @@ deriveKey(string,string,uint32)(uint256)
 deriveKey(string,uint32,string)(uint256)
 deriveKey(string,string,uint32,string)(uint256)
 rememberKey(uint256)(address)
+
+createWallet(string)(Wallet)
+createWallet(uint256)(Wallet)
+createWallet(uint256,string)(Wallet)
+sign(Wallet,bytes32)(uint8,bytes32,bytes32)
+getNonce(Wallet)(uint64)
 
 prank(address)
 prank(address,address)
