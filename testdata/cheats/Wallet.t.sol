6e3fe1df1c40c704123bed3ebb440acfca41dd05
Trevor Johnson <27569194+trevorgjohnson@users.noreply.github.com>
2023-07-07 11:26:09 -0500

    test: add tests for getNonce/sign in Wallet.t.sol

--- testdata/cheats/Wallet.t.sol
+++ testdata/cheats/Wallet.t.sol
@@ -4,6 +4,8 @@ pragma solidity 0.8.18;
 import "ds-test/test.sol";
 import "./Cheats.sol";
 
+contract Foo {}
+
 contract WalletTest is DSTest {
     Cheats constant cheats = Cheats(HEVM_ADDRESS);
 
@@ -38,5 +40,36 @@ contract WalletTest is DSTest {
 
         string memory expectedLabel = cheats.getLabel(wallet.addr);
         assertEq(expectedLabel, label, "labelled address != wallet.addr");
+    }
+
+    function testSignWithWalletDigest(uint248 pk, bytes32 digest) public {
+        cheats.assume(pk != 0);
+        Cheats.Wallet memory wallet = cheats.createWallet(uint256(pk));
+
+        (uint8 v, bytes32 r, bytes32 s) = cheats.sign(wallet, digest);
+
+        address recovered = ecrecover(digest, v, r, s);
+        assertEq(recovered, wallet.addr);
+    }
+
+    function testSignWithWalletMessage(uint248 pk, bytes memory message)
+        public
+    {
+        testSignWithWalletDigest(pk, keccak256(message));
+    }
+
+    function testGetNonceWallet(uint248 pk) public {
+        cheats.assume(pk != 0);
+        Cheats.Wallet memory wallet = cheats.createWallet(uint256(pk));
+
+        uint64 nonce1 = cheats.getNonce(wallet);
+
+        cheats.startPrank(wallet.addr);
+        new Foo();
+        new Foo();
+        cheats.stopPrank();
+
+        uint64 nonce2 = cheats.getNonce(wallet);
+        assertEq(nonce1 + 2, nonce2);
     }
 }
