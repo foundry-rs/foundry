504b9303124c57cb315cb8e3a04702cbf0f93711
Enrique Ortiz <hi@enriqueortiz.dev>
2023-07-12 16:18:08 -0400

    Revert "feat: V1"

--- cli/tests/it/integration.rs
+++ cli/tests/it/integration.rs
@@ -1,11 +1,9 @@
 use foundry_cli_test_utils::{forgetest_external, util::setup_forge_remote};
 
 forgetest_external!(solmate, "transmissions11/solmate");
-// Commenting until PRB migration.
-// forgetest_external!(prb_math, "PaulRBerg/prb-math");
+forgetest_external!(prb_math, "PaulRBerg/prb-math");
 forgetest_external!(prb_proxy, "PaulRBerg/prb-proxy");
-// Commenting until solady PR merged
-// forgetest_external!(solady, "Vectorized/solady");
+forgetest_external!(solady, "Vectorized/solady");
 forgetest_external!(
     geb,
     "reflexer-labs/geb",
