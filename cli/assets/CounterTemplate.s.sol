091414cecabc055fc5c6a75491414e0feb7c5a33
Prince Allwin <princeallwin199@gmail.com>
2023-07-25 20:46:52 +0530

    Add named imports in script
    Co-authored-by: evalir <hi@enriqueortiz.dev>

--- cli/assets/CounterTemplate.s.sol
+++ cli/assets/CounterTemplate.s.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Script} "forge-std/Script.sol";
+import {Script} from "forge-std/Script.sol";
 
 contract CounterScript is Script {
     function setUp() public {}
