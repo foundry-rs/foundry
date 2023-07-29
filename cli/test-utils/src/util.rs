8d41b1b90fdb65d1a044eb5a5cda353e9d5cd5a4
sam bacha <sam@manifoldfinance.com>
2023-07-29 06:17:47 -0700

    chore(update constants): increment solc versions for install_commonly… (#5504)
    * chore(update constants): increment solc versions for install_commonly_used_solc
    
    function `install_commonly_used_solc` pre-installs commonly used solc versions
    
    increment based of latest solc version update
    
    * rename vars
    
    ---------
    
    Co-authored-by: Matthias Seitz <matthias.seitz@outlook.de>

--- cli/test-utils/src/util.rs
+++ cli/test-utils/src/util.rs
@@ -191,9 +191,9 @@ pub fn setup_cast_project(test: TestProject) -> (TestProject, TestCommand) {
 fn install_commonly_used_solc() {
     let mut is_preinstalled = PRE_INSTALL_SOLC_LOCK.lock();
     if !*is_preinstalled {
-        let v0_8_18 = std::thread::spawn(|| Solc::blocking_install(&"0.8.18".parse().unwrap()));
         let v0_8_19 = std::thread::spawn(|| Solc::blocking_install(&"0.8.19".parse().unwrap()));
         let v0_8_20 = std::thread::spawn(|| Solc::blocking_install(&"0.8.20".parse().unwrap()));
+        let v0_8_21 = std::thread::spawn(|| Solc::blocking_install(&"0.8.21".parse().unwrap()));
 
         let wait = |res: std::thread::JoinHandle<_>| -> Result<(), ()> {
             if let Err(err) = res.join().unwrap() {
@@ -208,7 +208,7 @@ fn install_commonly_used_solc() {
         };
 
         // only set to installed if succeeded
-        *is_preinstalled = wait(v0_8_18).and(wait(v0_8_19)).and(wait(v0_8_20)).is_ok();
+        *is_preinstalled = wait(v0_8_19).and(wait(v0_8_20)).and(wait(v0_8_21)).is_ok();
     }
 }
 
