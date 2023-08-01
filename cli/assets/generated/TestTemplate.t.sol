d7ab547611d3766ce09479e94f88b59d9d731b50
Rahul Ravindran <ravindranrahul@users.noreply.github.com>
2023-08-01 09:56:02 +0530

    refactor: named imports

--- cli/assets/generated/TestTemplate.t.sol
+++ cli/assets/generated/TestTemplate.t.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
-import "../src/{contract_name}.sol"; 
+import {Test, console2} from "forge-std/Test.sol";
+import {{contract_name}} from "../src/{contract_name}.sol"; 
 
 contract {contract_name}Test is Test {
     {contract_name} public {instance_name};
