65b513de27ef23f4b4c4574e7f40f739a8fd9edb
Matthew Alexander <alexandermatthew12@gmail.com>
2023-08-03 22:41:41 +0800

    chore: removed `is_eip1559` boolean flag (#5534)
    * chore: removed is_eip1559 flag
    
    * chore: set eip1559 fields to "None" if not EIP1559 type

--- anvil/src/eth/backend/mem/mod.rs
+++ anvil/src/eth/backend/mem/mod.rs
@@ -1364,7 +1364,7 @@ impl Backend {
             let info = storage.transactions.get(&hash)?.info.clone();
             let tx = block.transactions.get(info.transaction_index as usize)?.clone();
 
-            let tx = transaction_build(Some(hash), tx, Some(block), Some(info), true, base_fee);
+            let tx = transaction_build(Some(hash), tx, Some(block), Some(info), base_fee);
             transactions.push(tx);
         }
         Some(transactions)
@@ -1982,7 +1982,6 @@ impl Backend {
             tx,
             Some(&block),
             Some(info),
-            true,
             block.header.base_fee_per_gas,
         ))
     }
@@ -2018,7 +2017,6 @@ impl Backend {
             tx,
             Some(&block),
             Some(info),
-            true,
             block.header.base_fee_per_gas,
         ))
     }
@@ -2245,7 +2243,6 @@ pub fn transaction_build(
     eth_transaction: MaybeImpersonatedTransaction,
     block: Option<&Block>,
     info: Option<TransactionInfo>,
-    is_eip1559: bool,
     base_fee: Option<U256>,
 ) -> Transaction {
     let mut transaction: Transaction = eth_transaction.clone().into();
@@ -2264,7 +2261,7 @@ pub fn transaction_build(
                 base_fee.checked_add(max_priority_fee_per_gas).unwrap_or_else(U256::max_value),
             );
         }
-    } else if !is_eip1559 {
+    } else {
         transaction.max_fee_per_gas = None;
         transaction.max_priority_fee_per_gas = None;
         transaction.transaction_type = None;
