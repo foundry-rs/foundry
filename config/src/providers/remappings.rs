767f8f8757ac9e50ed8a3cb918cd2324a1e43107
Enrique Ortiz <hi@enriqueortiz.dev>
2023-08-04 15:27:26 -0400

    chore: only dedupe from existing, not both ways

--- config/src/providers/remappings.rs
+++ config/src/providers/remappings.rs
@@ -33,7 +33,7 @@ impl Remappings {
     /// Push an element ot the remappings vector, but only if it's not already present.
     pub fn push(&mut self, remapping: Remapping) {
         if self.remappings.iter().any(|existing| {
-            existing.name.contains(&remapping.name) || remapping.name.contains(&existing.name)
+            existing.name.contains(&remapping.name)
         }) {
         } else {
             self.remappings.push(remapping)
