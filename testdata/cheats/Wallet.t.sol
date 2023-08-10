f4e105df3104591c626918d4d37dfef9964a774c
Trevor Johnson <27569194+trevorgjohnson@users.noreply.github.com>
2023-08-10 10:12:59 -0500

    Add 'bound' func, remove 'vm.assume(pk != 0)'

--- testdata/cheats/Wallet.t.sol
+++ testdata/cheats/Wallet.t.sol
@@ -9,11 +9,39 @@ contract Foo {}
 contract WalletTest is DSTest {
     Vm constant vm = Vm(HEVM_ADDRESS);
 
-    uint internal constant Q =
-        0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141;
+    uint256 internal constant Q = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141;
+    uint256 private constant UINT256_MAX =
+        115792089237316195423570985008687907853269984665640564039457584007913129639935;
 
     function addressOf(uint256 x, uint256 y) internal pure returns (address) {
         return address(uint160(uint256(keccak256(abi.encode(x, y)))));
+    }
+
+    function bound(uint256 x, uint256 min, uint256 max) internal pure virtual returns (uint256 result) {
+        require(min <= max, "min needs to be less than max");
+        // If x is between min and max, return x directly. This is to ensure that dictionary values
+        // do not get shifted if the min is nonzero. More info: https://github.com/foundry-rs/forge-std/issues/188
+        if (x >= min && x <= max) return x;
+
+        uint256 size = max - min + 1;
+
+        // If the value is 0, 1, 2, 3, wrap that to min, min+1, min+2, min+3. Similarly for the UINT256_MAX side.
+        // This helps ensure coverage of the min/max values.
+        if (x <= 3 && size > x) return min + x;
+        if (x >= UINT256_MAX - 3 && size > UINT256_MAX - x) return max - (UINT256_MAX - x);
+
+        // Otherwise, wrap x into the range [min, max], i.e. the range is inclusive.
+        if (x > max) {
+            uint256 diff = x - max;
+            uint256 rem = diff % size;
+            if (rem == 0) return max;
+            result = min + rem - 1;
+        } else if (x < min) {
+            uint256 diff = min - x;
+            uint256 rem = diff % size;
+            if (rem == 0) return min;
+            result = max - rem + 1;
+        }
     }
 
     function testCreateWalletStringPrivAndLabel() public {
@@ -33,7 +61,6 @@ contract WalletTest is DSTest {
     }
 
     function testCreateWalletPrivKeyNoLabel(uint256 pkSeed) public {
-        vm.assume(pkSeed != 0);
         uint256 pk = bound(pkSeed, 1, Q - 1);
 
         Vm.Wallet memory wallet = vm.createWallet(pk);
@@ -50,7 +77,6 @@ contract WalletTest is DSTest {
     function testCreateWalletPrivKeyWithLabel(uint256 pkSeed) public {
         string memory label = "labelled wallet";
 
-        vm.assume(pkSeed != 0);
         uint256 pk = bound(pkSeed, 1, Q - 1);
 
         Vm.Wallet memory wallet = vm.createWallet(pk, label);
@@ -68,7 +94,6 @@ contract WalletTest is DSTest {
     }
 
     function testSignWithWalletDigest(uint256 pkSeed, bytes32 digest) public {
-        vm.assume(pkSeed != 0);
         uint256 pk = bound(pkSeed, 1, Q - 1);
 
         Vm.Wallet memory wallet = vm.createWallet(pk);
@@ -79,15 +104,11 @@ contract WalletTest is DSTest {
         assertEq(recovered, wallet.addr);
     }
 
-    function testSignWithWalletMessage(
-        uint256 pkSeed,
-        bytes memory message
-    ) public {
-        testSignWithWalletDigest(pk, keccak256(message));
+    function testSignWithWalletMessage(uint256 pkSeed, bytes memory message) public {
+        testSignWithWalletDigest(pkSeed, keccak256(message));
     }
 
-    function testGetNonceWallet(uint256 pk) public {
-        vm.assume(pkSeed != 0);
+    function testGetNonceWallet(uint256 pkSeed) public {
         uint256 pk = bound(pkSeed, 1, Q - 1);
 
         Vm.Wallet memory wallet = vm.createWallet(pk);
