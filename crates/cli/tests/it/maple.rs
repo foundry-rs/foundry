8e6d029164bccb0d7dd834e82d81723ce5e49a61
Enrique Ortiz <hi@enriqueortiz.dev>
2023-08-15 17:24:59 -0400

    docs

--- crates/cli/tests/it/maple.rs
+++ crates/cli/tests/it/maple.rs
@@ -1,3 +1,6 @@
 use foundry_cli_test_utils::forgetest_external;
 
+// Runs an integration test for maple.
+// This is its own test because its extremely heavy, and as a result is not included
+// in the normal run.
 forgetest_external!(maple, "maple-labs/maple-core-v2");
