fe1c1fc0f4f73a45aeb2e889f96808c413aae1d3
evalir <hi@enriqueortiz.dev>
2023-08-18 12:05:32 -0400

    fix(tests): only run heavy integration tests by tightening filters (#5657)

--- crates/forge/tests/cli/heavy_integration.rs
+++ crates/forge/tests/cli/heavy_integration.rs
@@ -1,5 +1,6 @@
 //! Heavy integration tests that can take an hour to run or more.
+//! All tests are prefixed with heavy so they can be filtered by nextest.
 
 use foundry_test_utils::forgetest_external;
 
-forgetest_external!(maple, "maple-labs/maple-core-v2");
+forgetest_external!(heavy_maple, "maple-labs/maple-core-v2");
