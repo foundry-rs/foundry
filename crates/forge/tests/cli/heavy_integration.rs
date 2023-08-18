55b02c61479983a4f54fa8a258b4605283a87d18
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2023-08-18 19:44:26 +0200

    ci: improve test CI

--- crates/forge/tests/cli/heavy_integration.rs
+++ crates/forge/tests/cli/heavy_integration.rs
@@ -1,6 +1,5 @@
 //! Heavy integration tests that can take an hour to run or more.
-//! All tests are prefixed with heavy so they can be filtered by nextest.
 
 use foundry_test_utils::forgetest_external;
 
-forgetest_external!(heavy_maple, "maple-labs/maple-core-v2");
+forgetest_external!(maple, "maple-labs/maple-core-v2");
