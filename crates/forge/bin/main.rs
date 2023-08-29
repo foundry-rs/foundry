aaca7d34ff3896174f4b9830c8a17d22ef066129
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2023-08-29 18:44:59 +0200

    perf: use `jemalloc` as the global allocator on unix (#5690)

--- crates/forge/bin/main.rs
+++ crates/forge/bin/main.rs
@@ -9,6 +9,10 @@ mod opts;
 use cmd::{cache::CacheSubcommands, generate::GenerateSubcommands, watch};
 use opts::{Opts, Subcommands};
 
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static ALLOC: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+
 fn main() -> Result<()> {
     utils::load_dotenv();
     handler::install()?;
