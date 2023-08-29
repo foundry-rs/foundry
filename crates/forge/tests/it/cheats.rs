16245ce476445c651d4eb699aada0f24b351b3a7
Enrique Ortiz <hi@enriqueortiz.dev>
2023-08-28 22:22:56 -0700

    chore: use proper filter

--- crates/forge/tests/it/cheats.rs
+++ crates/forge/tests/it/cheats.rs
@@ -1,19 +1,24 @@
 //! forge tests for cheat codes
 
+use foundry_config::{Config, FsPermissions, fs_permissions::PathPermission};
+
 use crate::{
     config::*,
-    test_helpers::{filter::Filter, RE_PATH_SEPARATOR},
+    test_helpers::{filter::Filter, RE_PATH_SEPARATOR, PROJECT},
 };
 
 /// Executes all cheat code tests but not fork cheat codes
 #[tokio::test(flavor = "multi_thread")]
 async fn test_cheats_local() {
+    let mut config = Config::with_root(PROJECT.root());
+    config.fs_permissions = FsPermissions::new(vec![PathPermission::read_write("./")]);
+    let runner = runner_with_config(config);
     let filter =
-        Filter::new(".*", "Skip*", &format!(".*cheats{RE_PATH_SEPARATOR}*")).exclude_paths("Fork");
+        Filter::new(".*", ".*", &format!(".*cheats{RE_PATH_SEPARATOR}*")).exclude_paths("Fork");
 
     // on windows exclude ffi tests since no echo and file test that expect a certain file path
     #[cfg(windows)]
     let filter = filter.exclude_tests("(Ffi|File|Line|Root)");
 
-    TestConfig::filter(filter).await.run().await;
+    TestConfig::with_filter(runner.await, filter).run().await;
 }
