6396305a6d218d26b2f7800db222364cdc6a17d0
ruvaag <ruvaag@gmail.com>
2023-09-06 04:56:08 +0530

    fix: failing doctests

--- crates/cast/src/lib.rs
+++ crates/cast/src/lib.rs
@@ -1835,9 +1835,9 @@ impl SimpleCast {
     ///
     /// fn main() -> eyre::Result<()> {
     ///     let tx = "0x02f8f582a86a82058d8459682f008508351050808303fd84948e42f2f4101563bf679975178e880fd87d3efd4e80b884659ac74b00000000000000000000000080f0c1c49891dcfdd40b6e0f960f84e6042bcb6f000000000000000000000000b97ef9ef8734c71904d8002f8b6bc66dd9c48a6e00000000000000000000000000000000000000000000000000000000007ff4e20000000000000000000000000000000000000000000000000000000000000064c001a05d429597befe2835396206781b199122f2e8297327ed4a05483339e7a8b2022aa04c23a7f70fb29dda1b4ee342fb10a625e9b8ddc6a603fb4e170d4f6f37700cb8";
-    ///     let (tx, sig) = Cast::decode_raw_transaction(&tx);
-    ///     println!("{:?}", tx);
-    ///     println!("{:?}", sig);
+    ///     let (tx, sig) = Cast::decode_raw_transaction(&tx)?;
+    ///
+    ///     Ok(())
     /// }
     pub fn decode_raw_transaction(tx: &str) -> Result<(TypedTransaction, Signature)> {
         let tx_hex = hex::decode(strip_0x(tx))?;
