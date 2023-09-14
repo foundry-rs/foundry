a4f39a09f9d752119ecc318ecc38521a22e16cc6
bernard-wagner <bernard-wagner@users.noreply.github.com>
2023-09-14 12:08:09 +0200

    temp: run windows action

--- .github/scripts/matrices.py
+++ .github/scripts/matrices.py
@@ -45,7 +45,10 @@ class Expanded:
 
 default_target = Target("ubuntu-latest", "x86_64-unknown-linux-gnu")
 if os.environ.get("EVENT_NAME") == "pull_request":
-    targets = [default_target]
+    targets = [
+        default_target,
+        Target("windows-latest", "x86_64-pc-windows-msvc"),
+    ]
 else:
     targets = [
         default_target,
