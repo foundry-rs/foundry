d8bf70a6af60f9dfbfc59c8c61d159b89d943b00
Perelyn <64838956+Perelyn-sama@users.noreply.github.com>
2023-09-24 03:00:39 +0100

    add report-file and test

--- crates/forge/tests/cli/coverage.rs
+++ crates/forge/tests/cli/coverage.rs
@@ -4,3 +4,8 @@ forgetest!(basic_coverage, |_prj: TestProject, mut cmd: TestCommand| {
     cmd.args(["coverage"]);
     cmd.assert_success();
 });
+
+forgetest!(report_file_coverage, |_prj: TestProject, mut cmd: TestCommand| {
+    cmd.arg("coverage").args(["--report".to_string(), "lcov".to_string(), "--report-file".to_string(), "/path/to/lcov.info".to_string()]);
+    cmd.assert_success();
+});
