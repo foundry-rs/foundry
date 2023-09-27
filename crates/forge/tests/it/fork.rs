3e7d9ab07a27c0a11fd309f6ef166c7e1091cc8a
Enrique Ortiz <hi@enriqueortiz.dev>
2023-09-27 02:06:21 -0400

    chore: fixture

--- crates/forge/tests/it/fork.rs
+++ crates/forge/tests/it/fork.rs
@@ -28,7 +28,7 @@ async fn test_cheats_fork_revert() {
         for (_, result) in test_results {
             assert_eq!(
                 result.reason.unwrap(),
-                "Contract 0x5615dEB798BB3E4dFa0139dFa1b3D433Cc23b72f does not exist on active fork with id `1`\n        But exists on non active forks: `[0]`"
+                "Contract 0x5615deb798bb3e4dfa0139dfa1b3d433cc23b72f does not exist on active fork with id `1`\n        But exists on non active forks: `[0]`"
             );
         }
     }
