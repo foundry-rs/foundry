fec647720ad0e99ee3cee4c86aa156cc24cd34c6
vignesh <vigneshwar.sigamani@gmail.com>
2023-10-03 21:23:29 +0530

    Support all ether units in to-unit #5962

--- crates/cast/src/lib.rs
+++ crates/cast/src/lib.rs
@@ -1076,7 +1076,7 @@ impl SimpleCast {
     pub fn to_ascii(hex: &str) -> Result<String> {
         let bytes = hex::decode(hex)?;
         if !bytes.iter().all(u8::is_ascii) {
-            return Err(eyre::eyre!("Invalid ASCII bytes"))
+            return Err(eyre::eyre!("Invalid ASCII bytes"));
         }
         Ok(String::from_utf8(bytes).unwrap())
     }
@@ -1271,9 +1271,21 @@ impl SimpleCast {
             "eth" | "ether" => ethers_core::utils::format_units(value, 18)?
                 .trim_end_matches(".000000000000000000")
                 .to_string(),
+            "milli" | "milliether" => ethers_core::utils::format_units(value, 15)?
+                .trim_end_matchers(".000000000000000")
+                .to_string(),
+            "micro" | "microether" => ethers_core::utils::format_units(value, 12)?
+                .trim_end_matches(".000000000000")
+                .to_string(),
             "gwei" | "nano" | "nanoether" => ethers_core::utils::format_units(value, 9)?
                 .trim_end_matches(".000000000")
                 .to_string(),
+            "mwei" | "mega" | "megaether" => {
+                ethers_core::utils::format_units(value, 6)?.trim_end_matches(".000000").to_string()
+            }
+            "kwei" | "kilo" | "kiloether" => {
+                ethers_core::utils::format_units(value, 3)?.trim_end_matches(".000").to_string()
+            }
             "wei" => ethers_core::utils::format_units(value, 0)?.trim_end_matches(".0").to_string(),
             _ => eyre::bail!("invalid unit: \"{}\"", unit),
         })
@@ -1398,7 +1410,7 @@ impl SimpleCast {
         let base_in = Base::unwrap_or_detect(base_in, value)?;
         let base_out: Base = base_out.parse()?;
         if base_in == base_out {
-            return Ok(value.to_string())
+            return Ok(value.to_string());
         }
 
         let mut n = NumberWithBase::parse_int(value, Some(base_in.to_string()))?;
@@ -1467,7 +1479,7 @@ impl SimpleCast {
         let s = if let Some(stripped) = s.strip_prefix("000000000000000000000000") {
             stripped
         } else {
-            return Err(eyre::eyre!("Not convertible to address, there are non-zero bytes"))
+            return Err(eyre::eyre!("Not convertible to address, there are non-zero bytes"));
         };
 
         let lowercase_address_string = format!("0x{s}");
@@ -1933,7 +1945,7 @@ impl SimpleCast {
         }
         if optimize == 0 {
             let selector = HumanReadableParser::parse_function(signature)?.short_signature();
-            return Ok((hex::encode_prefixed(selector), String::from(signature)))
+            return Ok((hex::encode_prefixed(selector), String::from(signature)));
         }
         let Some((name, params)) = signature.split_once('(') else {
             eyre::bail!("Invalid signature");
@@ -1955,7 +1967,7 @@ impl SimpleCast {
 
                     if selector.iter().take_while(|&&byte| byte == 0).count() == optimize {
                         found.store(true, Ordering::Relaxed);
-                        return Some((nonce, hex::encode_prefixed(selector), input))
+                        return Some((nonce, hex::encode_prefixed(selector), input));
                     }
 
                     nonce += nonce_step;
