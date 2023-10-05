87283bc9f5657eed126ecb2d2370a471ff409bb7
Perelyn <64838956+Perelyn-sama@users.noreply.github.com>
2023-10-05 23:23:11 +0100

    added report-file option and new coverage test (#5888)
    * add report-file and test
    
    * chore: clean code by removing unused comments
    
    * fmt
    
    * add corrections
    
    * fmt
    
    ---------
    
    Co-authored-by: Enrique Ortiz <hi@enriqueortiz.dev>

--- crates/forge/tests/cli/coverage.rs
+++ crates/forge/tests/cli/coverage.rs
@@ -4,3 +4,13 @@ forgetest!(basic_coverage, |_prj: TestProject, mut cmd: TestCommand| {
     cmd.args(["coverage"]);
     cmd.assert_success();
 });
+
+forgetest!(report_file_coverage, |prj: TestProject, mut cmd: TestCommand| {
+    cmd.arg("coverage").args([
+        "--report".to_string(),
+        "lcov".to_string(),
+        "--report-file".to_string(),
+        prj.root().join("lcov.info").to_str().unwrap().to_string(),
+    ]);
+    cmd.assert_success();
+});
