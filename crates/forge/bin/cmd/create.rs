84329705a8f2da8f3feefa8ba742d79b56aef4f6
Matthias Seitz <matthias.seitz@outlook.de>
2023-10-07 10:41:58 +0200

    add sanity test

--- crates/forge/bin/cmd/create.rs
+++ crates/forge/bin/cmd/create.rs
@@ -118,8 +118,8 @@ impl CreateArgs {
         };
 
         // respect chain, if set explicitly via cmd args
-        let chain_id = if let Some(chain) = self.eth.etherscan.chain {
-            chain.id()
+        let chain_id = if let Some(chain_id) = self.chain_id() {
+            chain_id
         } else {
             provider.get_chainid().await?.as_u64()
         };
@@ -134,6 +134,11 @@ impl CreateArgs {
             let provider = provider.with_signer(signer);
             self.deploy(abi, bin, params, provider, chain_id).await
         }
+    }
+
+    /// Returns the provided chain id, if any.
+    fn chain_id(&self) -> Option<u64> {
+        self.eth.etherscan.chain.map(|chain| chain.id())
     }
 
     /// Ensures the verify command can be executed.
@@ -349,5 +354,20 @@ mod tests {
         ]);
         assert_eq!(args.retry.retries, 10);
         assert_eq!(args.retry.delay, 30);
+    }
+    #[test]
+    fn can_parse_chain_id() {
+        let args: CreateArgs = CreateArgs::parse_from([
+            "foundry-cli",
+            "src/Domains.sol:Domains",
+            "--verify",
+            "--retries",
+            "10",
+            "--delay",
+            "30",
+            "--chain-id",
+            "9999",
+        ]);
+        assert_eq!(args.chain_id(), Some(9999));
     }
 }
