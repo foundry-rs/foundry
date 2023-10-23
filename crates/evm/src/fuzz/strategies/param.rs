403de57f682aab8f6e8fe1af756dab5b1026b043
Enrique Ortiz <hi@enriqueortiz.dev>
2023-10-23 18:45:12 +0900

    chore: use proptest traits for fuzzer

--- crates/evm/src/fuzz/strategies/param.rs
+++ crates/evm/src/fuzz/strategies/param.rs
@@ -25,83 +25,34 @@ pub fn fuzz_param_from_state(
 
     // Select a value from the state
     let st = arc_state.clone();
-    let value = any::<prop::sample::Index>()
-        .prop_map(move |index| index.index(state_len))
-        .prop_map(move |index| *st.read().values().iter().nth(index).unwrap());
+    // TODO: How to reintegrate while already using the proptest traits from [DynSolValue]?
+    // let value = any::<prop::sample::Index>()
+    //     .prop_map(move |index| index.index(state_len))
+    //     .prop_map(move |index| *st.read().values().iter().nth(index).unwrap());
 
     // Convert the value based on the parameter type
     match param {
-        DynSolType::Address => value
-            .prop_map(move |value| DynSolValue::Address(Address::from_word(value.into())))
-            .boxed(),
-        DynSolType::Bytes => value.prop_map(move |value| DynSolValue::Bytes(value.into())).boxed(),
-        DynSolType::Int(n) => match n / 8 {
-            32 => value
-                .prop_map(move |value| {
-                    DynSolValue::Int(I256::from_raw(U256::from_be_bytes(value)), 256)
-                })
-                .boxed(),
-            y @ 1..=31 => value
-                .prop_map(move |value| {
-                    // Generate a uintN in the correct range, then shift it to the range of intN
-                    // by subtracting 2^(N-1)
-                    let uint =
-                        U256::from_be_bytes(value) % U256::from(2usize).pow(U256::from(y * 8));
-                    let max_int_plus1 = U256::from(2usize).pow(U256::from(y * 8 - 1));
-                    let num = I256::from_raw(uint.overflowing_sub(max_int_plus1).0);
-                    DynSolValue::Int(num, 256)
-                })
-                .boxed(),
-            _ => panic!("unsupported solidity type int{n}"),
-        },
-        DynSolType::Uint(n) => match n / 8 {
-            32 => value
-                .prop_map(move |value| DynSolValue::Uint(U256::from_be_bytes(value), 256))
-                .boxed(),
-            y @ 1..=31 => value
-                .prop_map(move |value| {
-                    DynSolValue::Uint(
-                        U256::from_be_bytes(value) % (U256::from(2usize).pow(U256::from(y * 8))),
-                        y * 8,
-                    )
-                })
-                .boxed(),
-            _ => panic!("unsupported solidity type uint{n}"),
-        },
-        DynSolType::Bool => value.prop_map(move |value| DynSolValue::Bool(value[31] == 1)).boxed(),
-        DynSolType::String => value
+        DynSolType::Address |
+        DynSolType::Bytes |
+        DynSolType::Int(_) |
+        DynSolType::Uint(_) |
+        DynSolType::FixedBytes(_) |
+        DynSolType::Array(_) |
+        DynSolType::FixedArray(_, _) |
+        DynSolType::Tuple(_) |
+        DynSolType::Function |
+        DynSolType::Bool => DynSolValue::type_strategy(param).boxed(),
+        DynSolType::String => DynSolValue::type_strategy(param)
             .prop_map(move |value| {
                 DynSolValue::String(
-                    String::from_utf8_lossy(&value[..]).trim().trim_end_matches('\0').to_string(),
+                    String::from_utf8_lossy(value.as_str().unwrap().as_bytes())
+                        .trim()
+                        .trim_end_matches('\0')
+                        .to_string(),
                 )
             })
             .boxed(),
-        DynSolType::Array(param) => {
-            proptest::collection::vec(fuzz_param_from_state(param, arc_state), 0..MAX_ARRAY_LEN)
-                .prop_map(DynSolValue::Array)
-                .boxed()
-        }
-        DynSolType::FixedBytes(size) => {
-            let size = *size;
-            value
-                .prop_map(move |value| {
-                    DynSolValue::FixedBytes(FixedBytes::from_slice(&value[32 - size..]), size)
-                })
-                .boxed()
-        }
-        DynSolType::FixedArray(param, size) => {
-            let fixed_size = *size;
-            proptest::collection::vec(fuzz_param_from_state(param, arc_state), fixed_size)
-                .prop_map(DynSolValue::FixedArray)
-                .boxed()
-        }
-        DynSolType::Tuple(params) => params
-            .iter()
-            .map(|p| fuzz_param_from_state(p, arc_state.clone()))
-            .collect::<Vec<_>>()
-            .prop_map(DynSolValue::Tuple)
-            .boxed(),
-        _ => unimplemented!(),
+        DynSolType::CustomStruct { name, prop_names, tuple } => panic!("unsupported type"),
     }
 }
 
@@ -114,7 +65,7 @@ mod tests {
 
     #[test]
     fn can_fuzz_array() {
-        let f = "function testArray(uint64[2] calldata values)";
+        let f = "testArray(uint64[2] calldata values)";
         let func = Function::parse(f).unwrap();
         let db = CacheDB::new(EmptyDB::default());
         let state = build_initial_state(&db, &FuzzDictionaryConfig::default());
