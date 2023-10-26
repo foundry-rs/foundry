ee112b2bfecc64be7927dd6ca2796d716091ff2d
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2023-10-26 22:32:28 +0200

    fix: fs tests

--- testdata/fs/Default.t.sol
+++ testdata/fs/Default.t.sol
@@ -4,60 +4,11 @@ pragma solidity 0.8.18;
 import "ds-test/test.sol";
 import "../cheats/Vm.sol";
 
-contract FsProxy is DSTest {
-    Vm constant vm = Vm(HEVM_ADDRESS);
-
-    function readFile(string calldata path) external returns (string memory) {
-        return vm.readFile(path);
-    }
-
-    function readDir(string calldata path) external returns (Vm.DirEntry[] memory) {
-        return vm.readDir(path);
-    }
-
-    function readFileBinary(string calldata path) external returns (bytes memory) {
-        return vm.readFileBinary(path);
-    }
-
-    function readLine(string calldata path) external returns (string memory) {
-        return vm.readLine(path);
-    }
-
-    function writeLine(string calldata path, string calldata data) external {
-        return vm.writeLine(path, data);
-    }
-
-    function writeFile(string calldata path, string calldata data) external {
-        return vm.writeLine(path, data);
-    }
-
-    function writeFileBinary(string calldata path, bytes calldata data) external {
-        return vm.writeFileBinary(path, data);
-    }
-
-    function removeFile(string calldata path) external {
-        return vm.removeFile(path);
-    }
-
-    function fsMetadata(string calldata path) external returns (Vm.FsMetadata memory) {
-        return vm.fsMetadata(path);
-    }
-
-    function createDir(string calldata path) external {
-        return vm.createDir(path, false);
-    }
-
-    function createDir(string calldata path, bool recursive) external {
-        return vm.createDir(path, recursive);
-    }
-}
-
 contract DefaultAccessTest is DSTest {
     Vm constant vm = Vm(HEVM_ADDRESS);
-    FsProxy public fsProxy;
 
     bytes constant FOUNDRY_WRITE_ERR =
-        "The path \"fixtures/File/write_file.txt\" is not allowed to be accessed for write operations.";
+        "the path fixtures/File/write_file.txt is not allowed to be accessed for write operations";
 
     function testReadFile() public {
         string memory path = "fixtures/File/read.txt";
@@ -72,34 +23,28 @@ contract DefaultAccessTest is DSTest {
     }
 
     function testWriteFile() public {
-        fsProxy = new FsProxy();
-
         string memory path = "fixtures/File/write_file.txt";
         string memory data = "hello writable world";
 
         vm.expectRevert(FOUNDRY_WRITE_ERR);
-        fsProxy.writeFile(path, data);
+        vm.writeFile(path, data);
 
         vm.expectRevert(FOUNDRY_WRITE_ERR);
-        fsProxy.writeFileBinary(path, bytes(data));
+        vm.writeFileBinary(path, bytes(data));
     }
 
     function testWriteLine() public {
-        fsProxy = new FsProxy();
-
         string memory path = "fixtures/File/write_file.txt";
         string memory data = "hello writable world";
 
         vm.expectRevert(FOUNDRY_WRITE_ERR);
-        fsProxy.writeLine(path, data);
+        vm.writeLine(path, data);
     }
 
     function testRemoveFile() public {
-        fsProxy = new FsProxy();
-
         string memory path = "fixtures/File/write_file.txt";
 
         vm.expectRevert(FOUNDRY_WRITE_ERR);
-        fsProxy.removeFile(path);
+        vm.removeFile(path);
     }
 }
