4860ead78dda38de65d33a45f3dd6946ee13ed8c
Enrique Ortiz <hi@enriqueortiz.dev>
2023-10-30 14:19:56 -0400

    add test

--- crates/forge/tests/it/repros.rs
+++ crates/forge/tests/it/repros.rs
@@ -6,6 +6,7 @@ use crate::{
 };
 use alloy_primitives::Address;
 use ethers::abi::{Event, EventParam, Log, LogParam, ParamType, RawLog, Token};
+use forge::result::TestStatus;
 use foundry_config::{fs_permissions::PathPermission, Config, FsPermissions};
 use std::str::FromStr;
 
@@ -307,4 +308,14 @@ async fn test_issue_5808() {
 #[tokio::test(flavor = "multi_thread")]
 async fn test_issue_6115() {
     test_repro!("Issue6115");
+}
+
+// <https://github.com/foundry-rs/foundry/issues/6170>
+#[tokio::test(flavor = "multi_thread")]
+async fn test_issue_6170() {
+    let mut res = run_test_repro!("Issue6170");
+    let mut res = res.remove("repros/Issue6170.t.sol:Issue6170Test").unwrap();
+    let test = res.test_results.remove("test()").unwrap();
+    assert_eq!(test.status, TestStatus::Failure);
+    assert_eq!(test.reason, Some("Log != expected log".to_string()));
 }
