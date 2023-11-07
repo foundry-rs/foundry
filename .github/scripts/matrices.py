a8c84fe617218eb22bd93084ab724cef04846b58
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2023-11-07 15:48:22 +0100

    ci: merge build and run steps together

--- .github/scripts/matrices.py
+++ .github/scripts/matrices.py
@@ -4,6 +4,7 @@ import json
 import os
 
 
+# A runner target
 class Target:
     # GitHub runner OS
     os_id: str
@@ -15,89 +16,94 @@ class Target:
         self.target = target
 
 
+# A single test suite to run
 class Case:
+    # Name of the test suite
     name: str
+    # Nextest filter expression
     filter: str
+    # Number of partitions to split the test suite into
     n_partitions: int
-    xplatform: bool
+    # Whether to run on non-Linux platforms for PRs
+    pr_cross_platform: bool
 
-    def __init__(self, name: str, filter: str, n_partitions: int, xplatform: bool):
+    def __init__(
+        self, name: str, filter: str, n_partitions: int, pr_cross_platform: bool
+    ):
         self.name = name
         self.filter = filter
         self.n_partitions = n_partitions
-        self.xplatform = xplatform
+        self.pr_cross_platform = pr_cross_platform
 
 
+# Expanded GHA matrix entry
 class Expanded:
+    name: str
     os: str
     target: str
-    name: str
     flags: str
     partition: int
 
-    def __init__(self, os: str, target: str, name: str, flags: str, partition: int):
+    def __init__(self, name: str, os: str, target: str, flags: str, partition: int):
+        self.name = name
         self.os = os
         self.target = target
-        self.name = name
         self.flags = flags
         self.partition = partition
 
 
-t_linux = Target("ubuntu-latest", "x86_64-unknown-linux-gnu")
+is_pr = os.environ.get("EVENT_NAME") == "pull_request"
+t_linux_x86 = Target("ubuntu-latest", "x86_64-unknown-linux-gnu")
+t_linux_arm = Target("ubuntu-latest", "aarch64-unknown-linux-gnu")
 t_macos = Target("macos-latest", "x86_64-apple-darwin")
 t_windows = Target("windows-latest", "x86_64-pc-windows-msvc")
-if os.environ.get("EVENT_NAME") == "pull_request":
-    targets = [t_linux, t_windows]
-else:
-    targets = [t_linux, t_macos, t_windows]
+targets = (
+    [t_linux_x86, t_windows]
+    if is_pr
+    else [t_linux_x86, t_linux_arm, t_macos, t_windows]
+)
 
 config = [
     Case(
         name="unit",
         filter="kind(lib) | kind(bench) | kind(proc-macro)",
         n_partitions=1,
-        xplatform=True,
+        pr_cross_platform=True,
     ),
     Case(
         name="integration",
         filter="kind(test) & !test(/issue|forge_std|ext_integration/)",
         n_partitions=3,
-        xplatform=True,
+        pr_cross_platform=True,
     ),
     Case(
-        name="integration/issue-repros",
+        name="integration / issue-repros",
         filter="package(=forge) & test(~issue)",
         n_partitions=2,
-        xplatform=False,
+        pr_cross_platform=False,
     ),
     Case(
-        name="integration/forge-std",
+        name="integration / forge-std",
         filter="package(=forge) & test(~forge_std)",
         n_partitions=1,
-        xplatform=False,
+        pr_cross_platform=False,
     ),
     Case(
-        name="integration/external",
+        name="integration / external",
         filter="package(=forge) & test(~ext_integration)",
         n_partitions=2,
-        xplatform=False,
+        pr_cross_platform=False,
     ),
 ]
 
 
-def build_matrix():
-    expanded = []
-    for target in targets:
-        expanded.append({"os": target.os_id, "target": target.target})
-    print_json({"include": expanded})
-
-
-def test_matrix():
+def main():
     expanded = []
     for target in targets:
         for case in config:
-            if not case.xplatform and target != t_linux:
-                continue
+            if is_pr:
+                if not case.pr_cross_platform and target != t_linux_x86:
+                    continue
 
             for partition in range(1, case.n_partitions + 1):
                 os_str = ""
@@ -113,9 +119,9 @@ def test_matrix():
                 name += os_str
 
                 obj = Expanded(
+                    name=name,
                     os=target.os_id,
                     target=target.target,
-                    name=name,
                     flags=flags,
                     partition=partition,
                 )
@@ -129,7 +135,4 @@ def print_json(obj):
 
 
 if __name__ == "__main__":
-    if int(os.environ.get("TEST", "0")) == 0:
-        build_matrix()
-    else:
-        test_matrix()
+    main()
