6bb9df7d19a66281bb41c788bea2137387575f7e
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2023-11-09 11:45:32 +0100

    move decoding out of utils

--- crates/evm/traces/src/decoder/mod.rs
+++ crates/evm/traces/src/decoder/mod.rs
@@ -1,12 +1,11 @@
 use crate::{
     identifier::{AddressIdentity, SingleSignaturesIdentifier, TraceIdentifier},
-    utils::{self, decode_cheatcode_outputs},
-    CallTrace, CallTraceArena, TraceCallData, TraceLog, TraceRetData,
+    utils, CallTrace, CallTraceArena, TraceCallData, TraceLog, TraceRetData,
 };
 use alloy_dyn_abi::{DecodedEvent, DynSolValue, EventExt, FunctionExt, JsonAbiExt};
 use alloy_json_abi::{Event, Function, JsonAbi as Abi};
 use alloy_primitives::{Address, Selector, B256};
-use foundry_common::{abi::get_indexed_event, SELECTOR_LEN};
+use foundry_common::{abi::get_indexed_event, fmt::format_token, SELECTOR_LEN};
 use foundry_evm_core::{
     abi::{CONSOLE_ABI, HARDHAT_CONSOLE_ABI, HEVM_ABI},
     constants::{
@@ -287,9 +286,7 @@ impl CallTraceDecoder {
         if data.len() >= SELECTOR_LEN {
             if trace.address == CHEATCODE_ADDRESS {
                 // Try to decode cheatcode inputs in a more custom way
-                if let Some(v) =
-                    utils::decode_cheatcode_inputs(func, data, &self.errors, self.verbosity)
-                {
+                if let Some(v) = decode_cheatcode_inputs(func, data, &self.errors, self.verbosity) {
                     args = Some(v);
                 }
             }
@@ -401,4 +398,86 @@ fn reconstruct_params(event: &Event, decoded: &DecodedEvent) -> Vec<DynSolValue>
 
 fn indexed_inputs(event: &Event) -> usize {
     event.inputs.iter().filter(|param| param.indexed).count()
+}
+
+/// Custom decoding of cheatcode calls
+fn decode_cheatcode_inputs(
+    func: &Function,
+    data: &[u8],
+    errors: &Abi,
+    verbosity: u8,
+) -> Option<Vec<String>> {
+    match func.name.as_str() {
+        "expectRevert" => Some(vec![decode::decode_revert(data, Some(errors), None)]),
+        "rememberKey" | "addr" | "startBroadcast" | "broadcast" => {
+            // these functions accept a private key as uint256, which should not be
+            // converted to plain text
+            if !func.inputs.is_empty() && func.inputs[0].ty != "uint256" {
+                // redact private key input
+                Some(vec!["<pk>".to_string()])
+            } else {
+                None
+            }
+        }
+        "sign" => {
+            // sign(uint256,bytes32)
+            let mut decoded = func.abi_decode_input(&data[SELECTOR_LEN..], false).ok()?;
+            if !decoded.is_empty() && func.inputs[0].ty != "uint256" {
+                decoded[0] = DynSolValue::String("<pk>".to_string());
+            }
+            Some(decoded.iter().map(format_token).collect())
+        }
+        "deriveKey" => Some(vec!["<pk>".to_string()]),
+        "parseJson" |
+        "parseJsonUint" |
+        "parseJsonUintArray" |
+        "parseJsonInt" |
+        "parseJsonIntArray" |
+        "parseJsonString" |
+        "parseJsonStringArray" |
+        "parseJsonAddress" |
+        "parseJsonAddressArray" |
+        "parseJsonBool" |
+        "parseJsonBoolArray" |
+        "parseJsonBytes" |
+        "parseJsonBytesArray" |
+        "parseJsonBytes32" |
+        "parseJsonBytes32Array" |
+        "writeJson" |
+        "keyExists" |
+        "serializeBool" |
+        "serializeUint" |
+        "serializeInt" |
+        "serializeAddress" |
+        "serializeBytes32" |
+        "serializeString" |
+        "serializeBytes" => {
+            if verbosity >= 5 {
+                None
+            } else {
+                let mut decoded = func.abi_decode_input(&data[SELECTOR_LEN..], false).ok()?;
+                let token =
+                    if func.name.as_str() == "parseJson" || func.name.as_str() == "keyExists" {
+                        "<JSON file>"
+                    } else {
+                        "<stringified JSON>"
+                    };
+                decoded[0] = DynSolValue::String(token.to_string());
+                Some(decoded.iter().map(format_token).collect())
+            }
+        }
+        _ => None,
+    }
+}
+
+/// Custom decoding of cheatcode return values
+fn decode_cheatcode_outputs(func: &Function, _data: &[u8], verbosity: u8) -> Option<String> {
+    match func.name.as_str() {
+        s if s.starts_with("env") => Some("<env var value>"),
+        "deriveKey" => Some("<pk>"),
+        "parseJson" if verbosity < 5 => Some("<encoded JSON value>"),
+        "readFile" if verbosity < 5 => Some("<file>"),
+        _ => None,
+    }
+    .map(Into::into)
 }
