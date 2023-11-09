5cdc2aa01be9e14f3f33f3b9b86af3b82c9c820c
Enrique Ortiz <hi@enriqueortiz.dev>
2023-11-09 14:50:49 -0400

    chore: only augment error message

--- testdata/cheats/Env.t.sol
+++ testdata/cheats/Env.t.sol
@@ -33,15 +33,15 @@ contract EnvTest is DSTest {
         string[numEnvUintTests] memory values = [
             "0",
             "115792089237316195423570985008687907853269984665640564039457584007913129639935",
-            "0x01",
+            "0x0a",
             "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
             "0x0000000000000000000000000000000000000000000000000000000000000000",
-            "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"
+            "0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"
         ];
         uint256[numEnvUintTests] memory expected = [
             type(uint256).min,
             type(uint256).max,
-            1,
+            10,
             77814517325470205911140941194401928579557062014761831930645393041380819009408,
             type(uint256).min,
             type(uint256).max
