4c11a23678074f8fde0bb8419bc88289b6255f47
Matthias Seitz <matthias.seitz@outlook.de>
2023-11-10 02:36:45 +0100

    feat: add --no-cache and respect cache config value (#6273)
    * feat: add --no-cache and respect cache config value
    
    * fix test
    
    * test: use forgetest_init! doh

--- crates/forge/tests/cli/cache.rs
+++ crates/forge/tests/cli/cache.rs
@@ -14,3 +14,12 @@ forgetest!(can_list_specific_chain, |_prj, cmd| {
     cmd.args(["cache", "ls", "mainnet"]);
     cmd.assert_success();
 });
+
+forgetest_init!(can_test_no_cache, |prj, cmd| {
+    cmd.args(["test", "--no-cache"]);
+    cmd.assert_success();
+    assert!(!prj.cache_path().exists(), "cache file should not exist");
+    cmd.forge_fuse().args(["test"]);
+    cmd.assert_success();
+    assert!(prj.cache_path().exists(), "cache file should exist");
+});
