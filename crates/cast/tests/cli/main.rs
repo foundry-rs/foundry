3ebeded4308b28533bd7b7036b593a92390faf67
Bakuchi <49754494+massun-onibakuchi@users.noreply.github.com>
2023-11-15 01:22:00 +0900

    fix(cast): upload error signature (#6312)
    * test(common): Add custom error test
    
    * fix(common): import error sigs in SelectorImportData
    
    * rustfmt
    
    ---------
    
    Co-authored-by: Matthias Seitz <matthias.seitz@outlook.de>

--- crates/cast/tests/cli/main.rs
+++ crates/cast/tests/cli/main.rs
@@ -184,6 +184,16 @@ casttest!(upload_signatures, |_prj, cmd| {
 
     assert!(output.contains("Event Transfer(address,uint256): 0x69ca02dd4edd7bf0a4abb9ed3b7af3f14778db5d61921c7dc7cd545266326de2"), "{}", output);
 
+    // test error prefix
+    cmd.args(["upload-signature", "error ERC20InsufficientBalance(address,uint256,uint256)"]);
+    let output = cmd.stdout_lossy();
+
+    assert!(
+        output.contains("Function ERC20InsufficientBalance(address,uint256,uint256): 0xe450d38c"),
+        "{}",
+        output
+    ); // Custom error is interpreted as function
+
     // test multiple sigs
     cmd.args([
         "upload-signature",
@@ -202,6 +212,7 @@ casttest!(upload_signatures, |_prj, cmd| {
         "upload-signature",
         "event Transfer(address,uint256)",
         "transfer(address,uint256)",
+        "error ERC20InsufficientBalance(address,uint256,uint256)",
         Path::new(env!("CARGO_MANIFEST_DIR"))
             .join("tests/fixtures/ERC20Artifact.json")
             .into_os_string()
@@ -216,6 +227,11 @@ casttest!(upload_signatures, |_prj, cmd| {
     assert!(output.contains("Function approve(address,uint256): 0x095ea7b3"), "{}", output);
     assert!(output.contains("Function decimals(): 0x313ce567"), "{}", output);
     assert!(output.contains("Function allowance(address,address): 0xdd62ed3e"), "{}", output);
+    assert!(
+        output.contains("Function ERC20InsufficientBalance(address,uint256,uint256): 0xe450d38c"),
+        "{}",
+        output
+    );
 });
 
 // tests that the `cast to-rlp` and `cast from-rlp` commands work correctly
