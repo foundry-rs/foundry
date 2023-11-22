4bef403806c2f81acd8276bc604bc45f8a0d15fe
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2023-11-22 17:52:15 +0100

    improve Retry

--- crates/common/src/retry.rs
+++ crates/common/src/retry.rs
@@ -1,5 +1,6 @@
 //! Retry utilities.
 
+use eyre::{Error, Result};
 use std::{future::Future, time::Duration};
 
 /// A type that keeps track of attempts.
@@ -15,19 +16,16 @@ impl Retry {
         Self { retries, delay }
     }
 
-    fn handle_err(&mut self, err: eyre::Report) {
-        self.retries -= 1;
-        warn!("erroneous attempt ({} tries remaining): {}", self.retries, err.root_cause());
-        if let Some(delay) = self.delay {
-            std::thread::sleep(delay);
-        }
-    }
-
     /// Runs the given closure in a loop, retrying if it fails up to the specified number of times.
-    pub fn run<F: FnMut() -> eyre::Result<T>, T>(mut self, mut callback: F) -> eyre::Result<T> {
+    pub fn run<F: FnMut() -> Result<T>, T>(mut self, mut callback: F) -> Result<T> {
         loop {
             match callback() {
-                Err(e) if self.retries > 0 => self.handle_err(e),
+                Err(e) if self.retries > 0 => {
+                    self.handle_err(e);
+                    if let Some(delay) = self.delay {
+                        std::thread::sleep(delay);
+                    }
+                }
                 res => return res,
             }
         }
@@ -35,16 +33,26 @@ impl Retry {
 
     /// Runs the given async closure in a loop, retrying if it fails up to the specified number of
     /// times.
-    pub async fn run_async<F, Fut, T>(mut self, mut callback: F) -> eyre::Result<T>
+    pub async fn run_async<F, Fut, T>(mut self, mut callback: F) -> Result<T>
     where
         F: FnMut() -> Fut,
-        Fut: Future<Output = eyre::Result<T>>,
+        Fut: Future<Output = Result<T>>,
     {
         loop {
             match callback().await {
-                Err(e) if self.retries > 0 => self.handle_err(e),
+                Err(e) if self.retries > 0 => {
+                    self.handle_err(e);
+                    if let Some(delay) = self.delay {
+                        tokio::time::sleep(delay).await;
+                    }
+                }
                 res => return res,
             };
         }
+    }
+
+    fn handle_err(&mut self, err: Error) {
+        self.retries -= 1;
+        warn!("erroneous attempt ({} tries remaining): {}", self.retries, err.root_cause());
     }
 }
