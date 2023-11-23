f429404b15684834c48ae4f7ae39508da1e0b9f9
beeb <703631+beeb@users.noreply.github.com>
2023-11-23 14:02:01 +0100

    style: format

--- testdata/cheats/RecordAccountAccesses.t.sol
+++ testdata/cheats/RecordAccountAccesses.t.sol
@@ -315,7 +315,7 @@ contract RecordAccountAccessesTest is DSTest {
         (succ,) = address(123469).call("hello world");
         (succ,) = address(5678).call("");
         // contract calls to self in constructor
-        SelfCaller caller = new SelfCaller{value: 2 ether}('hello2 world2');
+        SelfCaller caller = new SelfCaller{value: 2 ether}("hello2 world2");
 
         Vm.AccountAccess[] memory called = cheats.stopAndReturnStateDiff();
         assertEq(called.length, 6);
@@ -1011,8 +1011,8 @@ contract RecordAccountAccessesTest is DSTest {
     function testSelfDestruct() public {
         uint256 startingBalance = address(this).balance;
         this.startRecordingFromLowerDepth();
-        address a = address(new SelfDestructor{value:1 ether}(address(this)));
-        address b = address(new SelfDestructor{value:1 ether}(address(bytes20("doesn't exist yet"))));
+        address a = address(new SelfDestructor{value: 1 ether}(address(this)));
+        address b = address(new SelfDestructor{value: 1 ether}(address(bytes20("doesn't exist yet"))));
         Vm.AccountAccess[] memory called = cheats.stopAndReturnStateDiff();
         assertEq(called.length, 5, "incorrect length");
         assertEq(
