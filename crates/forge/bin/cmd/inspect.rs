dfa04eb93dd2e6ff57309c6516683c32f6ac4734
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2023-11-24 17:21:06 +0100

    fix

--- crates/forge/bin/cmd/inspect.rs
+++ crates/forge/bin/cmd/inspect.rs
@@ -89,7 +89,7 @@ impl InspectArgs {
                     .abi
                     .as_ref()
                     .ok_or_else(|| eyre::eyre!("Failed to fetch lossless ABI"))?;
-                print_abi(&abi.abi, pretty)?;
+                print_abi(&abi, pretty)?;
             }
             ContractArtifactField::Bytecode => {
                 let tval: Value = to_value(&artifact.bytecode)?;
@@ -165,7 +165,7 @@ impl InspectArgs {
             ContractArtifactField::Errors => {
                 let mut out = serde_json::Map::new();
                 if let Some(abi) = &artifact.abi {
-                    let abi = &abi.abi;
+                    let abi = &abi;
                     // Print the signature of all errors
                     for er in abi.errors.iter().flat_map(|(_, errors)| errors) {
                         let types = er.inputs.iter().map(|p| p.ty.clone()).collect::<Vec<_>>();
@@ -182,7 +182,7 @@ impl InspectArgs {
             ContractArtifactField::Events => {
                 let mut out = serde_json::Map::new();
                 if let Some(abi) = &artifact.abi {
-                    let abi = &abi.abi;
+                    let abi = &abi;
                     // print the signature of all events including anonymous
                     for ev in abi.events.iter().flat_map(|(_, events)| events) {
                         let types = ev.inputs.iter().map(|p| p.ty.clone()).collect::<Vec<_>>();
