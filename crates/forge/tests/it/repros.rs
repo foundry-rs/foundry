74db9e2ad0a161e12f0ad9d32924cd2769d757b5
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2023-11-27 14:38:46 +0100

    reorder

--- crates/forge/tests/it/repros.rs
+++ crates/forge/tests/it/repros.rs
@@ -7,6 +7,7 @@ use forge::result::TestStatus;
 use foundry_config::{fs_permissions::PathPermission, Config, FsPermissions};
 use foundry_test_utils::Filter;
 
+/// Creates a test that runs `testdata/repros/Issue{issue}.t.sol`.
 macro_rules! test_repro {
     ($issue_number:literal $(,)?) => {
         test_repro!($issue_number, false, None);
@@ -64,47 +65,35 @@ test_repro!(2956);
 // https://github.com/foundry-rs/foundry/issues/2984
 test_repro!(2984);
 
-// https://github.com/foundry-rs/foundry/issues/4640
-test_repro!(4640);
+// https://github.com/foundry-rs/foundry/issues/3055
+test_repro!(3055, true);
 
 // https://github.com/foundry-rs/foundry/issues/3077
 test_repro!(3077);
 
-// https://github.com/foundry-rs/foundry/issues/3055
-test_repro!(3055, true);
-
-// https://github.com/foundry-rs/foundry/issues/3192
-test_repro!(3192);
-
 // https://github.com/foundry-rs/foundry/issues/3110
 test_repro!(3110);
-
-// https://github.com/foundry-rs/foundry/issues/3189
-test_repro!(3189, true);
 
 // https://github.com/foundry-rs/foundry/issues/3119
 test_repro!(3119);
 
+// https://github.com/foundry-rs/foundry/issues/3189
+test_repro!(3189, true);
+
 // https://github.com/foundry-rs/foundry/issues/3190
 test_repro!(3190);
+
+// https://github.com/foundry-rs/foundry/issues/3192
+test_repro!(3192);
+
+// https://github.com/foundry-rs/foundry/issues/3220
+test_repro!(3220);
 
 // https://github.com/foundry-rs/foundry/issues/3221
 test_repro!(3221);
 
-// https://github.com/foundry-rs/foundry/issues/3437
-// 1.0 related
-//     test_repro!(3437);
-// async fn test_issue_3437() {
-// }
-
-// https://github.com/foundry-rs/foundry/issues/3708
-test_repro!(3708);
-
-// https://github.com/foundry-rs/foundry/issues/3221
+// https://github.com/foundry-rs/foundry/issues/3223
 test_repro!(3223, false, address!("F0959944122fb1ed4CfaBA645eA06EED30427BAA"));
-
-// https://github.com/foundry-rs/foundry/issues/3220
-test_repro!(3220);
 
 // https://github.com/foundry-rs/foundry/issues/3347
 test_repro!(3347, false, None, |res| {
@@ -133,14 +122,15 @@ test_repro!(3347, false, None, |res| {
     );
 });
 
-// https://github.com/foundry-rs/foundry/issues/3685
-test_repro!(3685);
-
-// https://github.com/foundry-rs/foundry/issues/3653
-test_repro!(3653);
+// https://github.com/foundry-rs/foundry/issues/3437
+// 1.0 related
+// test_repro!(3437);
 
 // https://github.com/foundry-rs/foundry/issues/3596
 test_repro!(3596, true, None);
+
+// https://github.com/foundry-rs/foundry/issues/3653
+test_repro!(3653);
 
 // https://github.com/foundry-rs/foundry/issues/3661
 test_repro!(3661);
@@ -148,8 +138,14 @@ test_repro!(3661);
 // https://github.com/foundry-rs/foundry/issues/3674
 test_repro!(3674, false, address!("F0959944122fb1ed4CfaBA645eA06EED30427BAA"));
 
+// https://github.com/foundry-rs/foundry/issues/3685
+test_repro!(3685);
+
 // https://github.com/foundry-rs/foundry/issues/3703
 test_repro!(3703);
+
+// https://github.com/foundry-rs/foundry/issues/3708
+test_repro!(3708);
 
 // https://github.com/foundry-rs/foundry/issues/3723
 // 1.0 related
@@ -158,11 +154,17 @@ test_repro!(3703);
 // https://github.com/foundry-rs/foundry/issues/3753
 test_repro!(3753);
 
-// https://github.com/foundry-rs/foundry/issues/4630
-test_repro!(4630);
+// https://github.com/foundry-rs/foundry/issues/3792
+test_repro!(3792);
 
 // https://github.com/foundry-rs/foundry/issues/4586
 test_repro!(4586);
+
+// https://github.com/foundry-rs/foundry/issues/4630
+test_repro!(4630);
+
+// https://github.com/foundry-rs/foundry/issues/4640
+test_repro!(4640);
 
 // https://github.com/foundry-rs/foundry/issues/4832
 // 1.0 related
@@ -171,14 +173,11 @@ test_repro!(4586);
 // https://github.com/foundry-rs/foundry/issues/5038
 test_repro!(5038);
 
-// https://github.com/foundry-rs/foundry/issues/3792
-test_repro!(3792);
+// https://github.com/foundry-rs/foundry/issues/5808
+test_repro!(5808);
 
 // https://github.com/foundry-rs/foundry/issues/6006
 test_repro!(6006);
-
-// https://github.com/foundry-rs/foundry/issues/5808
-test_repro!(5808);
 
 // https://github.com/foundry-rs/foundry/issues/6070
 test_repro!(6070);
