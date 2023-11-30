91df94b6a300e5899d49f2340b56fdd4dab0693c
Matthias Seitz <matthias.seitz@outlook.de>
2023-11-30 16:50:15 +0100

    test: integrate inline config in tests (#6473)

--- testdata/repros/Issue5948.t.sol
+++ testdata/repros/Issue5948.t.sol
@@ -2,30 +2,11 @@
 pragma solidity 0.8.18;
 
 import "ds-test/test.sol";
-import "./Vm.sol";
+import "../cheats/Vm.sol";
 
-contract SleepTest is DSTest {
+// https://github.com/foundry-rs/foundry/issues/5948
+contract Issue5948Test is DSTest {
     Vm constant vm = Vm(HEVM_ADDRESS);
-
-    function testSleep() public {
-        uint256 milliseconds = 1234;
-
-        string[] memory inputs = new string[](2);
-        inputs[0] = "date";
-        // OS X does not support precision more than 1 second
-        inputs[1] = "+%s000";
-
-        bytes memory res = vm.ffi(inputs);
-        uint256 start = vm.parseUint(string(res));
-
-        vm.sleep(milliseconds);
-
-        res = vm.ffi(inputs);
-        uint256 end = vm.parseUint(string(res));
-
-        // Limit precision to 1000 ms
-        assertGe(end - start, milliseconds / 1000 * 1000, "sleep failed");
-    }
 
     /// forge-config: default.fuzz.runs = 2
     function testSleepFuzzed(uint256 _milliseconds) public {
