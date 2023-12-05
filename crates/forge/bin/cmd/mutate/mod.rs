bf7d9cc57788fdec1fc451554b646a14e3d816e0
samparsky <8148384+samparsky@users.noreply.github.com>
2023-12-05 10:33:55 +0200

    chore: add mutate test args struct

--- crates/forge/bin/cmd/mutate/mod.rs
+++ crates/forge/bin/cmd/mutate/mod.rs
@@ -9,3 +9,45 @@ use std::{fmt, path::Path};
 
 mod filter;
 pub use filter::*;
+
+/// CLI arguments for `forge mutate`.
+#[derive(Debug, Clone, Parser)]
+#[clap(next_help_heading = "Mutation Test options")]
+pub struct MutateTestArgs {
+    /// Output Rutant test results in JSON format.
+    #[clap(long, short, help_heading = "Display options")]
+    json: bool,
+
+    #[clap(flatten)]
+    filter: MutationTestFilterArgs,
+
+    /// Exit with code 0 even if a test fails.
+    #[clap(long, env = "FORGE_ALLOW_FAILURE")]
+    allow_failure: bool,
+
+    /// Stop running mutation tests after the first failure
+    #[clap(long)]
+    pub fail_fast: bool,
+
+    /// List mutation tests instead of running them
+    #[clap(long, short, help_heading = "Display options")]
+    list: bool,
+
+    #[clap(flatten)]
+    evm_opts: EvmArgs,
+
+    #[clap(flatten)]
+    opts: CoreBuildArgs,
+
+    /// Export generated mutants to a directory
+    #[clap(long, default_value_t = false)]
+    pub export: bool,
+
+    /// Print mutation test summary table
+    #[clap(long, help_heading = "Display options", default_value_t = true)]
+    pub summary: bool,
+
+    /// Print detailed mutation test summary table
+    #[clap(long, help_heading = "Display options")]
+    pub detailed: bool,
+}
