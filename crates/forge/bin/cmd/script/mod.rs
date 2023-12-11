0fdcf21521e5de912f422d2d18c195b9925a3c28
Oliver Nordbjerg <hi@notbjerg.me>
2023-12-11 19:26:52 +0100

    chore: fmt

--- crates/forge/bin/cmd/script/mod.rs
+++ crates/forge/bin/cmd/script/mod.rs
@@ -560,9 +560,9 @@ impl ScriptArgs {
         }
 
         // Only prompt if we're broadcasting and we've not disabled interactivity.
-        if prompt_user
-            && !self.non_interactive
-            && !Confirm::new().with_prompt("Do you wish to continue?".to_string()).interact()?
+        if prompt_user &&
+            !self.non_interactive &&
+            !Confirm::new().with_prompt("Do you wish to continue?".to_string()).interact()?
         {
             eyre::bail!("User canceled the script.");
         }
