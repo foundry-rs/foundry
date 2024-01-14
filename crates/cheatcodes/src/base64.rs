63d6d02ae5efe1a6cfcabe6c5982cc4115a4b275
Andrew Tretyakov <42178850+0xAndoroid@users.noreply.github.com>
2024-01-13 22:01:21 -0500

    Replaced `string::parse` with `abi_encode()`

--- crates/cheatcodes/src/base64.rs
+++ crates/cheatcodes/src/base64.rs
@@ -1,17 +1,17 @@
-use crate::{string, Cheatcode, Cheatcodes, Result, Vm::*};
-use alloy_dyn_abi::DynSolType;
+use crate::{Cheatcode, Cheatcodes, Result, Vm::*};
+use alloy_sol_types::SolValue;
 use base64::prelude::*;
 
 impl Cheatcode for toBase64Call {
     fn apply(&self, _state: &mut Cheatcodes) -> Result {
         let Self { data } = self;
-        string::parse(&BASE64_STANDARD.encode(data), &DynSolType::String)
+        Ok(BASE64_STANDARD.encode(data).abi_encode())
     }
 }
 
 impl Cheatcode for toBase64URLCall {
     fn apply(&self, _state: &mut Cheatcodes) -> Result {
         let Self { data } = self;
-        string::parse(&BASE64_URL_SAFE.encode(data), &DynSolType::String)
+        Ok(BASE64_URL_SAFE.encode(data).abi_encode())
     }
 }
