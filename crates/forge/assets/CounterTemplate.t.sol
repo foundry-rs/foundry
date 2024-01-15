24abca6c9133618e0c355842d2be2dd4f36da46d
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2024-01-15 20:50:48 +0100

    feat: stop using console2 (#6804)

--- crates/forge/assets/CounterTemplate.t.sol
+++ crates/forge/assets/CounterTemplate.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console2} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {Counter} from "../src/Counter.sol";
 
 contract CounterTest is Test {
