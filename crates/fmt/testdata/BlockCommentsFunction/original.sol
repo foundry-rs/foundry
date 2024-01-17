d9e25fa713bf2c70f1e60bc607fd2cf6912e42ac
Matthias Seitz <matthias.seitz@outlook.de>
2024-01-17 23:50:21 +0100

    fix: skip trailing white space for doc comments (#6834)

---
+++ crates/fmt/testdata/BlockCommentsFunction/original.sol
@@ -0,0 +1,20 @@
+contract A {
+    Counter public counter;
+    /**
+     *  TODO: this fuzz use too much time to execute
+    function testGetFuzz(bytes[2][] memory kvs) public {
+        for (uint256 i = 0; i < kvs.length; i++) {
+            bytes32 root = trie.update(kvs[i][0], kvs[i][1]);
+            console.logBytes32(root);
+        }
+
+        for (uint256 i = 0; i < kvs.length; i++) {
+            (bool exist, bytes memory value) = trie.get(kvs[i][0]);
+            console.logBool(exist);
+            console.logBytes(value);
+            require(exist);
+            require(BytesSlice.equal(value, trie.getRaw(kvs[i][0])));
+        }
+    }
+    */
+}
