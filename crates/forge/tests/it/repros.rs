a1c35c95d7fa04c0ea14330081d22389f5f3e0ae
Ryan Pate <ryantpate97@gmail.com>
2024-02-01 11:46:54 -0800

    fix(cheatcodes): Properly record call to create2 factory in state diff

--- crates/forge/tests/it/repros.rs
+++ crates/forge/tests/it/repros.rs
@@ -310,3 +310,10 @@ test_repro!(5529; |config| {
   cheats_config.always_use_create_2_factory = true;
   config.runner.cheats_config = std::sync::Arc::new(cheats_config);
 });
+
+// https://github.com/foundry-rs/foundry/issues/6634
+test_repro!(6634; |config| {
+  let mut cheats_config = config.runner.cheats_config.as_ref().clone();
+  cheats_config.always_use_create_2_factory = true;
+  config.runner.cheats_config = std::sync::Arc::new(cheats_config);
+});
