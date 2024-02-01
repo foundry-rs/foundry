996c643bde2edf583adbb03567c1e21d5e7b0ff7
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2024-02-01 23:44:15 +0100

    fix: actually run all unit tests in CI (#6989)

--- crates/forge/bin/cmd/verify/etherscan/mod.rs
+++ crates/forge/bin/cmd/verify/etherscan/mod.rs
@@ -512,7 +512,7 @@ mod tests {
                 &config,
             )
             .unwrap();
-        assert_eq!(client.etherscan_api_url().as_str(), "https://api-testnet.polygonscan.com/?/");
+        assert_eq!(client.etherscan_api_url().as_str(), "https://api-testnet.polygonscan.com/");
 
         assert!(format!("{client:?}").contains("dummykey"));
 
@@ -539,7 +539,7 @@ mod tests {
                 &config,
             )
             .unwrap();
-        assert_eq!(client.etherscan_api_url().as_str(), "https://verifier-url.com/?/");
+        assert_eq!(client.etherscan_api_url().as_str(), "https://verifier-url.com/");
         assert!(format!("{client:?}").contains("dummykey"));
     }
 
