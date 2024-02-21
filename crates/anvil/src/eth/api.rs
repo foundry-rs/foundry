437917465464e74d38951e9b7b30618930e4a0ab
Enrique Ortiz <hi@enriqueortiz.dev>
2024-02-21 17:22:13 -0400

    use correct eip check

--- crates/anvil/src/eth/api.rs
+++ crates/anvil/src/eth/api.rs
@@ -2614,7 +2614,7 @@ impl EthApi {
         match &tx {
             TypedTransaction::EIP2930(_) => self.backend.ensure_eip2930_active(),
             TypedTransaction::EIP1559(_) => self.backend.ensure_eip1559_active(),
-            TypedTransaction::EIP4844(_) => self.backend.ensure_eip1559_active(),
+            TypedTransaction::EIP4844(_) => self.backend.ensure_eip4844_active(),
             TypedTransaction::Deposit(_) => self.backend.ensure_op_deposits_active(),
             TypedTransaction::Legacy(_) => Ok(()),
         }
