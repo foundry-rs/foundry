9fe9a3fc89f46294e38454d2b5106c707d82a31a
RPate97 <ryantpate97@gmail.com>
2024-02-21 16:16:37 -0800

    fix(forge): Optionally use create2 factory in tests and non-broadcasting scripts (#6656)
    * fix(forge): Optionally use create2 factory in tests
    
    * Update crates/cheatcodes/src/inspector.rs
    
    * Update crates/cheatcodes/src/inspector.rs
    
    Co-authored-by: Matthias Seitz <matthias.seitz@outlook.de>
    
    ---------
    
    Co-authored-by: Enrique <hi@enriqueortiz.dev>
    Co-authored-by: Matthias Seitz <matthias.seitz@outlook.de>

--- crates/config/src/lib.rs
+++ crates/config/src/lib.rs
@@ -242,6 +242,8 @@ pub struct Config {
     pub invariant: InvariantConfig,
     /// Whether to allow ffi cheatcodes in test
     pub ffi: bool,
+    /// Use the create 2 factory in all cases including tests and non-broadcasting scripts.
+    pub always_use_create_2_factory: bool,
     /// The address which will be executing all tests
     pub sender: Address,
     /// The tx.origin value during EVM execution
@@ -1842,6 +1844,7 @@ impl Default for Config {
             path_pattern_inverse: None,
             fuzz: Default::default(),
             invariant: Default::default(),
+            always_use_create_2_factory: false,
             ffi: false,
             sender: Config::DEFAULT_SENDER,
             tx_origin: Config::DEFAULT_SENDER,
@@ -3408,6 +3411,7 @@ mod tests {
                 revert_strings = "strip"
                 allow_paths = ["allow", "paths"]
                 build_info_path = "build-info"
+                always_use_create_2_factory = true
 
                 [rpc_endpoints]
                 optimism = "https://example.com/"
@@ -3459,6 +3463,7 @@ mod tests {
                         ),
                     ]),
                     build_info_path: Some("build-info".into()),
+                    always_use_create_2_factory: true,
                     ..Config::default()
                 }
             );
@@ -3510,6 +3515,7 @@ mod tests {
                 evm_version = 'london'
                 extra_output = []
                 extra_output_files = []
+                always_use_create_2_factory = false
                 ffi = false
                 force = false
                 gas_limit = 9223372036854775807
