85b4872377bf85c3180e49956a82fded49daff10
malik <aremumalik05@gmail.com>
2024-02-27 15:50:26 +0100

    test: add more positive fuzz test cases (#7101)
    * changes
    
    * changes
    
    * proposed fix
    
    * format
    
    * fixed
    
    * Update crates/forge/tests/it/fuzz.rs
    
    Co-authored-by: Oliver Nordbjerg <onbjerg@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Oliver Nordbjerg <onbjerg@users.noreply.github.com>

---
+++ testdata/fuzz/FuzzPositive.t.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity 0.8.18;
+
+import "ds-test/test.sol";
+
+contract FuzzPositive is DSTest {
+    function testSuccessChecker(uint256 val) public {
+        assertTrue(true);
+    }
+
+    function testSuccessChecker2(int256 val) public {
+        assert(val == val);
+    }
+
+    function testSuccessChecker3(uint32 val) public {
+        assert(val + 0 == val);
+    }
+}
