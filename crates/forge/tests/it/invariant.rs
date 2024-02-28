b279e5529e269aa5158756bef96520740000afbd
grandizzy <38490174+grandizzy@users.noreply.github.com>
2024-02-28 13:25:47 +0200

    invariant (bug): inconsistent shrinked call sequence sometimes (#7256)
    * When shrinking, recreate call sequence by respecting the order from shrunked call indices
    
    * Cover in tests

--- crates/forge/tests/it/invariant.rs
+++ crates/forge/tests/it/invariant.rs
@@ -241,6 +241,21 @@ async fn test_invariant_shrink() {
         // `fuzz_seed` at 119 makes this sequence shrinkable from 4 to 2.
         CounterExample::Sequence(sequence) => {
             assert_eq!(sequence.len(), 2);
+
+            // call order should always be preserved
+            let create_fren_sequence = sequence[0].clone();
+            assert_eq!(
+                create_fren_sequence.contract_name.unwrap(),
+                "fuzz/invariant/common/InvariantInnerContract.t.sol:Jesus"
+            );
+            assert_eq!(create_fren_sequence.signature.unwrap(), "create_fren()");
+
+            let betray_sequence = sequence[1].clone();
+            assert_eq!(
+                betray_sequence.contract_name.unwrap(),
+                "fuzz/invariant/common/InvariantInnerContract.t.sol:Judas"
+            );
+            assert_eq!(betray_sequence.signature.unwrap(), "betray()");
         }
     };
 }
