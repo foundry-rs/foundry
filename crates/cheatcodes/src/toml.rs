a27ffd948b3f12a3d9908093d51decef8d0a7ada
zerosnacks <zerosnacks@protonmail.com>
2024-03-01 14:59:33 +0000

    handle all value cases

--- crates/cheatcodes/src/toml.rs
+++ crates/cheatcodes/src/toml.rs
@@ -3,6 +3,7 @@
 use crate::{Cheatcode, Cheatcodes, Result, Vm::*};
 use alloy_dyn_abi::{DynSolType, DynSolValue};
 use alloy_primitives::{Address, B256, I256};
+use std::collections::BTreeMap;
 use toml::Value;
 
 impl Cheatcode for parseTomlCall {
@@ -33,11 +34,44 @@ fn parse_toml_str(toml: &str) -> Result<Value> {
 pub fn value_to_token(value: &Value) -> Result<DynSolValue> {
     match value {
         Value::Boolean(boolean) => Ok(DynSolValue::Bool(*boolean)),
-        // Integer
-        // Float
-        // Datetime
-        // Array
-        // Table
+        Value::Integer(integer) => {
+            let s = integer.to_string();
+
+            if let Ok(n) = s.parse() {
+                return Ok(DynSolValue::Uint(n, 256))
+            }
+
+            if let Ok(n) = s.parse() {
+                return Ok(DynSolValue::Int(n, 256))
+            }
+
+            Err(fmt_err!("unsupported TOML integer: {integer}"))
+        }
+        Value::Float(float) => {
+            // Check if the number has decimal digits because the EVM does not support floating
+            // point math
+            if float.fract() == 0.0 {
+                let s = float.to_string();
+
+                if s.contains('e') {
+                    if let Ok(n) = s.parse() {
+                        return Ok(DynSolValue::Uint(n, 256))
+                    }
+                    if let Ok(n) = s.parse() {
+                        return Ok(DynSolValue::Int(n, 256))
+                    }
+                }
+            }
+
+            Err(fmt_err!("unsupported TOML float: {float}"))
+        }
+        Value::Datetime(datetime) => Err(fmt_err!("unsupported TOML datetime: {datetime}")),
+        Value::Array(array) => {
+            array.iter().map(value_to_token).collect::<Result<_>>().map(DynSolValue::Array)
+        }
+        Value::Table(table) => {
+            table.values().map(value_to_token).collect::<Result<_>>().map(DynSolValue::Tuple)
+        }
         Value::String(string) => {
             if let Some(mut val) = string.strip_prefix("0x") {
                 let s;
