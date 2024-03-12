75d280302c56dde8dfa8d28d9ef3b205fdd470ea
Matthias Seitz <matthias.seitz@outlook.de>
2024-03-12 23:19:12 +0100

    clippy

--- crates/evm/core/src/opcodes.rs
+++ crates/evm/core/src/opcodes.rs
@@ -5,20 +5,21 @@ use revm::interpreter::OpCode;
 /// Returns true if the opcode modifies memory.
 /// <https://bluealloy.github.io/revm/crates/interpreter/memory.html#opcodes>
 /// <https://github.com/crytic/evm-opcodes>
-pub fn modifies_memory(opcode: OpCode) -> bool {
-    match opcode {
+#[inline]
+pub const fn modifies_memory(opcode: OpCode) -> bool {
+    matches!(
+        opcode,
         OpCode::EXTCODECOPY |
-        OpCode::MLOAD |
-        OpCode::MSTORE |
-        OpCode::MSTORE8 |
-        OpCode::MCOPY |
-        OpCode::CODECOPY |
-        OpCode::CALLDATACOPY |
-        OpCode::RETURNDATACOPY |
-        OpCode::CALL |
-        OpCode::CALLCODE |
-        OpCode::DELEGATECALL |
-        OpCode::STATICCALL => true,
-        _ => false,
-    }
+            OpCode::MLOAD |
+            OpCode::MSTORE |
+            OpCode::MSTORE8 |
+            OpCode::MCOPY |
+            OpCode::CODECOPY |
+            OpCode::CALLDATACOPY |
+            OpCode::RETURNDATACOPY |
+            OpCode::CALL |
+            OpCode::CALLCODE |
+            OpCode::DELEGATECALL |
+            OpCode::STATICCALL
+    )
 }
