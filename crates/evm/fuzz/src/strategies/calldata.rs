d0442de448876d84f95338f76d8ff0bd94b7f861
grandizzy <grandizzy.the.egg@gmail.com>
2024-03-20 09:17:10 +0200

    Update tests to reflect that random values are used if no fixtures

--- crates/evm/fuzz/src/strategies/calldata.rs
+++ crates/evm/fuzz/src/strategies/calldata.rs
@@ -30,3 +30,47 @@ pub fn fuzz_calldata(func: Function, fuzz_fixtures: &FuzzFixtures) -> impl Strat
             .into()
     })
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{strategies::fuzz_calldata, FuzzFixtures};
+    use alloy_dyn_abi::{DynSolValue, JsonAbiExt};
+    use alloy_json_abi::Function;
+    use alloy_primitives::{Address, I256};
+    use proptest::prelude::Strategy;
+    use std::collections::HashMap;
+
+    #[test]
+    fn can_fuzz_with_fixtures() {
+        let function = Function::parse("test_fuzzed_address(address addressFixture)").unwrap();
+
+        let address_fixture = DynSolValue::Address(Address::random());
+        let mut fixtures = HashMap::new();
+        fixtures.insert(
+            "addressFixture".to_string(),
+            DynSolValue::Array(vec![address_fixture.clone()]),
+        );
+
+        let expected = function.abi_encode_input(&[address_fixture]).unwrap();
+        let strategy = fuzz_calldata(function, &FuzzFixtures::new(fixtures));
+        let _ = strategy.prop_map(move |fuzzed| {
+            assert_eq!(expected, fuzzed);
+        });
+    }
+
+    #[test]
+    fn can_fuzz_with_wrong_fixtures_type() {
+        let function = Function::parse("test_fuzzed_uint256(uint256 uintFixture)").unwrap();
+
+        let mut fixtures = HashMap::new();
+        fixtures.insert(
+            "uintFixture".to_string(),
+            DynSolValue::Array(vec![DynSolValue::Int(I256::MAX, 8)]),
+        );
+
+        let strategy = fuzz_calldata(function, &FuzzFixtures::new(fixtures));
+        let _ = strategy.prop_map(move |fuzzed| {
+            assert!(fuzzed.len() > 0);
+        });
+    }
+}
