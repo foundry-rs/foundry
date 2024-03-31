184acc6a924b441b0c4c7a59abf74bd43d2804e5
William Aaron Cheung <git@troublor.xyz>
2024-03-31 14:00:11 -0500

    fix: bug in remapping

--- crates/forge/bin/cmd/clone.rs
+++ crates/forge/bin/cmd/clone.rs
@@ -331,7 +331,7 @@ fn dump_sources(meta: &Metadata, root: &PathBuf) -> Result<Vec<RelativeRemapping
                 std::fs::rename(e.path(), &dest)?;
                 remappings.push(Remapping {
                     context: None,
-                    name: e.file_name().to_string_lossy().to_string(),
+                    name: format!("contracts/{}", e.file_name().to_string_lossy().to_string()),
                     path: dest.to_string_lossy().to_string(),
                 });
             }
