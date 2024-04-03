1b3c1037882ab001d6314f16272b821ca175567f
Mario <mario.ethmax@gmail.com>
2024-04-03 22:41:44 +0300

    Updated to soldeer 0.2.8 and simplified the command parsing on foundry

--- crates/forge/bin/cmd/soldeer.rs
+++ crates/forge/bin/cmd/soldeer.rs
@@ -1,10 +1,7 @@
 use clap::Parser;
 use eyre::Result;
 
-extern crate soldeer_lib;
-
-use soldeer_lib::commands::{Args, Install, Login, Push, Subcommands, Update, VersionDryRun};
-use yansi::Paint;
+use soldeer::commands::Subcommands;
 
 // CLI arguments for `forge soldeer`.
 #[derive(Clone, Debug, Parser)]
@@ -15,91 +12,28 @@ use yansi::Paint;
     forge soldeer version")]
 pub struct SoldeerArgs {
     /// Command must be one of the following install/push/login/update/version.
-    #[clap(required = true)]
-    command: String,
-
-    /// Only push and install can have arguments.
-    #[clap(required = false)]
-    args: Option<Vec<String>>,
+    #[command(subcommand)]
+    command: Subcommands,
 }
 
 impl SoldeerArgs {
     pub fn run(self) -> Result<()> {
-        let args: Args;
-        match self.command.as_str() {
-            "install" => {
-                let remote_url: Option<String>;
-                match self.args.clone().unwrap().len() {
-                    1 => {
-                        remote_url = None;
-                    }
-                    2 => {
-                        remote_url = Some(self.args.clone().unwrap().get(1).unwrap().to_string());
-                    }
-                    _ => {
-                        panic!("Invalid number of arguments");
-                    }
-                }
-                args = Args {
-                    command: Subcommands::Install(Install {
-                        dependency: self.args.clone().unwrap().get(0).unwrap().to_string(),
-                        remote_url,
-                    }),
-                };
-            }
-            "update" => {
-                args = Args { command: Subcommands::Update(Update {}) };
-            }
-            "login" => {
-                args = Args { command: Subcommands::Login(Login {}) };
-            }
-            "push" => {
-                let custom_path: Option<String>;
-                match self.args.clone().unwrap().len() {
-                    1 => {
-                        custom_path = None;
-                    }
-                    2 => {
-                        custom_path = Some(self.args.clone().unwrap().get(1).unwrap().to_string());
-                    }
-                    _ => {
-                        panic!("Invalid number of arguments");
-                    }
-                }
-                args = Args {
-                    command: Subcommands::Push(Push {
-                        dependency: self.args.clone().unwrap().get(0).unwrap().to_string(),
-                        path: custom_path,
-                    }),
-                };
-            }
-            "version" => {
-                args = Args { command: Subcommands::VersionDryRun(VersionDryRun {}) };
-            }
-            _ => {
-                eprintln!("{}", Paint::red("Invalid soldeer command"));
-                std::process::exit(1)
-            }
-        }
-
-        match soldeer_lib::run(args) {
-            Ok(_) => {}
+        match soldeer::run(self.command) {
+            Ok(_) => Ok(()),
             Err(err) => {
-                eprintln!("{}", Paint::red(err.message))
+                Err(eyre::eyre!("Failed to run soldeer {}", err.message))
             }
         }
-
-        Ok(())
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use soldeer::commands::VersionDryRun;
 
     #[test]
     fn test_soldeer_version() {
-        let args: Args = Args { command: Subcommands::VersionDryRun(VersionDryRun {}) };
-        assert_eq!(soldeer_lib::run(args).is_err(), false);
+        assert_eq!(soldeer::run(Subcommands::VersionDryRun(VersionDryRun {})).is_err(), false);
     }
 }
--- crates/forge/tests/cli/config.rs
+++ crates/forge/tests/cli/config.rs
@@ -126,6 +126,8 @@ forgetest!(can_extract_config_values, |prj, cmd| {
         labels: Default::default(),
         cancun: true,
         isolate: true,
+        dependencies: Default::default(),
+
         __non_exhaustive: (),
         __warnings: vec![],
     };
