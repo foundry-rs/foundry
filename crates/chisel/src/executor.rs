b994a65719a36a4a5a775ee8a7f4b580f888babd
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2024-04-05 15:07:35 +0200

    chore: reduce logs in tests (#7566)

--- crates/chisel/src/executor.rs
+++ crates/chisel/src/executor.rs
@@ -1782,9 +1782,6 @@ mod tests {
     }
 
     fn init_tracing() {
-        if std::env::var_os("RUST_LOG").is_none() {
-            std::env::set_var("RUST_LOG", "debug");
-        }
         let _ = tracing_subscriber::FmtSubscriber::builder()
             .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
             .try_init();
