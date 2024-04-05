d68a0de0df2bfda66ca10f083703908274f5526e
zerosnacks <zerosnacks@protonmail.com>
2024-04-05 15:42:30 +0000

    finally works, stupid me didnt realize i had to cross call frames

--- testdata/default/cheats/RecordGas.t.sol
+++ testdata/default/cheats/RecordGas.t.sol
@@ -8,36 +8,16 @@ contract RecordGasTest is DSTest {
     Vm constant vm = Vm(HEVM_ADDRESS);
 
     function testRecordGasA() public {
-        _burn(1);
+        address(0).call("");
         uint64 gasUsed = vm.lastGasUsed();
         emit log_named_uint("gas A", gasUsed);
 
-        _burn(100);
+        address(0).call("");
         gasUsed = vm.lastGasUsed();
         emit log_named_uint("gas B", gasUsed);
 
-        _burn(10000);
+        address(0).call("");
         gasUsed = vm.lastGasUsed();
         emit log_named_uint("gas C", gasUsed);
-    }
-
-    function _burn(uint256 x) internal pure {
-        // Source: https://github.com/vectorized/solady/blob/main/src/utils/GasBurnerLib.sol
-        /// @solidity memory-safe-assembly
-        assembly {
-            mstore(0x10, or(1, x))
-            let n := mul(gt(x, 120), div(x, 91))
-            // We use keccak256 instead of blake2f precompile for better widespread compatibility.
-            for {
-                let i := 0
-            } iszero(eq(i, n)) {
-                i := add(i, 1)
-            } {
-                mstore(0x10, keccak256(0x10, 0x10)) // Yes.
-            }
-            if iszero(mload(0x10)) {
-                invalid()
-            }
-        }
     }
 }
