eea3102a9643ad8a0694b134b4bc812d95b6ab10
zerosnacks <zerosnacks@protonmail.com>
2024-04-05 17:02:16 +0000

    show gas refund and memory expansion

--- testdata/default/cheats/RecordGas.t.sol
+++ testdata/default/cheats/RecordGas.t.sol
@@ -4,35 +4,78 @@ pragma solidity 0.8.18;
 import "ds-test/test.sol";
 import "cheats/Vm.sol";
 
-contract RecordGasTest is DSTest {
-    Vm constant vm = Vm(HEVM_ADDRESS);
+contract Target {
+    uint256 public a;
 
-    function testRecordGasStruct() public {
-        address(0).call("");
-        Vm.Gas memory record = vm.lastGasUsed();
-        _logGasRecord(record);
+    function expandMemory() public pure returns (uint256) {
+        uint256[] memory arr = new uint256[](1000);
 
-        address(0).call("");
-        record = vm.lastGasUsed();
-        _logGasRecord(record);
+        for (uint256 i = 0; i < arr.length; i++) {
+            arr[i] = i;
+        }
 
-        address(0).call("");
-        record = vm.lastGasUsed();
-        _logGasRecord(record);
+        return arr.length;
     }
 
-    function testRecordGasField() public {
+    function set(uint256 _a) public {
+        a = _a;
+    }
+}
+
+contract RecordGasTest is DSTest {
+    Vm constant vm = Vm(HEVM_ADDRESS);
+    Target public target;
+
+    function testNoGasRecord() public {
+        Vm.Gas memory record = vm.lastGasUsed();
+        assertEq(record.gasLimit, 0);
+        assertEq(record.gasTotalUsed, 0);
+        assertEq(record.gasMemoryUsed, 0);
+        assertEq(record.gasRefunded, 0);
+        assertEq(record.gasRemaining, 0);
+    }
+
+    function testRecordGas() public {
         address(0).call("");
+        _logGasRecord();
+
+        address(0).call("");
+        _logGasRecord();
+
+        address(0).call("");
+        _logGasRecord();
+    }
+
+    function testRecordGasMemory() public {
+        target = new Target();
+        target.expandMemory();
+        _logGasRecord();
+    }
+
+    function testRecordGasRefund() public {
+        target = new Target();
+        target.set(1);
+        target.set(0);
+        _logGasRecord();
+    }
+
+    function testRecordGasSingleField() public {
+        address(0).call("");
+        _logGasTotalUsed();
+    }
+
+    function _logGasTotalUsed() internal {
         uint256 gasTotalUsed = vm.lastGasUsed().gasTotalUsed;
         emit log_named_uint("gasTotalUsed", gasTotalUsed);
     }
 
-    function _logGasRecord(Vm.Gas memory record) internal {
+    function _logGasRecord() internal {
+        Vm.Gas memory record = vm.lastGasUsed();
         emit log_named_uint("gasLimit", record.gasLimit);
         emit log_named_uint("gasTotalUsed", record.gasTotalUsed);
         emit log_named_uint("gasMemoryUsed", record.gasMemoryUsed);
         emit log_named_int("gasRefunded", record.gasRefunded);
         emit log_named_uint("gasRemaining", record.gasRemaining);
-        emit log_string("\n");
+        emit log_string("");
     }
 }
