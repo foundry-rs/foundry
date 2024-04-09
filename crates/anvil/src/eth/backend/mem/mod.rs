a1fe0641439898f149563fd6b3700e5341bd45c0
Alexander Gonzalez <alexfertel97@gmail.com>
2024-04-09 16:48:24 +0200

    lint(fmt): fix formatting

--- crates/anvil/src/eth/backend/mem/mod.rs
+++ crates/anvil/src/eth/backend/mem/mod.rs
@@ -621,7 +621,7 @@ impl Backend {
     /// Returns an error if op-stack deposits are not active
     pub fn ensure_op_deposits_active(&self) -> Result<(), BlockchainError> {
         if self.is_optimism() {
-            return Ok(());
+            return Ok(())
         }
         Err(BlockchainError::DepositTransactionUnsupported)
     }
@@ -809,6 +809,7 @@ impl Backend {
         env
     }
 
+    /// Creates an EVM instance with optionally injected precompiles.
     fn new_evm_with_inspector_ref<DB, I>(
         &self,
         db: DB,
@@ -1412,7 +1413,7 @@ impl Backend {
         }
 
         if let Some(fork) = self.get_fork() {
-            return Ok(fork.block_by_hash_full(hash).await?);
+            return Ok(fork.block_by_hash_full(hash).await?)
         }
 
         Ok(None)
@@ -1466,7 +1467,7 @@ impl Backend {
         if let Some(fork) = self.get_fork() {
             let number = self.convert_block_number(Some(number));
             if fork.predates_fork_inclusive(number) {
-                return Ok(fork.block_by_number(number).await?);
+                return Ok(fork.block_by_number(number).await?)
             }
         }
 
@@ -1485,7 +1486,7 @@ impl Backend {
         if let Some(fork) = self.get_fork() {
             let number = self.convert_block_number(Some(number));
             if fork.predates_fork_inclusive(number) {
-                return Ok(fork.block_by_number_full(number).await?);
+                return Ok(fork.block_by_number_full(number).await?)
             }
         }
 
@@ -1849,7 +1850,7 @@ impl Backend {
         }
 
         if let Some(fork) = self.get_fork() {
-            return Ok(fork.trace_transaction(hash).await?);
+            return Ok(fork.trace_transaction(hash).await?)
         }
 
         Ok(vec![])
@@ -1893,7 +1894,7 @@ impl Backend {
         }
 
         if let Some(fork) = self.get_fork() {
-            return Ok(fork.debug_trace_transaction(hash, opts).await?);
+            return Ok(fork.debug_trace_transaction(hash, opts).await?)
         }
 
         Ok(GethTrace::Default(Default::default()))
@@ -1919,7 +1920,7 @@ impl Backend {
 
         if let Some(fork) = self.get_fork() {
             if fork.predates_fork(number) {
-                return Ok(fork.trace_block(number).await?);
+                return Ok(fork.trace_block(number).await?)
             }
         }
 
@@ -2116,9 +2117,7 @@ impl Backend {
         if let Some(fork) = self.get_fork() {
             let number = self.convert_block_number(Some(number));
             if fork.predates_fork(number) {
-                return Ok(fork
-                    .transaction_by_block_number_and_index(number, index.into())
-                    .await?);
+                return Ok(fork.transaction_by_block_number_and_index(number, index.into()).await?)
             }
         }
 
@@ -2135,7 +2134,7 @@ impl Backend {
         }
 
         if let Some(fork) = self.get_fork() {
-            return Ok(fork.transaction_by_block_hash_and_index(hash, index.into()).await?);
+            return Ok(fork.transaction_by_block_hash_and_index(hash, index.into()).await?)
         }
 
         Ok(None)
@@ -2174,10 +2173,7 @@ impl Backend {
         }
 
         if let Some(fork) = self.get_fork() {
-            return fork
-                .transaction_by_hash(hash)
-                .await
-                .map_err(BlockchainError::AlloyForkProvider);
+            return fork.transaction_by_hash(hash).await.map_err(BlockchainError::AlloyForkProvider)
         }
 
         Ok(None)
@@ -2320,7 +2316,7 @@ fn get_pool_transactions_nonce(
         .max()
     {
         let tx_count = highest_nonce.saturating_add(U256::from(1));
-        return Some(tx_count);
+        return Some(tx_count)
     }
     None
 }
@@ -2350,8 +2346,8 @@ impl TransactionValidator for Backend {
             if chain_id.to::<u64>() != tx_chain_id {
                 if let Some(legacy) = tx.as_legacy() {
                     // <https://github.com/ethereum/EIPs/blob/master/EIPS/eip-155.md>
-                    if env.handler_cfg.spec_id >= SpecId::SPURIOUS_DRAGON
-                        && !meets_eip155(chain_id.to::<u64>(), legacy.signature().v())
+                    if env.handler_cfg.spec_id >= SpecId::SPURIOUS_DRAGON &&
+                        !meets_eip155(chain_id.to::<u64>(), legacy.signature().v())
                     {
                         warn!(target: "backend", ?chain_id, ?tx_chain_id, "incompatible EIP155-based V");
                         return Err(InvalidTransactionError::IncompatibleEIP155);
