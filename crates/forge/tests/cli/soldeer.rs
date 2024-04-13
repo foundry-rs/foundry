8acafae39bd661df0e16d02c90a308219bdca8cc
Mario <mario.ethmax@gmail.com>
2024-04-13 12:32:21 +0300

    added cli tests

---
+++ crates/forge/tests/cli/soldeer.rs
@@ -0,0 +1,142 @@
+//! Contains various tests related to `forge soldeer`.
+
+use std::{ fs::{ self, OpenOptions }, path::Path };
+
+use foundry_test_utils::forgesoldeer;
+use std::io::Write;
+forgesoldeer!(install_dependency, |prj, cmd| {
+    let command = "install";
+    let dependency = "forge-std~v1.8.1";
+
+    cmd.arg("soldeer").args([command, dependency]);
+    cmd.execute();
+
+    // Making sure the path was created to the dependency and that foundry.toml exists
+    // meaning that the dependencies were installed correctly
+    let path_dep_forge = prj
+        .root()
+        .join("dependencies")
+        .join("forge-std-v1.8.1")
+        .join("foundry.toml");
+    assert_eq!(path_dep_forge.exists(), true);
+
+    // Making sure the lock contents are the right ones
+    let path_lock_file = prj.root().join("soldeer.lock");
+    let lock_contents =
+        r#"
+[[dependencies]]
+name = "forge-std"
+version = "v1.8.1"
+source = "https://soldeer-revisions.s3.amazonaws.com/forge-std/v1_8_1_23-03-2024_00:05:44_forge-std-v1.8.1.zip"
+checksum = "0f7cd44f5670c31a9646d4031e70c66321cd3ed6ebac3c7278e4e57e4e5c5bd0"
+"#;
+
+    let actual_lock_contents = read_file_to_string(&path_lock_file);
+    assert_eq!(lock_contents, actual_lock_contents);
+
+    // Making sure the foundry contents are the right ones
+    let foundry_file = prj.root().join("foundry.toml");
+    let foundry_contents =
+        r#"[profile.default]
+src = "src"
+out = "out"
+libs = ["lib"]
+
+# See more config options https://github.com/foundry-rs/foundry/blob/master/crates/config/README.md#all-options
+
+[dependencies]
+forge-std = { version = "v1.8.1", url = "https://soldeer-revisions.s3.amazonaws.com/forge-std/v1_8_1_23-03-2024_00:05:44_forge-std-v1.8.1.zip" }
+"#;
+
+    let actual_foundry_contents = read_file_to_string(&foundry_file);
+    assert_eq!(foundry_contents, actual_foundry_contents);
+    // println!("foundry contents {:?}", foundry_contents);
+
+    // let paths = fs::read_dir(prj.root()).unwrap();
+
+    // for path in paths {
+    //     println!("Name: {}", path.unwrap().path().display())
+    // }
+});
+
+forgesoldeer!(update_dependencies, |prj, cmd| {
+    let command = "update";
+
+    // We need to write this into the foundry.toml to make the update install the dependency
+    let foundry_updates =
+        r#"
+[dependencies]
+forge-std = { version = "v1.8.1", url = "https://soldeer-revisions.s3.amazonaws.com/forge-std/v1_8_1_23-03-2024_00:05:44_forge-std-v1.8.1.zip" }
+"#;
+    let foundry_file = prj.root().join("foundry.toml");
+
+    let mut file = OpenOptions::new().write(true).append(true).open(&foundry_file).unwrap();
+
+    if let Err(e) = write!(file, "{}", foundry_updates) {
+        eprintln!("Couldn't write to file: {}", e);
+    }
+
+    cmd.arg("soldeer").arg(command);
+    cmd.execute();
+
+    // Making sure the path was created to the dependency and that foundry.toml exists
+    // meaning that the dependencies were installed correctly
+    let path_dep_forge = prj
+        .root()
+        .join("dependencies")
+        .join("forge-std-v1.8.1")
+        .join("foundry.toml");
+    assert_eq!(path_dep_forge.exists(), true);
+
+    // Making sure the lock contents are the right ones
+    let path_lock_file = prj.root().join("soldeer.lock");
+    let lock_contents =
+        r#"
+[[dependencies]]
+name = "forge-std"
+version = "v1.8.1"
+source = "https://soldeer-revisions.s3.amazonaws.com/forge-std/v1_8_1_23-03-2024_00:05:44_forge-std-v1.8.1.zip"
+checksum = "0f7cd44f5670c31a9646d4031e70c66321cd3ed6ebac3c7278e4e57e4e5c5bd0"
+"#;
+
+    let actual_lock_contents = read_file_to_string(&path_lock_file);
+    assert_eq!(lock_contents, actual_lock_contents);
+
+    // Making sure the foundry contents are the right ones
+    let foundry_contents =
+        r#"[profile.default]
+src = "src"
+out = "out"
+libs = ["lib"]
+
+# See more config options https://github.com/foundry-rs/foundry/blob/master/crates/config/README.md#all-options
+
+[dependencies]
+forge-std = { version = "v1.8.1", url = "https://soldeer-revisions.s3.amazonaws.com/forge-std/v1_8_1_23-03-2024_00:05:44_forge-std-v1.8.1.zip" }
+"#;
+
+    let actual_foundry_contents = read_file_to_string(&foundry_file);
+    assert_eq!(foundry_contents, actual_foundry_contents);
+});
+
+forgesoldeer!(login, |prj, cmd| {
+    let command = "login";
+
+    cmd.arg("soldeer").arg(command);
+    let output = cmd.unchecked_output();
+
+    // On login, we can only check if the prompt is displayed in the stdout
+    let stdout = String::from_utf8(output.stdout).expect("Could not parse the output");
+    assert_eq!(stdout.contains("Please enter your email"), true);
+});
+
+fn read_file_to_string(path: &Path) -> String {
+    let contents: String = match fs::read_to_string(path) {
+        Ok(c) => c,
+        Err(_) => {
+            // Return empty string
+            String::new()
+        }
+    };
+    contents
+}
