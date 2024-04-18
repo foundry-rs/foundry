89a867ae028a77f94639b8ce3c25fe23f56fabc2
kamuik16 <krishang.s@ahduni.edu.in>
2024-04-18 09:50:27 +0530

    rename path to root

--- crates/forge/bin/cmd/getsolc.rs
+++ crates/forge/bin/cmd/getsolc.rs
@@ -9,42 +9,37 @@ use yansi::Paint;
 pub struct GetSolcArgs {
     /// The path of the project to get solc for.
     #[arg(value_hint = ValueHint::DirPath, default_value = ".", value_name = "PATH")]
-    path: PathBuf,
+    root: PathBuf,
 }
 
 impl GetSolcArgs {
     pub fn run(self) -> Result<()> {
-        let GetSolcArgs { path } = self;
-        get_solc_from_path(path)?;
+        let GetSolcArgs { root } = self;
+        let files = utils::source_files(root);
+        let sources = Source::read_all(files)?;
+        let project = Project::builder().build()?;
+        let graph = Graph::resolve_sources(&project.paths, sources)?;
+        let (versions, _) = graph.into_sources_by_version(project.offline)?;
+        let sources_by_version = versions.get(&project)?;
+        let versions: Vec<_> =
+            sources_by_version.keys().map(|solc| solc.version_short().unwrap()).collect();
+        if versions.len() == 1 {
+            let version = &versions[0];
+            println!(
+                "{} {}.{}.{}",
+                Paint::green("Solc"),
+                version.major,
+                version.minor,
+                version.patch
+            );
+        } else {
+            let formatted_versions: Vec<_> = versions
+                .iter()
+                .map(|version| format!("{}.{}.{}", version.major, version.minor, version.patch))
+                .collect();
+            let versions_str = formatted_versions.join(", ");
+            println!("{} {}", Paint::green("Solcs"), versions_str);
+        }
         Ok(())
     }
-}
-
-pub fn get_solc_from_path(path: PathBuf) -> Result<()> {
-    let files = utils::source_files(path);
-    let sources = Source::read_all(files)?;
-    let project = Project::builder().build()?;
-    let graph = Graph::resolve_sources(&project.paths, sources)?;
-    let (versions, _) = graph.into_sources_by_version(project.offline)?;
-    let sources_by_version = versions.get(&project)?;
-    let versions: Vec<_> =
-        sources_by_version.keys().map(|solc| solc.version_short().unwrap()).collect();
-    if versions.len() == 1 {
-        let version = &versions[0];
-        println!(
-            "Solc {}",
-            Paint::green(format!("{}.{}.{}", version.major, version.minor, version.patch))
-        );
-    } else {
-        let formatted_versions: Vec<_> = versions
-            .iter()
-            .map(|version| {
-                Paint::green(format!("{}.{}.{}", version.major, version.minor, version.patch))
-                    .to_string()
-            })
-            .collect();
-        let versions_str = formatted_versions.join(", ");
-        println!("Solcs {}", versions_str);
-    }
-    Ok(())
 }
