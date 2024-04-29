6b6455e55bb3823da237ba1e0a3ce9ce2d28d252
Alisina Bahadori <alisina.bm@gmail.com>
2024-04-29 10:49:33 -0400

    Fix test signed result

--- crates/anvil/tests/it/sign.rs
+++ crates/anvil/tests/it/sign.rs
@@ -303,7 +303,7 @@ async fn can_sign_transaction() {
     // sign it via the eth_signTransaction API
     let signed_tx = api.sign_transaction(tx).await.unwrap();
 
-    assert_eq!(signed_tx, "0x02f868827a690a65648252089470997970c51812dc3a010c7d01b50e0d17dc79c88203e980c001a0e4de88aefcf87ccb04466e60de66a83192e46aa26177d5ea35efbfd43fd0ecdca00e3148e0e8e0b9a6f9b329efd6e30c4a461920f3a27497be3dbefaba996601da");
+    assert_eq!(signed_tx, "0x02f868827a690a65648252089470997970c51812dc3a010c7d01b50e0d17dc79c88203e980c082f4f6a0e4de88aefcf87ccb04466e60de66a83192e46aa26177d5ea35efbfd43fd0ecdca00e3148e0e8e0b9a6f9b329efd6e30c4a461920f3a27497be3dbefaba996601da");
 }
 
 #[tokio::test(flavor = "multi_thread")]
