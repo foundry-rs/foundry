cc7032a010cddeec577de2adc3d7f6ecb916203b
Meet Mangukiya <meet@flamy.dev>
2024-05-07 19:07:09 +0530

    fix test

--- testdata/default/cheats/Namehash.t.sol
+++ testdata/default/cheats/Namehash.t.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.18;
 import "ds-test/test.sol";
 import "cheats/Vm.sol";
 
-contract NamehashTest is Test {
+contract NamehashTest is DSTest {
     Vm constant vm = Vm(HEVM_ADDRESS);
 
     function testNamehash() public {
