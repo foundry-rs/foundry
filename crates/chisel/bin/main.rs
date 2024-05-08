a4d79ac5495b6294610c0a0bd5e4e13efdca13ee
Yash Atreya <44857776+yash-atreya@users.noreply.github.com>
2024-05-08 08:01:57 -0400

    chore: deprecate `forge bind --ethers` (#7886)
    Co-authored-by: DaniPopes <57450786+DaniPopes@users.noreply.github.com>

--- crates/chisel/bin/main.rs
+++ crates/chisel/bin/main.rs
@@ -272,10 +272,11 @@ async fn evaluate_prelude(
         load_prelude_file(dispatcher, prelude_dir).await?;
         println!("{}\n", "Prelude source file loaded successfully!".green());
     } else {
-        let prelude_sources = fs::files_with_ext(prelude_dir, "sol");
-        let print_success_msg = !prelude_sources.is_empty();
+        let prelude_sources = fs::files_with_ext(&prelude_dir, "sol");
+        let mut print_success_msg = false;
         for source_file in prelude_sources {
-            println!("{} {}", "Loading prelude source file:".yellow(), source_file.display(),);
+            print_success_msg = true;
+            println!("{} {}", "Loading prelude source file:".yellow(), source_file.display());
             load_prelude_file(dispatcher, source_file).await?;
         }
 
