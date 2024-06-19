52d69d9d7e2039408364298a6bb359e7c21297ec
fenghaojiang <fenghaojiang@bituniverse.org>
2024-06-19 11:03:13 +0800

    feat(cast): support readable time for cast block

--- crates/common/src/fmt/ui.rs
+++ crates/common/src/fmt/ui.rs
@@ -489,6 +489,7 @@ sha3Uncles           {}
 size                 {}
 stateRoot            {}
 timestamp            {}
+time                 {}
 withdrawalsRoot      {}
 totalDifficulty      {}{}",
         block.header.base_fee_per_gas.pretty(),
@@ -509,6 +510,7 @@ totalDifficulty      {}{}",
         block.size.pretty(),
         block.header.state_root.pretty(),
         block.header.timestamp.pretty(),
+        chrono::DateTime::from_timestamp(block.header.timestamp as i64, 0).expect("block timestamp in range").to_rfc2822(),
         block.header.withdrawals_root.pretty(),
         block.header.total_difficulty.pretty(),
         block.other.pretty()
