4af6cfaef200ad3ffa598ba419f79b15cb962e52
funnybird <davidakaucherzmq81@gmail.com>
2024-06-19 17:20:08 +0800

    feat(cast): ux upgrade cast block returning block time in readable format.  (#8195)
    * feat(cast): support readable time for cast block
    
    * fix: remove `time`
    
    * Update crates/common/src/fmt/ui.rs
    
    * fmt
    
    ---------
    
    Co-authored-by: fenghaojiang <fenghaojiang@bituniverse.org>
    Co-authored-by: DaniPopes <57450786+DaniPopes@users.noreply.github.com>

--- crates/common/src/fmt/ui.rs
+++ crates/common/src/fmt/ui.rs
@@ -488,7 +488,7 @@ receiptsRoot         {}
 sha3Uncles           {}
 size                 {}
 stateRoot            {}
-timestamp            {}
+timestamp            {} ({})
 withdrawalsRoot      {}
 totalDifficulty      {}{}",
         block.header.base_fee_per_gas.pretty(),
@@ -509,6 +509,9 @@ totalDifficulty      {}{}",
         block.size.pretty(),
         block.header.state_root.pretty(),
         block.header.timestamp.pretty(),
+        chrono::DateTime::from_timestamp(block.header.timestamp as i64, 0)
+            .expect("block timestamp in range")
+            .to_rfc2822(),
         block.header.withdrawals_root.pretty(),
         block.header.total_difficulty.pretty(),
         block.other.pretty()
