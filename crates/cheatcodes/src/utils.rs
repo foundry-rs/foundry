abd8d55c36e4b717de844b43395b22150a813a9e
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2024-06-24 18:45:39 +0200

    fix: overflow in randomUint (#8239)

--- crates/cheatcodes/src/utils.rs
+++ crates/cheatcodes/src/utils.rs
@@ -158,11 +158,12 @@ impl Cheatcode for randomUint_0Call {
 
 impl Cheatcode for randomUint_1Call {
     fn apply(&self, _state: &mut Cheatcodes) -> Result {
-        let Self { min, max } = self;
+        let Self { min, max } = *self;
+        ensure!(min <= max, "min must be less than or equal to max");
         // Generate random between range min..=max
         let mut rng = rand::thread_rng();
-        let range = *max - *min + U256::from(1);
-        let random_number = rng.gen::<U256>() % range + *min;
+        let range = max - min + U256::from(1);
+        let random_number = rng.gen::<U256>() % range + min;
         Ok(random_number.abi_encode())
     }
 }
