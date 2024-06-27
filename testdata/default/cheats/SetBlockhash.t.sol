0ece26755313cdbc4ad53885775113e74eba2f99
Tuan Tran <tropicaldog17@gmail.com>
2024-06-27 17:28:54 +0700

    fix test

--- testdata/default/cheats/SetBlockhash.t.sol
+++ testdata/default/cheats/SetBlockhash.t.sol
@@ -8,9 +8,9 @@ contract SetBlockhash is DSTest {
     Vm constant vm = Vm(HEVM_ADDRESS);
 
     function testSetBlockhash() public {
-        bytes32 blockhash = 0x1234567890123456789012345678901234567890123456789012345678901234;
-        vm.setBlockhash(1, blockhash);
-        bytes32 expected = vm.blockhash(1);
-        assertEq(actual, expected);
+        bytes32 blockHash = 0x1234567890123456789012345678901234567890123456789012345678901234;
+        vm.setBlockhash(1, blockHash);
+        bytes32 expected = blockhash(1);
+        assertEq(blockHash, expected);
     }
 }
