e00058e8457986b30221a70a47ab38747c12957e
Arsenii Kulikov <klkvrr@gmail.com>
2024-07-19 06:59:42 +0800

    fix tests

--- crates/forge/tests/cli/config.rs
+++ crates/forge/tests/cli/config.rs
@@ -147,6 +147,7 @@ forgetest!(can_extract_config_values, |prj, cmd| {
         warnings: vec![],
         assertions_revert: true,
         legacy_assertions: false,
+        extra_args: vec![],
         eof_version: None,
         _non_exhaustive: (),
     };
