f7f1240b971887ff5958bcde2961b99311896d28
Paul Razvan Berg <prberg@proton.me>
2024-07-26 17:24:23 +0300

    docs: document unit for --with-gas-price (#8370)
    * docs: document unit for --with-gas-price
    
    * chore: apply same doc style to clarify string with denoms can be used
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Enrique Ortiz <hi@enriqueortiz.dev>
    Co-authored-by: DaniPopes <57450786+DaniPopes@users.noreply.github.com>

--- crates/script/src/lib.rs
+++ crates/script/src/lib.rs
@@ -169,7 +169,10 @@ pub struct ScriptArgs {
     #[arg(long)]
     pub json: bool,
 
-    /// Gas price for legacy transactions, or max fee per gas for EIP1559 transactions.
+    /// Gas price for legacy transactions, or max fee per gas for EIP1559 transactions, either
+    /// specified in wei, or as a string with a unit type.
+    ///
+    /// Examples: 1ether, 10gwei, 0.01ether
     #[arg(
         long,
         env = "ETH_GAS_PRICE",
