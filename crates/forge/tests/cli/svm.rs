51647a24572cf45709f00d60c0a8786af33e79b1
zerosnacks <zerosnacks@protonmail.com>
2024-08-23 10:01:48 +0000

    fix placeholder redacts

--- crates/forge/tests/cli/svm.rs
+++ crates/forge/tests/cli/svm.rs
@@ -60,15 +60,15 @@ contract CounterTest is Test {{
     cmd.arg("test").assert_success().stdout_eq(str![[r#"
 ...
 Ran 1 test for test/Counter.sol:CounterTest
-[PASS] testAssert() (gas: [..])
-Suite result: ok. 1 passed; 0 failed; 0 skipped; finished in [..] ([..] CPU time)
+[PASS] testAssert() ([GAS])
+Suite result: ok. 1 passed; 0 failed; 0 skipped; [ELAPSED]
 ...
 Ran 2 tests for test/Counter.t.sol:CounterTest
-[PASS] testFuzz_SetNumber(uint256) (runs: [..], [..]: [..], [..]: [..])
-[PASS] test_Increment() (gas: [..])
-Suite result: ok. 2 passed; 0 failed; 0 skipped; finished in [..] ([..] CPU time)
+[PASS] testFuzz_SetNumber(uint256) (runs: 256, [AVG_GAS])
+[PASS] test_Increment() ([GAS])
+Suite result: ok. 2 passed; 0 failed; 0 skipped; [ELAPSED]
 
-Ran 2 test suites in [..] ([..] CPU time): 3 tests passed, 0 failed, 0 skipped (3 total tests)
+Ran 2 test suites [ELAPSED]: 3 tests passed, 0 failed, 0 skipped (3 total tests)
 
 "#]]);
 });
