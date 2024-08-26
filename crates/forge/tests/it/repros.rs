79a910a643cdde5f5740d6652996d7ac97a715f4
grandizzy <38490174+grandizzy@users.noreply.github.com>
2024-08-26 17:36:41 +0300

    fix(cheatcodes): reconcile gas when exiting frame (#8744)
    * Fix for 4370 + tests
    
    * assert on test kind gas

--- crates/forge/tests/it/repros.rs
+++ crates/forge/tests/it/repros.rs
@@ -9,7 +9,10 @@ use crate::{
 use alloy_dyn_abi::{DecodedEvent, DynSolValue, EventExt};
 use alloy_json_abi::Event;
 use alloy_primitives::{address, b256, Address, U256};
-use forge::{decode::decode_console_logs, result::TestStatus};
+use forge::{
+    decode::decode_console_logs,
+    result::{TestKind, TestStatus},
+};
 use foundry_config::{fs_permissions::PathPermission, Config, FsPermissions};
 use foundry_evm::{
     constants::HARDHAT_CONSOLE_ADDRESS,
@@ -363,7 +366,15 @@ test_repro!(8287);
 test_repro!(8168);
 
 // https://github.com/foundry-rs/foundry/issues/8383
-test_repro!(8383);
+test_repro!(8383, false, None, |res| {
+    let mut res = res.remove("default/repros/Issue8383.t.sol:Issue8383Test").unwrap();
+    let test = res.test_results.remove("testP256VerifyOutOfBounds()").unwrap();
+    assert_eq!(test.status, TestStatus::Success);
+    match test.kind {
+        TestKind::Unit { gas } => assert_eq!(gas, 3103),
+        _ => panic!("not a unit test kind"),
+    }
+});
 
 // https://github.com/foundry-rs/foundry/issues/1543
 test_repro!(1543);
