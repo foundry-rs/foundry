2dff96485bdc7433058cf1683488144a282804ff
zerosnacks <zerosnacks@protonmail.com>
2024-08-28 13:12:48 +0000

    basic tests

--- testdata/default/cheats/GasSnapshot.t.sol
+++ testdata/default/cheats/GasSnapshot.t.sol
@@ -5,9 +5,11 @@ import "ds-test/test.sol";
 import "cheats/Vm.sol";
 
 contract Flare {
+    bytes32[] public data;
+
     function run(uint256 n) public {
         for (uint256 i = 0; i < n; i++) {
-            keccak256(abi.encodePacked(i));
+            data.push(keccak256(abi.encodePacked(i)));
         }
     }
 }
@@ -28,16 +30,22 @@ contract GasSnapshotTest is DSTest {
     function testSnapshotGasSection() public {
         Flare a = new Flare();
 
-        a.run(100);
+        a.run(64);
 
         vm.startSnapshotGas("testSnapshotGasSection");
 
-        a.run(1000);
-        a.run(1000);
+        a.run(256); // 5_821_576 gas
+        a.run(512); // 11_617_936 gas
 
         (bool success, uint256 gasUsed) = vm.stopSnapshotGas(
             "testSnapshotGasSection"
         );
         assertTrue(success);
+        assertEq(gasUsed, 17_439_512); // 5_821_576 + 11_617_936 = 17_439_512 gas
+
+        string memory value = vm.readFile(
+            "snapshots/testSnapshotGasSection.json"
+        );
+        assertEq(value, '"17439512"');
     }
 }
