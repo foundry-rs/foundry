e65037143fe0ad40e5580f53a23040fe1751cc65
Felipe Buiras <f.morisan@gmail.com>
2024-08-30 11:45:19 -0300

    fix: add check for address parsing when input is one-off

--- crates/cheatcodes/src/json.rs
+++ crates/cheatcodes/src/json.rs
@@ -576,7 +576,10 @@ pub(super) fn json_value_to_token(value: &Value) -> Result<DynSolValue> {
         }
         Value::String(string) => {
             if let Some(mut val) = string.strip_prefix("0x") {
-                let s;
+                let s; 
+                if val.len() == 39 {
+                    return Err(format!("Cannot parse \"{val}\" as an address. If you want to specify bytes, prepend zeroes to the value.").into())
+                }
                 if val.len() % 2 != 0 {
                     s = format!("0{val}");
                     val = &s[..];
