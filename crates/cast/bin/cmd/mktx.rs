9f125b3b4f66ffd328eef17f76e60e6eeecbfc41
Arsenii Kulikov <klkvrr@gmail.com>
2024-09-03 00:06:41 +0400

    fix(eip7702): doc and small fixes

--- crates/cast/bin/cmd/mktx.rs
+++ crates/cast/bin/cmd/mktx.rs
@@ -6,7 +6,7 @@ use clap::Parser;
 use eyre::Result;
 use foundry_cli::{
     opts::{EthereumOpts, TransactionOpts},
-    utils::{self, get_provider},
+    utils::get_provider,
 };
 use foundry_common::ens::NameOrAddress;
 use foundry_config::Config;
@@ -83,9 +83,6 @@ impl MakeTxArgs {
         };
 
         let config = Config::from(&eth);
-        let provider = utils::get_provider(&config)?;
-
-        let tx_kind = tx::resolve_tx_kind(&provider, &code, &to).await?;
 
         // Retrieve the signer, and bail if it can't be constructed.
         let signer = eth.wallet.signer().await?;
@@ -97,7 +94,8 @@ impl MakeTxArgs {
 
         let (tx, _) = CastTxBuilder::new(provider, tx, &config)
             .await?
-            .with_tx_kind(tx_kind)
+            .with_to(to)
+            .await?
             .with_code_sig_and_args(code, sig, args)
             .await?
             .with_blob_data(blob_data)?
