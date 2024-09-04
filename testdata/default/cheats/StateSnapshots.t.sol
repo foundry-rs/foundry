1cacb51b80a92aa46fc9dfc93bc3752e900269f6
zerosnacks <zerosnacks@protonmail.com>
2024-09-04 12:55:06 +0000

    add test for deprecated cheatcodes

--- testdata/default/cheats/StateSnapshots.t.sol
+++ testdata/default/cheats/StateSnapshots.t.sol
@@ -98,8 +98,127 @@ contract StateSnapshotTest is DSTest {
 
         assert(vm.revertToState(snapshotId));
 
-        assertEq(block.number, num, "snapshot revert for block.number unsuccessful");
-        assertEq(block.timestamp, time, "snapshot revert for block.timestamp unsuccessful");
-        assertEq(block.prevrandao, prevrandao, "snapshot revert for block.prevrandao unsuccessful");
+        assertEq(
+            block.number,
+            num,
+            "snapshot revert for block.number unsuccessful"
+        );
+        assertEq(
+            block.timestamp,
+            time,
+            "snapshot revert for block.timestamp unsuccessful"
+        );
+        assertEq(
+            block.prevrandao,
+            prevrandao,
+            "snapshot revert for block.prevrandao unsuccessful"
+        );
+    }
+}
+
+contract DeprecatedStateSnapshotTest is DSTest {
+    Vm constant vm = Vm(HEVM_ADDRESS);
+
+    Storage store;
+
+    function setUp() public {
+        store.slot0 = 10;
+        store.slot1 = 20;
+    }
+
+    function testSnapshotState() public {
+        uint256 snapshotId = vm.snapshot();
+        store.slot0 = 300;
+        store.slot1 = 400;
+
+        assertEq(store.slot0, 300);
+        assertEq(store.slot1, 400);
+
+        vm.revertTo(snapshotId);
+        assertEq(store.slot0, 10, "snapshot revert for slot 0 unsuccessful");
+        assertEq(store.slot1, 20, "snapshot revert for slot 1 unsuccessful");
+    }
+
+    function testSnapshotStateRevertDelete() public {
+        uint256 snapshotId = vm.snapshot();
+        store.slot0 = 300;
+        store.slot1 = 400;
+
+        assertEq(store.slot0, 300);
+        assertEq(store.slot1, 400);
+
+        vm.revertToAndDelete(snapshotId);
+        assertEq(store.slot0, 10, "snapshot revert for slot 0 unsuccessful");
+        assertEq(store.slot1, 20, "snapshot revert for slot 1 unsuccessful");
+        // nothing to revert to anymore
+        assert(!vm.revertTo(snapshotId));
+    }
+
+    function testSnapshotStateDelete() public {
+        uint256 snapshotId = vm.snapshot();
+        store.slot0 = 300;
+        store.slot1 = 400;
+
+        vm.deleteSnapshot(snapshotId);
+        // nothing to revert to anymore
+        assert(!vm.revertTo(snapshotId));
+    }
+
+    function testSnapshotStateDeleteAll() public {
+        uint256 snapshotId = vm.snapshot();
+        store.slot0 = 300;
+        store.slot1 = 400;
+
+        vm.deleteSnapshots();
+        // nothing to revert to anymore
+        assert(!vm.revertTo(snapshotId));
+    }
+
+    // <https://github.com/foundry-rs/foundry/issues/6411>
+    function testSnapshotStatesMany() public {
+        uint256 snapshotId;
+        for (uint256 c = 0; c < 10; c++) {
+            for (uint256 cc = 0; cc < 10; cc++) {
+                snapshotId = vm.snapshot();
+                vm.revertToAndDelete(snapshotId);
+                assert(!vm.revertTo(snapshotId));
+            }
+        }
+    }
+
+    // tests that snapshots can also revert changes to `block`
+    function testBlockValues() public {
+        uint256 num = block.number;
+        uint256 time = block.timestamp;
+        uint256 prevrandao = block.prevrandao;
+
+        uint256 snapshotId = vm.snapshot();
+
+        vm.warp(1337);
+        assertEq(block.timestamp, 1337);
+
+        vm.roll(99);
+        assertEq(block.number, 99);
+
+        vm.prevrandao(uint256(123));
+        assertEq(block.prevrandao, 123);
+
+        assert(vm.revertTo(snapshotId));
+
+        assertEq(
+            block.number,
+            num,
+            "snapshot revert for block.number unsuccessful"
+        );
+        assertEq(
+            block.timestamp,
+            time,
+            "snapshot revert for block.timestamp unsuccessful"
+        );
+        assertEq(
+            block.prevrandao,
+            prevrandao,
+            "snapshot revert for block.prevrandao unsuccessful"
+        );
     }
 }
