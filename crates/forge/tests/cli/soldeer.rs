807b12ca177f786cb8364c3e0ad3bad9850c5647
Mario <mario.ethmax@gmail.com>
2024-09-04 14:57:55 -0400

    updated output

--- crates/forge/tests/cli/soldeer.rs
+++ crates/forge/tests/cli/soldeer.rs
@@ -223,17 +223,17 @@ Started HTTP download of @tt~1.6.1
 Started GIT download of mario~1.0
 Started GIT download of mario-custom-branch~1.0
 Started GIT download of mario-custom-tag~1.0
-Started HTTP download of forge-std~1.8.1
 Started HTTP download of solmate~6.7.0
+Started HTTP download of forge-std~1.8.1
 Dependency @tt~1.6.1 downloaded!
 Dependency solmate~6.7.0 downloaded!
-Dependency forge-std~1.8.1 downloaded!
-Successfully downloaded mario-custom-branch~1.0 the dependency via git
-Dependency mario-custom-branch~1.0 downloaded!
 Successfully downloaded mario-custom-tag~1.0 the dependency via git
 Dependency mario-custom-tag~1.0 downloaded!
+Dependency forge-std~1.8.1 downloaded!
 Successfully downloaded mario~1.0 the dependency via git
 Dependency mario~1.0 downloaded!
+Successfully downloaded mario-custom-branch~1.0 the dependency via git
+Dependency mario-custom-branch~1.0 downloaded!
 The dependency @tt~1.6.1 was unzipped!
 The dependency forge-std~1.8.1 was unzipped!
 The dependency solmate~6.7.0 was unzipped!
