fa0f82f23a3e3e34d6c4d3246a0477574fc78d7c
Vaishnavi Singh <gaursinghvaishnavi@gmail.com>
2024-09-12 01:15:55 +0530

    boundLog added

--- testdata/default/cheats/Wallet.t.sol
+++ testdata/default/cheats/Wallet.t.sol
@@ -9,10 +9,20 @@ contract Foo {}
 contract WalletTest is DSTest {
     Vm constant vm = Vm(HEVM_ADDRESS);
 
-    uint256 internal constant Q = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141;
+    uint256 internal constant Q = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141; // constant acc to secp256k1 for generating PK
     uint256 private constant UINT256_MAX =
-        115792089237316195423570985008687907853269984665640564039457584007913129639935;
+        115792089237316195423570985008687907853269984665640564039457584007913129639935; // max num stored in uin256
 
+    enum DistributionType { Uniform, Logarithmic } 
+    struct ParamConfig {
+        uint256 min;
+        uint256 max;
+        DistributionType distributionType;
+        uint256[] fixtures;
+        uint256[] excluded;
+    }
+
+    // converts Public key to Ethereum address using keccak256 hash
     function addressOf(uint256 x, uint256 y) internal pure returns (address) {
         return address(uint160(uint256(keccak256(abi.encode(x, y)))));
     }
@@ -44,6 +54,15 @@ contract WalletTest is DSTest {
         }
     }
 
+    
+
+
+
+
+
+
+
+    // tests that wallet is created with the address derived from PK and label is set correctly
     function testCreateWalletStringPrivAndLabel() public {
         bytes memory privKey = "this is a priv key";
         Vm.Wallet memory wallet = vm.createWallet(string(privKey));
@@ -60,6 +79,7 @@ contract WalletTest is DSTest {
         assertEq(label, string(privKey), "labelled address != wallet.addr");
     }
 
+    // tests creation of PK using a seed
     function testCreateWalletPrivKeyNoLabel(uint256 pkSeed) public {
         uint256 pk = bound(pkSeed, 1, Q - 1);
 
@@ -74,6 +94,7 @@ contract WalletTest is DSTest {
         assertEq(expectedAddr, wallet.addr);
     }
 
+     // tests creation of PK using a seed and checks labels too 
     function testCreateWalletPrivKeyWithLabel(uint256 pkSeed) public {
         string memory label = "labelled wallet";
 
@@ -92,7 +113,7 @@ contract WalletTest is DSTest {
         string memory expectedLabel = vm.getLabel(wallet.addr);
         assertEq(expectedLabel, label, "labelled address != wallet.addr");
     }
-
+    // tests signing a has using PK and checks the address recovered from the signautre is correct wallet address
     function testSignWithWalletDigest(uint256 pkSeed, bytes32 digest) public {
         uint256 pk = bound(pkSeed, 1, Q - 1);
 
@@ -103,7 +124,7 @@ contract WalletTest is DSTest {
         address recovered = ecrecover(digest, v, r, s);
         assertEq(recovered, wallet.addr);
     }
-
+    // tests signing a has using PK and checks the address recovered from the signautre is correct wallet address and also checks the signature is compact
     function testSignCompactWithWalletDigest(uint256 pkSeed, bytes32 digest) public {
         uint256 pk = bound(pkSeed, 1, Q - 1);
 
@@ -125,6 +146,177 @@ contract WalletTest is DSTest {
         address recovered = ecrecover(digest, v, r, s);
         assertEq(recovered, wallet.addr);
     }
+    // signs a message after performing the checks in above functions
+    function testSignWithWalletMessage(uint256 pkSeed, bytes memory message) public {
+        testSignWithWalletDigest(pkSeed, keccak256(message));
+    }
+    //     // signs a message after performing the checks in above functions in compact way 
+    function testSignCompactWithWalletMessage(uint256 pkSeed, bytes memory message) public {
+        testSignCompactWithWalletDigest(pkSeed, keccak256(message));
+    }
+    // check sthe nonces of the wallet before and after a prank
+    function testGetNonceWallet(uint256 pkSeed) public {
+        uint256 pk = bound(pkSeed, 1, Q - 1);
+
+        Vm.Wallet memory wallet = vm.createWallet(pk);
+
+        uint64 nonce1 = vm.getNonce(wallet);
+
+        vm.startPrank(wallet.addr);
+        new Foo();
+        new Foo();
+        vm.stopPrank();
+
+        uint64 nonce2 = vm.getNonce(wallet);
+        assertEq(nonce1 + 2, nonce2);
+    }
+}
+
+
+//////////////////////// @title A title that should describe the contract/interface
+/// @author The name of the author
+/// @notice Explain to an end user what this does
+/// @dev Explain to a developer any extra details
+
+
+
+/
+
+contract WalletTest is DSTest {
+    Vm constant vm = Vm(HEVM_ADDRESS);
+
+    uint256 internal constant Q = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141;
+    uint256 private constant UINT256_MAX =
+        115792089237316195423570985008687907853269984665640564039457584007913129639935;
+
+   
+
+    ParamConfig public pkConfig;
+
+    constructor() {
+        pkConfig = ParamConfig({
+            min: 1,
+            max: Q - 1,
+            distributionType: DistributionType.Logarithmic,
+            fixtures: new uint256[](0),
+            excluded: new uint256[](0)
+        });
+    }
+
+    function addressOf(uint256 x, uint256 y) internal pure returns (address) {
+        return address(uint160(uint256(keccak256(abi.encode(x, y)))));
+    }
+
+    function bound(uint256 x, ParamConfig memory config) internal pure returns (uint256) {
+        if (config.distributionType == DistributionType.Logarithmic) {
+            return boundLog(x, config.min, config.max);
+        }
+        
+        require(config.min <= config.max, "min needs to be less than max");
+        if (x >= config.min && x <= config.max) return x;
+        uint256 size = config.max - config.min + 1;
+        if (x <= 3 && size > x) return config.min + x;
+        if (x >= UINT256_MAX - 3 && size > UINT256_MAX - x) return config.max - (UINT256_MAX - x);
+        if (x > config.max) {
+            uint256 diff = x - config.max;
+            uint256 rem = diff % size;
+            if (rem == 0) return config.max;
+            return config.min + rem - 1;
+        } else if (x < config.min) {
+            uint256 diff = config.min - x;
+            uint256 rem = diff % size;
+            if (rem == 0) return config.min;
+            return config.max - rem + 1;
+        }
+    }
+
+    function boundLog(uint256 x, uint256 min, uint256 max) internal pure returns (uint256) {
+        require(min < max, "min must be less than max");
+        require(min > 0, "min must be greater than 0 for log distribution");
+
+        uint256 logMin = log2Approximation(min);
+        uint256 logMax = log2Approximation(max);
+        
+        uint256 logValue = bound(x, ParamConfig(logMin, logMax, DistributionType.Uniform, new uint256[](0), new uint256[](0)));
+        
+        return exp2Approximation(logValue);
+    }
+
+    function log2Approximation(uint256 x) internal pure returns (uint256) {
+        require(x > 0, "log2 of zero is undefined");
+        
+        uint256 n = 0;
+        while (x > 1) {
+            x >>= 1;
+            n++;
+        }
+        return n;
+    }
+
+    function exp2Approximation(uint256 x) internal pure returns (uint256) {
+        if (x == 0) return 1;
+        
+        uint256 result = 2;
+        for (uint256 i = 1; i < x; i++) {
+            result *= 2;
+        }
+        return result;
+    }
+
+    function testCreateWalletPrivKeyNoLabel(uint256 pkSeed) public {
+        uint256 pk = bound(pkSeed, pkConfig);
+
+        Vm.Wallet memory wallet = vm.createWallet(pk);
+
+        address expectedAddr = vm.addr(wallet.privateKey);
+        assertEq(expectedAddr, wallet.addr);
+
+        expectedAddr = addressOf(wallet.publicKeyX, wallet.publicKeyY);
+        assertEq(expectedAddr, wallet.addr);
+    }
+
+    function testCreateWalletPrivKeyWithLabel(uint256 pkSeed) public {
+        string memory label = "labelled wallet";
+
+        uint256 pk = bound(pkSeed, pkConfig);
+
+        Vm.Wallet memory wallet = vm.createWallet(pk, label);
+
+        address expectedAddr = vm.addr(wallet.privateKey);
+        assertEq(expectedAddr, wallet.addr);
+
+        expectedAddr = addressOf(wallet.publicKeyX, wallet.publicKeyY);
+        assertEq(expectedAddr, wallet.addr);
+
+        string memory expectedLabel = vm.getLabel(wallet.addr);
+        assertEq(expectedLabel, label, "labelled address != wallet.addr");
+    }
+
+    function testSignWithWalletDigest(uint256 pkSeed, bytes32 digest) public {
+        uint256 pk = bound(pkSeed, pkConfig);
+
+        Vm.Wallet memory wallet = vm.createWallet(pk);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(wallet, digest);
+
+        address recovered = ecrecover(digest, v, r, s);
+        assertEq(recovered, wallet.addr);
+    }
+
+    function testSignCompactWithWalletDigest(uint256 pkSeed, bytes32 digest) public {
+        uint256 pk = bound(pkSeed, pkConfig);
+
+        Vm.Wallet memory wallet = vm.createWallet(pk);
+
+        (bytes32 r, bytes32 vs) = vm.signCompact(wallet, digest);
+
+        bytes32 s = bytes32((uint256(vs) << 1) >> 1);
+
+        uint8 v = uint8(uint256(vs) >> 255) + 27;
+
+        address recovered = ecrecover(digest, v, r, s);
+        assertEq(recovered, wallet.addr);
+    }
 
     function testSignWithWalletMessage(uint256 pkSeed, bytes memory message) public {
         testSignWithWalletDigest(pkSeed, keccak256(message));
@@ -135,7 +327,7 @@ contract WalletTest is DSTest {
     }
 
     function testGetNonceWallet(uint256 pkSeed) public {
-        uint256 pk = bound(pkSeed, 1, Q - 1);
+        uint256 pk = bound(pkSeed, pkConfig);
 
         Vm.Wallet memory wallet = vm.createWallet(pk);
 
