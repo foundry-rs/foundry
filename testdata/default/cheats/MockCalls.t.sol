7452271871b9ae4a31d4789270299976771f3c86
Yotam Bar-On <tudmotu@gmail.com>
2024-10-03 20:33:46 +0300

    Add a test to vm.mockCalls

---
+++ testdata/default/cheats/MockCalls.t.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity 0.8.18;
+
+import "ds-test/test.sol";
+import "cheats/Vm.sol";
+
+contract MockCallsTest is DSTest {
+    Vm constant vm = Vm(HEVM_ADDRESS);
+
+    function testMockCalls() public {
+        address mockUser = vm.addr(vm.randomUint());
+        address mockErc20 = vm.addr(vm.randomUint());
+        bytes memory data = abi.encodeWithSignature('balanceOf(address)', mockUser);
+        bytes[] memory mocks = new bytes[](3);
+        mocks[0] = abi.encode(2 ether);
+        mocks[1] = abi.encode(1 ether);
+        mocks[2] = abi.encode(6.423 ether);
+        vm.mockCalls(mockErc20, data, mocks);
+        (, bytes memory ret1) = mockErc20.call(data);
+        assertEq(abi.decode(ret1, (uint)), 2 ether);
+        (, bytes memory ret2) = mockErc20.call(data);
+        assertEq(abi.decode(ret2, (uint)), 1 ether);
+        (, bytes memory ret3) = mockErc20.call(data);
+        assertEq(abi.decode(ret3, (uint)), 6.423 ether);
+    }
+}
