b1f205e72a2e1a3b745eed40749bbb972cdcb701
grandizzy <grandizzy.the.egg@gmail.com>
2024-10-09 17:04:22 +0300

    fix(chisel): consider assembly block return as final statement

--- crates/chisel/src/executor.rs
+++ crates/chisel/src/executor.rs
@@ -49,6 +49,21 @@ impl SessionSource {
             // Fetch the run function's body statement
             let run_func_statements = compiled.intermediate.run_func_body()?;
 
+            // Record loc of last yul block return statement (if any).
+            // This is used to decide which is the final statement within the `run()` method.
+            let last_yul_return = run_func_statements.iter().rev().find_map(|statement| {
+                if let pt::Statement::Assembly { loc: _, dialect: _, flags: _, block } = statement {
+                    if let Some(statement) = block.statements.last() {
+                        if let pt::YulStatement::FunctionCall(yul_call) = statement {
+                            if yul_call.id.name == "return" {
+                                return Some(statement.loc())
+                            }
+                        }
+                    }
+                }
+                None
+            });
+
             // Find the last statement within the "run()" method and get the program
             // counter via the source map.
             if let Some(final_statement) = run_func_statements.last() {
@@ -58,7 +73,7 @@ impl SessionSource {
                 //
                 // There is some code duplication within the arms due to the difference between
                 // the [pt::Statement] type and the [pt::YulStatement] types.
-                let source_loc = match final_statement {
+                let mut source_loc = match final_statement {
                     pt::Statement::Assembly { loc: _, dialect: _, flags: _, block } => {
                         if let Some(statement) = block.statements.last() {
                             statement.loc()
@@ -87,6 +102,13 @@ impl SessionSource {
                     }
                     _ => final_statement.loc(),
                 };
+
+                // Consider yul return statement as final statement (if it's loc is lower) .
+                if let Some(yul_return) = last_yul_return {
+                    if yul_return.end() < source_loc.start() {
+                        source_loc = yul_return;
+                    }
+                }
 
                 // Map the source location of the final statement of the `run()` function to its
                 // corresponding runtime program counter
