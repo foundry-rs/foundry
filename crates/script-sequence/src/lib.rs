95ad0433856bec1b30b9a2bc3eda6db900c7f04d
Yash Atreya <44857776+yash-atreya@users.noreply.github.com>
2024-10-11 19:00:47 +0530

    refac(`script`): extract script sequence and related types to new crate

---
+++ crates/script-sequence/src/lib.rs
@@ -0,0 +1,9 @@
+//! Script Sequence and related types.
+
+pub mod multi_sequence;
+pub mod sequence;
+pub mod transaction;
+
+pub use multi_sequence::*;
+pub use sequence::*;
+pub use transaction::*;
