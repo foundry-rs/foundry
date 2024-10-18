caea8550a08eb64cfa1b07ece76dc3989579b57d
Yash Atreya <44857776+yash-atreya@users.noreply.github.com>
2024-10-18 12:44:00 +0530

    fix

--- testdata/cheats/Vm.sol
+++ testdata/cheats/Vm.sol
@@ -22,7 +22,7 @@ interface Vm {
     struct StorageAccess { address account; bytes32 slot; bool isWrite; bytes32 previousValue; bytes32 newValue; bool reverted; }
     struct Gas { uint64 gasLimit; uint64 gasTotalUsed; uint64 gasMemoryUsed; int64 gasRefunded; uint64 gasRemaining; }
     struct DebugStep { uint256[] stack; bytes memoryInput; uint8 opcode; uint64 depth; bool isOutOfGas; address contractAddr; }
-    struct BroadcastTxSummary {  bytes32 txHash;  BroadcastTxType txType; address contractAddress; uint64 blockNumber; bool success; }
+    struct BroadcastTxSummary { bytes32 txHash; BroadcastTxType txType; address contractAddress; uint64 blockNumber; bool success; }
     function _expectCheatcodeRevert() external;
     function _expectCheatcodeRevert(bytes4 revertData) external;
     function _expectCheatcodeRevert(bytes calldata revertData) external;
