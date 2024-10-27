badc6fb4b67e3b9aa50438d9f321a661241df3f3
Arsenii Kulikov <klkvrr@gmail.com>
2024-10-27 18:23:55 +0400

    fix test

--- crates/cast/tests/cli/main.rs
+++ crates/cast/tests/cli/main.rs
@@ -81,6 +81,9 @@ stateRoot            [..]
 timestamp            [..]
 withdrawalsRoot      [..]
 totalDifficulty      [..]
+blobGasUsed          [..]
+excessBlobGas        [..]
+requestsHash         [..]
 transactions:        [
 ...
 ]
