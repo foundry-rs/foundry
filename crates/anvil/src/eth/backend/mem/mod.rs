aa5e2be136c7d68ae4f962b85b8df54e62ba8dfa
Yash Atreya <44857776+yash-atreya@users.noreply.github.com>
2024-10-30 13:46:15 +0530

    Revert "load accounts"
    This reverts commit b650f56fe52f79be3eb7c8ab4d2ad1aaca08a32f.

--- crates/anvil/src/eth/backend/mem/mod.rs
+++ crates/anvil/src/eth/backend/mem/mod.rs
@@ -937,10 +937,6 @@ impl Backend {
             .into());
         }
 
-        if let Some(mut fork) = self.get_fork() {
-            fork.load_state(state.clone());
-        }
-
         if let Some(historical_states) = state.historical_states {
             self.states.write().load_states(historical_states);
         }
