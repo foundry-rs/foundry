e4e20108963e2195f9fe626d0a60746b3c946ff5
James Wenzel <wenzel.james.r@gmail.com>
2024-11-01 17:01:04 -0700

    remove unnecessary tests from both test_cmd and AssumeNoRevert.t.sol

--- crates/forge/tests/cli/test_cmd.rs
+++ crates/forge/tests/cli/test_cmd.rs
@@ -2393,7 +2393,7 @@ contract Dummy {
     assert!(dump_path.exists());
 });
 
-forgetest_init!(assume_no_revert_tests, |prj, cmd| {
+forgetest_init!(test_assume_no_revert_with_data, |prj, cmd| {
     prj.wipe_contracts();
     let config = Config {
         fuzz: { FuzzConfig { runs: 256, seed: Some(U256::from(100)), ..Default::default() } },
@@ -2475,52 +2475,6 @@ forgetest_init!(assume_no_revert_tests, |prj, cmd| {
             reverter = new Reverter();
         }
     
-        /// @dev Test that `assumeNoRevert` anticipates and correctly rejects a specific error selector
-        function testAssumeSelector(uint256 x) public view {
-            _vm.assumeNoRevert(Reverter.MyRevert.selector);
-            reverter.revertIf2(x);
-        }
-    
-        /// @dev Test that `assumeNoRevert` anticipates and correctly rejects a specific error selector and data
-        function testAssumeWithDataSingle(uint256 x) public view {
-            _vm.assumeNoRevert(abi.encodeWithSelector(Reverter.RevertWithData.selector, 2));
-            reverter.revertWithDataIf2(x);
-        }
-    
-        /// @dev Test that `assumeNoPartialRevert` anticipates and correctly rejects a specific error selector with any extra data
-        function testAssumeWithDataPartial(uint256 x) public view {
-            _vm.assumeNoPartialRevert(Reverter.RevertWithData.selector);
-            reverter.revertWithDataIf2(x);
-        }
-        
-        /// @dev Test that `assumeNoRevert` assumptions are not cleared after a cheatcode call
-        function testAssumeNotClearedAfterCheatcodeCall(uint256 x) public {
-            _vm.assumeNoRevert(Reverter.MyRevert.selector);
-            _vm.warp(block.timestamp + 1000);
-            reverter.revertIf2(x);
-        }
-        
-        /// @dev Test that `assumeNoRevert` correctly rejects two different error selectors
-        function testMultipleAssumesPasses(uint256 x) public view {
-            _vm.assumeNoRevert(abi.encodeWithSelector(Reverter.MyRevert.selector), address(reverter));
-            _vm.assumeNoRevert(abi.encodeWithSelector(Reverter.RevertWithData.selector, 3), address(reverter));
-            reverter.twoPossibleReverts(x);
-        }
-      
-        /// @dev Test that `assumeNoPartialRevert` correctly interacts with `assumeNoRevert`
-        function testMultipleAssumes_Partial(uint256 x) public view {
-            _vm.assumeNoPartialRevert(Reverter.RevertWithData.selector);
-            _vm.assumeNoRevert(Reverter.MyRevert.selector);
-            reverter.twoPossibleReverts(x);
-        } 
-        
-        /// @dev Test that calling `assumeNoRevert` after `expectRevert` results in an error
-        function testExpectThenAssumeFails() public {
-            _vm._expectCheatcodeRevert();
-            _vm.assumeNoRevert();
-            reverter.revertIf2(1);
-        }
-    
         /// @dev Test that `assumeNoRevert` does not reject an unanticipated error selector
         function testAssume_wrongSelector_fails(uint256 x) public view {
             _vm.assumeNoRevert(Reverter.UnusedError.selector);
@@ -2598,13 +2552,6 @@ forgetest_init!(assume_no_revert_tests, |prj, cmd| {
         );
     }
 
-    match_test(&mut cmd, "testAssumeSelector").assert_success();
-    match_test(&mut cmd, "testAssumeWithDataSingle").assert_success();
-    match_test(&mut cmd, "testAssumeWithDataPartial").assert_success();
-    match_test(&mut cmd, "testMultipleAssumesPasses").assert_success();
-    match_test(&mut cmd, "testMultipleAssumes_Partial").assert_success();
-    match_test(&mut cmd, "testAssumeNotClearedAfterCheatcodeCall").assert_success();
-    match_test(&mut cmd, "testExpectThenAssumeFails");
     assert_failure_contains(
         &mut cmd,
         "testAssume_wrongSelector_fails",
