f18a0e8974fc4abe26f4db891846f2de5f2f22f5
evchip <evchip@pm.me>
2024-11-04 18:01:49 +0700

    refactor test to check for code on alice account and call execute on alice account through SimpleDelegateContract

--- testdata/default/cheats/AttachDelegation.t.sol
+++ testdata/default/cheats/AttachDelegation.t.sol
@@ -8,15 +8,15 @@ contract AttachDelegationTest is DSTest {
     event Executed(address indexed to, uint256 value, bytes data);
     Vm constant vm = Vm(HEVM_ADDRESS);
     uint256 alice_pk = 0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d;
-    address alice = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8;
+    address payable alice = payable(0x70997970C51812dc3A010C7d01b50e0d17dc79C8);
     uint256 bob_pk=0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a;
     address bob = 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC;
 
-    function testAttachDelegation(uint64 nonce) public {
+    function testAttachDelegation() public {
         SimpleDelegateContract implementation = new SimpleDelegateContract();
         ERC20 token = new ERC20(alice);
 
-        (uint8 v, bytes32 r, bytes32 s) = vm.signDelegation(address(implementation), nonce, alice_pk);
+        (uint8 v, bytes32 r, bytes32 s) = vm.signDelegation(address(implementation), alice_pk);
         SimpleDelegateContract.Call[] memory calls = new SimpleDelegateContract.Call[](1);
         bytes memory data = abi.encodeCall(ERC20.mint, (100, bob));
         calls[0] = SimpleDelegateContract.Call({
@@ -26,10 +26,11 @@ contract AttachDelegationTest is DSTest {
         });
         // executing as bob to make clear that we don't need to execute the tx as alice
         vm.broadcast(bob_pk);
-        vm.attachDelegation(address(implementation), nonce, v, r, s);
-        // TODO - how do we specify that the target of this tx is alice's EOA
-        // fails if msg.sender is not implementation address (minter)
-        implementation.execute(calls);
+        vm.attachDelegation(address(implementation), alice, v, r, s);
+
+        bytes memory code = address(alice).code;
+        require(code.length > 0, "no code written to alice");
+        SimpleDelegateContract(alice).execute(calls);
 
         assertEq(token.balanceOf(bob), 100);
     }
