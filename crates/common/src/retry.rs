36cbce7c78b56dd68359084a5d8b03f84efed8fb
grandizzy <38490174+grandizzy@users.noreply.github.com>
2024-11-14 20:34:12 +0200

    feat(forge): allow `--verifier custom` option (#9311)
    * feat(forge): allow `--verifier custom` option
    
    * Changes after review: add description of custom verifier, reorg err message, add custom verifier api key
    
    * Fix descriptions
    
    * Update crates/verify/src/provider.rs
    
    Co-authored-by: zerosnacks <95942363+zerosnacks@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: zerosnacks <95942363+zerosnacks@users.noreply.github.com>

--- crates/common/src/retry.rs
+++ crates/common/src/retry.rs
@@ -83,6 +83,6 @@ impl Retry {
 
     fn handle_err(&mut self, err: Error) {
         self.retries -= 1;
-        warn!("erroneous attempt ({} tries remaining): {}", self.retries, err.root_cause());
+        let _ = sh_warn!("{} ({} tries remaining)", err.root_cause(), self.retries);
     }
 }
