66273b69496a9d8384b24c2bc9d06cab132e9299
yipu <5365604+yipu3@users.noreply.github.com>
2024-12-04 15:42:16 +0800

    Update crates/cli/src/utils/abi.rs
    Co-authored-by: Yash Atreya <44857776+yash-atreya@users.noreply.github.com>

--- crates/cli/src/utils/abi.rs
+++ crates/cli/src/utils/abi.rs
@@ -51,7 +51,7 @@ pub async fn parse_function_args<T: Transport + Clone, P: Provider<T, AnyNetwork
         get_func(sig)?
     } else {
         let etherscan_api_key = etherscan_api_key.ok_or_eyre(
-            "Function signature does not contain parentheses. If you wish to fetch function data from EtherScan, please provide an API key.",
+            "Function signature does not contain parentheses. If you wish to fetch function data from Etherscan, please provide an API key.",
         )?;
         let to = to.ok_or_eyre("A 'to' address must be provided to fetch function data.")?;
         get_func_etherscan(sig, to, &args, chain, etherscan_api_key).await?
