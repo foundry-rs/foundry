a78c15c93627370e8ce27784bc94eb1f3b86122d
Odysseas.eth <odyslam@gmail.com>
2024-12-07 01:02:06 +0200

    chore: interfaces

---
+++ crates/cheatcodes/src/credible.rs
@@ -0,0 +1,8 @@
+
+use crate::{Cheatcode, CheatsCtxt, Result, Vm::*};
+
+impl Cheatcode for assertionExCall {
+    fn apply_stateful(&self, ccx: &mut CheatsCtxt) -> Result {
+        unimplemented!()
+    }
+}
