360bcc74177bfc5c1e5963c6394ca5d5a8c4b710
Anukul Sangwan <anukulsangwan@icloud.com>
2024-12-12 17:25:28 +0200

    run cargofmt

--- crates/config/src/endpoints.rs
+++ crates/config/src/endpoints.rs
@@ -39,11 +39,7 @@ impl RpcEndpoints {
     /// Returns all (alias -> rpc_endpoint) pairs
     pub fn resolved(self) -> ResolvedRpcEndpoints {
         ResolvedRpcEndpoints {
-            endpoints: self
-                .endpoints
-                .into_iter()
-                .map(|(name, e)| (name, e.resolve()))
-                .collect(),
+            endpoints: self.endpoints.into_iter().map(|(name, e)| (name, e.resolve())).collect(),
         }
     }
 }
