68dad345ca53892bb1b8f7e24d98964119bd05e6
0xKitsune <0xkitsune@protonmail.com>
2024-12-23 01:20:33 -0500

    wip

--- crates/lint/testdata/DivideBeforeMultiply.sol
+++ crates/lint/testdata/DivideBeforeMultiply.sol
@@ -1,5 +1,37 @@
 contract Contract0 {
     function arithmetic() public {
         (1 / 2) * 3; // Unsafe
+        (1 * 2) / 3; // Safe
+        (1 / 2) * 3; // Unsafe
+        (1 * 2) / 3; // Safe
+        ((1 / 2) * 3) * 4; // Unsafe
+        ((1 * 2) / 3) * 4; // Unsafe
+        (1 / 2 / 3) * 4; // Unsafe
+        (1 / (2 + 3)) * 4; // Unsafe
+        (1 / 2 + 3) * 4; // Safe
+        (1 / 2 - 3) * 4; // Safe
+        (1 + 2 / 3) * 4; // Safe
+        (1 / 2 - 3) * 4; // Safe
+        ((1 / 2) % 3) * 4; // Safe
+        1 / (2 * 3 + 3); // Safe
+        1 / ((2 / 3) * 3); // Unsafe
+        1 / ((2 * 3) + 3); // Safe
+
+        // uint256 x = 5;
+        // x /= 2 * 3; // Unsafe
+        // x /= (2 * 3); // Unsafe
+        // x /= 2 * 3 - 4; // Unsafe
+        // x /= (2 * 3) % 4; // Unsafe
+        // x /= (2 * 3) | 4; // Unsafe
+        // x /= (2 * 3) & 4; // Unsafe
+        // x /= (2 * 3) ^ 4; // Unsafe
+        // x /= (2 * 3) << 4; // Unsafe
+        // x /= (2 * 3) >> 4; // Unsafe
+        // x /= 3 % 4; // Safe
+        // x /= 3 | 4; // Safe
+        // x /= 3 & 4; // Safe
+        // x /= 3 ^ 4; // Safe
+        // x /= 3 << 4; // Safe
+        // x /= 3 >> 4; // Safe
     }
 }
