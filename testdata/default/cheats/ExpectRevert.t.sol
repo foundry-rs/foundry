49bae44174ac0d217966ef97949849c3f14199e0
Yash Atreya <44857776+yash-atreya@users.noreply.github.com>
2025-01-09 18:00:20 +0530

    fix + forge fmt

--- testdata/default/cheats/ExpectRevert.t.sol
+++ testdata/default/cheats/ExpectRevert.t.sol
@@ -153,10 +153,7 @@ contract ExpectRevertTest is DSTest {
 
         Dummy dummy = new Dummy();
         vm.expectRevert();
-        reverter.callThenRevert(
-            dummy,
-            "revert message 4 i ran out of synonims for also"
-        );
+        reverter.callThenRevert(dummy, "revert message 4 i ran out of synonims for also");
 
         vm.expectRevert();
         reverter.revertWithoutReason();
@@ -286,22 +283,13 @@ contract ExpectRevertWithReverterTest is DSTest {
         vm.expectPartialRevert(CContractError.selector, address(cContract));
         aContract.callAndRevertInCContractThroughBContract();
         // Test expect revert with exact data match and reverter at second subcall.
-        vm.expectRevert(
-            abi.encodeWithSelector(
-                CContractError.selector,
-                "Reverted by CContract"
-            ),
-            address(cContract)
-        );
+        vm.expectRevert(abi.encodeWithSelector(CContractError.selector, "Reverted by CContract"), address(cContract));
         aContract.callAndRevertInCContract();
     }
 
     function testExpectRevertsWithReverterInConstructor() public {
         // Test expect revert with reverter when constructor reverts.
-        vm.expectRevert(
-            abi.encodePacked("Reverted by DContract"),
-            address(cContract)
-        );
+        vm.expectRevert(abi.encodePacked("Reverted by DContract"), address(cContract));
         cContract.createDContract();
 
         vm.expectRevert(address(bContract));
