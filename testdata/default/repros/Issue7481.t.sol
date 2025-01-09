b4b97e12cafaeafb75dbd900639f344d9c9d6f75
Yash Atreya <44857776+yash-atreya@users.noreply.github.com>
2025-01-09 17:25:31 +0530

    fix: repro_7481

--- testdata/default/repros/Issue7481.t.sol
+++ testdata/default/repros/Issue7481.t.sol
@@ -14,10 +14,15 @@ contract Issue7481Test is DSTest {
         vm.createSelectFork("mainnet", 19514903);
 
         // Transfer some funds to sender of tx being transacted to ensure that it appears in journaled state
-        payable(address(0x5C60cD7a3D50877Bfebd484750FBeb245D936dAD)).call{value: 1}("");
-        vm.transact(0xccfd66fc409a633a99b5b75b0e9a2040fcf562d03d9bee3fefc1a5c0eb49c999);
+        payable(address(0x5C60cD7a3D50877Bfebd484750FBeb245D936dAD)).call{
+            value: 1
+        }("");
+        vm.transact(
+            0xccfd66fc409a633a99b5b75b0e9a2040fcf562d03d9bee3fefc1a5c0eb49c999
+        );
 
         // Revert the current call to ensure that revm can revert state journal
+        vm.expectRevert();
         revert("HERE");
     }
 }
