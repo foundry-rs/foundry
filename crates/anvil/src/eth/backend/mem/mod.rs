8c0af9973fe12a940f0a611a3102448a48474c05
Nisheeth Barthwal <nbaztec@gmail.com>
2025-01-21 21:20:09 +0100

    fix base fee update

--- crates/anvil/src/eth/backend/mem/mod.rs
+++ crates/anvil/src/eth/backend/mem/mod.rs
@@ -527,12 +527,8 @@ impl Backend {
                 .await?
                 .ok_or(BlockchainError::BlockNotFound)?;
             // update all settings related to the forked block
-            {
-                // If rpc url is unspecified, then update the fork with the new block number and
-                // existing rpc url
-                let maybe_rpc_url = { self.node_config.read().await.eth_rpc_url.clone() };
-
-                if let Some(fork_url) = forking.json_rpc_url.or(maybe_rpc_url) {
+            {                
+                if let Some(fork_url) = forking.json_rpc_url {
                     // Set the fork block number
                     let mut node_config = self.node_config.write().await;
                     node_config.fork_choice = Some(ForkChoice::Block(fork_block_number));
@@ -546,6 +542,27 @@ impl Backend {
                     *self.fork.write() = Some(fork);
                     *self.env.write() = env;
                 } else {
+                    // If rpc url is unspecified, then update the fork with the new block number and
+                    // existing rpc url, this updates the cache path
+                    {
+                        let maybe_fork_url = { self.node_config.read().await.eth_rpc_url.clone() };
+                        if let Some(fork_url) = maybe_fork_url {
+                            // Set the fork block number
+                            let mut node_config = self.node_config.write().await;
+                            node_config.fork_choice = Some(ForkChoice::Block(fork_block_number));
+
+                            let mut env = self.env.read().clone();
+                            let (forked_db, client_fork_config) = node_config
+                                .setup_fork_db_config(fork_url, &mut env, &self.fees)
+                                .await?;
+
+                            *self.db.write().await = Box::new(forked_db);
+                            let fork = ClientFork::new(client_fork_config, Arc::clone(&self.db));
+                            *self.fork.write() = Some(fork);
+                            *self.env.write() = env;
+                        }
+                    }
+
                     let gas_limit = self.node_config.read().await.fork_gas_limit(&fork_block);
                     let mut env = self.env.write();
 
