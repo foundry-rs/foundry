685631bb9964de8839fd6cdf22ba587fc6870549
grandizzy <38490174+grandizzy@users.noreply.github.com>
2025-01-31 12:15:22 +0200

    chore: ignore solady CI failure in win (#9797)

--- crates/forge/tests/cli/ext_integration.rs
+++ crates/forge/tests/cli/ext_integration.rs
@@ -59,7 +59,10 @@ fn sablier_v2_core() {
 }
 
 // <https://github.com/Vectorized/solady>
+// Fails on windows because "/*Transient*" does not skip transient contracts
+// (should be "*/*Transient*").
 #[test]
+#[cfg_attr(windows, ignore = "Windows cannot skip transient pattern")]
 fn solady() {
     ExtTester::new("Vectorized", "solady", "de9aee59648862bb98affd578248d1e75c7073ad").run();
 }
