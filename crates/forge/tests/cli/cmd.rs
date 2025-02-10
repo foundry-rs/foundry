db3d9fc95398450dbed83d4841042c62c155bcfc
Yash Atreya <44857776+yash-atreya@users.noreply.github.com>
2025-02-10 17:51:17 +0530

    feat(`common`): `PathOrContractInfo` arg type  (#9770)
    * feat(`common`): `PathOrContractInfo`
    
    * fix
    
    * docs
    
    * nit
    
    * test
    
    * move find abi helper to ContractsByArtifact
    
    * nit
    
    * fix
    
    * nit
    
    * fix
    
    * nit
    
    * ensure sol file
    
    * account for vyper contracts
    
    * nit

--- crates/forge/tests/cli/cmd.rs
+++ crates/forge/tests/cli/cmd.rs
@@ -3283,6 +3283,12 @@ const CUSTOM_COUNTER: &str = r#"
     }
 }
     "#;
+
+const ANOTHER_COUNTER: &str = r#"
+    contract AnotherCounter is Counter {
+        constructor(uint256 _number) Counter(_number) {}
+    }
+"#;
 forgetest!(inspect_custom_counter_abi, |prj, cmd| {
     prj.add_source("Counter.sol", CUSTOM_COUNTER).unwrap();
 
@@ -3345,6 +3351,43 @@ forgetest!(inspect_custom_counter_errors, |prj, cmd| {
     prj.add_source("Counter.sol", CUSTOM_COUNTER).unwrap();
 
     cmd.args(["inspect", "Counter", "errors"]).assert_success().stdout_eq(str![[r#"
+
+╭-------------------------------+----------╮
+| Error                         | Selector |
++==========================================+
+| CustomErr(Counter.ErrWithMsg) | 0625625a |
+|-------------------------------+----------|
+| NumberIsZero()                | de5d32ac |
+╰-------------------------------+----------╯
+
+
+"#]]);
+});
+
+forgetest!(inspect_path_only_identifier, |prj, cmd| {
+    prj.add_source("Counter.sol", CUSTOM_COUNTER).unwrap();
+
+    cmd.args(["inspect", "src/Counter.sol", "errors"]).assert_success().stdout_eq(str![[r#"
+
+╭-------------------------------+----------╮
+| Error                         | Selector |
++==========================================+
+| CustomErr(Counter.ErrWithMsg) | 0625625a |
+|-------------------------------+----------|
+| NumberIsZero()                | de5d32ac |
+╰-------------------------------+----------╯
+
+
+"#]]);
+});
+
+forgetest!(test_inspect_contract_with_same_name, |prj, cmd| {
+    let source = format!("{CUSTOM_COUNTER}\n{ANOTHER_COUNTER}");
+    prj.add_source("Counter.sol", &source).unwrap();
+
+    cmd.args(["inspect", "src/Counter.sol", "errors"]).assert_failure().stderr_eq(str![[r#"Error: Multiple contracts found in the same file, please specify the target <path>:<contract> or <contract>[..]"#]]);
+
+    cmd.forge_fuse().args(["inspect", "Counter", "errors"]).assert_success().stdout_eq(str![[r#"
 
 ╭-------------------------------+----------╮
 | Error                         | Selector |
