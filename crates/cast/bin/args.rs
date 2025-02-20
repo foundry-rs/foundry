4be7109c76373c6ecb7c66a7fa4e71a59ca1c718
zerosnacks <zerosnacks@protonmail.com>
2025-02-20 17:44:22 +0100

    change 4byte-decode -> 4byte-calldata in non breaking way, 4byte-decode and shorthands are hidden aliases

--- crates/cast/bin/args.rs
+++ crates/cast/bin/args.rs
@@ -650,8 +650,8 @@ pub enum CastSubcommand {
     },
 
     /// Decode ABI-encoded calldata using https://openchain.xyz.
-    #[command(name = "4byte-decode", visible_aliases = &["4d", "4bd"])]
-    FourByteDecode {
+    #[command(name = "4byte-calldata", aliases = &["4byte-decode", "4d", "4bd"], visible_aliases = &["4bc", "4c"])]
+    FourByteCalldata {
         /// The ABI-encoded calldata.
         calldata: Option<String>,
     },
