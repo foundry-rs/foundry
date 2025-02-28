c0ffeef42ff4b8638ade7c49f002cc922a8f10e2
drgorillamd <83670532+drgorillamd@users.noreply.github.com>
2025-02-28 12:22:53 +0100

    chore: visiting contracts

--- crates/forge/src/mutation.rs
+++ crates/forge/src/mutation.rs
@@ -1,14 +1,9 @@
 // Generate mutants then run tests (reuse the whole unit test flow for now, including compilation to select mutants)
 // Use Solar: 
 use solar_parse::{
-    token::{Token, TokenKind},
-    Lexer,
     ast::{
-        interface::{self, Session},
-        Arena, CommentKind, Item, ItemKind,
-        SourceUnit
-    },
-    Parser,
+        interface::{self, Session}, Arena, CommentKind, Item, ItemContract, ItemKind, SourceUnit, ContractKind, ExprKind
+    }, token::{Token, TokenKind}, Lexer, Parser
 };
 use std::sync::Arc;
 
@@ -51,11 +46,12 @@ impl<'a> MutationCampaign<'a> {
 
         // Iterate over all contract in contracts_to_mutate
         for contract in &self.contracts_to_mutate {
-            // Parse and get the ast (in an Rc for Rayon later)
-            self.lex_parse_contract(contract);
+                // Rayon from here (enter_parallel)
+                // Parse and get the ast
+                let ast = self.lex_parse_contract(contract);
 
-            // Rayon from here (enter_parallel)
-                // Mutate
+                // Mutate and spin a new thread for each mutated ast
+
                 // let mutated_ast = mutate(Rc::clone(&ast));
 
                 // Emit solidity to a new tmp file - let's use tempfile::SpooledTempFile to keep them in memory!
@@ -70,7 +66,7 @@ impl<'a> MutationCampaign<'a> {
 
                 // Delete tmp file
 
-            println!("{:?}", contract);
+            println!("{} has been processed", contract.display());
         }
 
         // Print results
@@ -82,16 +78,49 @@ impl<'a> MutationCampaign<'a> {
         let ast = sess.enter(|| -> solar_parse::interface::Result<_> {
             let arena = solar_parse::ast::Arena::new();
             let mut parser = Parser::from_file(&sess, &arena, target)?;
-            let ast = parser.parse_file().map_err(|e| e.emit())?;
+            let ast = Arc::new(parser.parse_file().map_err(|e| e.emit())?);
 
             let visited = format!("{:?}", ast);
 
+            // visitor -> mutate, emit, compile and run tests
+            self.visit_contracts_only(Arc::clone(&ast));
+
             Ok(visited)
             }).ok();
-
-        dbg!(&ast);
     }
+
+    fn visit_contracts_only(&self, ast: Arc<SourceUnit>) {
+        for node in ast.items.iter() {
+            // @todo we should probable exclude interfaces before this point (even tho the overhead is minimal)
+            match &node.kind {
+                ItemKind::Contract(contract) => {
+                    match contract.kind {
+                        ContractKind::Contract | ContractKind::AbstractContract => {
+                            // dbg!(contract.name);
+
+                            dbg!(&contract.body);
+
+                        }
+                        _ => {} // Not the interfaces or libs
+                    }
+                }
+                // we'll probably never mutate pragma directive or imports...
+                _ => {}
+            }
+            // Check if mutation candidate
+            
+            // dbg!(node);
+            // if node.is_mutation_candidate(node) {
+            //     // if so, emit with the mutation
+            // }
+        }
+    }
+
 }
 
-pub fn mutate(target: &PathBuf) {
-}
+
+// /// A type of mutation, for a given ItemKind or Statement
+// struct Mutation<T> {
+//     target_item: T,
+
+// }
