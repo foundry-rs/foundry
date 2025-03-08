8e0c54f2b93cc3b65a69e69439359ee2757a3f91
Cizeon <cizeon@cizeon.xyz>
2025-03-08 08:53:31 +0100

    Adding a proxy parameter to the cast storage command, allowing manual selection of a proxy address to retrieve the full storage layout.

--- crates/cast/bin/cmd/storage.rs
+++ crates/cast/bin/cmd/storage.rs
@@ -51,6 +51,10 @@ pub struct StorageArgs {
     #[arg(value_parser = parse_slot)]
     slot: Option<B256>,
 
+    /// The proxy contract address. If provided, it gets the full storage layout from the proxy address.
+    #[arg(long,value_parser = NameOrAddress::from_str)]
+    proxy: Option<NameOrAddress>,
+
     /// The block height to query at.
     ///
     /// Can also be the tags earliest, finalized, safe, latest, or pending.
@@ -134,7 +138,10 @@ impl StorageArgs {
         let chain = utils::get_chain(config.chain, &provider).await?;
         let api_key = config.get_etherscan_api_key(Some(chain)).unwrap_or_default();
         let client = Client::new(chain, api_key)?;
-        let source = find_source(client, address).await?;
+        let source = match self.proxy {
+            Some(proxy) => find_source(client, proxy.resolve(&provider).await?).await?,
+            _ => find_source(client, address).await?,
+        };
         let metadata = source.items.first().unwrap();
         if metadata.is_vyper() {
             eyre::bail!("Contract at provided address is not a valid Solidity contract")
