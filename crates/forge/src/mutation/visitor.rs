c0ffee056225cad43478646344b9fdde104ceaa1
drgorillamd <83670532+drgorillamd@users.noreply.github.com>
2025-03-30 11:25:27 +0200

    chore: refactor visitor

--- crates/forge/src/mutation/visitor.rs
+++ crates/forge/src/mutation/visitor.rs
@@ -14,16 +14,26 @@ pub enum AssignVarTypes {
                          * maybe will have to switch back if validating more aggressively */
 }
 
+/// A wrapper around the Solar macro-generated visitor, in order to use the default implementation
+/// of the fn we override in MutantVisitor
+pub struct SolarVisitorWrapper {}
+
+impl<'ast> Visit<'ast> for SolarVisitorWrapper {
+    type BreakValue = ();
+}
+
+
 /// A visitor which collect all expression to mutate as well as the mutation types
 pub struct MutantVisitor {
     pub mutation_to_conduct: Vec<Mutant>,
     pub mutator_registry: MutatorRegistry,
+    default_visitor: SolarVisitorWrapper,
 }
 
 impl MutantVisitor {
     /// Use all mutator from registry::default
     pub fn default() -> Self {
-        Self { mutation_to_conduct: Vec::new(), mutator_registry: MutatorRegistry::default() }
+        Self { mutation_to_conduct: Vec::new(), mutator_registry: MutatorRegistry::default(), default_visitor: SolarVisitorWrapper {} }
     }
 
     /// Use only a set of mutators
@@ -31,13 +41,14 @@ impl MutantVisitor {
         Self {
             mutation_to_conduct: Vec::new(),
             mutator_registry: MutatorRegistry::new_with_mutators(mutators),
+            default_visitor: SolarVisitorWrapper {}
         }
     }
 }
 
 impl<'ast> Visit<'ast> for MutantVisitor {
     type BreakValue = ();
-
+    
     fn visit_variable_definition(
         &mut self,
         var: &'ast VariableDefinition<'ast>,
@@ -50,28 +61,7 @@ impl<'ast> Visit<'ast> for MutantVisitor {
 
         self.mutation_to_conduct.extend(self.mutator_registry.generate_mutations(&context));
 
-        // Rest is Solar visitor:
-        let VariableDefinition {
-            span,
-            ty,
-            visibility: _,
-            mutability: _,
-            data_location: _,
-            override_: _,
-            indexed: _,
-            name,
-            initializer,
-        } = var;
-        self.visit_span(span)?;
-        self.visit_ty(ty)?;
-        if let Some(name) = name {
-            self.visit_ident(name)?;
-        }
-        if let Some(initializer) = initializer {
-            self.visit_expr(initializer)?;
-        }
-
-        ControlFlow::Continue(())
+        self.default_visitor.visit_variable_definition(var)
     }
 
     fn visit_expr(&mut self, expr: &'ast Expr<'ast>) -> ControlFlow<Self::BreakValue> {
@@ -80,88 +70,6 @@ impl<'ast> Visit<'ast> for MutantVisitor {
 
         self.mutation_to_conduct.extend(self.mutator_registry.generate_mutations(&context));
 
-        // Rest is Solar visitor:
-        let Expr { span, kind } = expr;
-        self.visit_span(span)?;
-        match kind {
-            ExprKind::Array(exprs) => {
-                for expr in exprs.iter() {
-                    self.visit_expr(expr)?;
-                }
-            }
-            ExprKind::Assign(lhs, _op, rhs) => {
-                self.visit_expr(lhs)?;
-                self.visit_expr(rhs)?;
-            }
-            ExprKind::Binary(lhs, _op, rhs) => {
-                self.visit_expr(lhs)?;
-                self.visit_expr(rhs)?;
-            }
-            ExprKind::Call(lhs, args) => {
-                self.visit_expr(lhs)?;
-                self.visit_call_args(args)?;
-            }
-            ExprKind::CallOptions(lhs, args) => {
-                self.visit_expr(lhs)?;
-                self.visit_named_args(args)?;
-            }
-            ExprKind::Delete(expr) => {
-                self.visit_expr(expr)?;
-            }
-            ExprKind::Ident(ident) => {
-                self.visit_ident(ident)?;
-            }
-            ExprKind::Index(lhs, kind) => {
-                self.visit_expr(lhs)?;
-                match kind {
-                    IndexKind::Index(expr) => {
-                        if let Some(expr) = expr {
-                            self.visit_expr(expr)?;
-                        }
-                    }
-                    IndexKind::Range(start, end) => {
-                        if let Some(start) = start {
-                            self.visit_expr(start)?;
-                        }
-                        if let Some(end) = end {
-                            self.visit_expr(end)?;
-                        }
-                    }
-                }
-            }
-            ExprKind::Lit(lit, _sub) => {
-                self.visit_lit(lit)?;
-            }
-            ExprKind::Member(expr, member) => {
-                self.visit_expr(expr)?;
-                self.visit_ident(member)?;
-            }
-            ExprKind::New(ty) => {
-                self.visit_ty(ty)?;
-            }
-            ExprKind::Payable(args) => {
-                self.visit_call_args(args)?;
-            }
-            ExprKind::Ternary(cond, true_, false_) => {
-                self.visit_expr(cond)?;
-                self.visit_expr(true_)?;
-                self.visit_expr(false_)?;
-            }
-            ExprKind::Tuple(exprs) => {
-                for expr in exprs.iter().flatten() {
-                    self.visit_expr(expr)?;
-                }
-            }
-            ExprKind::TypeCall(ty) => {
-                self.visit_ty(ty)?;
-            }
-            ExprKind::Type(ty) => {
-                self.visit_ty(ty)?;
-            }
-            ExprKind::Unary(_op, expr) => {
-                self.visit_expr(expr)?;
-            }
-        }
-        ControlFlow::Continue(())
+        self.default_visitor.visit_expr(expr)
     }
 }
