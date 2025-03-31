897597bc9a843106facebf63fb4bc38b3ce78df4
crStiv <cryptostiv7@gmail.com>
2025-03-31 22:36:28 +0200

    Update handler.rs

--- crates/anvil/src/server/handler.rs
+++ crates/anvil/src/server/handler.rs
@@ -1,28 +1,73 @@
 //! Contains RPC handlers
 use crate::{
-    eth::error::to_rpc_result,
+    eth::EthApi,
     pubsub::{EthSubscription, LogsSubscription},
-    EthApi,
 };
 use alloy_rpc_types::{
     pubsub::{Params, SubscriptionKind},
     FilteredParams,
 };
-use anvil_core::eth::{subscription::SubscriptionId, EthPubSub, EthRequest, EthRpcCall};
-use anvil_rpc::{error::RpcError, response::ResponseResult};
-use anvil_server::{PubSubContext, PubSubRpcHandler, RpcHandler};
+use anvil_core::eth::{
+    subscription::SubscriptionId,
+    EthPubSub,
+    EthRequest,
+    EthRpcCall,
+};
+use anvil_rpc::{
+    error::RpcError,
+    response::ResponseResult,
+};
+use anvil_server::{
+    PubSubContext,
+    PubSubRpcHandler,
+    RpcHandler,
+};
+use axum::{
+    extract::State,
+    http::{Method, StatusCode},
+    response::IntoResponse,
+    routing::post,
+    Router,
+};
+use serde_json::Value;
+use std::sync::Arc;
+
+struct SubscriptionIdWrapper(SubscriptionId);
+
+impl From<SubscriptionId> for SubscriptionIdWrapper {
+    fn from(id: SubscriptionId) -> Self {
+        Self(id)
+    }
+}
+
+impl From<SubscriptionIdWrapper> for Value {
+    fn from(wrapper: SubscriptionIdWrapper) -> Self {
+        Value::String(wrapper.0.to_string())
+    }
+}
 
 /// A `RpcHandler` that expects `EthRequest` rpc calls via http
 #[derive(Clone)]
 pub struct HttpEthRpcHandler {
-    /// Access to the node
+    /// The eth api instance that handles the requests
     api: EthApi,
+    /// Custom headers to add to responses
+    headers: Vec<String>,
 }
 
 impl HttpEthRpcHandler {
     /// Creates a new instance of the handler using the given `EthApi`
     pub fn new(api: EthApi) -> Self {
-        Self { api }
+        Self {
+            api,
+            headers: Vec::new(),
+        }
+    }
+
+    /// Sets custom headers for responses
+    pub fn with_headers(mut self, headers: Vec<String>) -> Self {
+        self.headers = headers;
+        self
     }
 }
 
@@ -32,6 +77,10 @@ impl RpcHandler for HttpEthRpcHandler {
 
     async fn on_request(&self, request: Self::Request) -> ResponseResult {
         self.api.execute(request).await
+    }
+
+    fn get_anvil_headers(&self) -> Option<&Vec<String>> {
+        Some(&self.headers)
     }
 }
 
@@ -47,67 +96,6 @@ impl PubSubEthRpcHandler {
     pub fn new(api: EthApi) -> Self {
         Self { api }
     }
-
-    /// Invoked for an ethereum pubsub rpc call
-    async fn on_pub_sub(&self, pubsub: EthPubSub, cx: PubSubContext<Self>) -> ResponseResult {
-        let id = SubscriptionId::random_hex();
-        trace!(target: "rpc::ws", "received pubsub request {:?}", pubsub);
-        match pubsub {
-            EthPubSub::EthUnSubscribe(id) => {
-                trace!(target: "rpc::ws", "canceling subscription {:?}", id);
-                let canceled = cx.remove_subscription(&id).is_some();
-                ResponseResult::Success(canceled.into())
-            }
-            EthPubSub::EthSubscribe(kind, params) => {
-                let filter = match *params {
-                    Params::None => None,
-                    Params::Logs(filter) => Some(*filter),
-                    Params::Bool(_) => {
-                        return ResponseResult::Error(RpcError::invalid_params(
-                            "Expected params for logs subscription",
-                        ))
-                    }
-                };
-                let params = FilteredParams::new(filter);
-
-                let subscription = match kind {
-                    SubscriptionKind::Logs => {
-                        trace!(target: "rpc::ws", "received logs subscription {:?}", params);
-                        let blocks = self.api.new_block_notifications();
-                        let storage = self.api.storage_info();
-                        EthSubscription::Logs(Box::new(LogsSubscription {
-                            blocks,
-                            storage,
-                            filter: params,
-                            queued: Default::default(),
-                            id: id.clone(),
-                        }))
-                    }
-                    SubscriptionKind::NewHeads => {
-                        trace!(target: "rpc::ws", "received header subscription");
-                        let blocks = self.api.new_block_notifications();
-                        let storage = self.api.storage_info();
-                        EthSubscription::Header(blocks, storage, id.clone())
-                    }
-                    SubscriptionKind::NewPendingTransactions => {
-                        trace!(target: "rpc::ws", "received pending transactions subscription");
-                        EthSubscription::PendingTransactions(
-                            self.api.new_ready_transactions(),
-                            id.clone(),
-                        )
-                    }
-                    SubscriptionKind::Syncing => {
-                        return RpcError::internal_error_with("Not implemented").into()
-                    }
-                };
-
-                cx.add_subscription(id.clone(), subscription);
-
-                trace!(target: "rpc::ws", "created new subscription: {:?}", id);
-                to_rpc_result(id)
-            }
-        }
-    }
 }
 
 #[async_trait::async_trait]
@@ -117,10 +105,63 @@ impl PubSubRpcHandler for PubSubEthRpcHandler {
     type Subscription = EthSubscription;
 
     async fn on_request(&self, request: Self::Request, cx: PubSubContext<Self>) -> ResponseResult {
-        trace!(target: "rpc", "received pubsub request {:?}", request);
         match request {
             EthRpcCall::Request(request) => self.api.execute(*request).await,
-            EthRpcCall::PubSub(pubsub) => self.on_pub_sub(pubsub, cx).await,
+            EthRpcCall::PubSub(pubsub) => match pubsub {
+                EthPubSub::EthSubscribe(kind, params) => {
+                    let filter = match *params {
+                        Params::None => None,
+                        Params::Logs(filter) => Some(*filter),
+                        Params::Bool(_) => {
+                            return ResponseResult::Error(RpcError::invalid_params(
+                                "Expected params for logs subscription",
+                            ))
+                        }
+                    };
+                    let params = FilteredParams::new(filter);
+
+                    let (subscription, id) = match kind {
+                        SubscriptionKind::Logs => {
+                            let blocks = self.api.new_block_notifications();
+                            let storage = self.api.storage_info();
+                            let id = SubscriptionId::random_hex();
+                            let subscription = EthSubscription::Logs(Box::new(LogsSubscription {
+                                blocks,
+                                storage,
+                                filter: params,
+                                queued: Default::default(),
+                                id: id.clone(),
+                            }));
+                            (subscription, id)
+                        }
+                        SubscriptionKind::NewHeads => {
+                            let blocks = self.api.new_block_notifications();
+                            let storage = self.api.storage_info();
+                            let id = SubscriptionId::random_hex();
+                            let subscription = EthSubscription::Header(blocks, storage, id.clone());
+                            (subscription, id)
+                        }
+                        SubscriptionKind::NewPendingTransactions => {
+                            let id = SubscriptionId::random_hex();
+                            let subscription = EthSubscription::PendingTransactions(
+                                self.api.new_ready_transactions(),
+                                id.clone(),
+                            );
+                            (subscription, id)
+                        }
+                        SubscriptionKind::Syncing => {
+                            return ResponseResult::Error(RpcError::internal_error_with("Not implemented"))
+                        }
+                    };
+
+                    cx.add_subscription(id.clone(), subscription);
+                    ResponseResult::Success(SubscriptionIdWrapper::from(id).into())
+                }
+                EthPubSub::EthUnSubscribe(id) => {
+                    let removed = cx.remove_subscription(&id).is_some();
+                    ResponseResult::Success(removed.into())
+                }
+            },
         }
     }
 }
