75f0bc2f70ed4565a431c56e3431023bfd4316b5
Ayush Dubey <61616662+Ayushdubey86@users.noreply.github.com>
2025-04-05 22:04:09 +0530

    Update opts.rs

--- crates/cast/src/opts.rs
+++ crates/cast/src/opts.rs
@@ -1036,14 +1036,7 @@ pub enum CastSubcommand {
 
     /// Decodes a raw signed EIP 2718 typed transaction
     #[command(visible_aliases = &["dt", "decode-tx"])]
-    DecodeTransaction {
-        /// Hex or RLP-encoded transaction string.
-        tx: Option<String>,
-
-        /// Treat input as RLP-encoded.
-        #[arg(long)]
-        from_rlp: bool,
-    },
+    DecodeTransaction { tx: Option<String> },
     /// Extracts function selectors and arguments from bytecode
     #[command(visible_alias = "sel")]
     Selectors {
