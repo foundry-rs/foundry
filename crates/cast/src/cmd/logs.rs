613072430a3838c4c6fbb161510e791b5636d68b
leopardracer <136604165+leopardracer@users.noreply.github.com>
2025-04-07 20:17:37 +0300

    Update logs.rs

--- crates/cast/src/cmd/logs.rs
+++ crates/cast/src/cmd/logs.rs
@@ -269,8 +269,7 @@ mod tests {
 
     #[test]
     fn test_build_filter_sig_with_arguments() {
-        let addr = ADDRESS;
-        let addr = U256::from(U160::from_be_bytes(addr.0 .0));
+        let addr = U256::from(U160::from_be_bytes(ADDRESS.0 .0));
         let expected = Filter {
             block_option: FilterBlockOption::Range { from_block: None, to_block: None },
             address: vec![].into(),
@@ -294,8 +293,7 @@ mod tests {
 
     #[test]
     fn test_build_filter_sig_with_skipped_arguments() {
-        let addr = ADDRESS;
-        let addr = U256::from(U160::from_be_bytes(addr.0 .0));
+        let addr = U256::from(U160::from_be_bytes(ADDRESS.0 .0));
         let expected = Filter {
             block_option: FilterBlockOption::Range { from_block: None, to_block: None },
             address: vec![].into(),
