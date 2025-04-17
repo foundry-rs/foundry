7a6b9c1d0bc4ac9f05f98a3f293aefd0d2315a1d
zerosnacks <zerosnacks@protonmail.com>
2025-04-17 14:06:30 +0200

    forge + script: fix types

--- crates/forge/src/multi_runner.rs
+++ crates/forge/src/multi_runner.rs
@@ -23,6 +23,7 @@ use foundry_evm::{
     opts::EvmOpts,
     revm,
     traces::{InternalTraceMode, TraceMode},
+    Env,
 };
 use foundry_linking::{LinkOutput, Linker};
 use rayon::prelude::*;
@@ -279,7 +280,7 @@ pub struct TestRunnerConfig {
     /// EVM configuration.
     pub evm_opts: EvmOpts,
     /// EVM environment.
-    pub env: revm::primitives::Env,
+    pub env: Env,
     /// EVM version.
     pub spec_id: SpecId,
     /// The address which will be used to deploy the initial contracts and send all transactions.
@@ -472,7 +473,7 @@ impl MultiContractRunnerBuilder {
         self,
         root: &Path,
         output: &ProjectCompileOutput,
-        env: revm::primitives::Env,
+        env: Env,
         evm_opts: EvmOpts,
     ) -> Result<MultiContractRunner> {
         let contracts = output
--- crates/script/src/simulate.rs
+++ crates/script/src/simulate.rs
@@ -139,7 +139,7 @@ impl PreSimulationState {
 
                 // Simulate mining the transaction if the user passes `--slow`.
                 if self.args.slow {
-                    runner.executor.env_mut().block.number += U256::from(1);
+                    runner.executor.env_mut().evm_env.block_env.number += 1;
                 }
 
                 let is_noop_tx = if let Some(to) = to {
