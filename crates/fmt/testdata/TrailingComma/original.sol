872da09ee2ae2e38875de8a8434d92a150acb49f
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2025-04-22 23:59:51 +0200

    test: function parameters cannot be empty

--- crates/fmt/testdata/TrailingComma/original.sol
+++ crates/fmt/testdata/TrailingComma/original.sol
@@ -1,12 +1,8 @@
 contract C is Contract {
-    modifier m(uint256, ,,, ) {}
-    // invalid solidity code, but valid pt
-    modifier m2(uint256) returns (uint256,,,) {}
-
     function f(uint256 a, ) external {}
-    function f2(uint256 a, , , ,bytes32 b) external returns (uint256,,,,) {}
+    function f2(uint256 a, bytes32 b,) external returns (uint256,) {}
 
     function f3() external {
-        try some.invoke() returns (uint256,,,uint256) {} catch {}
+        try some.invoke() returns (uint256,uint256,) {} catch {}
     }
 }
