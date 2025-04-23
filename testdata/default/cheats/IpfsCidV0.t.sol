ca89033f6eabd2b08fc7e13f18f5b8a8b8efb35b
Ayush Dubey <61616662+Ayushdubey86@users.noreply.github.com>
2025-04-23 20:54:22 +0530

    test case

---
+++ testdata/default/cheats/IpfsCidV0.t.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity ^0.8.18;
+
+import "ds-test/test.sol";
+import "cheats/Vm.sol";
+
+contract IpfsCidV0Test is DSTest {
+    Vm constant vm = Vm(HEVM_ADDRESS);
+
+    function testIpfsCidV0() public {
+        string memory filePath = "./testdata/test.txt";
+        string memory fileContents = "---\ntitle: TestTitle\ndiscussions: TestDiscussion\nauthor: TestAuthor\n---";
+
+        vm.writeFile(filePath, fileContents);
+
+        bytes32 cid = vm.ipfsCidV0(filePath);
+
+        bytes32 expectedCid = 0x94da694df5cf2e139206cddcdd6f855baa45e519c5fdbc2e6aa1cf803cfd65d5;
+
+        assertEq(cid, expectedCid);
+    }
+}
