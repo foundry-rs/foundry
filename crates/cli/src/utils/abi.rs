2016d96b0e92563b5095e90554922ed309c7eeac
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2025-04-30 00:18:26 +0200

    info log

--- crates/cli/src/utils/abi.rs
+++ crates/cli/src/utils/abi.rs
@@ -46,6 +46,7 @@ pub async fn parse_function_args<P: Provider<AnyNetwork>>(
         // a regular function signature with parentheses
         get_func(sig)?
     } else {
+        info!("function signature does not contain parentheses, fetching function data from Etherscan");
         let etherscan_api_key = etherscan_api_key.ok_or_eyre(
             "Function signature does not contain parentheses. If you wish to fetch function data from Etherscan, please provide an API key.",
         )?;
