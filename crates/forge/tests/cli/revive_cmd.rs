859295bbd0d602c96eb41299612e62c23847aaa2
Sebastian Miasojed <s.miasojed@gmail.com>
2025-05-13 12:47:14 +0200

    Test import paths (#120)
    * Add import tests
    
    * Add test for compilation to the same output dir (#105)
    
    * Merge branch 'master' into sm/path
    
    * Added extra newline at the end of expected compiler output
    
    * Added extra newline at the end of expected compiler output
    
    ---------
    
    Co-authored-by: filip-parity <filip.baciu@parity.io>

--- crates/forge/tests/cli/revive_cmd.rs
+++ crates/forge/tests/cli/revive_cmd.rs
@@ -1,6 +1,6 @@
-use std::{fs, path::Path};
+use std::{fs, path::Path, str::FromStr};
 
-use foundry_compilers::artifacts::{ConfigurableContractArtifact, Metadata};
+use foundry_compilers::artifacts::{ConfigurableContractArtifact, Metadata, Remapping};
 use foundry_config::SolidityErrorCode;
 use foundry_test_utils::{snapbox::IntoData, TestProject};
 
@@ -934,4 +934,90 @@ forgetest!(can_bind_enum_modules_for_resolc, |prj, cmd| {
 Compiler run successful!
 Generating bindings for 1 contracts
 Bindings have been generated to [..]"#]]);
+});
+
+// Tests that direct import paths are handled correctly
+forgetest!(can_handle_direct_imports_into_src_for_resolc, |prj, cmd| {
+    prj.add_source(
+        "Foo",
+        r#"
+import {FooLib} from "src/FooLib.sol";
+struct Bar {
+    uint8 x;
+}
+contract Foo {
+    mapping(uint256 => Bar) bars;
+    function checker(uint256 id) external {
+        Bar memory b = bars[id];
+        FooLib.check(b);
+    }
+    function checker2() external {
+        FooLib.check2(this);
+    }
+}
+   "#,
+    )
+    .unwrap();
+
+    prj.add_source(
+        "FooLib",
+        r#"
+import {Foo, Bar} from "src/Foo.sol";
+library FooLib {
+    function check(Bar memory b) internal {}
+    function check2(Foo f) internal {}
+}
+   "#,
+    )
+    .unwrap();
+
+    cmd.args(["build", "--resolc"]).assert_success().stdout_eq(str![[r#"
+[COMPILING_FILES] with [RESOLC_VERSION]
+[RESOLC_VERSION] [ELAPSED]
+Compiler run successful!
+
+"#]]);
+});
+
+forgetest!(can_use_absolute_imports_for_resolc, |prj, cmd| {
+    prj.update_config(|config| {
+        let remapping = prj.paths().libraries[0].join("myDependency");
+        config.remappings =
+            vec![Remapping::from_str(&format!("myDependency/={}", remapping.display()))
+                .unwrap()
+                .into()];
+    });
+
+    prj.add_lib(
+        "myDependency/src/interfaces/IConfig.sol",
+        r"
+    interface IConfig {}
+   ",
+    )
+    .unwrap();
+
+    prj.add_lib(
+        "myDependency/src/Config.sol",
+        r#"
+        import "src/interfaces/IConfig.sol";
+    contract Config {}
+   "#,
+    )
+    .unwrap();
+
+    prj.add_source(
+        "Greeter",
+        r#"
+        import "myDependency/src/Config.sol";
+    contract Greeter {}
+   "#,
+    )
+    .unwrap();
+
+    cmd.args(["build", "--resolc"]).assert_success().stdout_eq(str![[r#"
+[COMPILING_FILES] with [RESOLC_VERSION]
+[RESOLC_VERSION] [ELAPSED]
+Compiler run successful!
+
+"#]]);
 });
