e1b854b6cc40c27cc9ba0dba2dbb2857c315a281
zerosnacks <zerosnacks@protonmail.com>
2025-05-13 18:23:59 +0200

    restore test

--- crates/anvil/src/evm.rs
+++ crates/anvil/src/evm.rs
@@ -5,7 +5,6 @@ use alloy_evm::{
     precompiles::{DynPrecompile, PrecompilesMap},
     Database, Evm,
 };
-use foundry_evm::backend::DatabaseError;
 use foundry_evm_core::either_evm::EitherEvm;
 use op_revm::OpContext;
 use revm::{precompile::PrecompileWithAddress, Inspector};
@@ -22,19 +21,85 @@ pub fn inject_precompiles<DB, I>(
     evm: &mut EitherEvm<DB, I, PrecompilesMap>,
     precompiles: Vec<PrecompileWithAddress>,
 ) where
-    DB: Database<Error = DatabaseError>,
+    DB: Database,
     I: Inspector<EthEvmContext<DB>> + Inspector<OpContext<DB>>,
 {
-    for item in precompiles {
+    for p in precompiles {
         evm.precompiles_mut()
-            .apply_precompile(item.address(), |_| Some(DynPrecompile::from(*item.precompile())));
+            .apply_precompile(p.address(), |_| Some(DynPrecompile::from(*p.precompile())));
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use alloy_evm::{eth::EthEvmContext, precompiles::PrecompilesMap, EthEvm, Evm};
+    use alloy_primitives::{address, Address, Bytes};
+    use foundry_evm::Env;
+    use foundry_evm_core::either_evm::EitherEvm;
+    use itertools::Itertools;
+    use revm::{
+        context::{Evm as RevmEvm, JournalTr, LocalContext},
+        database::EmptyDB,
+        handler::{instructions::EthInstructions, EthPrecompiles},
+        inspector::NoOpInspector,
+        interpreter::interpreter::EthInterpreter,
+        precompile::{PrecompileOutput, PrecompileResult, PrecompileWithAddress},
+        Journal,
+    };
+
+    use crate::{inject_precompiles, PrecompileFactory};
+
     #[test]
     fn build_evm_with_extra_precompiles() {
-        // TODO: add test
+        const PRECOMPILE_ADDR: Address = address!("0x0000000000000000000000000000000000000071");
+
+        fn my_precompile(_input: &[u8], _gas_limit: u64) -> PrecompileResult {
+            Ok(PrecompileOutput { bytes: Bytes::new(), gas_used: 0 })
+        }
+
+        #[derive(Debug)]
+        struct CustomPrecompileFactory;
+
+        impl PrecompileFactory for CustomPrecompileFactory {
+            fn precompiles(&self) -> Vec<PrecompileWithAddress> {
+                vec![PrecompileWithAddress::from((
+                    PRECOMPILE_ADDR,
+                    my_precompile as fn(&[u8], u64) -> PrecompileResult,
+                ))]
+            }
+        }
+
+        let env = Env::default();
+        let evm_context = EthEvmContext {
+            journaled_state: Journal::new(EmptyDB::default()),
+            block: env.evm_env.block_env.clone(),
+            cfg: env.evm_env.cfg_env.clone(),
+            tx: env.tx.clone(),
+            chain: (),
+            local: LocalContext::default(),
+            error: Ok(()),
+        };
+
+        let mut evm = EitherEvm::Eth(EthEvm::new(
+            RevmEvm::new_with_inspector(
+                evm_context,
+                NoOpInspector,
+                EthInstructions::<EthInterpreter, EthEvmContext<EmptyDB>>::default(),
+                PrecompilesMap::from_static(EthPrecompiles::default().precompiles),
+            ),
+            true,
+        ));
+
+        assert!(!evm.precompiles_mut().addresses().contains(&PRECOMPILE_ADDR));
+
+        inject_precompiles(&mut evm, CustomPrecompileFactory.precompiles());
+
+        assert!(evm.precompiles_mut().addresses().contains(&PRECOMPILE_ADDR));
+
+        let result = match &mut evm {
+            EitherEvm::Eth(evm_eth) => evm_eth.transact(env.tx).unwrap(),
+            _ => unreachable!(),
+        };
+        assert!(result.result.is_success());
     }
 }
