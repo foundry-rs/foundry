0848d9b80d99948836313eccf0ec7b5a6a0f1cb9
zerosnacks <zerosnacks@protonmail.com>
2025-05-14 16:18:08 +0200

    clean up and optimize

--- crates/evm/core/src/utils.rs
+++ crates/evm/core/src/utils.rs
@@ -3,7 +3,7 @@ use alloy_json_abi::{Function, JsonAbi};
 use alloy_network::{AnyTxEnvelope, TransactionResponse};
 use alloy_primitives::{Address, Selector, TxKind, B256, U256};
 use alloy_provider::{network::BlockResponse, Network};
-use alloy_rpc_types::{AccessList, Transaction, TransactionRequest};
+use alloy_rpc_types::{Transaction, TransactionRequest};
 use eyre::ensure;
 use foundry_common::is_impersonated_tx;
 use foundry_config::NamedChain;
@@ -134,50 +134,46 @@ pub fn configure_tx_req_env(
     env.tx.data = input.input().cloned().unwrap_or_default();
     env.tx.chain_id = chain_id;
 
-    // Type 0, Legacy, default transaction type.
-    let mut tx_type = TransactionType::Legacy;
-
-    // Type 1, EIP-2930, derived from existence of `access_list`.
-    if let Some(access_list) = access_list {
-        // If the transaction type is legacy we need to upgrade it to EIP-2930 transaction type
-        // in order to use access lists. Other transaction types support access lists
-        // themselves.
-        tx_type = TransactionType::Eip2930;
-        env.tx.access_list = access_list.clone();
-    } else {
-        env.tx.access_list = AccessList::default();
-    }
-
-    // Type 2, EIP-1559, derived from existence of `Some(max_fee_per_gas)`.
-    if max_fee_per_gas.is_some() {
-        tx_type = TransactionType::Eip1559;
-    }
+    env.tx.access_list = access_list.clone().unwrap_or_default();
     env.tx.gas_price = gas_price.or(max_fee_per_gas).unwrap_or_default();
     env.tx.gas_priority_fee = max_priority_fee_per_gas;
-
-    // Type 3, EIP-4844, derived from existence of `Some(max_fee_per_blob_gas)`.
-    if max_fee_per_blob_gas.is_some() {
-        ensure!(to.is_some(), "EIP-4844 transactions must have a `to` field set");
-        tx_type = TransactionType::Eip4844;
-    }
     env.tx.blob_hashes = blob_versioned_hashes.clone().unwrap_or_default();
     env.tx.max_fee_per_blob_gas = max_fee_per_blob_gas.unwrap_or_default();
 
-    // Type 4, EIP-7702, derived from existence of `authorization_list`.
-    if let Some(authorization_list) = authorization_list {
-        // If the transaction has an authorization list, we need to set the transaction type to
-        // EIP-7702.
-        ensure!(to.is_some(), "EIP-7702 transactions must have a `to` field set");
-        tx_type = TransactionType::Eip7702;
-        env.tx.set_signed_authorization(authorization_list.clone());
-    }
-
-    // If the transaction type is not already set we use the derived transaction type.
-    if let Some(transaction_type) = transaction_type {
-        env.tx.tx_type = transaction_type;
+    let tx_type = if let Some(transaction_type) = transaction_type {
+        transaction_type
     } else {
-        env.tx.tx_type = tx_type as u8;
-    }
+        // Type 2, EIP-1559, derived from existence of `Some(max_fee_per_gas)`.
+        let derived = if max_fee_per_gas.is_some() {
+            TransactionType::Eip1559
+        }
+        // Type 3, EIP-4844, derived from existence of `Some(max_fee_per_blob_gas)`.
+        else if max_fee_per_blob_gas.is_some() {
+            ensure!(to.is_some(), "EIP-4844 tx must have `to` set");
+            TransactionType::Eip4844
+        }
+        // Type 4, EIP-7702, derived from existence of `authorization_list`.
+        else if let Some(auth) = authorization_list {
+            ensure!(to.is_some(), "EIP-7702 tx must have `to` set");
+            env.tx.set_signed_authorization(auth.clone());
+            TransactionType::Eip7702
+        }
+        // Type 1, EIP-2930, derived from existence of `access_list`.
+        else if access_list.is_some() {
+            // At this point we know that the transaction is not EIP-1559, EIP-4844 or EIP-7702.
+            // If the transaction type is legacy we need to upgrade it to EIP-2930 transaction type
+            // in order to use access lists. Other transaction types support access lists
+            // themselves.
+            TransactionType::Eip2930
+        }
+        // Type 0, Legacy, default transaction type.
+        else {
+            TransactionType::Legacy
+        };
+        derived as u8
+    };
+
+    env.tx.tx_type = tx_type;
 
     Ok(())
 }
