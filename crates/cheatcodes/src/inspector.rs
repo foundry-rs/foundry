db6ab365b043d77ea41587480e8c0a46357af8ee
grandizzy <grandizzy.the.egg@gmail.com>
2025-05-16 10:01:25 +0300

    fix(forge): mark prank applied on contract creation too

--- crates/cheatcodes/src/inspector.rs
+++ crates/cheatcodes/src/inspector.rs
@@ -696,14 +696,25 @@ impl Cheatcodes {
         // Apply our prank
         if let Some(prank) = &self.get_prank(curr_depth) {
             if curr_depth >= prank.depth && input.caller() == prank.prank_caller {
+                let mut prank_applied = false;
+
                 // At the target depth we set `msg.sender`
                 if ecx.journaled_state.depth() == prank.depth {
                     input.set_caller(prank.new_caller);
+                    prank_applied = true;
                 }
 
                 // At the target depth, or deeper, we set `tx.origin`
                 if let Some(new_origin) = prank.new_origin {
                     ecx.env.tx.caller = new_origin;
+                    prank_applied = true;
+                }
+
+                // If prank applied for first time, then update
+                if prank_applied {
+                    if let Some(applied_prank) = prank.first_time_applied() {
+                        self.pranks.insert(curr_depth, applied_prank);
+                    }
                 }
             }
         }
