1349b77f2c26451223a441361cc7fc031190177b
zerosnacks <95942363+zerosnacks@users.noreply.github.com>
2025-05-22 15:38:48 +0200

    ci: use Optimism mainnet instead of DRPC due to limits (#10592)
    use Optimism mainnet instead of DRPC due to limits

--- crates/test-utils/src/rpc.rs
+++ crates/test-utils/src/rpc.rs
@@ -147,6 +147,10 @@ fn next_url(is_ws: bool, chain: NamedChain) -> String {
         return "https://mainnet.base.org".to_string();
     }
 
+    if matches!(chain, Optimism) {
+        return "https://mainnet.optimism.io".to_string();
+    }
+
     if matches!(chain, BinanceSmartChainTestnet) {
         return "https://bsc-testnet-rpc.publicnode.com".to_string();
     }
@@ -166,7 +170,6 @@ fn next_url(is_ws: bool, chain: NamedChain) -> String {
         let key = DRPC_KEYS[idx];
 
         let network = match chain {
-            Optimism => "optimism",
             Arbitrum => "arbitrum",
             Polygon => "polygon",
             Sepolia => "sepolia",
