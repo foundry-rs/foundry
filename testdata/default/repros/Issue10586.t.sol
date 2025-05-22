6be13e15b51fc47745b98400dd9d1bd94a65d003
zerosnacks <zerosnacks@protonmail.com>
2025-05-22 12:36:14 +0200

    add previous trace for comparison

--- testdata/default/repros/Issue10586.t.sol
+++ testdata/default/repros/Issue10586.t.sol
@@ -53,6 +53,28 @@ contract Issue10586Test is DSTest {
 
     /// forge-config: default.isolate = true
     function testGetChainIdAfterSetIsolated() public {
+        // Previous test failed with the following error:
+        //
+        // [FAIL: EvmError: Revert] testGetChainIdAfterSetIsolated() (gas: 30322)
+        // Traces:
+        // [208460] DefaultTestContract::setUp()
+        //     ├─ [170920] → new Target@0xCe71065D4017F316EC606Fe4422e11eB2c47c246
+        //     │   └─ ← [Return] 743 bytes of code
+        //     └─ ← [Stop]
+        //
+        // [30322] DefaultTestContract::testGetChainIdAfterSetIsolated()
+        //     ├─ [24037] Target::setChainId()
+        //     │   ├─ [0] VM::chainId(123)
+        //     │   │   └─ ← [Return]
+        //     │   └─ ← [Stop]
+        //     ├─ [0] VM::chainId(100)
+        //     │   └─ ← [Return]
+        //     ├─ [0] Target::setChainId()
+        //     │   └─ ← [Revert] EvmError: Revert
+        //     └─ ← [Revert] EvmError: Revert
+        //
+        // Suite result: FAILED. 1 passed; 1 failed; 0 skipped; finished in 2.89ms (1.79ms CPU time)
+
         runTest();
     }
 }
