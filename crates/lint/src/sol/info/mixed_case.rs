8093b835ff118b928fd34b243d3070f6c5731bc8
0xrusowsky <0xrusowsky@proton.me>
2025-05-23 08:35:41 -0500

    fix: wildcard on `invariant_` rather than `invariant`

--- crates/lint/src/sol/info/mixed_case.rs
+++ crates/lint/src/sol/info/mixed_case.rs
@@ -64,7 +64,7 @@ pub fn is_mixed_case(s: &str, is_fn: bool) -> bool {
         return true
     }
 
-    // Ignore `fn test*`, `fn invariant*`, and `fn statefulFuzz` patterns, as they usually contain
+    // Ignore `fn test*`, `fn invariant_*`, and `fn statefulFuzz*` patterns, as they usually contain
     // (allowed) underscores.
-    is_fn && (s.starts_with("test") || s.starts_with("invariant") || s.starts_with("statefulFuzz"))
+    is_fn && (s.starts_with("test") || s.starts_with("invariant_") || s.starts_with("statefulFuzz"))
 }
