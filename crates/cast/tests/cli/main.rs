ffeefa267ade81cf2b5e54ec0c0c33b802119170
mablr <59505383+mablr@users.noreply.github.com>
2025-05-23 12:51:57 +0200

    feat(cast): display public_key on wallet creation with "new" and "new-mnemonic" subcommands
    The method used to obtain public_key with the wallet is taken from "public-key" subcommand.
    
    Update tests to match the new outputs:
      - match pubkey
      - update redactions for pubkey and to match various spacings
      - use raw data for assertion in "wallet_mnemonic_from_entropy" to avoid conflict with redactions

--- crates/cast/tests/cli/main.rs
+++ crates/cast/tests/cli/main.rs
@@ -11,6 +11,7 @@ use foundry_test_utils::{
         next_etherscan_api_key, next_http_archive_rpc_url, next_http_rpc_endpoint,
         next_rpc_endpoint, next_ws_rpc_endpoint,
     },
+    snapbox::IntoData,
     str,
     util::OutputExt,
 };
@@ -163,6 +164,7 @@ casttest!(new_wallet_keystore_with_password, |_prj, cmd| {
         .stdout_eq(str![[r#"
 Created new encrypted keystore file: [..]
 [ADDRESS]
+[PUBLIC_KEY]
 
 "#]]);
 });
@@ -422,24 +424,34 @@ casttest!(wallet_list_local_accounts, |prj, cmd| {
         .stdout_eq(str![[r#"
 Created new encrypted keystore file: [..]
 [ADDRESS]
+[PUBLIC_KEY]
 Created new encrypted keystore file: [..]
 [ADDRESS]
+[PUBLIC_KEY]
 Created new encrypted keystore file: [..]
 [ADDRESS]
+[PUBLIC_KEY]
 Created new encrypted keystore file: [..]
 [ADDRESS]
+[PUBLIC_KEY]
 Created new encrypted keystore file: [..]
 [ADDRESS]
+[PUBLIC_KEY]
 Created new encrypted keystore file: [..]
 [ADDRESS]
+[PUBLIC_KEY]
 Created new encrypted keystore file: [..]
 [ADDRESS]
+[PUBLIC_KEY]
 Created new encrypted keystore file: [..]
 [ADDRESS]
+[PUBLIC_KEY]
 Created new encrypted keystore file: [..]
 [ADDRESS]
+[PUBLIC_KEY]
 Created new encrypted keystore file: [..]
 [ADDRESS]
+[PUBLIC_KEY]
 
 "#]]);
 
@@ -481,18 +493,21 @@ test test test test test test test test test test test junk
 Accounts:
 - Account 0:
 Address:     0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
+Public key:  0x8318535b54105d4a7aae60c08fc45f9687181b4fdfc625bd1a753fa7397fed753547f11ca8696646f2f3acb08e31016afac23e630c5d11f59f61fef57b0d2aa5
 Private key: 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
 
 - Account 1:
 Address:     0x70997970C51812dc3A010C7d01b50e0d17dc79C8
+Public key:  0xba5734d8f7091719471e7f7ed6b9df170dc70cc661ca05e688601ad984f068b0d67351e5f06073092499336ab0839ef8a521afd334e53807205fa2f08eec74f4
 Private key: 0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d
 
 - Account 2:
 Address:     0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC
+Public key:  0x9d9031e97dd78ff8c15aa86939de9b1e791066a0224e331bc962a2099a7b1f0464b8bbafe1535f2301c72c2cb3535b172da30b02686ab0393d348614f157fbdb
 Private key: 0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a
 
 
-"#]]);
+"#]].raw());
 });
 
 // tests that `cast wallet new-mnemonic --json` outputs the expected mnemonic
@@ -513,14 +528,17 @@ casttest!(wallet_mnemonic_from_entropy_json, |_prj, cmd| {
   "accounts": [
     {
       "address": "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+      "public_key": "0x8318535b54105d4a7aae60c08fc45f9687181b4fdfc625bd1a753fa7397fed753547f11ca8696646f2f3acb08e31016afac23e630c5d11f59f61fef57b0d2aa5",
       "private_key": "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
     },
     {
       "address": "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
+      "public_key": "0xba5734d8f7091719471e7f7ed6b9df170dc70cc661ca05e688601ad984f068b0d67351e5f06073092499336ab0839ef8a521afd334e53807205fa2f08eec74f4",
       "private_key": "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d"
     },
     {
       "address": "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC",
+      "public_key": "0x9d9031e97dd78ff8c15aa86939de9b1e791066a0224e331bc962a2099a7b1f0464b8bbafe1535f2301c72c2cb3535b172da30b02686ab0393d348614f157fbdb",
       "private_key": "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a"
     }
   ]
