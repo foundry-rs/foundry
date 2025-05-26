49b9afc54576a9f5bd5aaff153366769a48524bd
Pavlo Khrystenko <45178695+pkhry@users.noreply.github.com>
2025-05-26 16:04:44 +0200

    Update Compilers fork (#137)
    * update compilers

--- crates/verify/src/etherscan/flatten.rs
+++ crates/verify/src/etherscan/flatten.rs
@@ -86,11 +86,19 @@ impl EtherscanFlattenedSource {
             SolcLanguage::Solidity,
             version.clone(),
         );
+        let compiler = SolcCompiler::Specific(solc);
 
-        let out = SolcCompiler::Specific(solc).compile(&input)?;
+        let out = compiler.compile(&input)?;
+        let compiler_version = compiler.compiler_version(&input);
+        let compund_version = compound_version(compiler_version, &input.version);
         if out.errors.iter().any(|e| e.is_error()) {
             let mut o = AggregatedCompilerOutput::<SolcCompiler>::default();
-            o.extend(version, RawBuildInfo::new(&input, &out, false)?, "default", out);
+            o.extend(
+                version,
+                RawBuildInfo::new(&input, &out, &compund_version, false)?,
+                "default",
+                out,
+            );
             let diags = o.diagnostics(&[], &[], Default::default());
 
             eyre::bail!(
@@ -118,4 +126,27 @@ fn strip_build_meta(version: Version) -> Version {
     } else {
         version
     }
+}
+
+fn compound_version(mut compiler_version: Version, input_version: &Version) -> Version {
+    if compiler_version != *input_version {
+        let build = if compiler_version.build.is_empty() {
+            semver::BuildMetadata::new(&format!(
+                "{}.{}.{}",
+                input_version.major, input_version.minor, input_version.patch,
+            ))
+            .expect("can't fail due to parsing")
+        } else {
+            semver::BuildMetadata::new(&format!(
+                "{}-{}.{}.{}",
+                compiler_version.build.as_str(),
+                input_version.major,
+                input_version.minor,
+                input_version.patch,
+            ))
+            .expect("can't fail due to parsing")
+        };
+        compiler_version.build = build;
+    };
+    compiler_version
 }
