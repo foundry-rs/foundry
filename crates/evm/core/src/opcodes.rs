a711c98bcad58325464688f96c07a3d60770fa99
MozirDmitriy <dmitriymozir@gmail.com>
2025-06-03 19:56:58 +0300

    Update broken REVM memory opcodes documentation link

--- crates/evm/core/src/opcodes.rs
+++ crates/evm/core/src/opcodes.rs
@@ -3,7 +3,7 @@
 use revm::bytecode::opcode::OpCode;
 
 /// Returns true if the opcode modifies memory.
-/// <https://bluealloy.github.io/revm/crates/interpreter/memory.html#opcodes>
+/// <https://reth.rs/docs/reth_ethereum/evm/revm/revm/bytecode/opcode/struct.OpCode.html#method.modifies_memory>
 /// <https://github.com/crytic/evm-opcodes>
 #[inline]
 pub const fn modifies_memory(opcode: OpCode) -> bool {
