9043dc8c4c8e4a8f03cbc40ce73590c9a893e151
Matthias Seitz <matthias.seitz@outlook.de>
2025-06-04 13:33:29 +0200

    chore: add additional check for is_impersonanted

--- crates/common/src/constants.rs
+++ crates/common/src/constants.rs
@@ -65,7 +65,9 @@ pub fn is_impersonated_tx(tx: &AnyTxEnvelope) -> bool {
 pub fn is_impersonated_sig(sig: &Signature, ty: u8) -> bool {
     let impersonated_sig =
         Signature::from_scalars_and_parity(B256::with_last_byte(1), B256::with_last_byte(1), false);
-    if ty != SYSTEM_TRANSACTION_TYPE && sig == &impersonated_sig {
+    if ty != SYSTEM_TRANSACTION_TYPE &&
+        (sig == &impersonated_sig || sig.r() == impersonated_sig.r())
+    {
         return true;
     }
     false
