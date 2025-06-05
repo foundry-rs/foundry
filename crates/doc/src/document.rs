973ff85432560a3c2bf152835bba480036b51d84
zerosnacks <zerosnacks@protonmail.com>
2025-06-05 19:11:27 +0200

    fix clippy

--- crates/doc/src/document.rs
+++ crates/doc/src/document.rs
@@ -2,7 +2,6 @@ use crate::{DocBuilder, ParseItem, PreprocessorId, PreprocessorOutput};
 use alloy_primitives::map::HashMap;
 use std::{
     path::{Path, PathBuf},
-    slice::IterMut,
     sync::Mutex,
 };
 
@@ -133,27 +132,6 @@ pub struct ParseItemIter<'a> {
 
 impl<'a> Iterator for ParseItemIter<'a> {
     type Item = &'a ParseItem;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if let Some(next) = self.next.take() {
-            return Some(next)
-        }
-        if let Some(other) = self.other.as_mut() {
-            return other.next()
-        }
-
-        None
-    }
-}
-
-#[derive(Debug)]
-pub struct ParseItemIterMut<'a> {
-    next: Option<&'a mut ParseItem>,
-    other: Option<IterMut<'a, ParseItem>>,
-}
-
-impl<'a> Iterator for ParseItemIterMut<'a> {
-    type Item = &'a mut ParseItem;
 
     fn next(&mut self) -> Option<Self::Item> {
         if let Some(next) = self.next.take() {
