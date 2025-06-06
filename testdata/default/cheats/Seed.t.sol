ae4ee9134c1e038495955a70eb33013a4dc4c09d
anotherdev <joseluismanco37@gmail.com>
2025-06-05 21:21:51 -0500

    fix(test): correct implementation of randomUint() chore: format

--- testdata/default/cheats/Seed.t.sol
+++ testdata/default/cheats/Seed.t.sol
@@ -13,33 +13,26 @@ contract SeedTest is DSTest {
         vm.setSeed(seed);
 
         // Call a foundry cheatcode to get a random value (this depends on the integration)
-        uint256 rand1 = uint256(vm.randUint());
+        uint256 rand1 = uint256(vm.randomUint());
 
         // Reset the seed and verify the result is the same
         vm.setSeed(seed);
-        uint256 rand2 = uint256(vm.randUint());
+        uint256 rand2 = uint256(vm.randomUint());
 
         // If the seed is the same, the random value must be equal
-        assertEq(
-            rand1,
-            rand2,
-            "Random value must be equal if seed is the same"
-        );
+        assertEq(rand1, rand2, "Random value must be equal if seed is the same");
     }
 
     function testSeedChangesRandom() public {
         // Use one seed
         vm.setSeed(1);
-        uint256 randA = uint256(vm.randUint());
+        uint256 randA = uint256(vm.randomUint());
 
         // Use a different seed
         vm.setSeed(2);
-        uint256 randB = uint256(vm.randUint());
+        uint256 randB = uint256(vm.randomUint());
 
         // Values must be different
-        assertTrue(
-            randA != randB,
-            "Random value must be different if seed is different"
-        );
+        assertTrue(randA != randB, "Random value must be different if seed is different");
     }
 }
