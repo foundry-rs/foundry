fc71f7b3449756f6f56a18d0614ecf1f563b4982
0xrusowsky <0xrusowsky@proton.me>
2025-06-19 13:51:10 +0200

    style: fmt

--- crates/lint/src/sol/high/mod.rs
+++ crates/lint/src/sol/high/mod.rs
@@ -7,7 +7,7 @@ mod incorrect_shift;
 mod unchecked_calls;
 
 use incorrect_shift::INCORRECT_SHIFT;
-use unchecked_calls::{UNCHECKED_CALL, ERC20_UNCHECKED_TRANSFER};
+use unchecked_calls::{ERC20_UNCHECKED_TRANSFER, UNCHECKED_CALL};
 
 register_lints!(
     (IncorrectShift, (INCORRECT_SHIFT)),
