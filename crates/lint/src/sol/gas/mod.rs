4d6369b236d67b2efbb2436d966d4c626576a68e
clandestine.eth <96172957+0xClandestine@users.noreply.github.com>
2025-07-09 00:59:30 -0400

    fmt

--- crates/lint/src/sol/gas/mod.rs
+++ crates/lint/src/sol/gas/mod.rs
@@ -6,4 +6,7 @@ use keccak::ASM_KECCAK256;
 mod unwrapped_modifier_logic;
 use unwrapped_modifier_logic::UNWRAPPED_MODIFIER_LOGIC;
 
-register_lints!((AsmKeccak256, (ASM_KECCAK256)), (UnwrappedModifierLogic, (UNWRAPPED_MODIFIER_LOGIC)));
+register_lints!(
+    (AsmKeccak256, (ASM_KECCAK256)),
+    (UnwrappedModifierLogic, (UNWRAPPED_MODIFIER_LOGIC))
+);
