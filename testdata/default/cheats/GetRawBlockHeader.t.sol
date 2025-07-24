fc68929b4db721577573dd12f6d6a980a8b5e6bb
grandizzy <grandizzy.the.egg@gmail.com>
2025-07-24 07:53:28 +0300

    Fix fmt

--- testdata/default/cheats/GetRawBlockHeader.t.sol
+++ testdata/default/cheats/GetRawBlockHeader.t.sol
@@ -8,10 +8,9 @@ contract GetRawBlockHeaderTest is DSTest {
     Vm constant vm = Vm(HEVM_ADDRESS);
 
     function testGetRawBlockHeaderWithFork() public {
-        // TODO: Not sure how RPCs are handled in tests.
         vm.createSelectFork("mainnet");
         assertEq(
-            keccak256(vm.getRawBlockHeader(22985278)), 
+            keccak256(vm.getRawBlockHeader(22985278)),
             // `cast keccak256 $(cast block 22985278 --raw)`
             0x492419d85d2817f50577807a287742fbdcaae00ce89f2ea885e419ee4493b00f
         );
