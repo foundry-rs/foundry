b9799e62c46b0f11a8aaf44beeb05f72bec33b26
0xrusowsky <0xrusowsky@proton.me>
2025-07-24 16:59:01 +0200

    refactor: move to new `codesize` lint group

--- crates/lint/src/sol/gas/mod.rs
+++ crates/lint/src/sol/gas/mod.rs
@@ -3,10 +3,4 @@ use crate::sol::{EarlyLintPass, LateLintPass, SolLint};
 mod keccak;
 use keccak::ASM_KECCAK256;
 
-mod unwrapped_modifier_logic;
-use unwrapped_modifier_logic::UNWRAPPED_MODIFIER_LOGIC;
-
-register_lints!(
-    (AsmKeccak256, late, (ASM_KECCAK256)),
-    (UnwrappedModifierLogic, late, (UNWRAPPED_MODIFIER_LOGIC))
-);
+register_lints!((AsmKeccak256, late, (ASM_KECCAK256)),);
