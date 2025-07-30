9f4a82bffb2c74f8b5c82ad897311380886ae95d
Ishika Choudhury <117741714+Rimeeeeee@users.noreply.github.com>
2025-07-30 17:02:53 +0530

    feat(cast): transaction to transaction request (#11151)
    * feat(cast): transaction to transaction request
    
    * fixes
    
    * fixes
    
    * fixes
    
    * fixes
    
    * kept json

--- crates/cast/src/args.rs
+++ crates/cast/src/args.rs
@@ -507,7 +507,7 @@ pub async fn run_command(args: CastArgs) -> Result<()> {
         }
         CastSubcommand::Run(cmd) => cmd.run().await?,
         CastSubcommand::SendTx(cmd) => cmd.run().await?,
-        CastSubcommand::Tx { tx_hash, from, nonce, field, raw, rpc } => {
+        CastSubcommand::Tx { tx_hash, from, nonce, field, raw, rpc, to_request } => {
             let config = rpc.load_config()?;
             let provider = utils::get_provider(&config)?;
 
@@ -516,7 +516,9 @@ pub async fn run_command(args: CastArgs) -> Result<()> {
 
             sh_println!(
                 "{}",
-                Cast::new(&provider).transaction(tx_hash, from, nonce, field, raw).await?
+                Cast::new(&provider)
+                    .transaction(tx_hash, from, nonce, field, raw, to_request)
+                    .await?
             )?
         }
 
