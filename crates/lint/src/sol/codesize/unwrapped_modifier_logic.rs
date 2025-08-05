eaf9d60164dde21588a795f30217d78377fa5abb
0xrusowsky <0xrusowsky@proton.me>
2025-08-05 09:43:55 -0700

    docs: expand cmnts

--- crates/lint/src/sol/codesize/unwrapped_modifier_logic.rs
+++ crates/lint/src/sol/codesize/unwrapped_modifier_logic.rs
@@ -63,7 +63,8 @@ impl UnwrappedModifierLogic {
     /// Checks if a block of statements is complex and should be wrapped in a helper function.
     ///
     /// This always is 'false' the modifier contains assembly. We assume that if devs know how to
-    /// reduce the codesize of their contracts and they have a good reason to do it.
+    /// use assembly, they will also know how to reduce the codesize of their contracts and they
+    /// have a good reason to use it on their modifiers.
     ///
     /// This is 'true' if the block contains:
     /// 1. Any statement that is not a placeholder or a valid expression.
@@ -79,7 +80,9 @@ impl UnwrappedModifierLogic {
                     }
                     has_valid_stmt = true;
                 }
-                hir::StmtKind::Err(_) => return false, // HIR doesn't support assembly yet
+                // HIR doesn't support assembly yet:
+                // <https://github.com/paradigmxyz/solar/blob/d25bf38a5accd11409318e023f701313d98b9e1e/crates/sema/src/hir/mod.rs#L977-L982>
+                hir::StmtKind::Err(_) => return false,
                 _ => res = true,
             }
         }
