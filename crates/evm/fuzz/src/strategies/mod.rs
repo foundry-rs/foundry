4f1134b2a19740ccebceb497ba360aa3afc5b17c
grandizzy <38490174+grandizzy@users.noreply.github.com>
2025-08-12 15:22:37 +0300

    Update crates/evm/fuzz/src/strategies/mod.rs
    Co-authored-by: DaniPopes <57450786+DaniPopes@users.noreply.github.com>

--- crates/evm/fuzz/src/strategies/mod.rs
+++ crates/evm/fuzz/src/strategies/mod.rs
@@ -14,6 +14,6 @@ mod state;
 pub use state::EvmFuzzState;
 
 mod invariants;
-mod mutators;
-
 pub use invariants::{fuzz_contract_with_calldata, invariant_strat, override_call_strat};
+
+mod mutators;
