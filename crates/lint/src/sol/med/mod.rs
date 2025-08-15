97c2b02296acb9cbb1549818a6920207ac148882
Tuan Tran <tropicaldog17@gmail.com>
2025-08-15 12:40:48 +0700

    feat(lint): add UnsafeTypecast lint (#11046)
    ---------
    
    Co-authored-by: 0xrusowsky <90208954+0xrusowsky@users.noreply.github.com>
    Co-authored-by: clandestine.eth <96172957+0xClandestine@users.noreply.github.com>
    Co-authored-by: 0xrusowsky <0xrusowsky@proton.me>

--- crates/lint/src/sol/med/mod.rs
+++ crates/lint/src/sol/med/mod.rs
@@ -3,4 +3,10 @@ use crate::sol::{EarlyLintPass, LateLintPass, SolLint};
 mod div_mul;
 use div_mul::DIVIDE_BEFORE_MULTIPLY;
 
-register_lints!((DivideBeforeMultiply, early, (DIVIDE_BEFORE_MULTIPLY)));
+mod unsafe_typecast;
+use unsafe_typecast::UNSAFE_TYPECAST;
+
+register_lints!(
+    (DivideBeforeMultiply, early, (DIVIDE_BEFORE_MULTIPLY)),
+    (UnsafeTypecast, late, (UNSAFE_TYPECAST))
+);
