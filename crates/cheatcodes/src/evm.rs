1b48399a527dea6652e2b54b87c1d558406a8eec
Matthias Seitz <matthias.seitz@outlook.de>
2025-08-19 21:27:49 +0200

    fix: force 4844 txtype in blobhashes setter (#11355)
    * test: add blobhashes repro
    
    * fix: force 4844 tx type

--- crates/cheatcodes/src/evm.rs
+++ crates/cheatcodes/src/evm.rs
@@ -7,6 +7,7 @@ use crate::{
 };
 use alloy_consensus::TxEnvelope;
 use alloy_genesis::{Genesis, GenesisAccount};
+use alloy_network::eip2718::EIP4844_TX_TYPE_ID;
 use alloy_primitives::{Address, B256, Bytes, U256, map::HashMap};
 use alloy_rlp::Decodable;
 use alloy_sol_types::SolValue;
@@ -437,6 +438,8 @@ impl Cheatcode for blobhashesCall {
              see EIP-4844: https://eips.ethereum.org/EIPS/eip-4844"
         );
         ccx.ecx.tx.blob_hashes.clone_from(hashes);
+        // force this as 4844 txtype
+        ccx.ecx.tx.tx_type = EIP4844_TX_TYPE_ID;
         Ok(Default::default())
     }
 }
