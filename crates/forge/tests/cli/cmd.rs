ea2e831ffa38ec0a37ffd93779b5fbf3fa11122f
zerosnacks <zerosnacks@protonmail.com>
2025-08-21 17:55:55 +0200

    add basic smoke test for regular forge init to make sure no regression was introduced

--- crates/forge/tests/cli/cmd.rs
+++ crates/forge/tests/cli/cmd.rs
@@ -775,7 +775,35 @@ forgetest!(can_clone_keep_directory_structure, |prj, cmd| {
     let _config: BasicConfig = parse_with_profile(&s).unwrap().unwrap().1;
 });
 
-// checks that init works with vyper flag
+// checks that `forge init` works.
+forgetest!(can_init_project, |prj, cmd| {
+    prj.wipe();
+
+    cmd.args(["init"]).arg(prj.root()).assert_success().stdout_eq(str![[r#"
+Initializing [..]...
+Installing forge-std in [..] (url: Some("https://github.com/foundry-rs/forge-std"), tag: None)
+    Installed forge-std[..]
+    Initialized forge project
+
+"#]]);
+
+    assert!(prj.root().join("foundry.toml").exists());
+    assert!(prj.root().join("lib/forge-std").exists());
+
+    assert!(prj.root().join("src").exists());
+    assert!(prj.root().join("src").join("Counter.sol").exists());
+
+    assert!(prj.root().join("test").exists());
+    assert!(prj.root().join("test").join("Counter.t.sol").exists());
+
+    assert!(prj.root().join("script").exists());
+    assert!(prj.root().join("script").join("Counter.s.sol").exists());
+
+    assert!(prj.root().join(".github").join("workflows").exists());
+    assert!(prj.root().join(".github").join("workflows").join("test.yml").exists());
+});
+
+// checks that `forge init --vyper` works.
 forgetest!(can_init_vyper_project, |prj, cmd| {
     prj.wipe();
 
@@ -799,6 +827,9 @@ Installing forge-std in [..] (url: Some("https://github.com/foundry-rs/forge-std
 
     assert!(prj.root().join("script").exists());
     assert!(prj.root().join("script").join("Counter.s.sol").exists());
+
+    assert!(prj.root().join(".github").join("workflows").exists());
+    assert!(prj.root().join(".github").join("workflows").join("test.yml").exists());
 });
 
 // checks that clone works with raw src containing `node_modules`
