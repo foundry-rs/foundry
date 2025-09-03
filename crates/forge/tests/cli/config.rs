612452838bd09d22ba25800c516602f1f64de20a
zerosnacks <zerosnacks@protonmail.com>
2025-09-03 12:40:38 +0200

    add URI (e.g. tokenURI from ERC721 / ERC6909) as exception

--- crates/forge/tests/cli/config.rs
+++ crates/forge/tests/cli/config.rs
@@ -1081,7 +1081,7 @@ severity = []
 exclude_lints = []
 ignore = []
 lint_on_build = true
-mixed_case_exceptions = ["ERC"]
+mixed_case_exceptions = ["ERC", "URI"]
 
 [doc]
 out = "docs"
@@ -1308,7 +1308,8 @@ exclude = []
     "ignore": [],
     "lint_on_build": true,
     "mixed_case_exceptions": [
-      "ERC"
+      "ERC",
+      "URI"
     ]
   },
   "doc": {
