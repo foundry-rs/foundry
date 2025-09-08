5291595f3ad42fb7f8c2399c81c842d348c8ab70
grandizzy <grandizzy.the.egg@gmail.com>
2025-09-08 10:38:40 +0300

    fix(forge): check if supported chain in verifier client

--- crates/verify/src/verify.rs
+++ crates/verify/src/verify.rs
@@ -253,7 +253,7 @@ impl VerifyArgs {
         {
             sh_println!("Constructor args: {args}")?
         }
-        self.verifier.verifier.client(self.etherscan.key().as_deref())?.verify(self, context).await.map_err(|err| {
+        self.verifier.verifier.client(self.etherscan.key().as_deref(), self.etherscan.chain)?.verify(self, context).await.map_err(|err| {
             if let Some(verifier_url) = verifier_url {
                  match Url::parse(&verifier_url) {
                     Ok(url) => {
@@ -277,7 +277,7 @@ impl VerifyArgs {
 
     /// Returns the configured verification provider
     pub fn verification_provider(&self) -> Result<Box<dyn VerificationProvider>> {
-        self.verifier.verifier.client(self.etherscan.key().as_deref())
+        self.verifier.verifier.client(self.etherscan.key().as_deref(), self.etherscan.chain)
     }
 
     /// Resolves [VerificationContext] object either from entered contract name or by trying to
@@ -476,7 +476,11 @@ impl VerifyCheckArgs {
             "Checking verification status on {}",
             self.etherscan.chain.unwrap_or_default()
         )?;
-        self.verifier.verifier.client(self.etherscan.key().as_deref())?.check(self).await
+        self.verifier
+            .verifier
+            .client(self.etherscan.key().as_deref(), self.etherscan.chain)?
+            .check(self)
+            .await
     }
 }
 
