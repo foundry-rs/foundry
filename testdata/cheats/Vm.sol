d72d14bedcf413d77f321c36090a76642f85e0af
Ectario <61197119+Ectario@users.noreply.github.com>
2025-09-16 23:17:17 +0200

    feat(cheatcodes): Add vm.signWithNonce(privateKey, digest, nonce) cheatcode (Crypto) (#11267)
    * feat: adding sign_wih_nonce in cheatcodes crates
    
    * test: adding sign with nonce solidity unit tests
    
    * style: formatting
    
    * test: fixing solidity invalid nonce test
    
    * style: fix formatting in Sign.t.sol
    
    * ci: fix cheatcode specs test problem
    
    * Update crates/cheatcodes/spec/src/vm.rs
    
    review modification
    
    Co-authored-by: onbjerg <onbjerg@users.noreply.github.com>
    
    * Update crates/cheatcodes/src/crypto.rs
    
    review modification (better format)
    
    Co-authored-by: onbjerg <onbjerg@users.noreply.github.com>
    
    * refactor: changing cheatcode name (append Unsafe to it)
    
    ---------
    
    Co-authored-by: onbjerg <onbjerg@users.noreply.github.com>

--- testdata/cheats/Vm.sol
+++ testdata/cheats/Vm.sol
@@ -497,6 +497,7 @@ interface Vm {
     function signDelegation(address implementation, uint256 privateKey, uint64 nonce) external returns (SignedDelegation memory signedDelegation);
     function signDelegation(address implementation, uint256 privateKey, bool crossChain) external returns (SignedDelegation memory signedDelegation);
     function signP256(uint256 privateKey, bytes32 digest) external pure returns (bytes32 r, bytes32 s);
+    function signWithNonceUnsafe(uint256 privateKey, bytes32 digest, uint256 nonce) external pure returns (uint8 v, bytes32 r, bytes32 s);
     function sign(Wallet calldata wallet, bytes32 digest) external returns (uint8 v, bytes32 r, bytes32 s);
     function sign(uint256 privateKey, bytes32 digest) external pure returns (uint8 v, bytes32 r, bytes32 s);
     function sign(bytes32 digest) external pure returns (uint8 v, bytes32 r, bytes32 s);
