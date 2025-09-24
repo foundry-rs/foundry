7c6777bee4e1ce08fa9903065e49b5ce1ee74293
VolodymyrBg <aqdrgg19@gmail.com>
2025-09-24 09:18:54 +0300

    Update SameFileUnion.t.sol

--- testdata/default/linking/samefile_union/SameFileUnion.t.sol
+++ testdata/default/linking/samefile_union/SameFileUnion.t.sol
@@ -1,1 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.18;
 
+import "./Libs.sol";
+
+contract UsesBoth {
+    uint public x;
+    constructor() {
+        // used only in в creation bytecode
+        x = LInit.f();
+    }
+    function y() external pure returns (uint) {
+        // used only in deployed bytecode
+        return LRun.g();
+    }
+}
