01fe0b6bbdb2b73d9256a0abf2e12469819ea7f0
grandizzy <38490174+grandizzy@users.noreply.github.com>
2025-09-25 08:12:27 +0300

    chore: fix flaky fuzz test, pin seed (#11808)

--- testdata/default/repros/Issue2851.t.sol
+++ testdata/default/repros/Issue2851.t.sol
@@ -22,6 +22,7 @@ contract Issue2851Test is DSTest {
         back = new Backdoor();
     }
 
+    /// forge-config: default.fuzz.seed = '111'
     function invariantNotZero() public {
         assertEq(back.number(), 1);
     }
