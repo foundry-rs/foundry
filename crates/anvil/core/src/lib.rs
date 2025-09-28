2d6f94a8e6c7a1ac8b18c8873c7d51f913201ab6
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2025-09-28 11:05:49 +0200

    chore: remove feature(doc_auto_cfg) (#11852)

--- crates/anvil/core/src/lib.rs
+++ crates/anvil/core/src/lib.rs
@@ -3,7 +3,7 @@
 //! Core Ethereum types for Anvil.
 
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
-#![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 
 /// Various Ethereum types
 pub mod eth;
