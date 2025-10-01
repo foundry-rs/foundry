49c5727f1c2dc8b79d0db2e4224db533ec23a2ba
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2025-10-01 18:51:31 +0200

    test: partially revert e9764bb567df83a3e69c392c9d195500cb3e722b

--- crates/cast/tests/cli/selectors.rs
+++ crates/cast/tests/cli/selectors.rs
@@ -1,22 +1,16 @@
-casttest!(
-    #[ignore = "selector missing upstream"]
-    error_decode_with_sourcify,
-    |prj, cmd| {
-        prj.clear_cache();
-        cmd.args(["decode-error",
-            "0xcf47918100000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000100000000"
-        ]).assert_success().stdout_eq(str![[r#" InsufficientBalance(uint256,uint256)
-256
-4294967296 [4.294e9]
+casttest!(error_decode_with_openchain, |prj, cmd| {
+    prj.clear_cache();
+    cmd.args(["decode-error", "0x7a0e198500000000000000000000000000000000000000000000000000000000000000650000000000000000000000000000000000000000000000000000000000000064"]).assert_success().stdout_eq(str![[r#"
+ValueTooHigh(uint256,uint256)
+101
+100
 
 "#]]);
-    }
-);
+});
 
 casttest!(fourbyte, |_prj, cmd| {
     cmd.args(["4byte", "0xa9059cbb"]).assert_success().stdout_eq(str![[r#"
 transfer(address,uint256)
-_____$_$__$___$$$___$$___$__$$(address,uint256)
 
 "#]]);
 });
@@ -30,46 +24,45 @@ For more information, try '--help'.
 "#]]);
 });
 
-casttest!(
-    #[ignore = "selector missing upstream"]
-    fourbyte_calldata,
-    |_prj, cmd| {
-        cmd.args(["4byte-calldata",
-            "0xa9059cbb0000000000000000000000000a2ac0c368dc8ec680a0c98c907656bd970675950000000000000000000000000000000000000000000000000000000767954a79"
-        ]).assert_success().stdout_eq(str![[r#"
+casttest!(fourbyte_calldata, |_prj, cmd| {
+    cmd.args(["4byte-calldata", "0xa9059cbb0000000000000000000000000a2ac0c368dc8ec680a0c98c907656bd970675950000000000000000000000000000000000000000000000000000000767954a79"]).assert_success().stdout_eq(str![[r#"
 1) "transfer(address,uint256)"
 0x0A2AC0c368Dc8eC680a0c98C907656BD97067595
 31802608249 [3.18e10]
 
 "#]]);
-    }
-);
+});
 
 casttest!(fourbyte_calldata_only_selector, |_prj, cmd| {
     cmd.args(["4byte-calldata", "0xa9059cbb"]).assert_success().stdout_eq(str![[r#"
 transfer(address,uint256)
-_____$_$__$___$$$___$$___$__$$(address,uint256)
 
 "#]]);
 });
 
 casttest!(fourbyte_calldata_alias, |_prj, cmd| {
-    cmd
-        .stdin(b"1\n")
-        .args(["4byte-decode", "0xa9059cbb0000000000000000000000000a2ac0c368dc8ec680a0c98c907656bd970675950000000000000000000000000000000000000000000000000000000767954a79"]).assert_success().stdout_eq(str![[r#"
+    cmd.args(["4byte-decode", "0xa9059cbb0000000000000000000000000a2ac0c368dc8ec680a0c98c907656bd970675950000000000000000000000000000000000000000000000000000000767954a79"]).assert_success().stdout_eq(str![[r#"
 1) "transfer(address,uint256)"
-2) "_____$_$__$___$$$___$$___$__$$(address,uint256)"
-Select a function signature by number: 0x0A2AC0c368Dc8eC680a0c98C907656BD97067595
+0x0A2AC0c368Dc8eC680a0c98C907656BD97067595
 31802608249 [3.18e10]
 
 "#]]);
 });
 
 casttest!(fourbyte_event, |_prj, cmd| {
-    cmd.args(["4byte-event", "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"])
+    cmd.args(["4byte-event", "0x7e1db2a1cd12f0506ecd806dba508035b290666b84b096a87af2fd2a1516ede6"])
         .assert_success()
         .stdout_eq(str![[r#"
-Transfer(address,address,uint256)
+updateAuthority(address,uint8)
+
+"#]]);
+});
+
+casttest!(fourbyte_event_2, |_prj, cmd| {
+    cmd.args(["4byte-event", "0xb7009613e63fb13fd59a2fa4c206a992c1f090a44e5d530be255aa17fed0b3dd"])
+        .assert_success()
+        .stdout_eq(str![[r#"
+canCall(address,address,bytes4)
 
 "#]]);
 });
@@ -91,8 +84,8 @@ casttest!(event_decode_with_sig, |_prj, cmd| {
 "#]]);
 });
 
-// tests cast can decode event with Sourcify API
-casttest!(event_decode, |prj, cmd| {
+// tests cast can decode event with Openchain API
+casttest!(event_decode_with_openchain, |prj, cmd| {
     prj.clear_cache();
     cmd.args(["decode-event", "0xe27c4c1372396a3d15a9922f74f9dfc7c72b1ad6d63868470787249c356454c1000000000000000000000000000000000000000000000000000000000000004e00000000000000000000000000000000000000000000000000000dd00000004e"]).assert_success().stdout_eq(str![[r#"
 BaseCurrencySet(address,uint256)
