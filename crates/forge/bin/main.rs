715ddaad72119f4827abc9f33b021e271fcf1840
radik878 <radikpadik76@gmail.com>
2025-10-03 15:05:59 +0300

    forge(bin): suppress error printing in quiet/json modes; keep non-zero exit code

--- crates/forge/bin/main.rs
+++ crates/forge/bin/main.rs
@@ -8,7 +8,9 @@ static ALLOC: foundry_cli::utils::Allocator = foundry_cli::utils::new_allocator(
 
 fn main() {
     if let Err(err) = run() {
-        let _ = foundry_common::sh_err!("{err:?}");
+        if !(foundry_common::shell::is_quiet() || foundry_common::shell::is_json()) {
+            let _ = foundry_common::sh_err!("{err:?}");
+        }
         std::process::exit(1);
     }
 }
