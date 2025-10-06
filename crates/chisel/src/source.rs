e1e50a4e47a4da7fc598e5b7a229697661bdb9ce
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2025-10-06 17:59:44 +0200

    refactor(chisel): remove gas limit binary search (#11989)
    * refactor(chisel): remove gas limit binary search
    
    * log

--- crates/chisel/src/source.rs
+++ crates/chisel/src/source.rs
@@ -14,7 +14,7 @@ use foundry_compilers::{
     solc::Solc,
 };
 use foundry_config::{Config, SolcReq};
-use foundry_evm::{backend::Backend, opts::EvmOpts};
+use foundry_evm::{backend::Backend, core::bytecode::InstIter, opts::EvmOpts};
 use semver::Version;
 use serde::{Deserialize, Serialize};
 use solang_parser::pt;
@@ -190,18 +190,19 @@ impl IntermediateOutput {
         let final_pc = {
             let offset = source_loc.start() as u32;
             let length = (source_loc.end() - source_loc.start()) as u32;
+            trace!(%offset, %length, "find pc");
             contract
                 .get_source_map_deployed()
                 .unwrap()
                 .unwrap()
                 .into_iter()
-                .zip(InstructionIter::new(deployed_bytecode_bytes))
+                .zip(InstIter::new(deployed_bytecode_bytes).with_pc().map(|(pc, _)| pc))
                 .filter(|(s, _)| s.offset() == offset && s.length() == length)
-                .map(|(_, i)| i.pc)
+                .map(|(_, pc)| pc)
                 .max()
-                .unwrap_or_default()
         };
-        Ok(Some(final_pc))
+        trace!(?final_pc);
+        Ok(final_pc)
     }
 
     pub fn run_func_body(&self) -> Result<&Vec<pt::Statement>> {
@@ -400,7 +401,7 @@ impl SessionSourceConfig {
             && let Some(version) = self.foundry_config.solc_version()
             && version < MIN_VM_VERSION
         {
-            tracing::info!(%version, minimum=%MIN_VM_VERSION, "Disabling VM injection");
+            info!(%version, minimum=%MIN_VM_VERSION, "Disabling VM injection");
             self.no_vm = true;
         }
         Ok(())
@@ -513,7 +514,7 @@ impl SessionSource {
     fn parse_fragment(&self, buffer: &str) -> Option<(Self, ParseTreeFragment)> {
         #[track_caller]
         fn debug_errors(errors: &EmittedDiagnostics) {
-            tracing::debug!("{errors}");
+            debug!("{errors}");
         }
 
         let mut this = self.clone();
@@ -904,43 +905,4 @@ enum ParseTreeFragment {
     Contract,
     /// Code for the "run()" function
     Function,
-}
-
-#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-struct Instruction {
-    pub pc: usize,
-    pub opcode: u8,
-    pub data: [u8; 32],
-    pub data_len: u8,
-}
-
-struct InstructionIter<'a> {
-    bytes: &'a [u8],
-    offset: usize,
-}
-
-impl<'a> InstructionIter<'a> {
-    pub fn new(bytes: &'a [u8]) -> Self {
-        Self { bytes, offset: 0 }
-    }
-}
-
-impl Iterator for InstructionIter<'_> {
-    type Item = Instruction;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let pc = self.offset;
-        self.offset += 1;
-        let opcode = *self.bytes.get(pc)?;
-        let (data, data_len) = if matches!(opcode, 0x60..=0x7F) {
-            let mut data = [0; 32];
-            let data_len = (opcode - 0x60 + 1) as usize;
-            data[..data_len].copy_from_slice(&self.bytes[self.offset..self.offset + data_len]);
-            self.offset += data_len;
-            (data, data_len as u8)
-        } else {
-            ([0; 32], 0)
-        };
-        Some(Instruction { pc, opcode, data, data_len })
-    }
 }
