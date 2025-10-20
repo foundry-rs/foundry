0a5f3891e982f6c58e04d3e3f1bd4991df55f6ef
Gengar <creeptogengar@gmail.com>
2025-10-20 19:18:15 +0300

    Update api.rs

--- crates/anvil/src/eth/api.rs
+++ crates/anvil/src/eth/api.rs
@@ -2678,16 +2678,21 @@ impl EthApi {
         node_info!("anvil_setRpcUrl");
         if let Some(fork) = self.backend.get_fork() {
             let mut config = fork.config.write();
-            // let interval = config.provider.get_interval();
+            // Get the current mining interval from the miner
+            let interval = self.miner.get_interval().map(Duration::from_secs);
+            let mut builder = ProviderBuilder::new(&url).max_retry(10).initial_backoff(1000);
+            
+            // Set the interval if it exists
+            if let Some(interval) = interval {
+                builder = builder.interval(interval);
+            }
+            
             let new_provider = Arc::new(
-                ProviderBuilder::new(&url).max_retry(10).initial_backoff(1000).build().map_err(
-                    |_| {
-                        TransportErrorKind::custom_str(
-                            format!("Failed to parse invalid url {url}").as_str(),
-                        )
-                    },
-                    // TODO: Add interval
-                )?, // .interval(interval),
+                builder.build().map_err(|_| {
+                    TransportErrorKind::custom_str(
+                        format!("Failed to parse invalid url {url}").as_str(),
+                    )
+                })?,
             );
             config.provider = new_provider;
             trace!(target: "backend", "Updated fork rpc from \"{}\" to \"{}\"", config.eth_rpc_url, url);
