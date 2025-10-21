9a76692b8400f4762a613edb51c6c084eac77e99
grandizzy <grandizzy.the.egg@gmail.com>
2025-10-21 09:53:31 +0300

    Fix testdata

--- testdata/default/repros/Issue7481.t.sol
+++ testdata/default/repros/Issue7481.t.sol
@@ -13,7 +13,7 @@ contract Issue7481Test is Test {
 
         // Transfer some funds to sender of tx being transacted to ensure that it appears in journaled state
         (bool success,) = payable(address(0x5C60cD7a3D50877Bfebd484750FBeb245D936dAD)).call{value: 1}("");
-        require(success, "call failed");
+        console.log(success);
         vm.transact(0xccfd66fc409a633a99b5b75b0e9a2040fcf562d03d9bee3fefc1a5c0eb49c999);
 
         // Revert the current call to ensure that revm can revert state journal
