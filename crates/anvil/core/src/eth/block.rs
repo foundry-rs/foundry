6b6f1a3f2ab10e6adb26b1b5c89f4304e69918ae
Gengar <creeptogengar@gmail.com>
2025-10-31 02:33:49 +0200

    Update block.rs

--- crates/anvil/core/src/eth/block.rs
+++ crates/anvil/core/src/eth/block.rs
@@ -32,9 +32,9 @@ impl Block {
         T: Into<Transaction>,
     {
         let transactions: Vec<_> = transactions.into_iter().map(Into::into).collect();
-        let transactions1: Vec<super::transaction::TypedTransaction> =
-            transactions.clone().into_iter().map(Into::into).collect();
-        let transactions_root = calculate_transaction_root(&transactions1);
+        let transactions_for_root: Vec<_> =
+            transactions.iter().map(|t| t.transaction.clone()).collect();
+        let transactions_root = calculate_transaction_root(&transactions_for_root);
 
         Self {
             header: Header {
