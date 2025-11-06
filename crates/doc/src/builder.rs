ebcd59b575e1ff8a1fecda3f1411afa3dd0e57fb
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2025-11-06 20:31:51 +0100

    Revert "fix(forge doc): use relative path instead of full path" (#12477)
    Revert "fix(forge doc): use relative path instead of full path (#12373)"
    
    This reverts commit 118e12edb96c4593be5809e0161000dc29a26654.

--- crates/doc/src/builder.rs
+++ crates/doc/src/builder.rs
@@ -124,7 +124,6 @@ impl DocBuilder {
             .collect::<Vec<_>>();
 
         let out_dir = self.out_dir()?;
-        let out_target_dir = out_dir.clone();
         let documents = compiler.enter_mut(|compiler| -> eyre::Result<Vec<Vec<Document>>> {
             let gcx = compiler.gcx();
             let documents = combined_sources
@@ -198,7 +197,7 @@ impl DocBuilder {
                                     path.clone(),
                                     target_path,
                                     from_library,
-                                    out_target_dir.clone(),
+                                    self.config.out.clone(),
                                 )
                                 .with_content(DocumentContent::Single(item), ident))
                             })
@@ -232,7 +231,7 @@ impl DocBuilder {
                                     path.clone(),
                                     target_path,
                                     from_library,
-                                    out_target_dir.clone(),
+                                    self.config.out.clone(),
                                 )
                                 .with_content(DocumentContent::Constants(consts), identity),
                             )
@@ -251,7 +250,7 @@ impl DocBuilder {
                                         path.clone(),
                                         target_path,
                                         from_library,
-                                        out_target_dir.clone(),
+                                        self.config.out.clone(),
                                     )
                                     .with_content(
                                         DocumentContent::OverloadedFunctions(funcs),
