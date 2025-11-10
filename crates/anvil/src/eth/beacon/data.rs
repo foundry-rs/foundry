04223e032240e7f72e2544952ea2542a7a1f49aa
StackOverflowExcept1on <109800286+StackOverflowExcept1on@users.noreply.github.com>
2025-11-10 13:35:23 +0300

    fix(anvil): serialize genesis time as string

--- crates/anvil/src/eth/beacon/data.rs
+++ crates/anvil/src/eth/beacon/data.rs
@@ -2,11 +2,14 @@
 
 use alloy_primitives::{B256, aliases::B32};
 use serde::{Deserialize, Serialize};
+use serde_with::{DisplayFromStr, serde_as};
 
 /// Ethereum Beacon chain genesis details
+#[serde_as]
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct GenesisDetails {
     /// The genesis_time configured for the beacon chain
+    #[serde_as(as = "DisplayFromStr")]
     pub genesis_time: u64,
     /// The genesis validators root
     pub genesis_validators_root: B256,
