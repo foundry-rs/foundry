b5e3c8a8b2a6d94db011c8a1525464b75a4c1e2a
Mablr <59505383+mablr@users.noreply.github.com>
2025-11-11 21:40:05 +0100

    chore(anvil): replace `PartialHeader` with `Header` from Alloy

--- crates/anvil/core/src/eth/block.rs
+++ crates/anvil/core/src/eth/block.rs
@@ -1,6 +1,5 @@
 use super::transaction::{TransactionInfo, TypedReceipt};
 use alloy_consensus::{EMPTY_OMMER_ROOT_HASH, Header, proofs::calculate_transaction_root};
-use alloy_primitives::{Address, B64, B256, Bloom, Bytes, U256};
 use alloy_rlp::{RlpDecodable, RlpEncodable};
 
 // Type alias to optionally support impersonated transactions
@@ -27,97 +26,24 @@ impl Block {
     ///
     /// Note: if the `impersonate-tx` feature is enabled this will also accept
     /// `MaybeImpersonatedTransaction`.
-    pub fn new<T>(partial_header: PartialHeader, transactions: impl IntoIterator<Item = T>) -> Self
+    pub fn new<T>(mut header: Header, transactions: impl IntoIterator<Item = T>) -> Self
     where
         T: Into<Transaction>,
     {
         let transactions: Vec<_> = transactions.into_iter().map(Into::into).collect();
         let transactions_root = calculate_transaction_root(&transactions);
 
-        Self {
-            header: Header {
-                parent_hash: partial_header.parent_hash,
-                beneficiary: partial_header.beneficiary,
-                ommers_hash: EMPTY_OMMER_ROOT_HASH,
-                state_root: partial_header.state_root,
-                transactions_root,
-                receipts_root: partial_header.receipts_root,
-                logs_bloom: partial_header.logs_bloom,
-                difficulty: partial_header.difficulty,
-                number: partial_header.number,
-                gas_limit: partial_header.gas_limit,
-                gas_used: partial_header.gas_used,
-                timestamp: partial_header.timestamp,
-                extra_data: partial_header.extra_data,
-                mix_hash: partial_header.mix_hash,
-                withdrawals_root: partial_header.withdrawals_root,
-                blob_gas_used: partial_header.blob_gas_used,
-                excess_blob_gas: partial_header.excess_blob_gas,
-                parent_beacon_block_root: partial_header.parent_beacon_block_root,
-                nonce: partial_header.nonce,
-                base_fee_per_gas: partial_header.base_fee,
-                requests_hash: partial_header.requests_hash,
-            },
-            transactions,
-            ommers: vec![],
-        }
-    }
-}
+        header.transactions_root = transactions_root;
+        header.ommers_hash = EMPTY_OMMER_ROOT_HASH;
 
-/// Partial header definition without ommers hash and transactions root
-#[derive(Clone, Debug, Default, PartialEq, Eq)]
-pub struct PartialHeader {
-    pub parent_hash: B256,
-    pub beneficiary: Address,
-    pub state_root: B256,
-    pub receipts_root: B256,
-    pub logs_bloom: Bloom,
-    pub difficulty: U256,
-    pub number: u64,
-    pub gas_limit: u64,
-    pub gas_used: u64,
-    pub timestamp: u64,
-    pub extra_data: Bytes,
-    pub mix_hash: B256,
-    pub blob_gas_used: Option<u64>,
-    pub excess_blob_gas: Option<u64>,
-    pub parent_beacon_block_root: Option<B256>,
-    pub nonce: B64,
-    pub base_fee: Option<u64>,
-    pub withdrawals_root: Option<B256>,
-    pub requests_hash: Option<B256>,
-}
-
-impl From<Header> for PartialHeader {
-    fn from(value: Header) -> Self {
-        Self {
-            parent_hash: value.parent_hash,
-            beneficiary: value.beneficiary,
-            state_root: value.state_root,
-            receipts_root: value.receipts_root,
-            logs_bloom: value.logs_bloom,
-            difficulty: value.difficulty,
-            number: value.number,
-            gas_limit: value.gas_limit,
-            gas_used: value.gas_used,
-            timestamp: value.timestamp,
-            extra_data: value.extra_data,
-            mix_hash: value.mix_hash,
-            nonce: value.nonce,
-            base_fee: value.base_fee_per_gas,
-            blob_gas_used: value.blob_gas_used,
-            excess_blob_gas: value.excess_blob_gas,
-            parent_beacon_block_root: value.parent_beacon_block_root,
-            requests_hash: value.requests_hash,
-            withdrawals_root: value.withdrawals_root,
-        }
+        Self { header, transactions, ommers: vec![] }
     }
 }
 
 #[cfg(test)]
 mod tests {
     use alloy_primitives::{
-        b256,
+        Address, B64, B256, Bloom, U256, b256,
         hex::{self, FromHex},
     };
     use alloy_rlp::Decodable;
--- crates/anvil/src/eth/backend/mem/storage.rs
+++ crates/anvil/src/eth/backend/mem/storage.rs
@@ -10,7 +10,7 @@ use crate::eth::{
     },
     pool::transactions::PoolTransaction,
 };
-use alloy_consensus::constants::EMPTY_WITHDRAWALS;
+use alloy_consensus::{Header, constants::EMPTY_WITHDRAWALS};
 use alloy_eips::eip7685::EMPTY_REQUESTS_HASH;
 use alloy_primitives::{
     B256, Bytes, U256,
@@ -24,7 +24,7 @@ use alloy_rpc_types::{
     },
 };
 use anvil_core::eth::{
-    block::{Block, PartialHeader},
+    block::Block,
     transaction::{MaybeImpersonatedTransaction, ReceiptResponse, TransactionInfo, TypedReceipt},
 };
 use foundry_evm::{
@@ -279,21 +279,30 @@ impl BlockchainStorage {
         let is_prague = spec_id >= SpecId::PRAGUE;
 
         // create a dummy genesis block
-        let partial_header = PartialHeader {
-            timestamp,
-            base_fee,
-            gas_limit: env.evm_env.block_env.gas_limit,
+        let header = Header {
+            parent_hash: Default::default(),
+            ommers_hash: Default::default(),
             beneficiary: env.evm_env.block_env.beneficiary,
+            state_root: Default::default(),
+            transactions_root: Default::default(),
+            receipts_root: Default::default(),
+            logs_bloom: Default::default(),
             difficulty: env.evm_env.block_env.difficulty,
+            number: genesis_number,
+            gas_limit: env.evm_env.block_env.gas_limit,
+            gas_used: 0,
+            timestamp,
+            extra_data: Default::default(),
+            mix_hash: Default::default(),
+            nonce: Default::default(),
+            base_fee_per_gas: base_fee,
             blob_gas_used: env.evm_env.block_env.blob_excess_gas_and_price.as_ref().map(|_| 0),
             excess_blob_gas: env.evm_env.block_env.blob_excess_gas(),
-            number: genesis_number,
             parent_beacon_block_root: is_cancun.then_some(Default::default()),
             withdrawals_root: is_shanghai.then_some(EMPTY_WITHDRAWALS),
             requests_hash: is_prague.then_some(EMPTY_REQUESTS_HASH),
-            ..Default::default()
         };
-        let block = Block::new::<MaybeImpersonatedTransaction>(partial_header, vec![]);
+        let block = Block::new::<MaybeImpersonatedTransaction>(header, vec![]);
         let genesis_hash = block.header.hash_slow();
         let best_hash = genesis_hash;
         let best_number = genesis_number;
@@ -679,12 +688,11 @@ mod tests {
     fn test_storage_dump_reload_cycle() {
         let mut dump_storage = BlockchainStorage::empty();
 
-        let partial_header = PartialHeader { gas_limit: 123456, ..Default::default() };
+        let header = Header { gas_limit: 123456, ..Default::default() };
         let bytes_first = &mut &hex::decode("f86b02843b9aca00830186a094d3e8763675e4c425df46cc3b5c0f6cbdac39604687038d7ea4c68000802ba00eb96ca19e8a77102767a41fc85a36afd5c61ccb09911cec5d3e86e193d9c5aea03a456401896b1b6055311536bf00a718568c744d8c1f9df59879e8350220ca18").unwrap()[..];
         let tx: MaybeImpersonatedTransaction =
             TypedTransaction::decode(&mut &bytes_first[..]).unwrap().into();
-        let block =
-            Block::new::<MaybeImpersonatedTransaction>(partial_header.clone(), vec![tx.clone()]);
+        let block = Block::new::<MaybeImpersonatedTransaction>(header.clone(), vec![tx.clone()]);
         let block_hash = block.header.hash_slow();
         dump_storage.blocks.insert(block_hash, block);
 
@@ -697,7 +705,7 @@ mod tests {
         load_storage.load_transactions(serialized_transactions);
 
         let loaded_block = load_storage.blocks.get(&block_hash).unwrap();
-        assert_eq!(loaded_block.header.gas_limit, { partial_header.gas_limit });
+        assert_eq!(loaded_block.header.gas_limit, { header.gas_limit });
         let loaded_tx = loaded_block.transactions.first().unwrap();
         assert_eq!(loaded_tx, &tx);
     }
