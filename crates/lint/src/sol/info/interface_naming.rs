9ca2234503cc965fc8f083d5ae1d531b99b14cf7
Milos Djurica <milosdjurica.work@gmail.com>
2025-11-24 20:00:12 +0100

    feat: add interface-file-naming linting rule

---
+++ crates/lint/src/sol/info/interface_naming.rs
@@ -0,0 +1,59 @@
+use crate::{
+    linter::{EarlyLintPass, Lint, LintContext},
+    sol::{Severity, SolLint, info::InterfaceFileNaming},
+};
+
+use solar::ast::{self as ast};
+
+declare_forge_lint!(
+    INTERFACE_FILE_NAMING,
+    Severity::Info,
+    "interface-file-naming",
+    "interface file names should be prefixed with 'I'"
+);
+
+impl<'ast> EarlyLintPass<'ast> for InterfaceFileNaming {
+    fn check_full_source_unit(
+        &mut self,
+        ctx: &LintContext<'ast, '_>,
+        unit: &'ast ast::SourceUnit<'ast>,
+    ) {
+        if !ctx.is_lint_enabled(INTERFACE_FILE_NAMING.id()) {
+            return;
+        }
+
+        // Get first item in file and exit if the unit contains no items
+        let Some(first_item) = unit.items.first() else { return };
+
+        // Get file from first item
+        let file = ctx.session().source_map().lookup_source_file(first_item.span.lo());
+
+        // Get file name from file
+        let Some(file_name) = file.name.as_real().and_then(|path| path.file_name()?.to_str())
+        else {
+            return;
+        };
+
+        // If file name starts with 'I', skip lint
+        if file_name.starts_with('I') {
+            return;
+        }
+
+        let mut first_interface_span = None;
+        for item in unit.items.iter() {
+            if let ast::ItemKind::Contract(c) = &item.kind {
+                match c.kind {
+                    ast::ContractKind::Interface => {
+                        first_interface_span.get_or_insert(c.name.span);
+                    }
+                    _ => return, // Mixed file, skip lint
+                }
+            }
+        }
+
+        // Emit if file contains ONLY interfaces. Emit only on the first interface.
+        if let Some(span) = first_interface_span {
+            ctx.emit(&INTERFACE_FILE_NAMING, span);
+        }
+    }
+}
---
+++ crates/lint/testdata/SoloInterfaces.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+interface ISoloInterfaces {
+    function foo() external;
+}
+
+interface I2 {
+    function foo() external;
+}
+
+interface I3 {
+    function foo() external;
+}
+
