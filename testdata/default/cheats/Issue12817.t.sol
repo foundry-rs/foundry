08387cea63114c94ca44c07fa82c29a10fffb294
subwaycookiecrunch <lucid7006@gmail.com>
2025-12-19 16:45:52 +0530

    fix(test): resolve CI flakiness in Issue12817 and Issue2851

--- testdata/default/cheats/Issue12817.t.sol
+++ testdata/default/cheats/Issue12817.t.sol
@@ -4,14 +4,16 @@ pragma solidity ^0.8.18;
 import "utils/Test.sol";
 
 contract Issue12817Test is Test {
-    function test_fuzz_randomUintRange(uint256) public {
+    function test_fuzz_randomUintRange(uint256 seed) public {
+        vm.seed(seed);
         uint256 randomUint = vm.randomUint(0, 3);
         if (randomUint != 1) {
             revert("Random value was not 1");
         }
     }
 
-    function test_fuzz_randomUint(uint256) public {
+    function test_fuzz_randomUint(uint256 seed) public {
+        vm.seed(seed);
         uint256 randomUint = vm.randomUint() % 4;
         if (randomUint != 1) {
             revert("Random value was not 1");
