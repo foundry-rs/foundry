fdad166d8ca338d6a891d42c6c7dc153bf1d9c77
Aganis <aganisgash@gmail.com>
2025-12-30 05:33:33 +0800

    Remove duplicate logic in TxSigner::address() implementations

--- crates/wallets/src/signer.rs
+++ crates/wallets/src/signer.rs
@@ -322,7 +322,7 @@ impl Signer for WalletSigner {
 #[async_trait]
 impl TxSigner<Signature> for WalletSigner {
     fn address(&self) -> Address {
-        delegate!(self, inner => alloy_signer::Signer::address(inner))
+        Signer::address(self)
     }
 
     async fn sign_transaction(
