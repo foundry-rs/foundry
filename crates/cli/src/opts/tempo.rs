1ce1298d485408d8420d8d7c3dd20d792b5ba15f
Oliver Nordbjerg <hi@notbjerg.me>
2026-01-02 15:40:24 +0100

    feat(cast): tempo nonce keys

--- crates/cli/src/opts/tempo.rs
+++ crates/cli/src/opts/tempo.rs
@@ -1,4 +1,4 @@
-use alloy_primitives::Address;
+use alloy_primitives::{Address, ruint::aliases::U256};
 use clap::Parser;
 
 /// CLI options for Tempo transactions.
@@ -14,4 +14,14 @@ pub struct TempoOpts {
     /// for more information.
     #[arg(long = "tempo.fee-token")]
     pub fee_token: Option<Address>,
+
+    /// Nonce sequence key for Tempo transactions.
+    ///
+    /// When set, builds a Tempo (type 0x76) transaction with the specified nonce sequence key.
+    ///
+    /// If this is not set, the protocol sequence key (0) will be used.
+    ///
+    /// For more information see <https://docs.tempo.xyz/protocol/transactions/spec-tempo-transaction#parallelizable-nonces>.
+    #[arg(long = "tempo.seq")]
+    pub sequence_key: Option<U256>,
 }
