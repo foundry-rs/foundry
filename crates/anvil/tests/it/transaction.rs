0f005ee55e055a0af4b6c3aa05fd4740f487450b
Echo <echowanderer11@gmail.com>
2026-01-09 15:57:38 +0800

    feat: uncomment transaction replacement test after alloy fix

--- crates/anvil/tests/it/transaction.rs
+++ crates/anvil/tests/it/transaction.rs
@@ -180,17 +180,15 @@ async fn can_replace_transaction() {
     assert_eq!(block.transactions.len(), 1);
     assert_eq!(BlockTransactions::Hashes(vec![higher_tx_hash]), block.transactions);
 
-    // FIXME: Unable to get receipt despite hotfix in https://github.com/alloy-rs/alloy/pull/614
-
     // lower priced transaction was replaced
-    // let _lower_priced_receipt = lower_priced_pending_tx.get_receipt().await.unwrap();
-    // let higher_priced_receipt = higher_priced_pending_tx.get_receipt().await.unwrap();
+    let _lower_priced_receipt = lower_priced_pending_tx.get_receipt().await.unwrap();
+    let higher_priced_receipt = higher_priced_pending_tx.get_receipt().await.unwrap();
 
-    // assert_eq!(1, block.transactions.len());
-    // assert_eq!(
-    //     BlockTransactions::Hashes(vec![higher_priced_receipt.transaction_hash]),
-    //     block.transactions
-    // );
+    assert_eq!(1, block.transactions.len());
+    assert_eq!(
+        BlockTransactions::Hashes(vec![higher_priced_receipt.transaction_hash]),
+        block.transactions
+    );
 }
 
 #[tokio::test(flavor = "multi_thread")]
