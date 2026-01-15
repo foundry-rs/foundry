00317d8a10fdf4816e7d07d7b4e0e373f4495e57
Alberto Viera <albertoviera@gmail.com>
2026-01-15 11:16:37 +0100

    fmt

--- crates/test-utils/src/rpc.rs
+++ crates/test-utils/src/rpc.rs
@@ -108,10 +108,7 @@ pub fn rpc_endpoints() -> RpcEndpoints {
         ("bsc", RpcEndpointUrl::Url(next_rpc_endpoint(NamedChain::BinanceSmartChain))),
         ("avaxTestnet", RpcEndpointUrl::Url("https://api.avax-test.network/ext/bc/C/rpc".into())),
         ("moonbeam", RpcEndpointUrl::Url("https://moonbeam-rpc.publicnode.com".into())),
-        (
-            "polkadotTestnet",
-            RpcEndpointUrl::Url("https://eth-rpc-testnet.polkadot.io".into()),
-        ),
+        ("polkadotTestnet", RpcEndpointUrl::Url("https://eth-rpc-testnet.polkadot.io".into())),
         ("rpcEnvAlias", RpcEndpointUrl::Env("${RPC_ENV_ALIAS}".into())),
     ])
 }
