39afc89af1173040159c2b9966b101374c6bdbc0
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2026-01-20 03:41:11 +0100

    test: update build_with_invalid_natspec snapshot

--- crates/forge/tests/cli/build.rs
+++ crates/forge/tests/cli/build.rs
@@ -439,19 +439,19 @@ contract ContractB {
     cmd.args(["build", "src/ContractWithInvalidNatspec.sol"]).assert_success().stderr_eq(str![[
         r#"
 warning: invalid natspec tag '@deprecated', custom tags must use format '@custom:name'
- [FILE]:5:5
-  |
-5 |     /// @deprecated quoteExactOutputSingle and exactOutput. Use QuoterV2 instead.
-  |     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  |
+  ╭▸ src/ContractWithInvalidNatspec.sol:5:5
+  │
+5 │     /// @deprecated quoteExactOutputSingle and exactOutput. Use QuoterV2 instead.
+  │     ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+  │
 ...
 
 warning: invalid natspec tag '@note', custom tags must use format '@custom:name'
- [FILE]:9:1
-  |
-9 | /// @note foo bar
-  | ^^^^^^^^^^^^^^^^^
-  |
+  ╭▸ src/ContractWithInvalidNatspec.sol:9:1
+  │
+9 │ /// @note foo bar
+  │ ━━━━━━━━━━━━━━━━━
+  │
 ...
 
 
