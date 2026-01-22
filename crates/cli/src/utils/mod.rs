fa00a92e9d6705af091d71a7e15c1abe843b1a3b
zerosnacks <95942363+zerosnacks@users.noreply.github.com>
2026-01-22 16:44:17 +0100

    Update mod.rs

--- crates/cli/src/utils/mod.rs
+++ crates/cli/src/utils/mod.rs
@@ -87,8 +87,6 @@ pub fn subscriber() {
     let registry = tracing_subscriber::Registry::default().with(env_filter());
     #[cfg(feature = "tracy")]
     let registry = registry.with(tracing_tracy::TracyLayer::default());
-    registry
-        .with(tracing_subscriber::fmt::layer().with_writer(std::io::stderr))
     registry.with(tracing_subscriber::fmt::layer().with_writer(std::io::stderr)).init()
 }
 
