f2ee1a9def4d190b9261fc8e74ba0e4cebf602e9
zerosnacks <95942363+zerosnacks@users.noreply.github.com>
2026-01-23 17:37:48 +0100

    Add unary_op_mutator_test module to tests

--- crates/forge/src/mutation/mutators/tests/mod.rs
+++ crates/forge/src/mutation/mutators/tests/mod.rs
@@ -1,10 +1,7 @@
 mod assignment_mutator_test;
 mod binary_op_mutator_test;
-
 mod delete_expression_mutator_test;
-
 mod elim_delegate_mutator_test;
+mod unary_op_mutator_test;
 
 mod helper;
-
-mod unary_op_mutator_test;
