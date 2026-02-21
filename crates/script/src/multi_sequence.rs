5330977d2a5ed73844e77d7f6b923b3552a9e4e4
Alvarez <140459501+prestoalvarez@users.noreply.github.com>
2026-02-21 17:02:32 +0100

    Update multi_sequence.rs

--- crates/script/src/multi_sequence.rs
+++ crates/script/src/multi_sequence.rs
@@ -7,7 +7,6 @@ use foundry_compilers::ArtifactId;
 use foundry_config::Config;
 use serde::{Deserialize, Serialize};
 use std::{
-    io::{BufWriter, Write},
     path::PathBuf,
 };
 
@@ -119,9 +118,7 @@ impl MultiChainSequence {
 
         // broadcast writes
         //../Contract-latest/run.json
-        let mut writer = BufWriter::new(fs::create_file(&self.path)?);
-        serde_json::to_writer_pretty(&mut writer, &self)?;
-        writer.flush()?;
+        fs::write_pretty_json_file(&self.path, self)?;
 
         if save_ts {
             //../Contract-[timestamp]/run.json
@@ -133,9 +130,7 @@ impl MultiChainSequence {
 
         // cache writes
         //../Contract-latest/run.json
-        let mut writer = BufWriter::new(fs::create_file(&self.sensitive_path)?);
-        serde_json::to_writer_pretty(&mut writer, &sensitive_sequence)?;
-        writer.flush()?;
+        fs::write_pretty_json_file(&self.sensitive_path, &sensitive_sequence)?;
 
         if save_ts {
             //../Contract-[timestamp]/run.json
