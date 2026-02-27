270851dc8e58e33d09b2a1d91abe5f67baba1ff2
steven <corderosteven6@gmail.com>
2026-02-27 12:34:02 -0600

    chore: prep alloy 2.0

--- crates/anvil/tests/it/beacon_api.rs
+++ crates/anvil/tests/it/beacon_api.rs
@@ -1,5 +1,8 @@
 use crate::utils::http_provider;
-use alloy_consensus::{Blob, SidecarBuilder, SimpleCoder, Transaction};
+use alloy_consensus::{
+    Blob, BlobTransactionSidecar, BlobTransactionSidecarVariant, SidecarBuilder, SimpleCoder,
+    Transaction,
+};
 use alloy_network::{TransactionBuilder, TransactionBuilder4844};
 use alloy_primitives::{B256, FixedBytes, U256, b256};
 use alloy_provider::Provider;
@@ -54,7 +57,7 @@ async fn test_beacon_api_get_blobs() {
     // Send all transactions without waiting for receipts
     for (i, data) in blob_data.iter().enumerate() {
         let sidecar: SidecarBuilder<SimpleCoder> = SidecarBuilder::from_slice(data.as_slice());
-        let sidecar = sidecar.build().unwrap();
+        let sidecar: BlobTransactionSidecar = sidecar.build().unwrap();
 
         let tx = TransactionRequest::default()
             .with_from(from)
@@ -63,7 +66,7 @@ async fn test_beacon_api_get_blobs() {
             .with_max_fee_per_blob_gas(gas_price + 1)
             .with_max_fee_per_gas(eip1559_est.max_fee_per_gas)
             .with_max_priority_fee_per_gas(eip1559_est.max_priority_fee_per_gas)
-            .with_blob_sidecar(sidecar)
+            .with_blob_sidecar(BlobTransactionSidecarVariant::Eip4844(sidecar))
             .value(U256::from(100));
 
         let tx = WithOtherFields::new(tx);
