ed711ea936d026fb6cc9c2b109381462e20d398e
Gustavo Figueiredo <me@figtracer.com>
2026-04-22 17:10:29 +0100

    test(lint): cover direct-name ERC721 interfaces

--- crates/lint/testdata/IncorrectERC721Interface.sol
+++ crates/lint/testdata/IncorrectERC721Interface.sol
@@ -5,6 +5,12 @@ pragma solidity ^0.8.18;
 
 interface IERC721 {}
 
+// SHOULD FAIL: Interface named ERC721 with incorrect function signatures
+interface ERC721 {
+    function balanceOf(address owner) external view returns (bool); //~WARN: incorrect ERC721 function interface
+    function ownerOf(uint256 tokenId) external view returns (bool); //~WARN: incorrect ERC721 function interface
+}
+
 // SHOULD FAIL: Interface inheriting from IERC721 with incorrect function signatures
 interface IERC721Incorrect is IERC721 {
     function balanceOf(address owner) external view returns (bool); //~WARN: incorrect ERC721 function interface
@@ -31,6 +37,12 @@ interface IERC721Correct is IERC721 {
     function getApproved(uint256 tokenId) external view returns (address);
     function isApprovedForAll(address owner, address operator) external view returns (bool);
     function supportsInterface(bytes4 interfaceId) external view returns (bool);
+}
+
+// SHOULD PASS: Interface named IERC721 with correct function signatures
+interface IERC721NamedCorrect {
+    function balanceOf(address owner) external view returns (uint256);
+    function ownerOf(uint256 tokenId) external view returns (address);
 }
 
 // SHOULD PASS: Contract that is NOT named ERC721 and does not inherit from one
