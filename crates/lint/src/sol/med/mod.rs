987c0dc80e4459ac06871a4c956385f03943a5ba
Gustavo Figueiredo <me@figtracer.com>
2026-04-22 13:34:38 +0100

    feat(lint): add incorrect ERC721 interface lint

--- crates/lint/src/sol/med/mod.rs
+++ crates/lint/src/sol/med/mod.rs
@@ -3,10 +3,14 @@ use crate::sol::{EarlyLintPass, LateLintPass, SolLint};
 mod div_mul;
 use div_mul::DIVIDE_BEFORE_MULTIPLY;
 
+mod incorrect_erc721_interface;
+use incorrect_erc721_interface::INCORRECT_ERC721_INTERFACE;
+
 mod unsafe_typecast;
 use unsafe_typecast::UNSAFE_TYPECAST;
 
 register_lints!(
     (DivideBeforeMultiply, early, (DIVIDE_BEFORE_MULTIPLY)),
+    (IncorrectERC721Interface, late, (INCORRECT_ERC721_INTERFACE)),
     (UnsafeTypecast, late, (UNSAFE_TYPECAST))
 );
