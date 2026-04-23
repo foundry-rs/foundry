5e348c7a1517999c8e09c2ddc81d2185dc2913cb
Gustavo Figueiredo <me@figtracer.com>
2026-04-23 16:08:08 +0100

    test(lint): cover direct-name IERC20 case

--- crates/lint/testdata/IncorrectERC20Interface.sol
+++ crates/lint/testdata/IncorrectERC20Interface.sol
@@ -3,7 +3,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-interface IERC20 {}
+// SHOULD PASS: Interface named IERC20 with correct function signatures
+interface IERC20 {
+    function transfer(address to, uint256 value) external returns (bool);
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
+    function approve(address spender, uint256 value) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function totalSupply() external view returns (uint256);
+}
 
 // SHOULD FAIL: Interface named ERC20 with incorrect function signatures
 interface ERC20 {
@@ -29,12 +37,6 @@ interface IERC20Correct is IERC20 {
     function allowance(address owner, address spender) external view returns (uint256);
     function balanceOf(address account) external view returns (uint256);
     function totalSupply() external view returns (uint256);
-}
-
-// SHOULD PASS: Interface named IERC20 with correct function signatures
-interface IERC20NamedCorrect {
-    function transfer(address to, uint256 value) external returns (bool);
-    function balanceOf(address account) external view returns (uint256);
 }
 
 // SHOULD PASS: Contract that is NOT named ERC20 and does not inherit from one
