64c994a33d4fc422d9bc988654732a103e4db5db
Gustavo Figueiredo <me@figtracer.com>
2026-05-11 13:38:39 +0100

    feat(lint): detect unlimited-gas ETH reentrancy

--- crates/lint/src/sol/high/mod.rs
+++ crates/lint/src/sol/high/mod.rs
@@ -1,15 +1,18 @@
 use crate::sol::{EarlyLintPass, LateLintPass, SolLint};
 
 mod incorrect_shift;
+mod reentrancy;
 mod rtlo;
 mod unchecked_calls;
 
 use incorrect_shift::INCORRECT_SHIFT;
+use reentrancy::REENTRANCY_UNLIMITED_GAS;
 use rtlo::RTLO;
 use unchecked_calls::{ERC20_UNCHECKED_TRANSFER, UNCHECKED_CALL};
 
 register_lints!(
     (IncorrectShift, early, (INCORRECT_SHIFT)),
+    (ReentrancyUnlimitedGas, late, (REENTRANCY_UNLIMITED_GAS)),
     (UncheckedCall, early, (UNCHECKED_CALL)),
     (UncheckedTransferERC20, late, (ERC20_UNCHECKED_TRANSFER)),
     (Rtlo, early, (RTLO))
