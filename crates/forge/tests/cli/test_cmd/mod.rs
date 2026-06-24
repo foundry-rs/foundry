366119d7fc872b220064f95c246e0c43320dd233
Mablr <59505383+mablr@users.noreply.github.com>
2026-06-24 18:48:37 +0200

    test: update fuzz failure output snapshot

--- crates/forge/tests/cli/test_cmd/mod.rs
+++ crates/forge/tests/cli/test_cmd/mod.rs
@@ -1072,6 +1072,7 @@ Encountered 1 failing test in test/CounterFuzz.t.sol:CounterTest
 Encountered a total of 1 failing tests, 0 tests succeeded
 
 Tip: Run `forge test --rerun` to retry only the 1 failed test
+Tip: Run `forge test --debug --match-test <TEST_NAME>` to inspect one failing test in the debugger
 
 [SEED] (use `--fuzz-seed` to reproduce)
 
