1718a54d4f059d91861c9c3f624b32dc6329c6b8
Matthias Seitz <matthias.seitz@outlook.de>
2026-06-30 05:41:58 +0200

    feat(anvil): support debug set head

--- crates/anvil/src/eth/api.rs
+++ crates/anvil/src/eth/api.rs
@@ -1218,6 +1218,12 @@ impl<N: Network> EthApi<N> {
         self.backend.debug_db_get(key).await
     }
 
+    /// Handles reth's no-op `debug_setHead` endpoint.
+    pub fn debug_set_head(&self, _number: U64) -> Result<()> {
+        node_info!("debug_setHead");
+        Ok(())
+    }
+
     /// Returns traces for the transaction hash via parity's tracing endpoint
     ///
     /// Handler for RPC call: `trace_transaction`
@@ -1679,6 +1685,7 @@ impl EthApi<FoundryNetwork> {
                 self.debug_code_by_hash(hash, block).await.to_rpc_result()
             }
             EthRequest::DebugDbGet(key) => self.debug_db_get(key).await.to_rpc_result(),
+            EthRequest::DebugSetHead(number) => self.debug_set_head(number).to_rpc_result(),
             EthRequest::DebugTraceBlockByHash(block_hash, opts) => {
                 self.debug_trace_block_by_hash(block_hash, opts).await.to_rpc_result()
             }
